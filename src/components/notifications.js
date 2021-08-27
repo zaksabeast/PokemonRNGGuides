1f45c1609a3ac79b2173391353c7aa05cf075e87
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2021-08-27 13:55:56 -0500

    add notifications

--- src/components/notifications.js
+++ src/components/notifications.js
@@ -10,9 +10,11 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Grow from '@material-ui/core/Grow';
+import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
+import messages from '../../notifications.json';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
     transformOrigin: 'top right',
   },
@@ -25,18 +27,33 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
   },
+  divider: {
+    margin: theme.spacing(1, 0),
+  },
 }));
+
+function getCookie(name) {
+  const regex = new RegExp(`(?:(?:^|.*;*)${name}*=*([^;]*).*$)|^.*$`);
+  return document.cookie.replace(regex, '$1');
+}
+
+const lastSeen = getCookie('lastSeenNotification');
+const lastSeenNotification = lastSeen === '' ? 0 : parseInt(lastSeen, 10);
+const messageList = messages.reverse();
 
 export const Notifications = () => {
   const classes = useStyles();
+
   const [open, setOpen] = React.useState(false);
   const [tooltipOpen, setTooltipOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
 
   const handleToggle = () => {
-    setOpen(prevOpen => !prevOpen);
+    setOpen((prevOpen) => !prevOpen);
     setTooltipOpen(false);
+    document.cookie = `lastSeenNotification=${messageList[0].id};path=/;max-age=31536000`;
   };
+
   return (
     <React.Fragment>
       <Tooltip
@@ -58,7 +75,14 @@ export const Notifications = () => {
           aria-haspopup="true"
           aria-label={'toggleNotifications'}
         >
-          <Badge badgeContent={1} color="secondary">
+          <Badge
+            badgeContent={messageList.reduce(
+              (count, message) =>
+                message.id > lastSeenNotification ? count + 1 : count,
+              0,
+            )}
+            color="secondary"
+          >
             <NotificationsIcon />
           </Badge>
         </IconButton>
@@ -80,15 +104,37 @@ export const Notifications = () => {
             <Grow in={open} {...TransitionProps}>
               <Paper className={classes.paper}>
                 <List className={classes.list}>
-                  <ListItem
-                    alignItems="flex-start"
-                    className={classes.listItem}
-                  >
-                    <Typography gutterBottom>New Notification</Typography>
-                    <Typography gutterBottom variant="body2">
-                      Notification for latest changes goes here.
-                    </Typography>
-                  </ListItem>
+                  {messageList.map((message, index) => (
+                    <React.Fragment key={message.id}>
+                      <ListItem
+                        alignItems="flex-start"
+                        className={classes.listItem}
+                      >
+                        <Typography gutterBottom>{message.title}</Typography>
+                        <Typography gutterBottom variant="body2">
+                          <span
+                            id="notification-message"
+                            dangerouslySetInnerHTML={{ __html: message.text }}
+                          />
+                        </Typography>
+                        {message.date && (
+                          <Typography variant="caption" color="textSecondary">
+                            {new Date(message.date).toLocaleDateString(
+                              'en-US',
+                              {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                              },
+                            )}
+                          </Typography>
+                        )}
+                      </ListItem>
+                      {index < messageList.length - 1 ? (
+                        <Divider className={classes.divider} />
+                      ) : null}
+                    </React.Fragment>
+                  ))}
                 </List>
               </Paper>
             </Grow>

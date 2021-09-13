571e12e507442bad05407c4710c1c1ab61e4709d
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2021-09-13 15:59:45 -0500

    create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from 'gatsby';
+import Typography from '@material-ui/core/Typography';
+import {MainLayout} from "../layouts/main";
+
+const ErrorPage = () => {
+  return (
+    <MainLayout>
+      <Typography variant="h3" gutterBottom>Page not found</Typography>
+      <Typography variant="body1" gutterBottom>Oops! The page you are looking for has been removed or relocated.</Typography>
+      <Typography variant="body2"><Link to="/">To Home</Link></Typography>
+  </MainLayout>
+  )
+}
+
+export default ErrorPage

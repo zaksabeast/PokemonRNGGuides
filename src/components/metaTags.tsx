c9be00ea23f452f7ad4d4d6299ed88575ce25bb9
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-02-12 09:31:44 -0600

    Add static meta tags

--- src/components/metaTags.tsx
+++ src/components/metaTags.tsx
@@ -1,4 +1,3 @@
-import JirachiPng from "~/assets/jirachi.png";
 import { useActiveRoute } from "~/hooks/useActiveRoute";
 import { getGuide } from "~/guides";
 
@@ -9,26 +8,32 @@ export const MetaTags = () => {
   const title = guide?.title ?? "PokemonRNG.com";
   const description = guide?.description ?? "Guaranteed perfect shinies";
 
+  // It's undefined behavior if these meta tags will replace the ones in index.html.
+  // If they do, great.
+  // If not, no worries.
+
+  // At some point I should probably build static pages for better SEO.
+
   return (
     <>
       <title>{title}</title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
       <meta name="theme-color" content="#ffffff" />
-      <link rel="icon" type="image/png" href={JirachiPng} />
+      <link rel="icon" type="image/png" href="/jirachi.png?v=1" />
 
       {/* Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={JirachiPng} />
+      <meta property="og:image" content="/jirachi.png?v=1" />
       <meta property="og:url" content="https://pokemonrng.com" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={JirachiPng} />
+      <meta property="twitter:image" content="/jirachi.png?v=1" />
     </>
   );
 };

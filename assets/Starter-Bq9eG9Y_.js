190e944ecf0bba92d8907974c7f1166a625cf582
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-02-16 17:02:29 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@dd66636117f1218dfe1781dbb51f8699d2605eb0 🚀

---
+++ assets/Starter-Bq9eG9Y_.js
@@ -0,0 +1,18 @@
+import{u as h,j as n}from"./index-CO9ioSnB.js";const a={title:"Starter Crystal RNG",description:"Get Shiny Starters in Crystal",slug:"gen2-starters",subCategory:"Emulator"};function i(t){const e={a:"a",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",ul:"ul",...h(),...t.components},{Countdown:r,Flex:s}=e;return r||o("Countdown"),s||o("Flex"),n.jsxs(n.Fragment,{children:[n.jsx(e.h3,{children:"Gen 2 Starter RNG – Live on Twitch!"}),`
+`,n.jsx(e.p,{children:n.jsx(e.strong,{children:"Gen 2 Starter RNG is finally here, and it’s easy to do!"})}),`
+`,n.jsxs(e.p,{children:["This is the ",n.jsx(e.strong,{children:"initial breakthrough"}),", and while it works, there’s still room for improvement. We’ll go over what’s been solved, what still needs refining, and how the community can help push it further."]}),`
+`,n.jsx(e.h4,{children:"When?"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.strong,{children:"Saturday, February 21st, 2025"})}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsxs(s,{align:"center",gap:16,children:[n.jsx(e.p,{children:n.jsx(e.strong,{children:"3 PM ET | 8 PM UTC"})}),n.jsx(r,{date:"2025-02-21T20:00:00Z"})]}),`
+`]}),`
+`,n.jsx(e.li,{children:n.jsx(e.strong,{children:n.jsx(e.a,{href:"https://www.twitch.tv/zaksabeastdev",children:"Watch live on Twitch!"})})}),`
+`]}),`
+`,n.jsx(e.h4,{children:"What’s happening on stream?"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Full walkthrough on how to RNG your perfect Gen 2 starter"}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Live Q&A"})," – Get help in real time!"]}),`
+`,n.jsx(e.li,{children:"The current state of Gen 2 RNG – what’s solved, what needs work, and how you can contribute"}),`
+`]}),`
+`,n.jsx(e.p,{children:"This has been a huge Pokémon RNG mystery for so long, and now it’s finally within reach. Don’t miss it!"})]})}function c(t={}){const{wrapper:e}={...h(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}function o(t,e){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{c as default,a as frontmatter};

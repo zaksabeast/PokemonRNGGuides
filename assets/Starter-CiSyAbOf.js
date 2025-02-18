cbcf788650f172abf5bf3f9da7d80c9de91825d4
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-02-18 22:55:15 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@c6e785a3e66b66f581d73a37a0eb3bb1c57801ec 🚀

---
+++ assets/Starter-CiSyAbOf.js
@@ -0,0 +1,19 @@
+import{u as l,j as n}from"./index-DoaNXjS6.js";const c={title:"Starter Crystal RNG",description:"Get Shiny Starters in Crystal",slug:"gen2-starters",subCategory:"Emulator"};function h(t){const e={a:"a",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",ul:"ul",...l(),...t.components},{Countdown:s,Flex:i,YouTubeVideo:o}=e;return s||r("Countdown"),i||r("Flex"),o||r("YouTubeVideo"),n.jsxs(n.Fragment,{children:[n.jsx(e.h3,{children:"Gen 2 Starter RNG – Live on Twitch!"}),`
+`,n.jsx(e.p,{children:n.jsx(e.strong,{children:"Gen 2 Starter RNG is finally here, and it’s easy to do!"})}),`
+`,n.jsxs(e.p,{children:["This is the ",n.jsx(e.strong,{children:"initial breakthrough"}),", and while it works, there’s still room for improvement. We’ll go over what’s been solved, what still needs refining, and how the community can help push it further."]}),`
+`,n.jsx(e.h4,{children:"When?"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Friday, February 21st"})," and ",n.jsx(e.strong,{children:"Saturday, February 22nd"})]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsxs(i,{align:"center",gap:16,children:[n.jsx(e.p,{children:n.jsx(e.strong,{children:"3 PM ET | 8 PM UTC"})}),n.jsx(s,{date:"2025-02-21T20:00:00Z"})]}),`
+`]}),`
+`,n.jsx(e.li,{children:n.jsx(e.strong,{children:n.jsx(e.a,{href:"https://www.twitch.tv/zaksabeastdev",children:"Watch live on Twitch!"})})}),`
+`]}),`
+`,n.jsx(o,{src:"https://www.youtube.com/embed/Cc1i9YIpWog"}),`
+`,n.jsx(e.h4,{children:"What’s happening on stream?"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Full walkthrough on how to RNG your perfect Gen 2 starter"}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Live Q&A"})," – Get help in real time!"]}),`
+`,n.jsx(e.li,{children:"The current state of Gen 2 RNG – what’s solved, what needs work, and how you can contribute"}),`
+`]}),`
+`,n.jsx(e.p,{children:"This has been a huge Pokémon RNG mystery for so long, and now it’s finally within reach. Don’t miss it!"})]})}function d(t={}){const{wrapper:e}={...l(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(h,{...t})}):h(t)}function r(t,e){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{d as default,c as frontmatter};

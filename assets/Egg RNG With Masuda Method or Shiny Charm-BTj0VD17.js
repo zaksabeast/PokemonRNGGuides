c9fd95f353b68d2c486574e1ae1550b64b39eced
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-02-19 20:33:24 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@776e5c17ae18ecd149adc41e310c0f9b19997767 🚀

---
+++ assets/Egg RNG With Masuda Method or Shiny Charm-BTj0VD17.js
@@ -0,0 +1,67 @@
+import{u as i,j as e}from"./index-DbyejMP8.js";const o={title:"Egg RNG Guide with Masuda Method or Shiny Charm",description:"RNG your perfect Pokemon at the daycare",slug:"retail-oras-egg-mmsc",subCategory:"Custom Firmware"};function r(t){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.pre,{children:e.jsx(n.code,{children:`This method differs from using without the Masuda Method or Shiny Charm. Here, you RNG for the egg once but waiting times for a target frame can be longer.
+`})}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["A 3DS with PCalc (",e.jsx(n.a,{href:"https://www.pokemonrng.com/misc-3ds-installing-pcalc",children:"PCalc Install Guide"}),")."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Enter RNG info"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Enter your game version and TSV (",e.jsx(n.code,{children:"Start + Up"})," in PCalc will reveal TSV in the Game View window)."]}),`
+`,e.jsxs(n.li,{children:["Enter the initial seed (",e.jsx(n.code,{children:"Start + Up"})," in PCalc will reveal this in the Game View window)."]}),`
+`,e.jsx(n.li,{children:"Tick the Shiny Charm box if you have it."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`To RNG the egg with a specific ESV that's not yours, click on Edit TSV List and add TSV(s). Then check the Other TSVs Shiny box.
+
+Input YOUR TSV in the upper right so the RNG works correctly.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Input parent information"}),`
+`,e.jsx(n.p,{children:"Enter details according to the parents you are using. Tick the Masuda Method box if parents are of different languages."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`The Pokemon's region doesn't affect anything. It's the language that matters for the Masuda Method.
+`})}),`
+`,e.jsx(n.p,{children:"For a Ditto and genderless Pokémon pairing, Ditto will be the female."}),`
+`,e.jsx(n.h2,{children:"Step 3: Egg Seeds, Filters, and Target Frame"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Input the current egg seeds of your game using PCalc's egg seed window (",e.jsx(n.code,{children:"Start + Down"}),")."]}),`
+`,e.jsx(n.li,{children:"Provide info for the egg you want in Filters."}),`
+`,e.jsx(n.li,{children:"Tick the Shiny Only box for a shiny egg."}),`
+`,e.jsxs(n.li,{children:["Open PCalc's Game Info screen with ",e.jsx(n.code,{children:"Start + Up"})," and pause the game with ",e.jsx(n.code,{children:"Start + Select"}),"."]}),`
+`,e.jsx(n.li,{children:"Enter your current frame and increase the frame range for more frames to search."}),`
+`,e.jsx(n.li,{children:`Select "To Accept" for the first egg or "To Reject" if not. We recommend accepting the first egg to verify frame -1. The Consider Delay counter will reset to 0 if you're rejecting the egg.`}),`
+`,e.jsx(n.li,{children:"Finally, click on Calculate for target frames. Increase frame range and recalculate if there are no results."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 4: RNGing the Egg Traits"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Choose your target frame."}),`
+`,e.jsxs(n.li,{children:["If you need an odd frame from an even one, or vice versa, save the game once after unpausing with ",e.jsx(n.code,{children:"Start"}),"."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Remember:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Frames advance by 2 outside of battles."}),`
+`,e.jsx(n.li,{children:"Frames are either always even or always odd."}),`
+`,e.jsx(n.li,{children:"Save once to switch from even to odd frames and vice versa."}),`
+`,e.jsx(n.li,{children:"The first egg collected/rejected is NOT the egg you RNG’d for."}),`
+`,e.jsx(n.li,{children:"Eggs are generated when the previous egg was collected/rejected and won't change."}),`
+`,e.jsx(n.li,{children:"Upon collecting/rejecting an egg, the NEXT egg will have the traits you RNG’d for."}),`
+`,e.jsx(n.li,{children:"Frame -1 signifies the current egg’s traits."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 4: RNGing the Egg Traits (Exluding PID/ESV)"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["In the game, talk to the daycare man until a specific dialogue appears.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Accepting the egg: “You’ll be wanting it, won’t you?” (ORAS) or “You do want it, don’t you?” (XY). Wait on Yes."}),`
+`,e.jsx(n.li,{children:"Rejecting the first egg: “Well then, I'll hang on to it. Thank you!” (XY/ORAS) after selecting No from previous dialogue."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"Start + Select"})," to pause the game near your target frame, then press ",e.jsx(n.code,{children:"Select"})," to advance frame by frame."]}),`
+`,e.jsxs(n.li,{children:["At your target frame, hold ",e.jsx(n.code,{children:"A"})," to both unpause the game and collect/reject the egg."]}),`
+`,e.jsxs(n.li,{children:["The egg collected now will match the frame -1 from the 3DSRNGTool. PCalc's ",e.jsx(n.code,{children:"Start + Right"})," shows egg info without hatching and ",e.jsx(n.code,{children:"Select + Right"})," cycles through the party slots."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Collect the NEXT egg, check or hatch it to confirm your Pokémon. Congratulations!"}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Verify the TSV in the upper right matches your save by opening the party view menu with ",e.jsx(n.code,{children:"Start + Right"})," then ",e.jsx(n.code,{children:"Select + Left"})," for slot 7."]}),`
+`,e.jsx(n.li,{children:"Ensure you're using the correct daycare view for ORAS if the first egg collected doesn't match frame -1."}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"Select + Right/Left"})," for egg seeds window to switch daycare views."]}),`
+`,e.jsx(n.li,{children:"Egg seeds should change after each egg. If they don't change, you might need the latest PCalc and game version."}),`
+`]})]})}function l(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{l as default,o as frontmatter};

5805489ecbaf488639015cc059cf25f260a3a687
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-02-23 00:23:16 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@2f766fe03ef1f3c114c7c81f605ea8d53114eefd 🚀

---
+++ assets/Starter-O7znhR3W.js
@@ -0,0 +1,18 @@
+import{u as s,j as n}from"./index-Csb-3nBq.js";const l={title:"Starter Crystal RNG",description:"Get Shiny Starters in Crystal",slug:"gen2-starters",subCategory:"Emulator"};function r(o){const e={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",ul:"ul",...s(),...o.components},{Gen2StarterRng:t}=e;return t||i("Gen2StarterRng"),n.jsxs(n.Fragment,{children:[n.jsx(e.h2,{children:"Tools"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,n.jsx(e.h2,{children:"Set up"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"Save in front of the starter you want, in case you need to reset the game."}),`
+`,n.jsxs(e.li,{children:["Choose your starter and stop when you see the ",n.jsx(e.code,{children:"Player received Pokemon"})," text with the flashing arrow to continue."]}),`
+`,n.jsxs(e.li,{children:["Go into PokeReader's RNG screen and wait until it stops saying ",n.jsx(e.code,{children:"Finding ADIV Index"})," and ",n.jsx(e.code,{children:"Finding SDIV Index"}),"."]}),`
+`,n.jsxs(e.li,{children:["Pause PokeReader with ",n.jsx(e.code,{children:"L + R"}),"."]}),`
+`,n.jsxs(e.li,{children:["Type the information on PokeReader into the RNG Tool below and press ",n.jsx(e.code,{children:"Generate"}),"."]}),`
+`,n.jsxs(e.li,{children:["Advance the game with ",n.jsx(e.code,{children:"L"})," until PokeReader's ",n.jsx(e.code,{children:"Advance"})," number matches one of the generated ",n.jsx(e.code,{children:"Advance"})," columns below."]}),`
+`,n.jsx(e.li,{children:"(Optional) Create a save state if you're on an emulator, in case you need to reset."}),`
+`,n.jsxs(e.li,{children:["When you land on your desired advance, press ",n.jsx(e.code,{children:"A"})," to accept the Pokemon."]}),`
+`,n.jsx(e.li,{children:"If you got your Pokemon, congrats! If not, keep trying! It won't take long."}),`
+`]}),`
+`,n.jsx(e.h2,{children:"RNG Tool"}),`
+`,n.jsx(t,{})]})}function a(o={}){const{wrapper:e}={...s(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(r,{...o})}):r(o)}function i(o,e){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{a as default,l as frontmatter};

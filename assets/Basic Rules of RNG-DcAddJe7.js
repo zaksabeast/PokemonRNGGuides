8b9ed65d27873c178fcdd6684ece713691b076ce
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-02-27 23:57:12 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@85e9152ee18a90025f6a8c840bf1b1dd98996fd8 🚀

---
+++ assets/Basic Rules of RNG-DcAddJe7.js
@@ -0,0 +1,16 @@
+import{u as o,j as e}from"./index-DcbTsy5O.js";const a={title:"Basic rules of Gen 3 RNG",description:"How to advance the RNG and tips to make everything more stable",slug:"frlg-tips-rng",subCategory:"Basic Knowledge",isRoughDraft:!0};function s(t){const n={code:"code",h2:"h2",h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Intro"}),`
+`,e.jsx(n.p,{children:"FRLG provides a helpful tool that's essential for any RNG work. It's one of the best tools you'll get when starting the game. However, vblank can be tricky. Follow these simple tips for a smoother RNG experience!"}),`
+`,e.jsx(n.h2,{children:"RNG Methods"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"For stationary Pokémon: Method 1."}),`
+`,e.jsx(n.li,{children:"For wild encounters: Method H-1."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Speeding Up RNG Advances"}),`
+`,e.jsx(n.h3,{children:"Teachy TV"}),`
+`,e.jsxs(n.p,{children:["This item, obtained during the tutorial, can be a game-changer. Using it advances the RNG ",e.jsx(n.strong,{children:"313 times faster"}),", allowing you to hit targets millions of advances away in seconds. Get comfortable with it; it will be your ally."]}),`
+`,e.jsx(n.h3,{children:"In-Battle"}),`
+`,e.jsx(n.p,{children:"In any Gen 3 game, being in battle speeds up RNG advancement by a factor of two."}),`
+`,e.jsx(n.h2,{children:"Trainer Card Flip"}),`
+`,e.jsxs(n.p,{children:["In Gen 3, doing a Trainer Card Flip resets noise-related delays, helping you focus on the default delay. It's a crucial step to do almost automatically. Access your Trainer Card and press ",e.jsx(n.code,{children:"A"}),". Do this between 10k-5k advances before your target advance. This can save you a lot of trouble."]}),`
+`,e.jsx(n.h2,{children:"Minimizing Noise"}),`
+`,e.jsx(n.p,{children:"To reduce vblank and noise issues, open your Trainer Card when starting your RNG, perform a flip, close it, and advance to your target advance. It sounds simple, but it's an effective strategy to protect yourself from unexpected surprises."})]})}function i(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{i as default,a as frontmatter};

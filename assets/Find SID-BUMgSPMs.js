dd78c96275ccdd477c5d36958e8e8a1f9e9b3ca7
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-02 18:10:46 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@526122afc35dda48e84a9194ed41d0d5a4beffbb 🚀

---
+++ assets/Find SID-BUMgSPMs.js
@@ -0,0 +1,62 @@
+import{u as o,j as e}from"./index-rxcn4Qg7.js";const a={title:"Find Gen 3 SID",description:"Various methods to finding an SID in Gen 3",slug:"gen3-sid",subCategory:"Retail",isRoughDraft:!0};function r(n){const s={a:"a",li:"li",ol:"ol",p:"p",ul:"ul",...o(),...n.components},{Alert:i}=s;return i||t("Alert"),e.jsxs(e.Fragment,{children:[e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're using an emulator or GBA flash cart"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you already have a Shiny"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Use EV-reducing berries if the Pokémon has battled to reset EVs."}),`
+`,e.jsxs(s.li,{children:["Open ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"})," and go to Tools > IV Calculator."]}),`
+`,e.jsx(s.li,{children:"Input the Pokémon's info to determine its IVs."}),`
+`,e.jsx(s.li,{children:"Open Gen 3 Tools > IVs to PID."}),`
+`,e.jsx(s.li,{children:"Enter the Pokémon’s IVs, nature, and your Trainer ID."}),`
+`,e.jsxs(s.li,{children:["Search for results and determine the correct method:",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Wild Pokémon: Methods 1-4."}),`
+`,e.jsx(s.li,{children:"Legendaries: Method 1."}),`
+`]}),`
+`]}),`
+`,e.jsx(s.li,{children:"If multiple SIDs appear, attempt to RNG a shiny using each until you find the correct one."}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you have a hacked Wii or Gamecube and a GBA-GC cable"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Dump your save with ",e.jsx(s.a,{href:"https://github.com/FIX94/gba-link-cable-dumper/releases/tag/v1.6",children:"FIX94's gba-link-cable-dumper"}),"."]}),`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you have an NDS and NDS flashcart"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Dump your save with ",e.jsx(s.a,{href:"https://projectpokemon.org/home/tutorials/save-editing/managing-gba-saves/using-gba-backup-tool-r55/",children:"Rudolph's GBA Backup Tool"}),"."]}),`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you can transfer Pokemon to a hacked dsi, 3ds, or switch"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Transfer a Pokemon up to your hacked console."}),`
+`,e.jsxs(s.li,{children:["Dump your hacked console's save",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/DS-Homebrew/GodMode9i/releases/latest",children:"GodMod9i for dsi"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/BernardoGiordano/Checkpoint/releases/latest",children:"Checkpoint for 3ds"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/J-D-K/JKSV/releases/latest",children:"JKSV for switch"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you can transfer Pokemon to Pokemon Home"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Transfer a Pokemon up to Pokemon Home."}),`
+`,e.jsxs(s.li,{children:["Join the ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"}),"."]}),`
+`,e.jsx(s.li,{children:"Ask someone to check the SID of the Pokemon."}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're okay using ACE and play Emerald"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Set up ACE with ",e.jsx(s.a,{href:"https://e-sh4rk.github.io/ACE3/emerald/getting-started/introduction/",children:"this guide"}),"."]}),`
+`,e.jsxs(s.li,{children:['Use the "Read SID from Pokémon in box 9 slot 27" code in ',e.jsx(s.a,{href:"https://pastebin.com/kYfBzVE3",children:"this pastebin"}),", which sets your TID to be your SID."]}),`
+`,e.jsx(s.li,{children:"View your Trainer Card, which will now show your SID instead of TID."}),`
+`,e.jsx(s.li,{children:"Soft reset to avoid saving the changes."}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're okay starting a new save in Emerald, Fire Red, or Leaf Green"})}),e.jsx("br",{}),e.jsx(i,{showIcon:!0,type:"warning",message:"This section is a work in progress and needs more details!"}),e.jsx("br",{}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Start eontimer same time as accepting name."}),`
+`,e.jsx(s.li,{children:"Estimate which SID was hit."}),`
+`,e.jsx(s.li,{children:"Try to rng a shiny starter for each potential SID until find shiny."}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're okay starting a new save in Ruby or Sapphire"})}),e.jsx("br",{}),e.jsx(i,{showIcon:!0,type:"warning",message:"This section is a work in progress and needs more details!"}),e.jsx("br",{}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Start new game and find Trainer ID."}),`
+`,e.jsx(s.li,{children:"Use this to deduce SID."}),`
+`,e.jsx(s.li,{children:"RNG shiny starter to confirm right SID."}),`
+`]})]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If nothing above applies to you (last resort option)"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Try to randomly encounter a shiny."}),`
+`,e.jsx(s.li,{children:'Follow the "If you already have a Shiny" section above.'}),`
+`]})]}),`
+`,e.jsxs(s.p,{children:["Are we missing a method? Join the ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"})," and let us know!"]})]})}function h(n={}){const{wrapper:s}={...o(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(r,{...n})}):r(n)}function t(n,s){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,a as frontmatter};

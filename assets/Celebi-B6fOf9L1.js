b7708559d0ba31c44ecfec57744e273a5e830ac4
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-10 01:49:03 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@6403c17b586724944a7af37b84bd6692dda3ba20 🚀

---
+++ assets/Celebi-B6fOf9L1.js
@@ -0,0 +1,23 @@
+import{u as d,j as e}from"./index-B1iwSKk2.js";const a={title:"Celebi",description:"How to get a shiny Fairy",slug:"gen2-celebi",subCategory:"Emulator",tag:"cfw",addedOn:"2025-03-02"};function l(o){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",pre:"pre",strong:"strong",ul:"ul",...d(),...o.components},{Alert:i,Gen2PokemonRng:s,YouTubeVideo:r}=n;return i||t("Alert"),s||t("Gen2PokemonRng"),r||t("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(i,{showIcon:!0,type:"warning",message:"Highly experimental!",description:"Gen 2 RNG is still in its early days. Expect to need multiple attempts!"}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Video guide"}),`
+`,e.jsx(r,{src:"https://www.youtube.com/embed/wTpI7MV_f4U?si=0qF9bFb0HrDyv2zw"}),`
+`,e.jsx(n.h2,{children:"Written guide"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Save in front of the Celebi shrine, in case you need to reset the game."}),`
+`,e.jsxs(n.li,{children:['Press "A" to interact with the shrine and stop when you see ',e.jsx(n.code,{children:"Player put in the GS Ball."}),"."]}),`
+`,e.jsxs(n.li,{children:["Go into PokeReader's RNG screen and wait until it stops saying ",e.jsx(n.code,{children:"Finding ADIV Index"})," and ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pause PokeReader with ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsxs(n.li,{children:["Type the information on PokeReader into the RNG Tool below and press ",e.jsx(n.code,{children:"Generate"}),"."]}),`
+`,e.jsxs(n.li,{children:["Advance the game with ",e.jsx(n.code,{children:"L"})," until PokeReader's ",e.jsx(n.code,{children:"Advance"})," number matches one of the generated ",e.jsx(n.code,{children:"Advance"})," columns below."]}),`
+`,e.jsx(n.li,{children:"(Optional) Create a save state if you're on an emulator, in case you need to reset."}),`
+`,e.jsxs(n.li,{children:["When you land on your desired advance, press ",e.jsx(n.code,{children:"A"})," to accept the Pokemon. ",e.jsx(n.strong,{children:"Do not press anything else until you see Celebi!"})]}),`
+`,e.jsx(n.li,{children:"If you got your Pokemon, congrats! If not, keep trying!"}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: This is less stable than starters!  Expect it to take longer!
+`})}),`
+`,e.jsx(n.h2,{children:"RNG Tool"}),`
+`,e.jsx(s,{type:"celebi"})]})}function h(o={}){const{wrapper:n}={...d(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(l,{...o})}):l(o)}function t(o,n){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,a as frontmatter};
---
+++ assets/Transporter Patches-BJFYQPz8.js
@@ -0,0 +1,44 @@
+import{u as o,j as e}from"./index-B1iwSKk2.js";const t={title:"Transporter Patches",description:"Various patches for use with Transporter",slug:"transporter-patches",subCategory:"3DS",tag:"cfw"};function r(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...o(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"A 3DS with CFW (Custom Firmware)"}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"Instructions for installing CFW"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Update Luma Settings"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Boot the console while holding ",e.jsx(n.code,{children:"Select"}),"."]}),`
+`,e.jsx(n.li,{children:'Select "Enable game patches".'}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Offline Patch + PKSM Patch"}),`
+`,e.jsx(n.p,{children:"This patch lets you use Transporter offline and keeps a stable delay for RNGing Pokémon from VC games. PKSM is needed as a destination for the transferred Pokémon."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Download the ",e.jsx(n.a,{href:"https://github.com/zaksabeast/Transporter-Offline-Patch/releases",children:"code.ips file"})," and copy it to the SD card at ",e.jsx(n.code,{children:"/luma/titles/00040000000C9C00/code.ips"}),"."]}),`
+`,e.jsxs(n.li,{children:['Open PKSM, enable "Edit during transfers", and create a new bank named ',e.jsx(n.code,{children:"transport"}),"."]}),`
+`,e.jsx(n.li,{children:"Transfer Pokémon using Transporter."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: For more info, visit the [PKSM wiki](https://github.com/FlagBrew/PKSM/wiki/Storage).
+`})}),`
+`,e.jsx(n.h3,{children:"Troubleshooting"}),`
+`,e.jsx(n.p,{children:'Q: Why does Transporter show, "At least one Pokémon remains in the Transport Box from your previous session"?'}),`
+`,e.jsx(n.p,{children:"A: This may happen if:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["You don't have a PKSM bank named ",e.jsx(n.code,{children:"transport"}),"."]}),`
+`,e.jsxs(n.li,{children:["You have Pokémon in Box 1 of your PKSM ",e.jsx(n.code,{children:"transport"})," bank."]}),`
+`,e.jsxs(n.li,{children:["Your PKSM ",e.jsx(n.code,{children:"transport"})," bank is invalid."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Cart Redirect Patch"}),`
+`,e.jsx(n.p,{children:"This patch redirects Transporter and Dream Radar to use a .sav file on the SD card instead of a game cartridge. It lets you use saves from an emulator without needing to inject them."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Download and unzip the ",e.jsx(n.a,{href:"https://github.com/zaksabeast/DreamRadarCartRedirect/releases",children:"zip file"}),"."]}),`
+`,e.jsx(n.li,{children:"Copy the IPS patch of your choice to the SD card."}),`
+`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["For Pokémon Transporter: ",e.jsx(n.code,{children:"/luma/titles/00040000000C9C00/code.ips"}),"."]}),`
+`,e.jsxs(n.li,{children:["For Japanese Pokémon Dream Radar: ",e.jsx(n.code,{children:"/luma/titles/0004000000073200/code.ips"}),"."]}),`
+`,e.jsxs(n.li,{children:["For All Regions Pokémon Dream Radar: ",e.jsx(n.code,{children:"/luma/titles/00040000000AE100/code.ips"}),"."]}),`
+`]}),`
+`,e.jsxs(n.ol,{start:"3",children:[`
+`,e.jsxs(n.li,{children:["Make sure you have a save file at ",e.jsx(n.code,{children:"/roms/nds/saves/white2.sav"}),", ",e.jsx(n.code,{children:"/roms/nds/saves/black2.sav"}),", ",e.jsx(n.code,{children:"/roms/nds/saves/black.sav"}),", or ",e.jsx(n.code,{children:"/roms/nds/saves/white.sav"}),"."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"Note: If using [TWiLightMenu](https://github.com/DS-Homebrew/TWiLightMenu), have a game file at `/roms/nds/white2.nds`, `/roms/nds/black2.nds`, `/roms/nds/black.nds`, or `/roms/nds/white.nds`.\n"})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Black and White are only supported by Transporter.
+`})})]})}function a(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}export{a as default,t as frontmatter};

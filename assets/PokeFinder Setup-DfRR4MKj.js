b572f5c2cc4a93378a8ad5bcfad3d6f8ca1417c6
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-16 14:39:24 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@ac4c02ffdd2a91eafdc5274f7e82dbc4222dfaf7 🚀

---
+++ assets/PokeFinder Setup-DfRR4MKj.js
@@ -0,0 +1,59 @@
+import{u as r,j as e}from"./index-D2lR0F5_.js";const s={title:"PokeFinder Setup",description:"How to setup PokeFinder for Diamond/Pearl/Platinum RNG",slug:"emulator-dppt-pokefinder-setup",subCategory:"Basic Knowledge",tag:"any"};function o(i){const n={a:"a",h2:"h2",h3:"h3",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Setting up Profiles"}),`
+`,e.jsx(n.p,{children:"PokeFinder has the ability to set up profiles for each of your individual saves. By setting up a profile for a save, you can have information such as TID/SID readily available for seed searching. This is useful because you only need to input save specific information once, and then it's available every time you relaunch PokeFinder."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Open PokeFinder and click on "Gen 4 Tools" then "Profile Manager".'}),`
+`,e.jsxs(n.li,{children:['Click on "New" in the Profile section, then fill in your details like TID, SID, and the game version you are using.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"This will only have to be done once for each profile you set up."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Diamond-Pearl-Platinum/PokeFinder/profile-editor.png",alt:""})}),`
+`,e.jsx(n.h2,{children:"Menu Options"}),`
+`,e.jsx(n.p,{children:"In the opening screen of PokeFinder there are options for which type of RNG you are doing. You will want to pick the correct one for the Pokemon you are going to RNG."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"[Stationary] - for Pokemon found in the overworld, or are received as gifts. See below for a more complete list."}),`
+`,e.jsx(n.li,{children:"[Wild] - for Pokemon found in the wild, such as grass, surfing, etc"}),`
+`,e.jsx(n.li,{children:"[Egg] - for Pokemon eggs collected from the daycare"}),`
+`,e.jsx(n.li,{children:"[TID/SID] - for finding a specific TID/SID to RNG"}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Diamond-Pearl-Platinum/PokeFinder/gen4-menu.png",alt:""})}),`
+`,e.jsx(n.h2,{children:"Generator and Searcher"}),`
+`,e.jsx(n.p,{children:'Within the "Stationary", "Wild", and "Egg" windows there are two different tabs for "Generator" and "Searcher".'}),`
+`,e.jsx(n.p,{children:"Here you can also select the profile you will be using through the dropdown besides profile. If you only have one profile set up then it will be chosen by default."}),`
+`,e.jsx(n.p,{children:'For most RNG processes, you will begin with the "Searcher" tab to find an initial seed, and then use the "Generator" tab with the initial seed found to RNG the Pokemon.'}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Diamond-Pearl-Platinum/PokeFinder/generator.png",alt:""})}),`
+`,e.jsx(n.h3,{children:"Searcher"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:['Navigate to the "Searcher" tab and select the correct method or encounter based on what you are RNGing.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"The guide you are following will explain which method or encounter to choose."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Choose any applicable leads if you have any like Synchronize or Cute Charm (Not applicable for Method 1).",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If you are not RNGing a genderless or gender fixed Pokemon and want to use Cute Charm, then follow the Cute Charm guide."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Adjust the filters for the Pokemon you are wanting."}),`
+`,e.jsx(n.li,{children:'Click "Search" and wait for PokeFinder to finish searching.'}),`
+`,e.jsxs(n.li,{children:["Choose one of the results to be your target seed.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If there are multiple results with the same seed then you can choose any of those."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:'Right click on the seed and choose "Generate times for seed".'}),`
+`]}),`
+`,e.jsx(n.p,{children:'The "Gen 4 Seed to Time" window should open and display the dates, times, and delays needed to RNG the initial seed. These will be used with the [Initial Seed RNG Guide].'}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Diamond-Pearl-Platinum/PokeFinder/searcher.png",alt:""})}),`
+`,e.jsx(n.h3,{children:"Generator"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'After finding a target seed, copy it into the "Seed" box of "Generator".'}),`
+`,e.jsx(n.li,{children:'Input everything to be the same as in "Searcher".'}),`
+`,e.jsx(n.li,{children:'Click "Generate" and then find the target advance for the Pokemon you are wanting.'}),`
+`]}),`
+`,e.jsx(n.p,{children:"You should now have everything set up to begin the RNG process."})]})}function l(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(o,{...i})}):o(i)}export{l as default,s as frontmatter};

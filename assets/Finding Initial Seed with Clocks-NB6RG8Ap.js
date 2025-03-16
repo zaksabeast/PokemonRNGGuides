abf436be595a1a042ca280559844a761a7d41181
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-16 14:13:07 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@758ac744a59b03d27cba5ef8733d809a62d1710d 🚀

---
+++ assets/Finding Initial Seed with Clocks-NB6RG8Ap.js
@@ -0,0 +1,59 @@
+import{u as t,j as e}from"./index-DODpnl6_.js";const l={title:"Finding your initial seed in Gen 7 with clocks",description:"This guide allows you to find your initial seed without using custom firmware.",slug:"retail-sm-initial-seed-clocks",subCategory:"Retail",tag:"retail"};function s(i){const n={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Video camera (your phone's camera should work)"}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"General principle"}),`
+`,e.jsxs(n.p,{children:["Each time you boot your game, an ",e.jsx(n.code,{children:"initial seed"})," (also called ",e.jsx(n.code,{children:"seed"}),") is created. This seed is used to RNG abuse wild encounters, events, in-game gifts, etc."]}),`
+`,e.jsxs(n.p,{children:[`At the "Continue" screen of the game, before your character picture loads, you can see a clock. By checking the clock needle's position, we can find the initial seed. You need to check between 8 and 10 clocks without restarting the game. You can leave the "Continue" screen by pressing `,e.jsx(n.code,{children:"B"}),"."]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"https://i.imgur.com/2Nh45HB.gif",alt:""})}),`
+`,e.jsx(n.h2,{children:"Step 1: Setup 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open the 3DSRNGTool you downloaded."}),`
+`,e.jsx(n.li,{children:'Select your game version in the top right ("Sun", "Moon", "Ultra Sun", or "Ultra Moon").'}),`
+`,e.jsx(n.li,{children:'Go to "Tools" > "Gen 7 Main RNG Tool".'}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Initial-Seed/Setup.png",alt:"Setup"})}),`
+`,e.jsxs(n.ol,{start:"4",children:[`
+`,e.jsxs(n.li,{children:["Ensure the ",e.jsx(n.code,{children:"InputBox"})," has ",e.jsx(n.code,{children:"End Position"})," selected with the number ",e.jsx(n.code,{children:"4"}),"."]}),`
+`,e.jsxs(n.li,{children:["Also select the ",e.jsx(n.code,{children:"Find Initial Seed via clock hands"})," option."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2: Recording the clock needles"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:['Boot your 3DS and open the game. Leave it on the "Press Start" screen.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"You can let the animations play; they do not affect the seed or frames."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Start recording your 3DS with your camera."}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"A"})," or ",e.jsx(n.code,{children:"Start"}),' to go to the "Continue" screen.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:'Record the entire clock movement: from when you enter the "Continue" screen until your character picture displays.'}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"B"}),' to return to the "Press Start" screen.']}),`
+`,e.jsx(n.li,{children:'Alternate between the "Press Start" and "Continue" screens until you have 10 clock movements.'}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: Finding the seed"}),`
+`,e.jsx(n.p,{children:"After recording 10 clock movements, list them in the tool to find the initial seed."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["In the ",e.jsx(n.code,{children:"InputBox"}),", select the clock that matches the final position of each clock.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"This End Position occurs just before the picture loads."}),`
+`,e.jsx(n.li,{children:"The End Position in the gif shown at the beginning is the last option in the Gen7 Main RNG Tool."}),`
+`,e.jsxs(n.li,{children:["You should see a number appear in the ",e.jsx(n.code,{children:"Needle List"})," (12 for the gif demo)."]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"The tool tries to find your seed after you input 8 needle positions."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Sun-Moon/Initial-Seed/Result.png",alt:"Result"})}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["You may get various results with only 8 needles; this is why we checked 10 clocks. Add the remaining 2 until you see a single seed.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"The last 2 needle positions confirm your seed."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"If you don't get any results, check your video again to find mistakes in the needle positions."}),`
+`,e.jsx(n.li,{children:"If you still can't find results, restart the game and record 10 new clocks."}),`
+`]}),`
+`,e.jsxs(n.p,{children:["Your seed should appear in the ",e.jsx(n.code,{children:"Results"})," field if you did everything correctly."]})]})}function r(i={}){const{wrapper:n}={...t(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}export{r as default,l as frontmatter};
---
+++ assets/Stationary and Gift RNG-oDU-iC4K.js
@@ -0,0 +1,105 @@
+import{u as i,j as e}from"./index-DODpnl6_.js";const o={title:"FRLG Stationary/Gift RNG",description:"Get shiny 6 IV stationaries from FRLG",slug:"emulator-flrg-stationary-and-gift",subCategory:"Emulator",tag:"emu"};function t(r){const n={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"VBA-rr"}),`
+`,e.jsxs(n.li,{children:["Lua scripts for FRLG (available ",e.jsx(n.a,{href:"https://projectpokemon.org/home/forums/topic/15187-gen-3-lua-scripts/?tab=comments#comment-127239",children:"here"})," - you'll need to copy the code, paste it to a notepad, and rename the file to 'whatyouwant.lua')"]}),`
+`,e.jsx(n.li,{children:"Knowledge of how to use the Memory Viewer"}),`
+`,e.jsx(n.li,{children:"RNG Reporter"}),`
+`,e.jsx(n.li,{children:"Your TID/SID"}),`
+`,e.jsx(n.li,{children:"A calculator and a notepad"}),`
+`,e.jsx(n.li,{children:"A way to read your save—PKHeX is recommended"}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Setup RNG Reporter"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open RNG Reporter."}),`
+`,e.jsx(n.li,{children:"Enter your TID/SID."}),`
+`,e.jsx(n.li,{children:"Use Method 1."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Pokemon that are Method 1 in FRLG:
+Bulbasaur, Charmander, Squirtle, Clefairy, Abra, Hypno, Voltorb, Electrode, Hitmonlee, Hitmonchan, Scyther, Pinsir, Magikarp, Lapras, Eevee, Porygon, Omanyte, Kabuto, Aerodactyl, Snorlax, Articuno, Zapdos, Moltres, Dratini, Mewtwo, Togepi, Lugia, Ho-Oh, Deoxys.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Setup VBA"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open your emulator."}),`
+`,e.jsx(n.li,{children:"Have a save in front of the stationary Pokémon or the NPC that gives you the Pokémon."}),`
+`,e.jsx(n.li,{children:'Load your Lua script by going to "Tools" => "Lua Scripting" => "New Lua Script". Select the Lua file you saved earlier.'}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: Setup the initial seed"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Load the game."}),`
+`,e.jsx(n.li,{children:'Enter the "Continue" menu.'}),`
+`,e.jsxs(n.li,{children:["Pause the emulator to find your initial seed.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"FRLG generates different spreads each time the game loads, preventing repeats."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Open the Memory Viewer and search at the address 0x02020000."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/FireRed-LeafGreen/Stationary/Memory-Viewer.png",alt:"Memory Viewer"})}),`
+`,e.jsxs(n.p,{children:['The "1330" is the seed for this run. It changes after pressing ',e.jsx(n.code,{children:"A"})," or ",e.jsx(n.code,{children:"Start"}),". You must use what you have."]}),`
+`,e.jsxs(n.ol,{start:"5",children:[`
+`,e.jsxs(n.li,{children:['Enter it in the "Seed (Hex)" box in RNG Reporter.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:'Enter your target settings for the Pokémon you want (shiny, IVs, nature, encounter slots, etc.) and hit "Generate".'}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/FireRed-LeafGreen/Stationary/Setup.png",alt:"Setup"})}),`
+`,e.jsx(n.h2,{children:"Step 4: RNG process"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Pick a frame to hit in RNG Reporter."}),`
+`,e.jsx(n.li,{children:"Your player should be in front of the right legendary, NPC, or Pokéball."}),`
+`,e.jsx(n.li,{children:"Create a save state to avoid mistakes."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Don't hesitate to overuse save states! This allows you to go back to lower frames and adjust since calibration is required every time.
+`})}),`
+`,e.jsxs(n.ol,{start:"4",children:[`
+`,e.jsxs(n.li,{children:["Advance to the final screen.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:`If the Pokémon has a dialog (like a cry) before the battle, that's the final screen before it's generated. The final screen for wild Pokémon is the cursor on "Sweet Scent". This may vary by case.`}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 5: Calibration"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Create a save state."}),`
+`,e.jsx(n.li,{children:"Write down the current frame."}),`
+`,e.jsxs(n.li,{children:["Hold the ",e.jsx(n.code,{children:"A"})," button and unpause the game.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"This triggers the battle or finishes the dialog for a gift on the final screen."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Check the stats of the Pokémon you've found. There are two cases:",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If it's stationary, the Lua will show all the IVs."}),`
+`,e.jsx(n.li,{children:"If it's a gift, save your game and load it with PKHeX."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Reload the save state you made to attempt the RNG again.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"This keeps the calibration intact while checking the Pokémon's stats."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Enter the Pokémon's stats into RNG Reporter to find the frame."}),`
+`,e.jsxs(n.li,{children:["Find how many frames you were off by:",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["(Frame you pressed ",e.jsx(n.code,{children:"A"}),") - (Frame from reporter)."]}),`
+`,e.jsx(n.li,{children:"It will often be a negative result around 90% of the time, which is normal."}),`
+`,e.jsxs(n.li,{children:["For example, if you pressed ",e.jsx(n.code,{children:"A"})," on frame 100 and hit Pokémon was frame 103, you were off by -3 frames."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Add the number of frames you were off to your original frame.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If you got a negative number, subtract it from your original frame."}),`
+`,e.jsxs(n.li,{children:["This new number is the new frame to press ",e.jsx(n.code,{children:"A"})," on."]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: You can calibrate whenever you want. For fast targets, doing it near your shiny frame helps. Calibrating too early can cause too low of a delay around your frame, requiring recalibration.
+`})}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsx(n.p,{children:"If you didn’t hit your target after calibration, retry the calibration step."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"You can use save states to avoid problems."}),`
+`]}),`
+`,e.jsx(n.p,{children:"If you still can't hit anything after a few tries, double-check your setup."}),`
+`,e.jsx(n.p,{children:"If you followed all steps, the Pokémon will be what you wanted after calibration. Congrats, you did your Gen 3 wild RNG!"})]})}function a(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{a as default,o as frontmatter};

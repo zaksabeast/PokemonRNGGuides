c9a819491310034bbac34c49731eca38dc63d613
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-16 14:55:51 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@5f1cec827cff9b251740fc728593881b79d0b202 🚀

---
+++ assets/TID SID-BiJjHpoK.js
@@ -0,0 +1,10 @@
+import{u as r,j as e}from"./index-B7H-cgmt.js";const i={title:"TID/SID RNG",description:"How to get a desired TID/SID combo",slug:"emulator-dppt-tid-sid",subCategory:"Emulator",isRoughDraft:!0,tag:"emu"};function o(n){const t={code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Tools"}),`
+`,e.jsx(t.h2,{children:"Step 1: Understanding PID"}),`
+`,e.jsx(t.p,{children:"Here's a basic overview to help with getting wanted PID."}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Pay attention to uppercase and lowercase letters for delay issues."}),`
+`,e.jsx(t.li,{children:"Consider the special tool for finding pairs faster if they're not found by PokeFinder search."}),`
+`]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:`Remember to check for any updates on tools or methods.
+`})}),`
+`,e.jsx(t.p,{children:"Congrats! You've now got a better grasp on TID/SID RNG!"})]})}function a(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{a as default,i as frontmatter};

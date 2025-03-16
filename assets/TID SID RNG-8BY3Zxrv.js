f6aaf8aff929dcd1480e50ddb3b7fcb2c355d48a
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-16 17:43:42 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@e370befa4d2ccb96085ebd76452132d316bf9a83 🚀

---
+++ assets/TID SID RNG-8BY3Zxrv.js
@@ -0,0 +1,11 @@
+import{u as s,j as e}from"./index-B0g2fIc3.js";const i={title:"TID/SID RNG",description:"How to RNG your TID/SID with CaptureSight",slug:"cfw-bdsp-tidandsid",subCategory:"CFW",isRoughDraft:!0,tag:"cfw"};function o(n){const t={code:"code",h2:"h2",li:"li",p:"p",ul:"ul",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.p,{children:"This guide will help you RNG the TID/SID combination of your choice. Both are linked, so one frame will give both TID and SID. Make sure to read the Basic Knowledge guides, especially the PokeFinder guide, since it will be used here."}),`
+`,e.jsx(t.h2,{children:"Tools"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"PokeFinder Builds"}),`
+`,e.jsx(t.li,{children:"CaptureSight"}),`
+`]}),`
+`,e.jsx(t.p,{children:"Open your game. Use CaptureSight to get the states. Open PokeFinder, go to the Gen 8 tab, and click on TID/SID."}),`
+`,e.jsx(t.p,{children:"Generate the results. Remember, this is a slow RNG, so avoid going for too high frames. No delay is needed for this RNG."}),`
+`,e.jsxs(t.p,{children:["The TID/SID is generated when you press ",e.jsx(t.code,{children:"Yes"})," after entering your name. States are frozen at the name screen, so hit your target when you select your player's hair and style."]}),`
+`,e.jsx(t.p,{children:"To advance states, watch the Munchlax in the intro blinking; this allows for +1 advancement."}),`
+`,e.jsx(t.p,{children:"You can also advance your states by +5 or +6 by returning to the Hat menu after selecting your name. This helps you reach your target faster."})]})}function a(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{a as default,i as frontmatter};

ac4c02ffdd2a91eafdc5274f7e82dbc4222dfaf7
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-16 09:37:59 -0500

    Fix direct navigation with conditional logic

--- src/components/showIf.tsx
+++ src/components/showIf.tsx
@@ -1,10 +1,18 @@
+import { useLocation } from "wouter";
+
 type Props = {
   slug: string;
   children: React.ReactNode;
 };
 
 export const ShowIf = ({ slug, children }: Props) => {
-  if (window.location.pathname === slug) {
+  const [location] = useLocation();
+  const trimmedLocation =
+    location.endsWith("/") && location !== "/"
+      ? location.slice(0, -1)
+      : location;
+
+  if (trimmedLocation === slug) {
     return <>{children}</>;
   }
   return null;

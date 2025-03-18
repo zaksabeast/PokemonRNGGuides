801bec4e1f8aa6540cb981ceefb00046fd6b66f4
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-18 17:24:53 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@cafede2600f0ce89f9d32469bce8e383a6eaa2de 🚀

---
+++ assets/Emu PokeReader-hTc22riq.js
@@ -0,0 +1,30 @@
+import{u as l,j as e}from"./index-BtvG23PZ.js";const o={title:"PokeReader Lime3DS",description:"Installing a tool on a 3DS emulator, such as Azahar, Lime3DS, Citra, to help RNG Pokémon",slug:"install-pokereader-emu",subCategory:"3DS",tag:"emu"};function r(s){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...l(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"PokeReader is a plugin to help RNG on emulators."}),`
+`,e.jsxs(n.p,{children:["For how to install PokeReader on a 3DS console, ",e.jsx(n.a,{href:"/install-pokereader",children:"go here"}),"."]}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/Lime3DS/lime3ds-archive/releases/",children:"Lime3DS"})," or Citra"]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Install PokeReader on Azahar/Lime3DS/Citra"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Download the ",e.jsx(n.a,{href:"https://github.com/zaksabeast/PokeReader/releases/latest",children:"latest release of PokeReader"}),"."]}),`
+`,e.jsx(n.li,{children:"Open the emulator's preferences."}),`
+`,e.jsxs(n.li,{children:["Navigate to ",e.jsx(n.code,{children:"System"})," settings."]}),`
+`,e.jsxs(n.li,{children:["Check the ",e.jsx(n.code,{children:"Enable 3GX Plugin Loader"})," box."]}),`
+`,e.jsxs(n.li,{children:["Go to the ",e.jsx(n.code,{children:"Storage"})," tab."]}),`
+`,e.jsxs(n.li,{children:["Click ",e.jsx(n.code,{children:"Open"})," next to ",e.jsx(n.code,{children:"SDMC Directory"}),"."]}),`
+`,e.jsxs(n.li,{children:["Create the ",e.jsx(n.code,{children:"/luma/plugins"})," folder if it does not exist."]}),`
+`,e.jsxs(n.li,{children:["Copy ",e.jsx(n.code,{children:"default.3gx"})," to the ",e.jsx(n.code,{children:"/luma/plugins/"})," folder."]}),`
+`,e.jsx(n.li,{children:"Update your Pokémon games - PokeReader requires the latest version."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"We will update with more instructions once Azahar is released."})}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"PokeReader does not load and my game crashes!"})}),`
+`,e.jsx(n.p,{children:"Make sure you have updated the game to the latest version. PokeReader will not work on older game versions."}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"/citrarng-setup",children:"This guide"})," has instructions for how to dump a game update from a CFW 3DS console."]}),`
+`,e.jsx(n.h2,{children:"What next?"}),`
+`,e.jsx(n.p,{children:"Try some of these RNGs!"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/gen2-celebi",children:"Shiny Celebi in Crystal"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/pcalc-xy-friend-safari",children:"XY Friend Safari RNG"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/retail-usum-island-scan",children:"Island Scan RNG"})}),`
+`]})]})}function t(s={}){const{wrapper:n}={...l(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}export{t as default,o as frontmatter};

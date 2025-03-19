e54bf9b615c64ad1e8d01492140b503d3767577f
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-03-19 02:25:35 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@d887124940f23ad7b60d0686d8a0d3e100af6225 🚀

---
+++ assets/Desmume Setup-C0Fa3XZj.js
@@ -0,0 +1,53 @@
+import{u as l,j as e}from"./index-BeAQrcwS.js";const o={title:"Desmume Setup",description:"Setup Desmume for RNG",slug:"desmume-setup",subCategory:"DS",tag:"emu"};function i(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...l(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"This guide helps you set up Desmume on a Windows PC. Mac and Linux users can try using Wine to run Desmume.exe, but this may not work. An updated CFW (Custom Firmware) 3DS console is needed to dump DS game data. Other methods for dumping DS games exist, but this guide won't cover them."}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://sourceforge.net/projects/desmume/files/desmume/0.9.11/desmume-0.9.11-win32-dev.zip/download",children:"Desmume v0.9.11"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://sourceforge.net/projects/luabinaries/files/5.1.5/Windows%20Libraries/Dynamic/lua-5.1.5_Win32_dll17_lib.zip/download",children:"Lua dll"})}),`
+`,e.jsxs(n.li,{children:["Lua Scripts - ",e.jsx(n.a,{href:"https://github.com/Real96/PokeLua/tree/main/Gen%204",children:"Gen 4"})," ",e.jsx(n.a,{href:"https://github.com/Real96/PokeLua/tree/main/Gen%205",children:"Gen 5"})]}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"A 3DS with CFW (Custom Firmware)"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/d0k3/GodMode9/releases",children:"Latest Godmode9"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/FlagBrew/Checkpoint/releases",children:"Checkpoint for the 3DS"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Dumping Game Data"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Load Godmode9 by holding ",e.jsx(n.code,{children:"Start"})," while turning on the console."]}),`
+`,e.jsxs(n.li,{children:["Select ",e.jsx(n.code,{children:"[C:] GAMECART"}),"."]}),`
+`,e.jsxs(n.li,{children:["Choose the ",e.jsx(n.code,{children:".nds"})," file."]}),`
+`,e.jsxs(n.li,{children:["Select ",e.jsx(n.code,{children:"Copy to 0:/gm9/out"}),"."]}),`
+`,e.jsxs(n.li,{children:["Exit GodMode9 and transfer the ",e.jsx(n.code,{children:".nds"})," file from ",e.jsx(n.code,{children:"sdmc:\\gm9\\out"})," to your PC."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Extracting Save"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Load Checkpoint on your 3DS."}),`
+`,e.jsx(n.li,{children:"Dump the save from the DS cart."}),`
+`,e.jsxs(n.li,{children:["The save is saved in ",e.jsx(n.code,{children:"sdmc:\\3ds\\Checkpoint\\saves"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Setting Up Desmume"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Download both Desmume and the lua dll."}),`
+`,e.jsxs(n.li,{children:["Rename ",e.jsx(n.code,{children:"lua5.1.dll"})," to ",e.jsx(n.code,{children:"lua51.dll"}),"."]}),`
+`,e.jsx(n.li,{children:"Place the dll in the same folder as the Desmume executable."}),`
+`,e.jsx(n.li,{children:"Download the lua script for your game version."}),`
+`,e.jsxs(n.li,{children:["Open Desmume and go to ",e.jsx(n.code,{children:"File"}),", select ",e.jsx(n.code,{children:"Open ROM"}),"."]}),`
+`,e.jsxs(n.li,{children:["Open the ",e.jsx(n.code,{children:".nds"})," file extracted from the DS cart."]}),`
+`,e.jsxs(n.li,{children:["Go to ",e.jsx(n.code,{children:"Tools"}),", select ",e.jsx(n.code,{children:"Lua Scripting"}),", then ",e.jsx(n.code,{children:"New Lua Script Window..."}),"."]}),`
+`,e.jsx(n.li,{children:"Choose the Lua Script you downloaded earlier."}),`
+`]}),`
+`,e.jsx(n.p,{children:"Desmume should now be running with your game and displaying RNG information."}),`
+`,e.jsx(n.h2,{children:"Importing a Save"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Go to ",e.jsx(n.code,{children:"File"})," and select ",e.jsx(n.code,{children:"Import Backup Memory..."}),"."]}),`
+`,e.jsxs(n.li,{children:["Load the ",e.jsx(n.code,{children:".sav"})," file extracted from the DS cart."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Exporting a Save"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Go to ",e.jsx(n.code,{children:"File"})," and select ",e.jsx(n.code,{children:"Export Backup Memory..."}),"."]}),`
+`,e.jsxs(n.li,{children:["Name your ",e.jsx(n.code,{children:".sav"})," file and choose where to save it."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["Everytime you restart the game, press the ",e.jsx(n.code,{children:"Restart"})," button in the lua window."]}),`
+`,e.jsxs(n.p,{children:["Every time you need to save or load a state, pause the game and hold ",e.jsx(n.code,{children:"Shift + F(n)"})," / ",e.jsx(n.code,{children:"F(n)"})," until you see the message ",e.jsx(n.code,{children:"Saved State (n)"})," / ",e.jsx(n.code,{children:"Loaded State (n)"})," appearing on the lower screen for less than a second."]}),`
+`,e.jsxs(n.p,{children:["For example, ",e.jsx(n.code,{children:"Shift + F1"})," to save a state in slot 1 or ",e.jsx(n.code,{children:"F1"})," to load the state in slot 1."]}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsx(n.h3,{children:"lua 51.dll was not found."}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Tools-and-Emulators/Desmume/Lua.png",alt:"Lua Error"})}),`
+`,e.jsx(n.p,{children:"If you see an error like the one above, the lua dll may be missing. Redownload the dll and ensure it is in the same folder as the Desmume executable. Verify that the dll was renamed correctly."})]})}function r(s={}){const{wrapper:n}={...l(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}export{r as default,o as frontmatter};
---
+++ assets/Wild RNG-CgD6rACo.js
@@ -0,0 +1,90 @@
+import{u as s,j as e}from"./index-BeAQrcwS.js";const o={title:"Wild RNG",description:"RNG for Pokémon encountered in the wild using Sweet Scent in FireRed/LeafGreen",slug:"emulator-frlg-wild",subCategory:"Emulator",tag:"emu"};function i(t){const n={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/mgba-setup",children:"mGBA with lua scripts"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"})}),`
+`,e.jsx(n.li,{children:"TID and SID (if going for shiny)"}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If you are doing this RNG for a shiny, make sure to set up a profile in PokeFinder with the TID and SID of the save.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 1: Set up PokeFinder"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:`Open PokeFinder and select "Wild" for Gen 3. Make sure you're on the "Generator" tab.`}),`
+`,e.jsx(n.li,{children:'Set "Method" to Method H-1.'}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: While there are other methods in FireRed/LeafGreen, H-1 is the most common and recommended. The other methods rely on v-blank and occur rarely.
+`})}),`
+`,e.jsxs(n.ol,{start:"3",children:[`
+`,e.jsx(n.li,{children:'Set "Location" to where the RNG will take place.'}),`
+`,e.jsxs(n.li,{children:['If you want a specific Pokémon, select it in "Pokemon" after selecting its location.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:`If the Pokémon you want isn't listed or you need a specific encounter slot, choose the number in the "Encounter Slot" box and leave "Pokemon" at the default.`}),`
+`,e.jsxs(n.li,{children:["Use ",e.jsx(n.a,{href:"https://sites.google.com/site/pokemonslots/gen-iii/emerald",children:"this site"})," to find a specific encounter slot."]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:'Set "Encounter" to "Grass" or "Surfing" since this guide focuses on using Sweet Scent to start an encounter.'}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: For "Lead," leave it as "Any" since Cute Charm and Synchronize do not work in FireRed/LeafGreen.
+`})}),`
+`,e.jsxs(n.ol,{start:"6",children:[`
+`,e.jsxs(n.li,{children:['Check the "Use Delay" box and input the delay for the Pokémon you are RNGing.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["This ",e.jsx(n.a,{href:"https://docs.google.com/spreadsheets/d/1cVweVvJXCXeTZOBVKVCBbcSI46rqBXV3ahbuoSGOnzk/edit#gid=1091733147",children:"Google Doc"})," has the delays for each area."]}),`
+`,e.jsx(n.li,{children:"Keep in mind there could still be a variation of +/- 1 delay."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2: Finding the initial seed"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Save the game in the location where you'll RNG the Pokémon."}),`
+`,e.jsx(n.li,{children:"Restart the emulator."}),`
+`,e.jsx(n.li,{children:"Once the game loads, enter the continue screen."}),`
+`,e.jsx(n.li,{children:"Pause the game to find your seed."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: FireRed/LeafGreen generates different seeds every time the game loads, unlike repeating seeds in dry battery Ruby and Sapphire, and Emerald. This can be used to reset for different seeds if needed.
+`})}),`
+`,e.jsxs(n.ol,{start:"5",children:[`
+`,e.jsx(n.li,{children:"The initial seed will be displayed on the screen from the Lua script."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/FireRed-LeafGreen/Wild/Initial-Seed.png",alt:"Initial Seed"})}),`
+`,e.jsx(n.h2,{children:"Step 3: Find a target advance"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Enter the initial seed into "Seed."'}),`
+`,e.jsxs(n.li,{children:["Enter your target settings for the Pokémon you wish to search for (shiny, IVs, nature, etc.).",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Finding a shiny perfect Pokémon may take a while to find a seed, which will likely have very high advances."}),`
+`,e.jsx(n.li,{children:"If no results show up, try lowering the filters."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:'Click "Generate."'}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/FireRed-LeafGreen/Wild/Setup.png",alt:"Setup"})}),`
+`,e.jsx(n.p,{children:"In the example above, the filters are set for any shiny on Route 5."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"Note: The initial seed can be changed by pressing `B` on the continue screen to play the intro again. The seed will change when pressing `A` to advance to the continue screen. You can repeat this process as needed to find a better seed if wanted.\n"})}),`
+`,e.jsx(n.h2,{children:"Step 4: RNG for the Pokémon"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Once you have a target advance, continue into the game."}),`
+`,e.jsx(n.li,{children:"Advance to within a few thousand advances of the target advance."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Teachy TV can be used to advance much faster. The RNG advances 313 times faster, allowing you to advance millions of frames in a few minutes.
+
+Open Teachy TV in Key Items and close it when a few thousand away from your target advance.
+`})}),`
+`,e.jsxs(n.ol,{start:"3",children:[`
+`,e.jsxs(n.li,{children:["Open your Trainer Card and press ",e.jsx(n.code,{children:"A"})," to flip it.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Flipping the Trainer Card resets the delay, which becomes more off the more advances you make."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Select the Pokémon in your party with Sweet Scent and hover over the move."}),`
+`,e.jsx(n.li,{children:"When close to your target advance, pause the emulator and create a save state, then advance a frame at a time to the target advance."}),`
+`,e.jsxs(n.li,{children:["Hold ",e.jsx(n.code,{children:"A"})," and unpause the emulator at the same time on your target advance.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If the Pokémon is not the one wanted, load the previous save state and try one advance before and/or one advance after."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:"If you followed all the above steps, the Pokémon should be what you wanted. Tada, you did your Gen 3 Wild RNG!"}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/FireRed-LeafGreen/Wild/Success.png",alt:"Success"})}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsx(n.p,{children:"If after trying the one advance difference above it didn't work, you'll have to adjust for your own delay."}),`
+`,e.jsx(n.p,{children:"Find what advance you are hitting and adjust as needed."})]})}function a(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{a as default,o as frontmatter};

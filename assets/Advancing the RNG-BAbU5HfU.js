88a1e2bb5d00e37eeb689e0db01ad0f9588e5259
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-03-19 02:35:23 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@c63cae854f8053d5473f84e25d9b962cf540d1c8 🚀

---
+++ assets/Advancing the RNG-BAbU5HfU.js
@@ -0,0 +1,12 @@
+import{u as s,j as e}from"./index-DLiN6mXq.js";const r={title:"Advancing the RNG",description:"The different methods the RNG can be advanced and events that influence the RNG",slug:"bdsp-advance-rng",subCategory:"Basic Knowledge",tag:"any"};function a(t){const n={h2:"h2",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"This explains how to advance and influence the RNG in Brilliant Diamond and Shining Pearl. The best way to advance the RNG is using the Pokedex, but there are other methods if it's not available."}),`
+`,e.jsx(n.h2,{children:"Character Movement"}),`
+`,e.jsx(n.p,{children:"When the player stands still, their character fidgets and blinks, advancing the RNG. However, this method is slow. It's still possible to land on a target advance in quiet areas. For eggs and mystery gifts, it's best to be upstairs in the player's house since only the player's character is advancing the RNG there."}),`
+`,e.jsx(n.p,{children:"Bringing up the in-game menu stops the character from fidgeting, which can help reduce RNG calls in noisy areas."}),`
+`,e.jsx(n.h2,{children:"NPCs"}),`
+`,e.jsx(n.p,{children:"NPCs advance the RNG even when they're off-screen. Areas with more NPCs will advance the RNG faster. It's better to avoid areas with NPCs, as they can skip over your target advance."}),`
+`,e.jsx(n.p,{children:"If the Pokedex isn't an option, being in an area with many NPCs, like towns, can help reach your target advance quickly. For example, nearby routes and towns can help with starters."}),`
+`,e.jsx(n.h2,{children:"Trainer Card"}),`
+`,e.jsx(n.p,{children:"Opening the Trainer Card advances the RNG quickly."}),`
+`,e.jsx(n.h2,{children:"Pokedex"}),`
+`,e.jsx(n.p,{children:"Using the Pokedex is the best method to advance the RNG."}),`
+`,e.jsx(n.p,{children:"Scrolling up and down while viewing Pokedex entries advances the RNG quickly. Viewing Aipom's, Azelf's, or Munchlax's entries will advance the RNG even faster, if available."})]})}function h(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a(t)}export{h as default,r as frontmatter};

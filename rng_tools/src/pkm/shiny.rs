cb418766e7e64aaf6e26a252aad1b230f5e2b8ba
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-22 20:35:45 -0500

    Shift unused g3 shiny value correctly

--- rng_tools/src/pkm/shiny.rs
+++ rng_tools/src/pkm/shiny.rs
@@ -1,28 +1,23 @@
 pub fn gen3_tsv(tid: u16, sid: u16) -> u16 {
-    (tid ^ sid) >> 4
-}
-
-pub fn gen3_psv(pid: u32) -> u16 {
-    let pidh = (pid >> 16) as u16;
-    let pidl = pid as u16;
-    (pidh ^ pidl) >> 4
-}
-
-pub fn gen6_tsv(tid: u16, sid: u16) -> u16 {
     (tid ^ sid) >> 3
 }
 
-pub fn gen6_psv(pid: u32) -> u16 {
+pub fn gen3_psv(pid: u32) -> u16 {
     let pidh = (pid >> 16) as u16;
     let pidl = pid as u16;
     (pidh ^ pidl) >> 3
 }
 
 pub fn gen3_shiny(pid: u32, tid: u16, sid: u16) -> bool {
+    gen3_psv(pid) == gen3_tsv(tid, sid)
+}
+
+pub fn gen6_tsv(tid: u16, sid: u16) -> u16 {
+    (tid ^ sid) >> 4
+}
+
+pub fn gen6_psv(pid: u32) -> u16 {
     let pidh = (pid >> 16) as u16;
     let pidl = pid as u16;
-    let psv = (pidh ^ pidl) >> 3;
-    let tsv = (tid ^ sid) >> 3;
-
-    psv == tsv
+    (pidh ^ pidl) >> 4
 }

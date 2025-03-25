f752d618aeef4961253694f2cc65e4e307928569
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-25 01:25:32 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@74bcfc47ead1cd6584e682218bc4f4784a919a82 🚀

---
+++ assets/Stationary RNG-Bi1nAaoE.js
@@ -0,0 +1,56 @@
+import{u as i,j as e}from"./index-Dd9K0D0t.js";const s={title:"Stationary RNG",description:"RNG the overworld Pokémon to have 6 IVs.",slug:"retail-usum-stationary",subCategory:"Custom Firmware",tag:"cfw"};function o(t){const n={a:"a",code:"code",em:"em",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["A 3DS with PCalc (",e.jsx(n.a,{href:"https://www.pokemonrng.com/misc-3ds-installing-pcalc",children:"PCalc Install Guide"}),")"]}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Recommended Reading"}),`
+`,e.jsxs(n.p,{children:["If you want to RNG a Mystery Gift (or Event) Pokémon, follow the ",e.jsx(n.a,{href:"https://www.pokemonrng.com/retail-usum-mystery-gift",children:"Gen 7 Mystery Gift RNG guide"}),"."]}),`
+`,e.jsxs(n.p,{children:["If you want to RNG a Pokémon in a Wormhole, follow the ",e.jsx(n.a,{href:"https://www.pokemonrng.com/retail-usum-wormhole",children:"USUM Wormhole RNG guide"}),"."]}),`
+`,e.jsx(n.h2,{children:"Final Screens"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Tapus: ",e.jsx(n.em,{children:"Tapu ko-ko-ko-kooo!!!"})," / ",e.jsx(n.em,{children:"Ta-pu-leeeh!"})," / ",e.jsx(n.em,{children:"Ta-pu-looooo"})," / ",e.jsx(n.em,{children:"Ta-pu-fiiieee!"}),"."]}),`
+`,e.jsx(n.li,{children:'In-game gifts/Fossils/Cosmog: "You received xxx!".'}),`
+`,e.jsxs(n.li,{children:["Necrozma: ",e.jsx(n.em,{children:"Linooo!"}),"."]}),`
+`,e.jsx(n.li,{children:`Starters: SM - "Having accepted one another, you'll surely be friends for life." - USUM - "You chose xxx!".`}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Set Up 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["In the upper right, input your game version and your TSV. With PCalc, press ",e.jsx(n.code,{children:"Start + Up"})," to open the Game View window and find your TSV at ",e.jsx(n.code,{children:"YOUR TSV"}),"."]}),`
+`,e.jsxs(n.li,{children:["Also in the upper right, input the initial seed. Press ",e.jsx(n.code,{children:"Start + Up"})," to access the Game View window, where the initial seed is listed as ",e.jsx(n.code,{children:"Init Seed:"}),"."]}),`
+`,e.jsx(n.li,{children:'If you have the Shiny Charm, check the "Shiny Charm" box.'}),`
+`,e.jsx(n.li,{children:`Ensure you're on the "Stationary RNG" tab in 3DSRNGTool. Choose the "Category" and then the Pokémon you are RNGing for.`}),`
+`,e.jsx(n.li,{children:'If you have a Pokémon with Synchronize in the first slot of your party, select its nature from the dropdown for "Synch Nature". Failure to do this if the lead Pokémon has Synchronize will result in incorrect frames.'}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: For gift Pokémon, Synchronize guarantees the Pokémon will have the same nature as the lead Pokémon. If aiming for a specific nature, having Synchronize in the first slot will significantly improve your chances of finding target frames, as every frame will have the desired nature.
+The gift Eevee egg and the move Pikachu gift are exceptions. Synchronize has no effect on their natures.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2 (with NPCs): Create a Timeline"}),`
+`,e.jsxs(n.p,{children:["Follow the ",e.jsx(n.a,{href:"https://www.pokemonrng.com/retail-usum-timeline",children:"timeline guide"})," to create a timeline and find a target frame."]}),`
+`,e.jsx(n.h2,{children:"Step 3: Obtaining the Wanted Pokémon"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Once you have a correct timeline and a target frame, the next step is to RNG the Pokémon."}),`
+`,e.jsxs(n.li,{children:["Advance to your target frame. When you land on it, press ",e.jsx(n.code,{children:"A"})," to obtain the Pokémon or start a battle with it."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Congrats! You should now have the Pokémon you wanted. If not, reset the game and try again."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"Note: You can view the Pokémon's info using PCalc. Press `Start + Left` to open the Wild View window or `Start + Right` to access the Party View window. Use `Select + Right` to cycle through party members in Party View.\n"})}),`
+`,e.jsx(n.h2,{children:"Step 2 (with 0 NPCs):"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Do not check the "Blink F Only" box.'}),`
+`,e.jsxs(n.li,{children:["Advance to the final screen for the Pokémon you want to RNG. Input the current frame in the frame range. You can find it in the PCalc Game Info window (",e.jsx(n.code,{children:"Start + Up"}),") and press ",e.jsx(n.code,{children:"Start + Select"})," to pause the game."]}),`
+`,e.jsx(n.li,{children:'Adjust filters as needed, then click "Calculate" and choose any result as your target frame. If there are no results, increase the frame range or restart the game to get a new initial seed. (Restart from the beginning of the guide if you restart the game.)'}),`
+`,e.jsxs(n.li,{children:["Advance frames by pressing ",e.jsx(n.code,{children:"Start"})," to unpause the game. Pause again near your target frame with ",e.jsx(n.code,{children:"Start + Select"}),", then slowly advance by pressing ",e.jsx(n.code,{children:"Select"})," while paused."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: With 0 NPCs, you can use Festival Plaza to advance frames quicker. Only do this with 0 NPCs, or it could mess up the timeline and cause you to miss your target frame.
+Exit Festival Plaza approximately a thousand frames before your target frame to avoid missing it.
+`})}),`
+`,e.jsxs(n.ol,{start:"5",children:[`
+`,e.jsxs(n.li,{children:["Advance to your target frame. When you land on it, press ",e.jsx(n.code,{children:"A"})," to obtain the Pokémon or start a battle with it."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Congrats! You should now have the Pokémon you wanted. If not, reset the game and try again."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"Note: You can view the Pokémon's info using PCalc. Press `Start + Left` to bring up the Wild View window or `Start + Right` to bring up the Party View window. Use `Select + Right` to cycle through party members in Party View.\n"})}),`
+`,e.jsx(n.h2,{children:"If You Did Not Obtain the Pokémon You Wanted"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Double-check that all info has been input correctly, especially the initial seed."}),`
+`,e.jsx(n.li,{children:"Restart the guide from the beginning and ensure to follow all instructions. Not getting the correct Pokémon is usually due to user error."}),`
+`,e.jsx(n.li,{children:"Make sure you are using the correct method based on whether there are NPCs or not. Failing to create a timeline when NPCs are over 0 will skip target frames."}),`
+`]})]})}function a(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{a as default,s as frontmatter};

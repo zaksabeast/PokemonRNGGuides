0c863fe168bec72cfc4afe1b0c20fb607d25cb04
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-26 17:34:12 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@1ed20a044b9c0100381e81d0a03cf8a4b466518f 🚀

---
+++ assets/Island Scan RNG-B3tmBvSo.js
@@ -0,0 +1,72 @@
+import{u as r,j as e}from"./index-B1sIwuQM.js";const t={title:"Island Scan",description:"Easy shinies in apricorn balls",slug:"retail-sm-island-scan",subCategory:"Custom Firmware",tag:"cfw"};function o(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["A 3DS with PCalc (",e.jsx(n.a,{href:"https://www.pokemonrng.com/misc-3ds-installing-pcalc",children:"PCalc Install Guide"}),")"]}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`,e.jsxs(n.li,{children:["The ",e.jsx(n.code,{children:"honey"})," item"]}),`
+`,e.jsx(n.li,{children:"Have started an Island Scan and know the Pokémon you've scanned for"}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://www.pokemonrng.com/misc-3ds-island-scan-sm",children:"List of Island Scan islands and days for SM"})}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: You can get the "honey" item in any store after clearing three trials.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 1: Setup PCalc"}),`
+`,e.jsx(n.p,{children:"Make sure PCalc's wild view and game view are open."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"Note: In PCalc, press `Start + Left` to open the wild view and `Start + Up` to open the game view.\n"})}),`
+`,e.jsx(n.h2,{children:"Step 2: Setup 3DSRNGTool"}),`
+`,e.jsx(n.p,{children:"Open 3DSRNGTool and set it up:"}),`
+`,e.jsx(n.h3,{children:"In the upper right of 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Type the ",e.jsx(n.code,{children:"Init Seed"})," from PCalc into the ",e.jsx(n.code,{children:"Seed"})," box."]}),`
+`,e.jsxs(n.li,{children:["Change the ",e.jsx(n.code,{children:"Game Version"})," dropdown to your game."]}),`
+`,e.jsxs(n.li,{children:["Change ",e.jsx(n.code,{children:"TSV"})," to your game's TSV."]}),`
+`,e.jsxs(n.li,{children:["If you have the shiny charm, check the ",e.jsx(n.code,{children:"Shiny Charm"})," box."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:'Note: Find your TSV in PCalc\'s Game Info window. Press `Start + Up` to bring up the menu if needed. The TSV is labeled "YOUR TSV".\n'})}),`
+`,e.jsx(n.h3,{children:"On the left of 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Click the ",e.jsx(n.code,{children:"Wild RNG"})," tab."]}),`
+`,e.jsxs(n.li,{children:["Change the ",e.jsx(n.code,{children:"Category"})," dropdown to ",e.jsx(n.code,{children:"Island Scan"}),"."]}),`
+`,e.jsxs(n.li,{children:["Change the ",e.jsx(n.code,{children:"Pokemon"})," dropdown to your target Pokémon."]}),`
+`]}),`
+`,e.jsx(n.h3,{children:"On the right of 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Follow the ",e.jsx(n.a,{href:"https://www.pokemonrng.com/retail-sm-timeline",children:"timeline guide"})," using the ",e.jsx(n.code,{children:"NPC Count"})," from PCalc. Wait at least 30 seconds after opening the window to allow the NPC counter to calibrate."]}),`
+`,e.jsx(n.li,{children:"You should have a target frame to hit."}),`
+`]}),`
+`,e.jsx(n.h3,{children:"In the middle of 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Change the IVs to your desired values for the Pokémon."}),`
+`,e.jsxs(n.li,{children:["If you want a shiny Pokémon, check the ",e.jsx(n.code,{children:"Shiny Only"})," box."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If you don't care about IVs, leave the ranges as 0-31.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 3: The RNG"}),`
+`,e.jsx(n.p,{children:"If you've followed the guide, you should now:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Have made a timeline."}),`
+`,e.jsx(n.li,{children:"Have a target frame."}),`
+`,e.jsx(n.li,{children:"Be standing in the area to catch your Island Scan Pokémon."}),`
+`,e.jsxs(n.li,{children:["Have the ",e.jsx(n.code,{children:"honey"})," item."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If any of these are not true, start the guide over. Moving your character will mess up the RNG.
+`})}),`
+`,e.jsx(n.h3,{children:"Hitting your target frame"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"X"})," to open the in-game menu."]}),`
+`,e.jsxs(n.li,{children:["Hold the cursor on the ",e.jsx(n.code,{children:"bag"})," option."]}),`
+`,e.jsxs(n.li,{children:["Watch the ",e.jsx(n.code,{children:"Frame"})," counter on PCalc.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["When the ",e.jsx(n.code,{children:"Frame"})," counter is close to your target frame, press ",e.jsx(n.code,{children:"Start + Select"})," to pause the game."]}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"Select"})," multiple times to slowly advance frames to your target frame."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Once at your target frame, press and hold ",e.jsx(n.code,{children:"A"})," to unpause the game while entering the bag."]}),`
+`,e.jsxs(n.li,{children:["Find and use the ",e.jsx(n.code,{children:"honey"})," item to start a wild encounter."]}),`
+`,e.jsxs(n.li,{children:["If the Pokémon you encountered was not the Island Scan Pokémon, see how many frames off you were and adjust.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Search all frames for the IVs of the Pokémon you got. Check the frame range instead of the timeline and search using the IVs."}),`
+`,e.jsx(n.li,{children:"After finding your actual frame, adjust as necessary from the delay and restart from the beginning."}),`
+`,e.jsx(n.li,{children:"This should not happen frequently."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"Note: If you paused the game too early, press `Start` to unpause the game.\n"})})]})}function i(s={}){const{wrapper:n}={...r(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(o,{...s})}):o(s)}export{i as default,t as frontmatter};

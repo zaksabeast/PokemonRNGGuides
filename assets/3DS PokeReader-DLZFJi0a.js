472d876bef8247b770ba244bee98fd36384dc9c5
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-03-31 20:31:19 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@8074ae0cb8a934eea2c67e20790158b4722046fe 🚀

---
+++ assets/3DS PokeReader-DLZFJi0a.js
@@ -0,0 +1,39 @@
+import{u as r,j as e}from"./index-BKqYYGGX.js";const i={title:"PokeReader 3DS",description:"Installing a tool on your 3DS to help RNG Pokémon",slug:"install-pokereader",subCategory:"3DS",tag:"cfw"};function l(s){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"PokeReader is a plugin to help RNG by bringing emulator-like functionality to physical consoles."}),`
+`,e.jsxs(n.p,{children:["For how to install PokeReader on a 3DS emulator, ",e.jsx(n.a,{href:"/install-pokereader-emu",children:"go here"}),"."]}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"A 3DS with CFW (Custom Firmware)"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/LumaTeam/Luma3DS/releases",children:"Luma version 13.0.0 or higher"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Install PokeReader on 3DS"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Download the ",e.jsx(n.a,{href:"https://github.com/zaksabeast/PokeReader/releases/latest",children:"latest release of PokeReader"}),"."]}),`
+`,e.jsxs(n.li,{children:["Create the ",e.jsx(n.code,{children:"/luma/plugins"})," folder if it does not exist."]}),`
+`,e.jsxs(n.li,{children:["Copy ",e.jsx(n.code,{children:"default.3gx"})," to the ",e.jsx(n.code,{children:"/luma/plugins/"})," folder."]}),`
+`,e.jsxs(n.li,{children:["Open Rosalina (",e.jsx(n.code,{children:"L"})," + ",e.jsx(n.code,{children:"Down"})," + ",e.jsx(n.code,{children:"Select"}),") and enable the ",e.jsx(n.code,{children:"Plugin Loader"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"PokeReader Controls"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Start"})," + ",e.jsx(n.code,{children:"Up"}),": Opens and closes the overlay"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Start"})," + ",e.jsx(n.code,{children:"Select"}),": Pauses the game"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Start"})," while paused: Advances the game by 1 frame"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Select"})," while paused: Unpauses the game"]}),`
+`,e.jsxs(n.li,{children:["Crystal VC only",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"L"})," + ",e.jsx(n.code,{children:"R"}),": Pauses the game"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"L"})," while paused: Advances the game by 1 frame"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"R"})," while paused: Unpauses the game"]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"PokeReader does not load and my game crashes!"})}),`
+`,e.jsx(n.p,{children:"Make sure you have updated the game to the latest version. PokeReader will not work on older game versions."}),`
+`,e.jsx(n.p,{children:"Game updates can be downloaded from the Nintendo eShop."}),`
+`,e.jsx(n.h2,{children:"What next?"}),`
+`,e.jsx(n.p,{children:"Try some of these RNGs!"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/gen2-celebi",children:"Shiny Celebi in Crystal"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng-offline",children:"Easy Transporter RNG for perfect Pokemon"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/retail-usum-wormhole",children:"Shiny Wormhole Legends in USUM"})}),`
+`]})]})}function d(s={}){const{wrapper:n}={...r(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(l,{...s})}):l(s)}export{d as default,i as frontmatter};

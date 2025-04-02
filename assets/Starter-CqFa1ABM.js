fd845ff5ab3e6de3c78509ac6c8aab24fdc0247f
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-02 02:13:00 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@cdb8917005abbb27e5d05108804cc4f8f78ea789 🚀

---
+++ assets/Starter-CqFa1ABM.js
@@ -0,0 +1,42 @@
+import{u as d,j as e}from"./index-CdUmV78P.js";const h=[{title:"Starter Crystal RNG",description:"Get Shiny Starters in Crystal",slug:"gen2-starters",tag:"cfw"},{title:"RNG de Iniciales en Cristal",description:"Get Shiny Starters in Crystal",slug:"es-gen2-starters",tag:"cfw",hideFromNavDrawer:!0}];function t(a){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",ul:"ul",...d(),...a.components},{Gen2PokemonRng:i,LanguageButton:l,ShowIf:r,YouTubeVideo:s}=n;return i||o("Gen2PokemonRng"),l||o("LanguageButton"),r||o("ShowIf"),s||o("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(l,{enSlug:"/gen2-starters",esSlug:"/es-gen2-starters"}),`
+`,e.jsxs(r,{slug:"/gen2-starters",children:[e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"Video guide"}),e.jsx(s,{src:"https://www.youtube.com/embed/1PFQv-S8a48?si=7N6RoITaJg0lyrPR"}),e.jsx(n.h2,{children:"Written guide"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Save in front of the starter you want, in case you need to reset the game."}),`
+`,e.jsxs(n.li,{children:["Choose your starter and stop when you see the ",e.jsx(n.code,{children:"Player received Pokemon"})," text with the flashing arrow to continue."]}),`
+`,e.jsxs(n.li,{children:["Go into PokeReader's RNG screen and wait until it stops saying ",e.jsx(n.code,{children:"Finding ADIV Index"})," and ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pause PokeReader with ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsx(n.li,{children:'Type the information on PokeReader into the RNG Tool below and press "Generate".'}),`
+`,e.jsxs(n.li,{children:["Advance the game with ",e.jsx(n.code,{children:"L"})," until PokeReader's ",e.jsx(n.code,{children:"Advance"})," number matches one of the generated ",e.jsx(n.code,{children:"Advance"})," columns below."]}),`
+`,e.jsx(n.li,{children:"(Optional) Create a save state if you're on an emulator, in case you need to reset."}),`
+`,e.jsxs(n.li,{children:["When you land on your desired advance, press ",e.jsx(n.code,{children:"A"})," to accept the Pokemon."]}),`
+`,e.jsx(n.li,{children:"If you got your Pokemon, congrats! If not, keep trying! It won't take long."}),`
+`]}),e.jsx(n.h2,{children:"RNG Tool"}),e.jsx(i,{type:"starter",language:"en"}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`]})]}),`
+`,e.jsxs(r,{slug:"/es-gen2-starters",children:[e.jsx(n.h2,{children:"Herramientas"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"Videoguía"}),e.jsx(s,{src:"https://www.youtube.com/embed/1PFQv-S8a48?si=7N6RoITaJg0lyrPR"}),e.jsx(n.h2,{children:"Guía escrita"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Guarda frente al inicial que quieras, en el caso en que necesites reiniciar el juego."}),`
+`,e.jsxs(n.li,{children:["Elige tu inicial y detente cuando veas el cuadro de texto ",e.jsx(n.code,{children:"Jugador recibió a Pokémon"})," con la flecha parpadeando para continuar."]}),`
+`,e.jsxs(n.li,{children:["Ve a la pantalla de RNG del Pokereader y espera hasta que deje de mostrar ",e.jsx(n.code,{children:"Finding ADIV Index"})," y ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pausa el Pokereader con ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsx(n.li,{children:'Escribe la información del Pokereader dentro de la herramienta de RNG aquí abajo y cuando termines, presiona "Generar".'}),`
+`,e.jsxs(n.li,{children:["Avanza el juego con ",e.jsx(n.code,{children:"L"})," hasta que los avances desplegados del Pokereader coincidan con lo generado en la columna ",e.jsx(n.code,{children:"Avance"})," aquí abajo."]}),`
+`,e.jsx(n.li,{children:"(Opcional) Crea un estado de guardado si es que estas en emulador, en el caso que necesites reiniciar el juego."}),`
+`,e.jsxs(n.li,{children:["Cuando alcances tu avance deseado, presiona ",e.jsx(n.code,{children:"A"})," para aceptar el Pokémon."]}),`
+`,e.jsx(n.li,{children:"Si conseguiste a tu Pokémon, ¡Felicidades! si no, ¡Sigue intentando! no tardará mucho."}),`
+`]}),e.jsx(n.h2,{children:"RNG Tool"}),e.jsx(i,{type:"starter",language:"es"}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`]}),e.jsx(n.h2,{children:"Creditos"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Todas las personas que han investigado a lo largo de los años la Gen 2: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast por elaborar el método actual de rng y construir la herramienta."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, y Lord Timothy en Discord por ayudar realizando pruebas."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano por ayudar con la traducción al español."}),`
+`]})]})]})}function u(a={}){const{wrapper:n}={...d(),...a.components};return n?e.jsx(n,{...a,children:e.jsx(t,{...a})}):t(a)}function o(a,n){throw new Error("Expected component `"+a+"` to be defined: you likely forgot to import, pass, or provide it.")}export{u as default,h as frontmatter};
---
+++ assets/Timeline Guide-Bvz1shpl.js
@@ -0,0 +1,43 @@
+import{u as r,j as e}from"./index-CdUmV78P.js";const s={title:"Timeline Guide",description:"Key skill for Gen 7 RNG with custom firmware",slug:"retail-usum-timeline",subCategory:"Custom Firmware",tag:"cfw"};function t(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["A 3DS with installed PCalc (",e.jsx(n.a,{href:"https://www.pokemonrng.com/misc-3ds-installing-pcalc",children:"PCalc Install Guide"}),")"]}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Setting Up 3DSRNGTool"}),`
+`,e.jsx(n.p,{children:"Only create a timeline if there are one or more NPCs in your area. If you have 0 NPCs, you don't need a timeline."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Enter your game version and TSV (",e.jsx(n.code,{children:"Start + Up"})," in PCalc will reveal TSV in the Game View window)."]}),`
+`,e.jsxs(n.li,{children:["Input the initial seed, found in the Game View window under ",e.jsx(n.code,{children:"Init Seed:"}),"."]}),`
+`,e.jsx(n.li,{children:"If you have the Shiny Charm, check the Shiny Charm box."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: In Gen 7 games, NPCs affect the RNG frames. As a rule, each NPC advances the frame by one. Therefore, an area with four NPCs typically progresses five frames each time. Things like Rotom or character blinks also have an effect. Accurate predictions can be achieved if the timeline is correctly created, ensuring you don't miss your target frame due to NPCs.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Timeline Creation"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Enable “Safe F Only” in 3DSRNGTool."}),`
+`,e.jsxs(n.li,{children:["Enter the count of NPCs for the area. Confirm this count with PCalc.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Use ",e.jsx(n.code,{children:"Start + Up"})," to view the NPC counter. It may need resetting via the same button combination and then displayed again. Let it run for about 30 seconds to a minute for accurate calibration."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["In the game, go advance to the Pokémon's final screen you are RNGing for. Input the current frame in the frame range, visible in PCalc Game Info window (",e.jsx(n.code,{children:"Start + Up"}),") and pause with ",e.jsx(n.code,{children:"Start + Select"}),"."]}),`
+`,e.jsx(n.li,{children:'Click "Calculate", then advance to any listed frames with a "-" in the Mark column. These are "Safe Frames", useful for accurate frame landing predictions.'}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"Start"})," to advance frames by unpausing the game. You can then pause again with ",e.jsx(n.code,{children:"Start + Select"})," when close to your frame and slowly advance with ",e.jsx(n.code,{children:"Select"})," button while paused."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: Timeline and NPC Number Confirmation"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'After reaching any safe frame, input your current frame into 3DSRNGTool, then enable “Create Timeline” and click "Calculate".'}),`
+`,e.jsxs(n.li,{children:["Advance several times by pressing ",e.jsx(n.code,{children:"Select"})," and compare with the subsequent frames in 3DSRNGTool.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If they match, the NPC count is correct."}),`
+`,e.jsx(n.li,{children:"If they do not match, recreate your timeline."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Errors in making the timeline are typically discovered at this point. Verify the initial seed and ensure PCalc's NPC counter hasn't changed during timeline creation."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Do not adjust filters during timeline creation. Only "Safe F Only" box should have been activated by this point. If any other filter has been adjusted, start again from the beginning.
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Retest the timeline occasionally for lengthy wait times, ensuring the NPC number hasn't shifted. Not all NPCs affect the RNG simultaneously, hence waiting for 30 seconds to a minute for accurate NPC count calibration is recommended. Testing the timeline again after a few minutes ensures no time is wasted waiting for an unattainable frame due to fluctuating NPCs.
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Check if your desired frame is still within the timeline by redoing the timeline with your current frame, then searching for your target. If it doesn't appear in the results, your timeline has shifted.
+`})})]})}function l(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{l as default,s as frontmatter};

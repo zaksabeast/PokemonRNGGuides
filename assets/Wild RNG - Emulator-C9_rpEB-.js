08b76b5217a57bca3ad43edba1eedf82bb8b4689
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-02 13:32:28 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@3a9f3ae01876e0c01c133524bf15b34597b6e30a 🚀

---
+++ assets/Wild RNG - Emulator-C9_rpEB-.js
@@ -0,0 +1,33 @@
+import{u as i,j as e}from"./index-DQbj1dfo.js";const o={title:"Wild RNG",description:"Wild RNG",slug:"emulator-hgss-wild",subCategory:"Emulator",isRoughDraft:!0,tag:"emu"};function r(t){const n={code:"code",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsx(n.p,{children:"You will need:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Desmume (any version; 9.11 is more stable)"}),`
+`,e.jsx(n.li,{children:"Lua scripts (check Noob forums for this)"}),`
+`,e.jsx(n.li,{children:"PokeFinder"}),`
+`,e.jsx(n.li,{children:"A Sweet Scenter"}),`
+`,e.jsx(n.li,{children:"Optional: A lead"}),`
+`]}),`
+`,e.jsx(n.p,{children:"Before starting, you must know how to hit an initial seed. Refer to the appropriate guide for that."}),`
+`,e.jsx(n.h2,{children:"Step 1: Find Your Target"}),`
+`,e.jsx(n.p,{children:"First, save in the area where you want to hunt."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open PokeFinder, select Gen 4, then Wild."}),`
+`,e.jsx(n.li,{children:"Go to the Searcher tab (this step is important), select or create your profile."}),`
+`,e.jsx(n.li,{children:"Choose the route and enter your desired parameters. Since it's HGSS, remember to use method K, not method J."}),`
+`,e.jsx(n.li,{children:"Consider using leads to increase possible frames."}),`
+`,e.jsx(n.li,{children:"Be aware that the delay can be high, as well as the frames."}),`
+`]}),`
+`,e.jsx(n.p,{children:'Once you have a target frame, right-click on it and select "Generate times for seed."'}),`
+`,e.jsx(n.h2,{children:"Step 2: Setup the Emulator"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open your emulator and load your Lua script."}),`
+`,e.jsx(n.li,{children:"Hit the initial seed."}),`
+`,e.jsxs(n.li,{children:["Open your menu with ",e.jsx(n.code,{children:"X"}),"."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"From there, you can advance PIDRNG Frames in a few ways:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Use a Chatot (preferably x2) with chatter activated. If it doesn’t work, use the record feature of Desmume to advance frames."}),`
+`,e.jsx(n.li,{children:"NPCs can advance frames randomly."}),`
+`,e.jsx(n.li,{children:"The radio (the bottom right one advances one by one, while Oak advances around 14-18 frames; the rest is more random)."}),`
+`]}),`
+`,e.jsx(n.p,{children:"Once you reach the right frame, just use your Sweet Scenter, and you're good to go!"})]})}function a(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{a as default,o as frontmatter};

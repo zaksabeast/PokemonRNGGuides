2b88c9b0ecb2f44c8263cc1327d77578dd0ec1bb
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-03 22:15:39 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@bb663d5d68bcde114399ca9a00dee5ddeeb63474 🚀

---
+++ assets/Egg-CRyO3u4r.js
@@ -0,0 +1,35 @@
+import{u as s,j as e}from"./index-ojmfz2wl.js";const i={title:"Egg RNG",description:"How to RNG eggs from the daycare",slug:"emulator-hgss-egg",subCategory:"Emulator",tag:"emu"};function o(t){const n={h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"PokeFinder"}),`
+`,e.jsx(n.li,{children:"Tools needed for RNG abuse with DesMuMe"}),`
+`,e.jsx(n.li,{children:"Access to the Daycare and info about your parents"}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Intro"}),`
+`,e.jsx(n.p,{children:"This guide will show you how to advance the RNG to get the Pokémon you want. It assumes you know some basics about Gen 4 RNG, like starting and advancing the RNG. The Masuda Method is introduced in this generation, but it's not essential to use."}),`
+`,e.jsx(n.h2,{children:"Setup"}),`
+`,e.jsx(n.p,{children:"Getting ready is easy:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open PokeFinder and go to Gen 4 => Eggs => Searcher."}),`
+`,e.jsx(n.li,{children:"Choose your profile and enter your parents' info. The Pokémon that went into the daycare first is Parent A, and the other is Parent B."}),`
+`,e.jsx(n.li,{children:"Drop off your parents at the daycare and save your game."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Hitting the PID using Egg Advances"}),`
+`,e.jsx(n.p,{children:"Aim for an egg that doesn't need to advance the RNG, as this is the easiest method:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"In PokeFinder, set your Advance Range to 0-0."}),`
+`,e.jsx(n.li,{children:"Choose the traits you want for your Pokémon, like Shiny, Nature, Gender, Ability, etc."}),`
+`,e.jsx(n.li,{children:"Generate results based on your criteria."}),`
+`,e.jsx(n.li,{children:"Select your target RNG advance."}),`
+`,e.jsx(n.li,{children:"RNG your Initial Seed."}),`
+`]}),`
+`,e.jsx(n.p,{children:"If you need to advance the RNG beyond 0, increase the egg advance before making your egg. Here are some ways to do it:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use the Magikarp Coin to advance the RNG by 1."}),`
+`,e.jsx(n.li,{children:"Open the Happiness App (loaded or used) – it advances the RNG 2 times the number of Pokémon on your team."}),`
+`,e.jsx(n.li,{children:"Calling Youngster Joey (available on Route 30) advances the RNG by 2."}),`
+`]}),`
+`,e.jsx(n.p,{children:"Remember to save your game in front of the old man."}),`
+`,e.jsx(n.h2,{children:"Hitting the IVs"}),`
+`,e.jsx(n.p,{children:"The next part of the RNG is standard. Switch to the 'IVs' Method on PokeFinder and set your IV filters. Consider the IVs from your parents, but getting some 31 IVs is usually easy."}),`
+`,e.jsx(n.p,{children:"Get a result, hit your target advance, and advance the RNG as usual."}),`
+`,e.jsx(n.p,{children:"Once you’ve done everything correctly, grab your egg, and you’ll have your desired Pokémon."})]})}function a(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{a as default,i as frontmatter};

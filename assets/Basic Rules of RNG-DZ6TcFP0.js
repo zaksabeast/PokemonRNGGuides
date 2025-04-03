52480354c764ee11a6f3fb6bd7dccb8253f459ea
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-03 21:45:23 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@353607d9c94e997fa9322aa950bd87e7ab44e5f2 🚀

---
+++ assets/Basic Rules of RNG-DZ6TcFP0.js
@@ -0,0 +1,18 @@
+import{u as a,j as e}from"./index-DPL8gWl_.js";const o={title:"RNG Info",description:"How to advance the RNG and tips to make everything more stable",slug:"e-tips-rng",subCategory:"Basic Knowledge",tag:"any"};function i(t){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...a(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"RNG Methods"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"For stationary Pokémon: Method 1"}),`
+`,e.jsx(n.li,{children:"For wild encounters: Method H-2"}),`
+`]}),`
+`,e.jsx(n.p,{children:"The different methods for wild encounters are caused by vblank, though H-2 is the most commonly seen."}),`
+`,e.jsx(n.h3,{children:"Painting RNG"}),`
+`,e.jsxs(n.p,{children:["Painting RNG is a great method to obtain any desired initial seed. It's a more advanced technique and is covered in ",e.jsx(n.a,{href:"/emerald-painting-rng",children:"this guide"}),"."]}),`
+`,e.jsx(n.h2,{children:"Trainer Card Flip"}),`
+`,e.jsx(n.p,{children:"In Gen 3, flipping your Trainer Card resets noise-related delays, helping you focus on the default delay. You should do this almost every RNG."}),`
+`,e.jsxs(n.p,{children:["To flip your trainer card, access your Trainer Card and press ",e.jsx(n.code,{children:"A"}),"."]}),`
+`,e.jsx(n.p,{children:"You can do this between 10,000 - 5,000 advances before your target advance."}),`
+`,e.jsx(n.h2,{children:"Speeding Up RNG Advances"}),`
+`,e.jsx(n.h3,{children:"Battle Video"}),`
+`,e.jsx(n.p,{children:"This method helps with seeding by preserving the RNG's state as an initial seed. Engage in a battle at the Battle Frontier and save it as a video. The video retains the seed from the battle, allowing you to set it as the initial seed when loading. For example, if your target is 1 million advances away, go up to 950k, save a battle video, and upon loading, you'll start at 950k advances."}),`
+`,e.jsx(n.p,{children:"While this method is helpful, it's a bit outdated compared to Painting RNG. However, it’s useful when you need to hit a specific advance multiple times."}),`
+`,e.jsx(n.h3,{children:"In-Battle"}),`
+`,e.jsx(n.p,{children:"Being in battle advances RNG twice as fast. This is particularly helpful for wild Pokémon RNG."})]})}function r(t={}){const{wrapper:n}={...a(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{r as default,o as frontmatter};
---
+++ assets/Timeline With Fidget Guide-D4pUkeeq.js
@@ -0,0 +1,26 @@
+import{u as o,j as e}from"./index-DPL8gWl_.js";const s={title:"Timeline with Fidget",description:"Using the timeline method with character fidgets",slug:"retail-sm-fidget",subCategory:"Custom Firmware",tag:"cfw"};function i(n){const t={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Tools"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["A 3DS with PCalc (",e.jsx(t.a,{href:"https://www.pokemonrng.com/misc-3ds-installing-pcalc",children:"PCalc Install Guide"}),")"]}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(t.h2,{children:"Step 1: Create a timeline"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Follow the ",e.jsx(t.a,{href:"https://www.pokemonrng.com/retail-sm-timeline",children:"timeline guide"})," to create a timeline and find a target frame."]}),`
+`]}),`
+`,e.jsx(t.h2,{children:"Step 2: Account for character fidgeting"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:`Note: This process isn't used often in Sun and Moon as it's only viable for Lunala and Solgaleo.
+`})}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:'After calibrating a basic timeline, watch for your character to "fidget" and pause the game. This must be the first fidget after calibrating your timeline. It does not have to be the first fidget when you load the game, just the first fidget after calibrating any timeline.'}),`
+`]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:`Note: The NPC count is a calculation, so when the fidget happens, the NPC count on the overlay will change because of the frame jump. This does not mean your base NPC count is incorrect; it is just the calculation taking the fidget into account (NPC count = max # of frames advanced - 1).
+`})}),`
+`,e.jsx(t.p,{children:"Character mid fidget:"}),`
+`,e.jsx(t.p,{children:e.jsx(t.img,{src:"/images/Sun-Moon/Fidget-Timeline/Fidget.png",alt:"Fidget"})}),`
+`,e.jsxs(t.ol,{start:"2",children:[`
+`,e.jsxs(t.li,{children:["Advance through the fidget using ",e.jsx(t.code,{children:"select"})," until you notice a jump in frames that doesn't show on your timeline. This will usually be 3-4 frames in the case of wormholes, as they all have 1 NPC. In the example below, the frames jumped from 1320-1324, which means 1320 should be used for the fidget setting (check the box and put it in)."]}),`
+`,e.jsxs(t.li,{children:["Once entered, hit ",e.jsx(t.code,{children:"Calculate"})," to see the frame jump accounted for in your timeline."]}),`
+`]}),`
+`,e.jsx(t.p,{children:e.jsx(t.img,{src:"/images/Sun-Moon/Fidget-Timeline/Before.jpg",alt:"Before Fidget"})}),`
+`,e.jsx(t.p,{children:e.jsx(t.img,{src:"/images/Sun-Moon/Fidget-Timeline/After.jpg",alt:"After Fidget"})}),`
+`,e.jsx(t.p,{children:"Now the timeline will take character fidgeting into account for future advancements. Do not move your character and do not open the menu while advancing to your target frame."})]})}function r(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{r as default,s as frontmatter};

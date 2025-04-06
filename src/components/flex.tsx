fe2cdb729506eab5fc6ba609509ff94bbad6e9b5
RC <rainingchain@gmail.com>
2025-04-06 14:49:23 -0400

    Modifs after pr comment

--- src/components/flex.tsx
+++ src/components/flex.tsx
@@ -1,13 +1,4 @@
 import { Flex as AntdFlex } from "antd";
 import { withCss } from "./withCss";
-import { Typography } from "antd";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export const Gist = ({ children }: Props) => {
-  return <Typography.Text strong>{children}</Typography.Text>;
-};
 
 export const Flex = withCss(AntdFlex);

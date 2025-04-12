285b400999f159219413fca3f526efbc75278ddb
RC <rainingchain@gmail.com>
2025-04-12 16:08:52 -0400

    .

--- src/rngToolsUi/gen3/shinyStarter.tsx
+++ src/rngToolsUi/gen3/shinyStarter.tsx
@@ -9,7 +9,7 @@ import { FormikInput } from "../../components/input";
 
 import { withCss } from "../../components/withCss";
 import { Button, BaseButton } from "../../components/button";
-import { Gender, Nature, rngTools } from "~/rngTools";
+import { Gender, Nature,Static3Result, Species, Ivs, rngTools } from "~/rngTools";
 import React from "react";
 import {MultiTimer} from "../../components/multiTimer";
 import {
@@ -44,13 +44,13 @@ type CaughtStatProps = {
   onValueChanged?: (selected: number) => void;
 };
 
-type FormState = {
+type FormStateFindShiny = {
   pokemonSpecies: StarterSpecies;
   tid: string;
   sid: string;
 };
 
-const getInitialValues = (): FormState => {
+const getInitialValuesFindShiny = (): FormStateFindShiny => {
   return {
     pokemonSpecies: "Mudkip",
     tid: "0",
@@ -58,8 +58,43 @@ const getInitialValues = (): FormState => {
   };
 };
 
-const generateResults = async function (values: FormState): Promise<Result[]> {
+const generateResults = async function (values: FormStateFindShiny): Promise<Result[]> {
   return [];
+};
+
+const findTargetAdvanceForShinyPokemon = async function (game:Game, values: FormStateFindShiny): Promise<number | null> {
+  const MINIMAL_ADV = 600;
+
+  for (let i = 0; i < 100; i++){
+    const seed = game === "emerald" ? 0 : 0x5a0;
+    const initial_advances = Math.max(i * 100_000, MINIMAL_ADV);
+    
+    const results = await rngTools.gen3_static_states({
+      species:values.pokemonSpecies,
+      method4:false,
+      initial_advances,
+      max_advances: 100_000,
+      seed,
+      offset:0,
+      tid:(+values.tid) || 0,
+      sid:(+values.sid) || 0,
+      bugged_roamer: false,
+      filter: {
+        shiny: true,
+        nature:undefined,
+        gender:undefined,
+        ability:undefined,
+        ivs: {
+          min_ivs: {hp:0, atk:0,def:0, spa: 0,spd: 0,spe: 0},
+          max_ivs: {hp:31, atk:31,def:31, spa: 31,spd: 31,spe: 31},
+        },
+      },
+    });
+    if (results.length)
+        return results[0].advance;
+  }
+
+  return null;
 };
 
 type Props = {
@@ -112,8 +147,39 @@ export const CaughtStatInput = ({statLabel,min,max,value:value_props,nature:natu
   )
 };
 
+const getStatRangeForStarter = (starter:StarterSpecies) => {
+  if (starter === "Mudkip")
+    return {
+      hp:  { min: 20, max: 21},
+      atk: { min: 10, max: 14},
+      def: { min: 9, max: 12},
+      spa: { min: 9, max: 12},
+      spd: { min: 9, max: 12},
+      spe: { min: 8, max: 11},
+    };
+  if (starter === "Treecko")
+    return {
+      hp:  { min: 19, max: 20},
+      atk: { min: 8, max: 12},
+      def: { min: 7, max: 11},
+      spa: { min: 9, max: 14},
+      spd: { min: 9, max: 13},
+      spe: { min: 10, max: 14},
+    };
+  if (starter === "Torchic")
+    return {
+      hp:  { min: 19, max: 21},
+      atk: { min: 9, max: 13},
+      def: { min: 8, max: 11},
+      spa: { min: 10, max: 14},
+      spd: { min: 9, max: 12},
+      spe: { min: 8, max: 12},
+    };
+  throw new Error('invalid starter: ' + starter);
+};
+
 export const Gen3ShinyStarter = ({ game = "emerald" }: Props) => {
-  const initialValues = getInitialValues();
+  const initialValues = getInitialValuesFindShiny();
   
   const [results, setResults] = React.useState<Result[]>([]);
   
@@ -128,23 +194,26 @@ export const Gen3ShinyStarter = ({ game = "emerald" }: Props) => {
     spd: CaughtStatProps;
     spe: CaughtStatProps;
   }>({
-    hp:  { statLabel:"HP", min: 17, max: 19, value: null, nature: "nochange" },
-    atk: { statLabel:"ATK", min: 11, max: 12, value: null, nature: "nochange" },
-    def: { statLabel:"DEF", min: 9, max: 10, value: null, nature: "nochange" },
-    spa: { statLabel:"SPA", min: 8, max: 9, value: null, nature: "nochange" },
-    spd: { statLabel:"SPD", min: 9, max: 10, value: null, nature: "nochange" },
-    spe: { statLabel:"SPE", min: 8, max: 9, value: null, nature: "nochange" },
+    hp:  { statLabel:"HP",  min: 19, max: 20, value: null, nature: "nochange" },
+    atk: { statLabel:"ATK", min: 8, max: 12, value: null, nature: "nochange" },
+    def: { statLabel:"DEF", min: 7, max: 11, value: null, nature: "nochange" },
+    spa: { statLabel:"SPA", min: 9, max: 14, value: null, nature: "nochange" },
+    spd: { statLabel:"SPD", min: 9, max: 13, value: null, nature: "nochange" },
+    spe: { statLabel:"SPE", min: 10, max: 14, value: null, nature: "nochange" },
   });
 
-  const onSubmitFindTarget = React.useCallback<RngToolSubmit<FormState>>(
+  const onSubmitFindTarget = React.useCallback<RngToolSubmit<FormStateFindShiny>>(
     async (values) => {
-      setTargetAdv((+values.sid) + (+values.tid) + 1503);
+      const adv = await findTargetAdvanceForShinyPokemon(game, values);
+      if (adv !== null)
+        setTargetAdv(adv);
     },
     [game],
   );
   
   const onSubmitFindMatchingCaught = React.useCallback<RngToolSubmit<{}>>(
     async (values) => {
+      //NO_PROD
       setResults([
         {adv: 1510, diffWithTarget:10, stats: '5/6/1/5/4/6', nature:"Adamant", gender:"Male"},
         {adv: 1484, diffWithTarget:-16, stats: '1/6/1/3/4/6', nature:"Quirky", gender:"Female"},
@@ -205,13 +274,13 @@ export const Gen3ShinyStarter = ({ game = "emerald" }: Props) => {
     const minutesBeforeTarget = Math.floor(milliseconds / 60000);
     const fields: Field[] = [
       {
-        label: "Target Advance for Shiny Pokémon",
+        label: "Target advance for shiny Pokémon",
         input: (
           <>{targetAdv}</>
         ),
       },
       {
-        label: "Latest Hit Advance (Calibration)",
+        label: "Latest hit advance (calibration)",
         input: (
           <>{latestHitAdv === null ? "-" : latestHitAdv}</>
         ),
@@ -266,12 +335,12 @@ export const Gen3ShinyStarter = ({ game = "emerald" }: Props) => {
 
   return (
     <div>
-      <RngToolForm<FormState, Result>
+      <RngToolForm<FormStateFindShiny, Result>
         fields={[
           {
             label: "Starter",
             input: (
-              <FormikRadio<FormState, "pokemonSpecies">
+              <FormikRadio<FormStateFindShiny, "pokemonSpecies">
               name="pokemonSpecies"
               options={["Mudkip", "Torchic", "Treecko"]}
               />
@@ -279,15 +348,15 @@ export const Gen3ShinyStarter = ({ game = "emerald" }: Props) => {
           },
           {
             label: "TID",
-            input: <FormikInput<FormState> name="tid" />,
+            input: <FormikInput<FormStateFindShiny> name="tid" />,
           },
           {
             label: "SID",
-            input: <FormikInput<FormState> name="sid" />,
+            input: <FormikInput<FormStateFindShiny> name="sid" />,
           }
         ]}
         initialValues={initialValues}
-        submitButtonLabel="Find Target Advance for Shiny Pokémon"
+        submitButtonLabel="Find target advance for shiny Pokémon"
         onSubmit={onSubmitFindTarget}
         submitTrackerId="shinyStarter_findTarget"
       />

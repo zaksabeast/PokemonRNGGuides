b0e3bea818a9ddd1aa99cd4fc304e98f3a64cd6f
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-13 23:17:21 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@d33e942f8b8df48c87b61cd830f9337b6b1c87fb 🚀

---
+++ assets/Remove Time Penalties-DAMUJ7us.js
@@ -0,0 +1,39 @@
+import{u as t,j as e}from"./index-BSisJWvV.js";const l={title:"Remove Time Penalties",description:"Change your Omega Ruby or Alpha Sapphire time without penalities!",slug:"oras-remove-time-penalty",category:"Omega Ruby and Alpha Sapphire",tag:"cfw",addedOn:"2025-03-14"};function s(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"The game will lock time based events for 24 hours when any of these happen:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"A save is moved between consoles."}),`
+`,e.jsx(n.li,{children:"A cartridge was saved on one console, then played on another."}),`
+`,e.jsx(n.li,{children:"A save is copied from a physical console to an emulator."}),`
+`,e.jsx(n.li,{children:"A physical console's time is adjusted (emulator is fine)."}),`
+`]}),`
+`,e.jsx(n.p,{children:"Adjusting time on emulators is fine because it acts as if real world time has passed, not as if you're changing console settings."}),`
+`,e.jsx(n.h2,{children:"Emulators (Citra, Lime3DS, Azahar)"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Set your emulator date to be earlier than your desired date."}),`
+`,e.jsx(n.li,{children:"Start the game and save."}),`
+`]}),`
+`,e.jsx(n.p,{children:"That's it! You can now set the date to any future date without penalty."}),`
+`,e.jsx(n.h2,{children:"Physical Consoles"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Update your Pokemon Game to the latest version."}),`
+`,e.jsx(n.li,{children:"Set your console date to be earlier than your desired date."}),`
+`,e.jsx(n.li,{children:"Start the game, save, then close the game."}),`
+`,e.jsxs(n.li,{children:["Enable IPS patching with ",e.jsx(n.a,{href:"/misc-3ds-ips-luma-citra",children:"our patch guide"}),"."]}),`
+`,e.jsxs(n.li,{children:["Create a Luma patch folder if it doesn't exist:",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Omega Ruby: ",e.jsx(n.code,{children:"/luma/titles/000400000011C400/"})]}),`
+`,e.jsxs(n.li,{children:["Alpha Sapphire: ",e.jsx(n.code,{children:"/luma/titles/000400000011C500/"})]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Download the ",e.jsx(n.code,{children:"code.ips"})," for your specific game and copy it to the patch folder",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/downloads/or-remove-time-penalty/code.ips",children:"Omega Ruby code.ips"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/downloads/as-remove-time-penalty/code.ips",children:"Alpha Sapphire code.ips"})}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:"If you run into any issues in the future, remove the patch and follow those steps again."}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Zaksabeast for looking into this and making the time penality removal patch."}),`
+`,e.jsx(n.li,{children:"Rebel on Discord for helping test."}),`
+`]})]})}function o(i={}){const{wrapper:n}={...t(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}export{o as default,l as frontmatter};
---
+++ assets/Stationary Wormhole RNG-BYvmLqc3.js
@@ -0,0 +1,63 @@
+import{u as i,j as e}from"./index-BSisJWvV.js";const o={title:"Wormhole Stationary RNG",description:"Get your own perfect legendary Pokémon!",slug:"retail-usum-wormhole",category:"Ultra Sun and Ultra Moon",tag:"cfw"};function r(t){const n={a:"a",code:"code",h2:"h2",h3:"h3",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["A 3DS with PCalc (",e.jsx(n.a,{href:"https://www.pokemonrng.com/misc-3ds-installing-pcalc",children:"PCalc Install Guide"}),")"]}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/blob/master/README.md#final-screen",children:"3DSRNGTool README"})," - list of final screens before Pokémon are generated"]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: This method works for all Pokémon found within the wormholes, including Ultra Beasts.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 1: Finding your seed"}),`
+`,e.jsxs(n.p,{children:['The seed is shown at the top of the overlay under "Init Seed:". For example, the seed is ',e.jsx(n.code,{children:"5DA5CA91"}),"."]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Setup.png",alt:"Setup"})}),`
+`,e.jsx(n.h2,{children:"Step 2: Creating a timeline and finding a target frame"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: When moving forward to initiate the encounter, be precise with the joystick. Always move in the same direction for consistent results.
+`})}),`
+`,e.jsx(n.p,{children:"There are two methods to RNG Pokémon in wormholes."}),`
+`,e.jsx(n.h3,{children:"Basic timeline with character fidgets"}),`
+`,e.jsx(n.p,{children:"This method creates a timeline and accounts for character fidgets. The disadvantage is the limited frames available based on the timeline."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Follow the ",e.jsx(n.a,{href:"https://www.pokemonrng.com/retail-usum-fidget",children:"timeline with fidget guide"})," to create a timeline and find a target frame."]}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Timeline leap"}),`
+`,e.jsx(n.p,{children:'This method finds a target frame and then "leaps" onto the needed timeline. The advantage is landing on any frame regardless of the timeline. You can also use Festival Plaza to advance frames faster for shorter wait times.'}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Follow the ",e.jsx(n.a,{href:"https://www.pokemonrng.com/retail-usum-timeleap",children:"timeline leap guide"})," to find a target frame and leap onto it."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: Hit your target frame"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Search for your target by entering what you want in the filters and hitting calculate."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Target.jpg",alt:"Target"})}),`
+`,e.jsxs(n.ol,{start:"2",children:[`
+`,e.jsx(n.li,{children:"Advance near your frame and pause."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Final-Screen.png",alt:"Final screen"})}),`
+`,e.jsxs(n.ol,{start:"3",children:[`
+`,e.jsxs(n.li,{children:["When you reach your frame, initiate the encounter. For UBs, just press ",e.jsx(n.code,{children:"A"}),", and for Legends, hold forward and then press ",e.jsx(n.code,{children:"A"}),". Save as close as possible to the spot that triggers the encounter."]}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Final-Screen-2.png",alt:"Final Screen 2"})}),`
+`,e.jsxs(n.ol,{start:"4",children:[`
+`,e.jsxs(n.li,{children:["Profit???",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Not so much."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Calibration.png",alt:"Calibration"})}),`
+`,e.jsx(n.h3,{children:"So what happened?"}),`
+`,e.jsx(n.p,{children:"This is normal as each 3DS/save has its own delay. The delay is the time from when you initiate the encounter to when the Pokémon is generated. Let's learn how to find your delay!"}),`
+`,e.jsx(n.h2,{children:"Step 4: Finding your delay and hitting it"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Use the IVs, nature, and PSV of the Pokémon you got to see what frame you actually hit. For example, if your target was 4973 and you hit 4969 (-4), the delay you hit versus the default setting of the 3DSRNGTool is -4 or 152."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Result.jpg",alt:"Result"})}),`
+`,e.jsxs(n.ol,{start:"2",children:[`
+`,e.jsx(n.li,{children:'Repeat this process with random targets to find the frame you hit relative to the default. Enter the most common delay you find after "Consider Delay." Adjust the delay to see how it changes the result.'}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Wait until you find a common delay before changing it. This can take 10-20 times or more, depending on your results.
+`})}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Delay.jpg",alt:"Delay"})}),`
+`,e.jsxs(n.ol,{start:"3",children:[`
+`,e.jsx(n.li,{children:"Once you find a consistent delay, enter it into the field and try harder targets! For example, here is the shiny Groudon I hit after finding my consistent delay of 152. Be aware that legendary Pokémon can be more inconsistent than UBs because you take a step before the animation occurs, adding more variables."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/UltraSun-UltraMoon/Wormhole/Success.png",alt:"Success"})})]})}function a(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{a as default,o as frontmatter};

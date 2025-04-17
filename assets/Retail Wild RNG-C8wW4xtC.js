82bbe90eccbd4ecd9e70715758b2ef898aa3da78
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-17 23:02:02 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@f21dd0b95a76041a6728c12838870bc6b0aba8df 🚀

---
+++ assets/Retail Wild RNG-C8wW4xtC.js
@@ -0,0 +1,80 @@
+import{u as h,j as e}from"./index-BHBhF5Ha.js";const d=[{title:"Wild Retail RNG",description:"RNG wild Pokemon on a retail console",slug:"retail-emerald-wild",subCategory:"Retail",tag:"retail"},{title:"实机野生乱数",description:"在真实主机上对野生宝可梦进行乱数操作",slug:"zh-retail-emerald-wild",subCategory:"Retail",tag:"retail",hideFromNavDrawer:!0}];function s(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...h(),...i.components},{LanguageButton:r,ShowIf:l}=n;return r||t("LanguageButton"),l||t("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsx(r,{enSlug:"/retail-emerald-wild",zhSlug:"/zh-retail-emerald-wild"}),`
+`,e.jsxs(l,{slug:"/retail-emerald-wild",children:[e.jsx(n.h2,{children:"Requirements:"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/DasAmpharos/EonTimer/releases",children:"EonTimer"})}),`
+`,e.jsx(n.li,{children:"TID and SID (only necessary for shinies)"}),`
+`,e.jsx(n.li,{children:"Pokemon with Sweet Scent"}),`
+`]}),e.jsx(n.h2,{children:"Step 1: Set Up PokeFinder"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open PokeFinder and select Gen 3 wild."}),`
+`,e.jsxs(n.li,{children:["Choose your profile with the TID/SID of the game you're using if you're hunting for shinies.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Follow ",e.jsx(n.a,{href:"/gen3-sid",children:"this guide to find your SID"})," if you don't already know it."]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:'Set the method to "Wild 2".'}),`
+`,e.jsx(n.li,{children:"Set the seed to 0."}),`
+`,e.jsx(n.li,{children:'Choose "Grass" or "Surfing" for the encounter.'}),`
+`,e.jsx(n.li,{children:"Set the location and Pokémon (if desired)."}),`
+`,e.jsx(n.li,{children:"Filter by the Pokémon you want."}),`
+`,e.jsx(n.li,{children:'Click "Generate" and then find a desired Pokémon from the results.'}),`
+`]}),e.jsx(n.h2,{children:"Step 2: Set Up EonTimer"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open EonTimer and click the cog in the lower-left corner."}),`
+`,e.jsx(n.li,{children:"Select the console you are using."}),`
+`,e.jsx(n.li,{children:"Then choose the Gen 3 tab."}),`
+`,e.jsx(n.li,{children:'Input the target advance from PokeFinder for the desired Pokémon in the "Target Frame" box.'}),`
+`,e.jsx(n.li,{children:'Click "Start".'}),`
+`]}),e.jsx(n.h2,{children:"Step 3: Calibration"}),e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["When the timer reaches 0, reset the game by pressing ",e.jsx(n.code,{children:"Start + Select + A + B"}),"."]}),`
+`,e.jsx(n.li,{children:"Open the in-game menu and go to your party."}),`
+`,e.jsx(n.li,{children:'Select the Pokémon with Sweet Scent and hover over "Sweet Scent."'}),`
+`,e.jsxs(n.li,{children:["Wait for the second countdown to end, then press ",e.jsx(n.code,{children:"A"}),"."]}),`
+`,e.jsx(n.li,{children:"Catch the Pokémon and use the IV calculator under filters."}),`
+`,e.jsx(n.li,{children:"Input the Pokémon's info to check its IVs."}),`
+`,e.jsx(n.li,{children:'Select "Any" for the Encounter Slot and click "Generate."'}),`
+`,e.jsx(n.li,{children:"Search for the Pokémon you just caught and input the advance you hit in EonTimer."}),`
+`,e.jsx(n.li,{children:'Click "Update."'}),`
+`]}),e.jsx(n.h2,{children:"Step 4: RNG the Pokemon"}),e.jsx(n.p,{children:`Now that you've calibrated for delay, it's all about timing.
+Keep catching Pokémon and checking what advance you landed on.
+Adjust EonTimer if needed.
+If off by one or two advances, try again without changes.`}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(l,{slug:"/zh-retail-emerald-wild",children:[e.jsx(n.h2,{children:"前置要求"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/DasAmpharos/EonTimer/releases",children:"EonTimer"})}),`
+`,e.jsx(n.li,{children:"TID 和 SID（仅在寻找异色宝可梦时需要）"}),`
+`,e.jsx(n.li,{children:"会使用甜甜香气的宝可梦"}),`
+`]}),e.jsx(n.h2,{children:"第一步：设置 PokeFinder"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"打开 PokeFinder，选择第三世代野生乱数。"}),`
+`,e.jsxs(n.li,{children:["如果要获取异色，请选择包含 TID/SID 的个人资料。",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["如果不知道 SID，可参考",e.jsx(n.a,{href:"/gen3-sid",children:"这个指南"})]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"将方式设为 Wild 2。"}),`
+`,e.jsx(n.li,{children:"Seed 设为 0。"}),`
+`,e.jsx(n.li,{children:"视情况选择草丛或冲浪遭遇。"}),`
+`,e.jsx(n.li,{children:"选择具体地点和目标宝可梦（可选）。"}),`
+`,e.jsx(n.li,{children:"使用筛选器找到你想要的宝可梦。"}),`
+`,e.jsx(n.li,{children:"点击计算结果，在结果中寻找合适的目标。"}),`
+`]}),e.jsx(n.h2,{children:"第二步：设置 EonTimer"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"打开 EonTimer，点击左下角的齿轮图标进入设置。"}),`
+`,e.jsx(n.li,{children:"选择你使用的主机类型。"}),`
+`,e.jsx(n.li,{children:"切换到 Gen 3 选项卡。"}),`
+`,e.jsx(n.li,{children:"在 Target Frame 栏输入 PokeFinder 提供的目标帧数。"}),`
+`,e.jsx(n.li,{children:"点击 Start 开始计时。"}),`
+`]}),e.jsx(n.h2,{children:"第三步：校准"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"当计时器第一次归零时，按 Start + Select + A + B 重置游戏。"}),`
+`,e.jsx(n.li,{children:"进入游戏菜单，打开队伍界面。"}),`
+`,e.jsx(n.li,{children:"选中带有甜甜香气的宝可梦，将光标悬停在该招式上。"}),`
+`,e.jsx(n.li,{children:"等待第二次倒计时结束后，立刻按 A 释放甜甜香气。"}),`
+`,e.jsx(n.li,{children:"捕获遇到的宝可梦，并在 PokeFinder 的 IV calculator中输入它的个体值。"}),`
+`,e.jsx(n.li,{children:"在遇敌种类选项中选择任何，然后点击生成。（若无结果请更改方式Wild 1或4）"}),`
+`,e.jsx(n.li,{children:"在结果中找到你刚捕获的宝可梦，并将实际帧数输入 EonTimer 进行修正"}),`
+`,e.jsx(n.li,{children:"点击 Update 保存校准结果。"}),`
+`]}),e.jsx(n.h2,{children:"第四步：执行乱数"}),e.jsx(n.p,{children:`校准延迟完成后，剩下的就是把握好时机：
+持续捕捉宝可梦，检查落点是否准确。
+如果推进数误差在 1~2 之间，可直接重试，无需调整计时器。
+如果误差较大，可适当调整 EonTimer 设定值，优化计时精度。`}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白白不是公主。"}),`
+`]})]})]})}function c(i={}){const{wrapper:n}={...h(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(s,{...i})}):s(i)}function t(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{c as default,d as frontmatter};

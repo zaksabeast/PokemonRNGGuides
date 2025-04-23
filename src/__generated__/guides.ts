f085bf71b2342ab1e69c7e5ed6bf4a42d58671cb
Hakuhiro <135146145+HaKu76@users.noreply.github.com>
2025-04-23 08:37:18 +0800

    FRLG Initial Seed Botting Chinese translation (#262)

--- src/__generated__/guides.ts
+++ src/__generated__/guides.ts
@@ -1202,7 +1202,7 @@ export const guides = {
       addedOn: null,
       file: "guides/FireRed and LeafGreen/Initial Seed Botting.mdx",
       category: "FireRed and LeafGreen",
-      translations: null,
+      translations: { en: "/frlg-seeding-bot", zh: "/zh-frlg-seeding-bot" },
     },
     Guide: React.lazy(
       () =>
@@ -2895,6 +2895,25 @@ export const guides = {
     },
     Guide: React.lazy(() => import("~/../guides/Emerald/Find SID.mdx")),
   },
+  "/zh-frlg-seeding-bot": {
+    meta: {
+      title: "初始种子自动刷取",
+      description: "使用初始种子自动脚本，实现更高自由度的乱数控制",
+      slug: "/zh-frlg-seeding-bot",
+      isRoughDraft: false,
+      tag: "emu",
+      hideFromNavDrawer: true,
+      addedOn: null,
+      translation: { enSlug: "/frlg-seeding-bot", language: "zh" },
+      file: "guides/FireRed and LeafGreen/Initial Seed Botting.mdx",
+      category: "FireRed and LeafGreen",
+      translations: { en: "/frlg-seeding-bot", zh: "/zh-frlg-seeding-bot" },
+    },
+    Guide: React.lazy(
+      () =>
+        import("~/../guides/FireRed and LeafGreen/Initial Seed Botting.mdx"),
+    ),
+  },
   "/zh-gen2-celebi": {
     meta: {
       title: "时拉比",
@@ -3206,6 +3225,7 @@ export const guideSlugs = [
   z.literal("/zh-emulator-emerald-egg"),
   z.literal("/zh-emulator-hgss-egg"),
   z.literal("/zh-frlg-gen3-sid"),
+  z.literal("/zh-frlg-seeding-bot"),
   z.literal("/zh-gen2-celebi"),
   z.literal("/zh-gen2-starters"),
   z.literal("/zh-gen3-sid"),

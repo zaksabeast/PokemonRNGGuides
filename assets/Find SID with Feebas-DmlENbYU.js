11273ed470d325a96c1f416d053ffd6f8bde716d
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-30 18:20:46 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@d2fd0659471062aefd65c6c08f2b6eb8d45637b1 🚀

---
+++ assets/Find SID with Feebas-DmlENbYU.js
@@ -0,0 +1,38 @@
+import{u as h,j as e}from"./index-D2UgzuAR.js";const c=[{title:"Find SID with Feebas in Emerald",navDrawerTitle:"Find SID with Feebas",description:"How to find your Secret ID (SID) in Emerald using Feebas.",slug:"emerald-sid-feebas",category:"Emerald",tag:"retail",addedOn:"2025-03-03"},{title:"Find SID with Feebas in Ruby and Sapphire",navDrawerTitle:"Find SID with Feebas",description:"How to find your Secret ID (SID) in Ruby or Sapphire using Feebas.",slug:"rs-sid-feebas",category:"Ruby and Sapphire",tag:"retail"},{title:"使用丑丑鱼查找 SID",description:"如何在《绿宝石》中利用丑丑鱼查找你的里ID (SID)。",slug:"zh-emerald-sid-feebas",category:"Emerald",tag:"retail",addedOn:"2025-04-16",translation:{enSlug:"emerald-sid-feebas",language:"zh"}},{title:"使用丑丑鱼查找 SID",description:"如何在《红宝石 / 蓝宝石》中利用丑丑鱼查找你的里ID (SID)。",slug:"zh-rs-sid-feebas",category:"Ruby and Sapphire",tag:"retail",translation:{enSlug:"rs-sid-feebas",language:"zh"}}];function d(i){const s={a:"a",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...h(),...i.components},{Alert:l,Gen3Sid:a,ShowIf:n,YouTubeVideo:t}=s;return l||r("Alert"),a||r("Gen3Sid"),n||r("ShowIf"),t||r("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsxs(n,{slugs:["/rs-sid-feebas","/emerald-sid-feebas"],children:[e.jsxs(n,{slug:"/rs-sid-feebas",children:[e.jsx(l,{showIcon:!0,type:"warning",message:"There's an easier method for Ruby and Sapphire!",description:e.jsxs(e.Fragment,{children:[`Follow the "If you're playing Ruby or Sapphire" section of`," ",e.jsx(s.a,{href:"/gen3-sid",children:"this guide"})," instead!"]})}),e.jsx(s.p,{children:"This method only works if:"}),e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"You have a dead battery."}),`
+`,e.jsx(s.li,{children:"You haven't changed the Dewford Trend."}),`
+`]})]}),e.jsxs(n,{slug:"/emerald-sid-feebas",children:[e.jsx(s.p,{children:"This method only works if:"}),e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"You have a dead battery or have played for less than 1 day."}),`
+`,e.jsx(s.li,{children:"You haven't changed the Dewford Trend."}),`
+`]})]}),e.jsx(s.h2,{children:"Video Guide"}),e.jsx(t,{src:"https://www.youtube.com/embed/nOCbSmMRXLA"}),e.jsx(s.h2,{children:"Find your Feebas Tiles"}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Travel to Dewford."}),`
+`,e.jsx(s.li,{children:"Talk to the NPC directly above the Pokemon Center to get the Dewford Trend."}),`
+`,e.jsxs(s.li,{children:["Use ",e.jsx(s.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator/",children:"mucksw's Feebas Tile Calculator"})," to get a list of possible Feebas seeds."]}),`
+`,e.jsx(s.li,{children:"Test each seed by fishing in the tiles provided by the calculator."}),`
+`]}),e.jsx(s.h2,{children:"Find your SID"}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Use the Feebas seed with the tool below to find your possible SIDs."}),`
+`,e.jsx(s.li,{children:"Try to RNG a shiny Pokemon to confirm the correct SID."}),`
+`]}),e.jsx(a,{game:window.location.pathname.startsWith("/rs-sid-feebas")?"rs":"emerald"}),e.jsx(s.h2,{children:"Credits"}),e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:["Lincoln, HappyLappy, and Shao for ",e.jsx(s.a,{href:"https://github.com/HappyLappy1/Lappy-Python-Scripts/tree/main/RSE_Trendy_Saying_2_SID",children:"the Python SID Finder"})," this is based on."]}),`
+`,e.jsxs(s.li,{children:["mucksw for the ",e.jsx(s.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator",children:"Feebas Tile Calculator"}),"."]}),`
+`,e.jsx(s.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(n,{slugs:["/zh-rs-sid-feebas","/zh-emerald-sid-feebas"],children:[e.jsxs(n,{slug:"/zh-rs-sid-feebas",children:[e.jsx(l,{showIcon:!0,type:"warning",message:"红宝石和蓝宝石有一个更简单的方法！",description:e.jsxs(e.Fragment,{children:["请参考",e.jsx(s.a,{href:"/gen3-sid",children:"本指南"}),`的"If you're playing Ruby or Sapphire"部分进行操作！`]})}),e.jsx(s.p,{children:"此方法仅适用于："}),e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"电池已耗尽。"}),`
+`,e.jsx(s.li,{children:"你没有更改武斗镇的流行话题。"}),`
+`]})]}),e.jsxs(n,{slug:"/zh-emerald-sid-feebas",children:[e.jsx(s.p,{children:"此方法仅适用于："}),e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"电池已耗尽或游戏时间少于一天。"}),`
+`,e.jsx(s.li,{children:"你没有更改武斗镇的流行话题。"}),`
+`]})]}),e.jsx(s.h2,{children:"视频指南"}),e.jsx(t,{src:"https://www.youtube.com/embed/nOCbSmMRXLA"}),e.jsx(s.h2,{children:"查找丑丑鱼钓点"}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"前往武斗镇。"}),`
+`,e.jsx(s.li,{children:"与宝可梦中心正上方的 NPC 交谈，获取当前的流行话题。"}),`
+`,e.jsxs(s.li,{children:["使用 ",e.jsx(s.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator/",children:"mucksw 的丑丑鱼钓点计算器"})," 获取可能的丑丑鱼种子列表。"]}),`
+`,e.jsx(s.li,{children:"在计算器提供的钓点逐一垂钓，测试每个种子。"}),`
+`]}),e.jsx(s.h2,{children:"查找你的 SID"}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"在下方工具中输入你的丑丑鱼种子，计算可能的 SID。"}),`
+`,e.jsx(s.li,{children:"通过乱数异色宝可梦来确认你的真实 SID。"}),`
+`]}),e.jsx(a,{game:window.location.pathname.startsWith("/rs-sid-feebas")?"rs":"emerald"}),e.jsx(s.h2,{children:"特别鸣谢"}),e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:["感谢 Lincoln、HappyLappy 和 Shao 提供的",e.jsx(s.a,{href:"https://github.com/HappyLappy1/Lappy-Python-Scripts/tree/main/RSE_Trendy_Saying_2_SID",children:"Python SID 查找工具"})," 。"]}),`
+`,e.jsxs(s.li,{children:["感谢 mucksw 制作的",e.jsx(s.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator",children:" 丑丑鱼钓点计算器"}),"。"]}),`
+`,e.jsx(s.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})]})}function u(i={}){const{wrapper:s}={...h(),...i.components};return s?e.jsx(s,{...i,children:e.jsx(d,{...i})}):d(i)}function r(i,s){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{u as default,c as frontmatter};

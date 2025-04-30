e579adfdd6c81a4e7193cf9786253f527327a952
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-04-30 15:22:57 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@2998f6c4ed0f39618dc9e4ef6094b73fb69f652a 🚀

---
+++ assets/PokeRadar-9H3P7Kgl.js
@@ -0,0 +1,34 @@
+import{u as s,j as e}from"./index-DH03H6uJ.js";const h=[{title:"Diamond, Pearl, and Platinum PokeRadar RNG",navDrawerTitle:"PokeRadar RNG",description:"Two different methods for PokeRadar RNG",slug:"dppt-pokeradar-rng",category:"Diamond, Pearl, and Platinum",tag:"any"},{title:"宝可追踪乱数",description:"两种不同的宝可追踪乱数方法",slug:"zh-dppt-pokeradar-rng",category:"Diamond, Pearl, and Platinum",tag:"any",translation:{enSlug:"dppt-pokeradar-rng",language:"zh"}}];function a(r){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...s(),...r.components},{ShowIf:i}=n;return i||t("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsxs(i,{slug:"/dppt-pokeradar-rng",children:[e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://mega.nz/file/cwY10TqT#qZxPCoSMGo64Xrw6TdAywqbBeUOk0il53MavrFqKr0Q",children:"This specific build of PokeFinder"})," by Real.96. It will be merged with the main PokeFinder in the future."]}),`
+`,e.jsx(n.li,{children:"Some repels."}),`
+`]}),e.jsx(n.h2,{children:"Intro"}),e.jsx(n.p,{children:"This guide explains how to do PokeRadar RNG based on a chain at 1. It assumes you know the basics of Gen 4 RNG and works for both emulated and retail play."}),e.jsx(n.h2,{children:"1-Chain Case"}),e.jsx(n.p,{children:"This allows for shiny chained spreads but requires more setup."}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Launch PokeFinder, assuming profile setup is done, and open Gen 4 => Wild => Searcher."}),`
+`,e.jsx(n.li,{children:"On the Settings tab, check PokeRadar, route, and Pokémon. Check the PokeRadar Shiny and Shiny Patch boxes."}),`
+`,e.jsx(n.li,{children:"Input filters, selecting one encounter slot. Advances is for IVs, nature, etc., and Shiny Patch is for the shiny appearance."}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: You need to defeat a Pokémon, walk to reset the PokeRadar, and search for a spread range of about 300-500 or more. Select a Shiny Patch around 200 advances. Anything less won't work.
+`})}),e.jsxs(n.ol,{start:"4",children:[`
+`,e.jsx(n.li,{children:"In-game, hit your initial seed. Trigger the PokeRadar, find your chain's Pokémon, and defeat it."}),`
+`,e.jsx(n.li,{children:"Walk until the PokeRadar resets."}),`
+`,e.jsx(n.li,{children:"Advance the RNG to your Shiny Patch. Find a big Grass Patch for the best results. Search within a 2 by 2 advance range for a closer Shiny Patch."}),`
+`,e.jsx(n.li,{children:"The Shiny Patch should appear. Stand next to it and open your bag."}),`
+`,e.jsx(n.li,{children:"Advance near your target (adjust if needed), then close the menu and head to your patch."}),`
+`]}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(i,{slug:"/zh-dppt-pokeradar-rng",children:[e.jsx(n.h2,{children:"所需工具"}),e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://mega.nz/file/cwY10TqT#qZxPCoSMGo64Xrw6TdAywqbBeUOk0il53MavrFqKr0Q",children:"Real.96 制作的特定版本 PokeFinder"})," 未来将合并至主 PokeFinder 版本"]}),`
+`,e.jsx(n.li,{children:"一些驱虫喷雾剂。"}),`
+`]}),e.jsx(n.h2,{children:"介绍"}),e.jsx(n.p,{children:"本指南讲解基于 1 连锁的宝可追踪乱数。假设你已经了解第四世代乱数的基础知识，该方法适用于模拟器和实机操作。"}),e.jsx(n.h2,{children:"1连锁状况"}),e.jsx(n.p,{children:"此方法允许获取连锁的异色个体，但需要更多设置。"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"启动 PokeFinder（假设个人资料已设置完成），进入 第四世代 => 野生乱数 => 检索器。"}),`
+`,e.jsx(n.li,{children:"在设置选项卡中，勾选 PokeRadar，选择路线和目标宝可梦。同时勾选 PokeRadar Shiny 和 Shiny Patch 选项。"}),`
+`,e.jsx(n.li,{children:"输入筛选条件，并选择一个遭遇种类。推进值用于 IV、性格等，而 Shiny Patch 选项用于决定闪光出现的时机。"}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意：你需要击败一只宝可梦，走动以重置宝可追踪，并搜索大约 300-500 或更多的帧数范围。建议在大约 200 帧附近选择 Shiny Patch，低于此值无法成功。
+`})}),e.jsxs(n.ol,{start:"4",children:[`
+`,e.jsx(n.li,{children:"在游戏中，先命中目标初始种子。触发宝可追踪，找到目标连锁的宝可梦并将其击败。"}),`
+`,e.jsx(n.li,{children:"走动直到宝可追踪重置。"}),`
+`,e.jsx(n.li,{children:"推进帧数至你的 Shiny Patch 目标值。寻找一个较大的草丛区域以获得最佳效果，并在 2×2 的推进值范围内寻找更接近的 Shiny Patch。"}),`
+`,e.jsx(n.li,{children:"当 Shiny Patch 出现后，站到它旁边并打开背包。"}),`
+`,e.jsx(n.li,{children:"在接近目标帧数时调整推进（如有必要），然后关闭菜单，进入 Shiny Patch。"}),`
+`]}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})]})}function d(r={}){const{wrapper:n}={...s(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(a,{...r})}):a(r)}function t(r,n){throw new Error("Expected component `"+r+"` to be defined: you likely forgot to import, pass, or provide it.")}export{d as default,h as frontmatter};

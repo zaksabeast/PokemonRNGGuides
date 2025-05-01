ac11207f0c4535dce480017fb2f22bd70ed826db
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-01 13:10:19 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@c70b35189fbe58701dc6ba6e2507ad115f351f82 🚀

---
+++ assets/Get Raid Seed Without CFW-CLXIzuT5.js
@@ -0,0 +1,26 @@
+import{u as s,j as e}from"./index-C0zE1Mnn.js";const o={title:"Sword and Shield find raid seed without custom firmware",navDrawerTitle:"Find Raid Seed",description:"Use this if you do not have a switch with custom firmware",slug:"retail-swsh-get-seed-without-cfw",category:"Sword and Shield",tag:"retail"};function i(n){const t={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Tools"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.a,{href:"https://github.com/zaksabeast/CaptureSight/releases",children:"CaptureSight"})," or ",e.jsx(t.a,{href:"https://github.com/Admiral-Fish/RaidFinder/releases/",children:"RaidFinder"})]}),`
+`]}),`
+`,e.jsx(t.h2,{children:"Method 1: Have a friend use CaptureSight"}),`
+`,e.jsx(t.p,{children:e.jsx(t.strong,{children:"Use this method if you have Nintendo online or know someone with custom firmware"})}),`
+`,e.jsx(t.h3,{children:"Method 1.a: Host the raid"}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Save the CFWless switch in front of the den and host the raid."}),`
+`,e.jsx(t.li,{children:"Have the CFW switch join the raid."}),`
+`,e.jsxs(t.li,{children:["On the CFW switch, press the ",e.jsx(t.code,{children:"Home"})," button and load CaptureSight."]}),`
+`,e.jsx(t.li,{children:'View the raid Pokémon in the "Wild Pokémon" menu and click the "Find Seed" button for the raid Pokémon.'}),`
+`,e.jsx(t.li,{children:"Have the CFWless switch restart the game without saving."}),`
+`,e.jsx(t.li,{children:"If a good RNG isn't found, beat the raid or use a wishing piece, then start again."}),`
+`]}),`
+`,e.jsx(t.h3,{children:"Method 1.b: Start a trade"}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Have the CFWless switch catch a raid Pokémon."}),`
+`,e.jsx(t.li,{children:"Have the CFWless switch join a trade with the CFW switch."}),`
+`,e.jsxs(t.li,{children:["On the CFW switch, press the ",e.jsx(t.code,{children:"Home"})," button and load CaptureSight."]}),`
+`,e.jsx(t.li,{children:'View the raid Pokémon in the "Wild Pokémon" menu and click the "Find Seed" button for the trade Pokémon.'}),`
+`,e.jsx(t.li,{children:"Do not trade the Pokémon and exit the trade so the CFWless switch doesn't save."}),`
+`,e.jsx(t.li,{children:"Restart the game on the CFWless switch so the den's seed is reset and can be RNG'd."}),`
+`]}),`
+`,e.jsx(t.h2,{children:"Method 2: Use RaidFinder"}),`
+`,e.jsx(t.p,{children:e.jsx(t.strong,{children:"Use this method if you must get the seed yourself"})})]})}function d(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{d as default,o as frontmatter};

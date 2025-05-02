77daaeb9670e052aa9b1506cd8ccb2999296549f
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-02 13:05:38 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@00cf723065d8d0d6fb2274a6f33417b3197ef748 🚀

---
+++ assets/Home-iak6cCDY.js
@@ -0,0 +1,14 @@
+import{u as i,j as e}from"./index-D1a8SAJY.js";const l={title:"PokemonRNG.com - Become The Very Best",description:"Train Smarter, Shine Brighter — Together.",slug:"",category:"Home",tag:"info"};function t(r){const n={a:"a",h3:"h3",li:"li",p:"p",ul:"ul",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h3,{children:"Train Smarter, Shine Brighter — Together."}),`
+`,e.jsx(n.p,{children:"Whether you're new to RNG or seeking a challenge, this site has everything you need:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Beginner-friendly guides"}),`
+`,e.jsx(n.li,{children:"Tools for desktop and mobile"}),`
+`,e.jsx(n.li,{children:"Troubleshooting tips for when things don't go as planned."}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Ready to get started?"}),`
+`,e.jsx(n.p,{children:"Check out these guides:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"Install PokeReader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/gen2-celebi",children:"Shiny Celebi in Crystal"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/emulator-emerald-egg",children:"Shiny Emerald Eggs"})}),`
+`]})]})}function o(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{o as default,l as frontmatter};

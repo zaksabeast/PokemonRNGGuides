b60127e4bf652dce37e88a61a5950d323a6f7c5f
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-03 17:02:27 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@b9916af0650584f4f22d6d607a9dc0a15edb1e59 🚀

---
+++ assets/Starter-VsNB6f7t.js
@@ -0,0 +1,66 @@
+import{u as d,j as e}from"./index-W2y165gb.js";const c=[{title:"Crystal Starter RNG",navDrawerTitle:"Starter RNG",description:"Learn how to RNG shiny starters in Pokémon Crystal.",slug:"gen2-starters",category:"Gold, Silver, Crystal",tag:"cfw"},{title:"RNG de Iniciales en Cristal",description:"Learn how to RNG shiny starters in Pokémon Crystal.",slug:"es-gen2-starters",category:"Gold, Silver, Crystal",tag:"cfw",translation:{enSlug:"gen2-starters",language:"es"}},{title:"水晶初始宝可梦乱数",description:"在水晶里获得异色初始宝可梦",slug:"zh-gen2-starters",category:"Gold, Silver, Crystal",tag:"cfw",translation:{enSlug:"gen2-starters",language:"zh"}}];function l(i){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",ul:"ul",...d(),...i.components},{Gen2PokemonRng:s,ShowIf:r,YouTubeVideo:a}=n;return s||o("Gen2PokemonRng"),r||o("ShowIf"),a||o("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsxs(r,{slug:"/gen2-starters",children:[e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"Video guide"}),e.jsx(a,{src:"https://www.youtube.com/embed/1PFQv-S8a48?si=7N6RoITaJg0lyrPR"}),e.jsx(n.h2,{children:"Written guide"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Save in front of the starter you want, in case you need to reset the game."}),`
+`,e.jsxs(n.li,{children:["Choose your starter and stop when you see the ",e.jsx(n.code,{children:"Player received Pokemon"})," text with the flashing arrow to continue."]}),`
+`,e.jsxs(n.li,{children:["Go into PokeReader's RNG screen and wait until it stops saying ",e.jsx(n.code,{children:"Finding ADIV Index"})," and ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pause PokeReader with ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsx(n.li,{children:'Type the information on PokeReader into the RNG Tool below and press "Generate".'}),`
+`,e.jsxs(n.li,{children:["Advance the game with ",e.jsx(n.code,{children:"L"})," until PokeReader's ",e.jsx(n.code,{children:"Advance"})," number matches one of the generated ",e.jsx(n.code,{children:"Advance"})," columns below."]}),`
+`,e.jsx(n.li,{children:"(Optional) Create a save state if you're on an emulator, in case you need to reset."}),`
+`,e.jsxs(n.li,{children:["When you land on your desired advance, press ",e.jsx(n.code,{children:"A"})," to accept the Pokemon."]}),`
+`,e.jsx(n.li,{children:"If you got your Pokemon, congrats! If not, keep trying! It won't take long."}),`
+`]}),e.jsx(n.h3,{children:"Helpful tips:"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"This is unstable! Expect it to take many tries!"}),`
+`,e.jsxs(n.li,{children:["Only use ",e.jsx(n.code,{children:"L + R"})," to pause and advance in Gen 2, not ",e.jsx(n.code,{children:"Start + Select"}),". ",e.jsx(n.code,{children:"Start + Select"})," will mess up the RNG!"]}),`
+`,e.jsxs(n.li,{children:["The gameboy takes more than one video frame to register a button press, but the 3ds notices it immediately. Pressing ",e.jsx(n.code,{children:"A"})," too fast will unpause the game without starting the encounter."]}),`
+`,e.jsxs(n.li,{children:["Holding ",e.jsx(n.code,{children:"A"})," too long might mess up the RNG! Press it for more than one frame, but no longer."]}),`
+`]}),e.jsx(n.h2,{children:"RNG Tool"}),e.jsx(s,{type:"starter",language:"en"}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`,e.jsx(n.li,{children:"Chinese translation: Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(r,{slug:"/es-gen2-starters",children:[e.jsx(n.h2,{children:"Herramientas"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"Videoguía"}),e.jsx(a,{src:"https://www.youtube.com/embed/1PFQv-S8a48?si=7N6RoITaJg0lyrPR"}),e.jsx(n.h2,{children:"Guía escrita"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Guarda frente al inicial que quieras, en el caso en que necesites reiniciar el juego."}),`
+`,e.jsxs(n.li,{children:["Elige tu inicial y detente cuando veas el cuadro de texto ",e.jsx(n.code,{children:"Jugador recibió a Pokémon"})," con la flecha parpadeando para continuar."]}),`
+`,e.jsxs(n.li,{children:["Ve a la pantalla de RNG del Pokereader y espera hasta que deje de mostrar ",e.jsx(n.code,{children:"Finding ADIV Index"})," y ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pausa el Pokereader con ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsx(n.li,{children:'Escribe la información del Pokereader dentro de la herramienta de RNG aquí abajo y cuando termines, presiona "Generar".'}),`
+`,e.jsxs(n.li,{children:["Avanza el juego con ",e.jsx(n.code,{children:"L"})," hasta que los avances desplegados del Pokereader coincidan con lo generado en la columna ",e.jsx(n.code,{children:"Avance"})," aquí abajo."]}),`
+`,e.jsx(n.li,{children:"(Opcional) Crea un estado de guardado si es que estas en emulador, en el caso que necesites reiniciar el juego."}),`
+`,e.jsxs(n.li,{children:["Cuando alcances tu avance deseado, presiona ",e.jsx(n.code,{children:"A"})," para aceptar el Pokémon."]}),`
+`,e.jsx(n.li,{children:"Si conseguiste a tu Pokémon, ¡Felicidades! si no, ¡Sigue intentando! no tardará mucho."}),`
+`]}),e.jsx(n.h2,{children:"RNG Tool"}),e.jsx(s,{type:"starter",language:"es"}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`]}),e.jsx(n.h2,{children:"Creditos"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Todas las personas que han investigado a lo largo de los años la Gen 2: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast por elaborar el método actual de rng y construir la herramienta."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, y Lord Timothy en Discord por ayudar realizando pruebas."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano por ayudar con la traducción al español."}),`
+`]})]}),`
+`,e.jsxs(r,{slug:"/zh-gen2-starters",children:[e.jsx(n.h2,{children:"所需工具"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"视频教程"}),e.jsx(a,{src:"https://www.youtube.com/embed/1PFQv-S8a48?si=7N6RoITaJg0lyrPR"}),e.jsx(n.h2,{children:"文字教程"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"在你想要选择的初始宝可梦前存档，以防需要复位游戏。"}),`
+`,e.jsxs(n.li,{children:["选择初始宝可梦后继续对话，直到在 ",e.jsx(n.code,{children:"玩家名 received 选择的宝可梦名"})," 这段文本下等待。"]}),`
+`,e.jsxs(n.li,{children:["进入PokeReader的RNG界面，等待直到停止显示 ",e.jsx(n.code,{children:"Finding ADIV Index"})," 和 ",e.jsx(n.code,{children:"Finding SDIV Index"})," 。"]}),`
+`,e.jsxs(n.li,{children:["按住 ",e.jsx(n.code,{children:"L + R"})," 键暂停PokeReader。"]}),`
+`,e.jsxs(n.li,{children:["将PokeReader上的信息输入下方的乱数工具中，然后点击 ",e.jsx(n.code,{children:"计算"}),"。"]}),`
+`,e.jsxs(n.li,{children:["持续按 ",e.jsx(n.code,{children:"L"})," 键推进帧数，直到PokeReader显示的",e.jsx(n.code,{children:"帧数"}),"与下方计算的某一列",e.jsx(n.code,{children:"帧数"}),"匹配。"]}),`
+`,e.jsx(n.li,{children:"（可选）如果你使用的是模拟器，可以使用即时存档以防失败复位。"}),`
+`,e.jsxs(n.li,{children:["当推进到目标帧数时，按 ",e.jsx(n.code,{children:"A"})," 键确认选择宝可梦。"]}),`
+`,e.jsx(n.li,{children:"如果获得目标宝可梦，恭喜你成功了！如果没有，继续多次尝试！很快就能成功。"}),`
+`]}),e.jsx(n.h2,{children:"乱数工具"}),e.jsx(s,{type:"starter",language:"zh"}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`,e.jsx(n.li,{children:"中文翻译：Hakuhiro/白希洛。"}),`
+`]})]})]})}function h(i={}){const{wrapper:n}={...d(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(l,{...i})}):l(i)}function o(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,c as frontmatter};

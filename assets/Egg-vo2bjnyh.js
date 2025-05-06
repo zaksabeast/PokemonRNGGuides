b467847b8a9ecd02a257c76273adf0f793c05aef
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-06 14:25:06 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@ef49375200407e8d650a81f0d8af5c6c423738d1 🚀

---
+++ assets/Egg-vo2bjnyh.js
@@ -0,0 +1,41 @@
+import{u as o,j as e}from"./index-CIHdflGl.js";const i={title:"Brilliant Diamond and Shining Pearl Egg RNG",navDrawerTitle:"Egg RNG",description:"Learn how to RNG eggs in Brilliant Diamond and Shining Pearl on a CFW Switch for shiny perfect Pokemon.",slug:"cfw-bdsp-egg",category:"Brilliant Diamond and Shining Pearl",isRoughDraft:!0,tag:"cfw"};function a(n){const t={code:"code",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.p,{children:"For this guide, we’ll go into more detail, but you need to have the Basic Knowledge guides, especially PokeFinder’s."}),`
+`,e.jsx(t.p,{children:"Requirements:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"PokeFinder Builds"}),`
+`,e.jsx(t.li,{children:"CaptureSight"}),`
+`,e.jsx(t.li,{children:"Shiny Charm and/or Masuda"}),`
+`,e.jsx(t.li,{children:"Fly"}),`
+`]}),`
+`,e.jsx(t.p,{children:"You must have either the Shiny Charm or Masuda to perform egg RNG. Without either, the egg is generated when you pick it up from the old man. Since Solaceon Town is noisy, it's hard to RNG there. Using Masuda or Shiny Charm changes the generation to when the egg can be picked up after a set number of steps. This lets you fly to a quiet place to advance frames and generate the egg."}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:`You won't need any delay if you advance at the right place.
+`})}),`
+`,e.jsx(t.p,{children:"Important: Before starting, you need to add your TID/SID to a profile in PokeFinder. The egg requires a profile with your TID/SID. Make sure to input your actual TID/SID, not your G8TID, etc. You can find these details using a save editor like PKHex on a self-obtained Pokémon. Check the Shiny Charm box if you have it."}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"Open PokeFinder, go to the Gen 8 tab, and click Egg. If you can't find this, make sure you’re using the correct build of PokeFinder."}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"Input the data for the parents, including IVs, Nature, Item (like Destiny Knot or Everstone), and gender (you can select if you're using Ditto). Check the Masuda box if you're using Masuda. Don’t forget to check Shiny Charm in your profile if you have it."}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"In-game, deposit your parents in the daycare. Open CaptureSight and go to the Daycare tab. You’ll see a step counter that starts at 180. When it hits 0, an egg will generate. Advance to your target while the counter is at 1, then take the final step."}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"The common method to advance is to fly to your home in Twinleaf Town and go upstairs. Advances increase at a stable rate of +1 here, making it easy to hit your target. You can save your game and restart if needed. Feel free to use another method to advance if you prefer."}),`
+`]}),`
+`]}),`
+`,e.jsx(t.p,{children:"Generate the results. Find your target advance, and use all methods to get close to it. Once at the right advance, move your character. For accuracy, move your character in the opposite direction of where they are standing and use the stick, not the pad."}),`
+`,e.jsxs(t.ol,{start:"5",children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"Input your seeds into PokeFinder, click generate, and use filters to find your exact target advance. When you decide on an advance, start advancing toward it."}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:["Unpause updates on CaptureSight if you haven’t already so you can see your advances incrementing. Press ",e.jsx(t.code,{children:"D-Pad Up"})," to control your game. Don’t move, or you’ll generate your egg before hitting your target. If your target is far away, use the Pokédex for quick advancement. Press ",e.jsx(t.code,{children:"X"})," to open the menu and go to the Pokédex. Hold left and right on your joystick to scroll quickly, which advances frames fast. You can also scroll down normally to advance at a slower rate. Be careful not to overshoot your advances."]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"When you are near your target, exit the Pokédex and menu, and get ready to take a step. When CaptureSight shows your target advance, take a step. Use your left joystick instead of the D-Pad to ensure you take a step."}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"After stepping, check if you hit your advance by going back to the Daycare tab of CaptureSight and confirming the Egg Seed matches your target. You can now return to the Daycare and collect your egg. Check again by looking at the PID of the egg using the Party tab of CaptureSight to see if it matches your target."}),`
+`]}),`
+`]})]})}function s(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(a,{...n})}):a(n)}export{s as default,i as frontmatter};
---
+++ assets/TID SID-BzzB_UcC.js
@@ -0,0 +1,10 @@
+import{u as r,j as e}from"./index-CIHdflGl.js";const i={title:"Diamond, Pearl, and Platinum TID/SID RNG",navDrawerTitle:"TID/SID RNG",description:"Learn how to obtain your desired Trainer ID (TID) and Secret ID (SID) combo in Diamond, Pearl, and Platinum.",slug:"emulator-dppt-tid-sid",category:"Diamond, Pearl, and Platinum",isRoughDraft:!0,tag:"emu"};function o(t){const n={code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsx(n.h2,{children:"Step 1: Understanding PID"}),`
+`,e.jsx(n.p,{children:"Here's a basic overview to help with getting wanted PID."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Pay attention to uppercase and lowercase letters for delay issues."}),`
+`,e.jsx(n.li,{children:"Consider the special tool for finding pairs faster if they're not found by PokeFinder search."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Remember to check for any updates on tools or methods.
+`})}),`
+`,e.jsx(n.p,{children:"Congrats! You've now got a better grasp on TID/SID RNG!"})]})}function s(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{s as default,i as frontmatter};

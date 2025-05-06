f629616eea6a35d4c851c5368f13119741877025
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-05-06 18:40:17 -0500

    Add selecting a target

--- src/rngToolsUi/gen3/rstid/index.tsx
+++ src/rngToolsUi/gen3/rstid/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Stepper } from "./stepper";
 import { RsTidSidGenerator } from "./rstid";
+import { Button, Flex } from "~/components";
 
 const items = [
   {
@@ -19,15 +20,50 @@ const items = [
 
 export const RsTid = () => {
   const [current, setCurrent] = React.useState(0);
+  const [targetSet, setTarget] = React.useState(false);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
 
   const onChange = (value: number) => {
     setCurrent(value);
   };
 
+  const handleTarget = (advance, value) => {
+    console.log(advance, value);
+    setTarget(true);
+    return advance;
+  };
+
+  const NextButton = () => (
+    <Flex justify="end">
+      <Button
+        trackerId="test"
+        type="primary"
+        size="middle"
+        onClick={() => next()}
+      >
+        Next
+      </Button>
+    </Flex>
+  );
+
   return (
     <React.Fragment>
       <Stepper onChange={onChange} current={current} items={items} />
-      {current < items.length - 1 && <RsTidSidGenerator />}
+
+      {current < items.length - 1 && (
+        <React.Fragment>
+          {targetSet && <NextButton />}
+          <RsTidSidGenerator handleTarget={handleTarget} />
+          {targetSet && <NextButton />}
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 };

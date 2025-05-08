e4b2dd23d73d9b13368806a3258b639ce170cbfb
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-08 15:24:12 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@267203a84d8adf01e78ad667f26d4b62f467fc69 🚀

---
+++ assets/Basic Rules of RNG-CCxPHHw_.js
@@ -0,0 +1,17 @@
+import{u as i,j as e}from"./index-C15-xmEY.js";const a={title:"Ruby and Sapphire RNG Info",navDrawerTitle:"RNG Info",description:"Learn how to advance the RNG and improve stability in Pokémon Ruby and Sapphire for consistent results.",slug:"rs-tips-rng",category:"Ruby and Sapphire",isRoughDraft:!0,tag:"any"};function t(r){const n={code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Methods"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Stationary: Method 1."}),`
+`,e.jsx(n.li,{children:"Wild: Method H-1."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: You might encounter Vblank issues with wild Pokémon, resulting in Method H-2 or H-4, but this is a rare occurrence.
+`})}),`
+`,e.jsx(n.h2,{children:"Intro"}),`
+`,e.jsx(n.p,{children:"Ruby and Sapphire may have fewer tips than FRLG or Emerald due to the Live Battery method, but there are still valuable insights to share."}),`
+`,e.jsx(n.h2,{children:"Trainer Card Flip"}),`
+`,e.jsxs(n.p,{children:["In Gen 3, flipping your Trainer Card resets noise-related delays, helping you focus on the default delay. It's important to do this reflexively. Access your Trainer Card and press ",e.jsx(n.code,{children:"A"}),". Try to do this between 10k-5k advances before your target advance. This can save you trouble."]}),`
+`,e.jsx(n.h2,{children:"Minimizing Noise"}),`
+`,e.jsx(n.p,{children:"To reduce vblank and noise issues, open your Trainer Card at the start of your RNG, perform a flip, close it, and then advance to your target. This simple strategy can protect you from unexpected surprises."}),`
+`,e.jsx(n.h2,{children:"Speeding Up RNG Advances"}),`
+`,e.jsx(n.p,{children:"Ruby and Sapphire have limited options for speeding up RNG advances compared to Emerald, Fire Red, or Leaf Green. However, like any Gen 3 game, being in a battle doubles the RNG advancement speed, which is useful for wild Pokémon RNG."}),`
+`,e.jsx(n.h3,{children:"Painting RNG"}),`
+`,e.jsx(n.p,{children:"Although it's not very useful for Live Battery, the Painting method does work in Ruby and Sapphire. As it's more advanced, it will be covered in a separate guide."})]})}function s(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{s as default,a as frontmatter};

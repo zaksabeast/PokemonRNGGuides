395778045b4177bfd1c03d3c4a421c9f1867aca9
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-08 16:50:17 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@1b9d8e709f831979d603d68029f3847cb13fd978 🚀

---
+++ assets/Find SID-DeWQUiYU.js
@@ -0,0 +1,168 @@
+import{u as l,j as e}from"./index-UwOjJzzj.js";const t=[{title:"Find Gen 3 SID",navDrawerTitle:"Find SID",description:"Various methods to finding an SID in Gen 3.",slug:"gen3-sid",category:"Emerald",addedOn:"2025-03-03",tag:"retail"},{title:"Find Gen 3 SID",navDrawerTitle:"Find SID",description:"Various methods to finding an SID in Gen 3.",slug:"rs-gen3-sid",category:"Ruby and Sapphire",tag:"retail"},{title:"Find Gen 3 SID",navDrawerTitle:"Find SID",description:"Various methods to finding an SID in Gen 3.",slug:"frlg-gen3-sid",category:"FireRed and LeafGreen",tag:"retail"},{title:"查找第三世代 SID",description:"多种方法查找第三世代的SID",slug:"zh-gen3-sid",category:"Emerald",addedOn:"2025-03-03",tag:"retail",translation:{enSlug:"gen3-sid",language:"zh"}},{title:"查找第三世代 SID",description:"多种方法查找第三世代的SID",slug:"zh-rs-gen3-sid",category:"Ruby and Sapphire",tag:"retail",translation:{enSlug:"rs-gen3-sid",language:"zh"}},{title:"查找第三世代 SID",description:"多种方法查找第三世代的SID",slug:"zh-frlg-gen3-sid",category:"FireRed and LeafGreen",tag:"retail",translation:{enSlug:"frlg-gen3-sid",language:"zh"}}];function r(n){const s={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",ul:"ul",...l(),...n.components},{ShowIf:i}=s;return i||h("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsxs(i,{slugs:["/gen3-sid","/rs-gen3-sid","/frlg-gen3-sid"],children:[e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're using an emulator or GBA flashcart"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're playing Ruby or Sapphire"})}),e.jsx("br",{}),e.jsx(s.h3,{children:"PokeFinder"}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Open ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"}),' and click the "IDs" button in the "Gen 3" tab.']}),`
+`,e.jsx(s.li,{children:'Select the "RS" tab in the new window that appears.'}),`
+`,e.jsx(s.li,{children:'Check "Dead Battery" if you have a dead battery (likely), otherwise fill in the time and date you started the save.'}),`
+`,e.jsx(s.li,{children:"Enter your TID in PokeFinder's filters."}),`
+`,e.jsx(s.li,{children:'Click "Generate" to get a list of possible SIDs.'}),`
+`,e.jsx(s.li,{children:"If you have multiple possible SIDs, save your game before choosing your starter."}),`
+`,e.jsx(s.li,{children:"Attempt to RNG your starter for each SID. If you get a shiny, you found your SID."}),`
+`]}),e.jsx(s.h3,{children:"Web tool with dead battery"}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Go to Lincoln's ",e.jsx(s.a,{href:"https://lincoln-lm.github.io/JS-Finder/Gen3/IDs.html",children:"SID Finder"}),"."]}),`
+`,e.jsx(s.li,{children:'Enter your TID and click the "TID" circle.'}),`
+`,e.jsxs(s.li,{children:["Enter ",e.jsx(s.code,{children:"5a0"}),' as the "Initial Seed".']}),`
+`,e.jsx(s.li,{children:'Click "Generate" to get a list of possible SIDs.'}),`
+`,e.jsx(s.li,{children:"If you have multiple possible SIDs, save your game before choosing your starter."}),`
+`,e.jsx(s.li,{children:"Attempt to RNG your starter for each SID. If you get a shiny, you found your SID."}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you already have a shiny"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Use EV-reducing berries if the Pokémon has battled to reset EVs."}),`
+`,e.jsxs(s.li,{children:["Open ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"}),' and go to "Gen 3 Tools" > "IV Calculator".']}),`
+`,e.jsx(s.li,{children:"Input the Pokémon's info to determine its IVs."}),`
+`,e.jsx(s.li,{children:'Open "Gen 3 Tools" > "IVs to PID".'}),`
+`,e.jsx(s.li,{children:"Enter the Pokémon’s IVs, nature, and your Trainer ID."}),`
+`,e.jsxs(s.li,{children:["Search for results and determine the correct method:",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Wild Pokémon: Methods 1-4."}),`
+`,e.jsx(s.li,{children:"Legendaries: Method 1."}),`
+`]}),`
+`]}),`
+`,e.jsx(s.li,{children:"If multiple SIDs appear, attempt to RNG a shiny using each until you find the correct one."}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you play Emerald, didn't alter Dewford Trends, and have a dead battery"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"/emerald-sid-feebas",children:"Follow this guide"})}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you play Emerald, didn't alter Dewford Trends, and have a save under a day old"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"/emerald-sid-feebas",children:"Follow this guide"})}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you have a hacked Wii or Gamecube and a GBA-GC cable"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Dump your save with ",e.jsx(s.a,{href:"https://github.com/FIX94/gba-link-cable-dumper/releases/tag/v1.6",children:"FIX94's gba-link-cable-dumper"}),"."]}),`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you have an NDS and NDS flashcart"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Dump your save with ",e.jsx(s.a,{href:"https://projectpokemon.org/home/tutorials/save-editing/managing-gba-saves/using-gba-backup-tool-r55/",children:"Rudolph's GBA Backup Tool"}),"."]}),`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you can transfer Pokemon to a hacked DSI, 3DS, or Switch"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Transfer a Pokemon up to your hacked console."}),`
+`,e.jsxs(s.li,{children:["Dump your hacked console's save",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/DS-Homebrew/GodMode9i/releases/latest",children:"GodMod9i for DSI"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/BernardoGiordano/Checkpoint/releases/latest",children:"Checkpoint for 3DS"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/J-D-K/JKSV/releases/latest",children:"JKSV for Switch"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(s.li,{children:["Load your save in ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," or ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," and view the SID."]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you have an NDS connected to WiFi and a Gen 4 game"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Join the ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"}),"."]}),`
+`,e.jsxs(s.li,{children:["Ask if someone is willing to check the SID of your Pokemon using the ",e.jsx(s.a,{href:"https://pkmnclassic.net/gts/",children:"PokeClassic Network GTS"}),"."]}),`
+`,e.jsx(s.li,{children:"Transfer a Pokemon up to a Gen 4 NDS game."}),`
+`,e.jsxs(s.li,{children:["Put a Pokemon on the ",e.jsx(s.a,{href:"https://pkmnclassic.net/gts/",children:"PokeClassic Network GTS"}),"."]}),`
+`,e.jsx(s.li,{children:"Tell the other person which Pokemon is yours."}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you can transfer Pokemon to Pokemon Home"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Transfer a Pokemon up to Pokemon Home."}),`
+`,e.jsxs(s.li,{children:["Join the ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"}),"."]}),`
+`,e.jsx(s.li,{children:"Ask someone to check the SID of the Pokemon."}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're okay using ACE and play Emerald"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Set up ACE with ",e.jsx(s.a,{href:"https://e-sh4rk.github.io/ACE3/emerald/getting-started/introduction/",children:"this guide"}),"."]}),`
+`,e.jsxs(s.li,{children:['Use the "Read SID from Pokémon in box 9 slot 27" code in ',e.jsx(s.a,{href:"https://pastebin.com/kYfBzVE3",children:"this pastebin"}),", which sets your TID to be your SID."]}),`
+`,e.jsx(s.li,{children:"View your Trainer Card, which will now show your SID instead of TID."}),`
+`,e.jsx(s.li,{children:"Soft reset to avoid saving the changes."}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're okay starting a new save in Emerald"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["Check the ",e.jsx(s.a,{href:"/emerald-shiny-starter",children:"Shiny Starter"})," guide."]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If you're okay starting a new save in Fire Red or Leaf Green"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Start a new save"}),`
+`,e.jsxs(s.li,{children:["Start a stopwatch when you press ",e.jsx(s.code,{children:"A"}),' to accept your name on the "YOUR NAME?" screen.']}),`
+`,e.jsx(s.li,{children:"Stop the stopwatch when your character shrinks and turns white, just before the character is in the moving truck."}),`
+`,e.jsxs(s.li,{children:["Open ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"}),' and click the "IDs" button in the "Gen 3" tab.']}),`
+`,e.jsx(s.li,{children:'Select the "FRLGE" tab in the new window that appears.'}),`
+`,e.jsxs(s.li,{children:["Enter your TID in the ",e.jsx(s.code,{children:"TID"})," field."]}),`
+`,e.jsx(s.li,{children:"Multiply the number of seconds on your stopwatch by 59.7275, and round down to get your likely advance."}),`
+`,e.jsxs(s.li,{children:["Subtract 50 from your likely adance and enter the result in the ",e.jsx(s.code,{children:"Initial Advances"})," field."]}),`
+`,e.jsxs(s.li,{children:["Add 50 from your likely adance and enter the result in the ",e.jsx(s.code,{children:"Max Advances"})," field."]}),`
+`,e.jsx(s.li,{children:'Click "Generate" to get a list of possible SIDs.'}),`
+`,e.jsx(s.li,{children:"If you have multiple possible SIDs, save your game before choosing your starter."}),`
+`,e.jsx(s.li,{children:"Attempt to RNG your starter for each SID. If you get a shiny, you found your SID."}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"If nothing above applies to you (last resort option)"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"Try to randomly encounter a shiny."}),`
+`,e.jsx(s.li,{children:'Follow the "If you already have a Shiny" section above.'}),`
+`]})]}),e.jsxs(s.p,{children:["Are we missing a method? Join the ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"})," and let us know!"]}),e.jsx(s.h2,{children:"Credits"}),e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(i,{slugs:["/zh-gen3-sid","/zh-rs-gen3-sid","/zh-frlg-gen3-sid"],children:[e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你使用模拟器或 GBA 烧录卡"})}),e.jsxs(s.p,{children:["1.使用 ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," 或者 ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"}),"加载存档查看你的SID。"]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你在游玩《红宝石 / 蓝宝石》"})}),e.jsx("br",{}),e.jsx(s.h3,{children:"PokeFinder"}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["打开 ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"}),'，在 "Gen 3" 选项卡中点击 "IDs" 按钮。']}),`
+`,e.jsx(s.li,{children:'在弹出的窗口中，选择 "RS" 选项卡。'}),`
+`,e.jsx(s.li,{children:'如果你的游戏电池已耗尽（通常是这种情况），勾选 "Dead Battery" 选项，否则选择创建存档时的时间和日期。'}),`
+`,e.jsx(s.li,{children:"在 PokeFinder 的筛选栏中输入你的 TID。"}),`
+`,e.jsx(s.li,{children:"点击生成可能的 SID 列表。"}),`
+`,e.jsx(s.li,{children:"如果结果中有多个可能的 SID，请在选择初始宝可梦前存档。"}),`
+`,e.jsx(s.li,{children:"依次尝试用这些 SID 进行乱数刷闪，如果成功获得异色宝可梦，你就找到了正确的 SID。"}),`
+`]}),e.jsx(s.h3,{children:"使用 Web 工具（仅适用于电池耗尽的情况）"}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["打开 Lincoln 的 ",e.jsx(s.a,{href:"https://lincoln-lm.github.io/JS-Finder/Gen3/IDs.html",children:"SID Finder"}),"。"]}),`
+`,e.jsx(s.li,{children:'输入你的 TID，然后点击 "TID" 按钮。'}),`
+`,e.jsx(s.li,{children:'在 "Initial Seed" 处输入 5a0。'}),`
+`,e.jsx(s.li,{children:"点击生成可能的 SID 列表。"}),`
+`,e.jsx(s.li,{children:"如果有多个可能的 SID，请在选择初始宝可梦前存档。"}),`
+`,e.jsx(s.li,{children:"依次尝试用这些 SID 进行乱数刷闪，如果成功获得异色宝可梦，你就找到了正确的 SID。"}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你已经拥有异色宝可梦"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"如果宝可梦曾参与战斗，使用降低努力值的树果重置努力值。"}),`
+`,e.jsxs(s.li,{children:["打开 ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"}),' ，进入 "工具" > "IV Calculator"。']}),`
+`,e.jsx(s.li,{children:"输入宝可梦的信息，以计算其个体值（IV）。"}),`
+`,e.jsx(s.li,{children:'进入 "第三世代工具" > "IVs to PID"。'}),`
+`,e.jsx(s.li,{children:"输入宝可梦的 IV、性格，以及你的 TID。"}),`
+`,e.jsxs(s.li,{children:["进行搜索，并根据不同情况选择正确的方法：",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"野生宝可梦：方式 1-4。"}),`
+`,e.jsx(s.li,{children:"传说宝可梦：方式 1。"}),`
+`]}),`
+`]}),`
+`,e.jsx(s.li,{children:"如果结果中出现多个 SID，请尝试用不同 SID 进行乱数刷闪，直至找到正确的 SID。"}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你在游玩《绿宝石》，且没有更改武斗镇的流行话题，并且游戏电池已耗尽"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"/emerald-sid-feebas",children:"请参考本指南"})}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你在游玩《绿宝石》，且没有更改武斗镇的流行话题，并且存档时间不足一天"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"/emerald-sid-feebas",children:"请参考本指南"})}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你有一台已破解的 Wii 或 GameCube，以及一条 GBA-GC 连接线"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["使用 ",e.jsx(s.a,{href:"https://github.com/FIX94/gba-link-cable-dumper/releases/tag/v1.6",children:"FIX94's gba-link-cable-dumper"}),"转储存档。"]}),`
+`,e.jsxs(s.li,{children:["使用 ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," 或 ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," 加载存档，即可查看 SID。"]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你有一台 NDS 和 NDS 烧录卡"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["使用 ",e.jsx(s.a,{href:"https://projectpokemon.org/home/tutorials/save-editing/managing-gba-saves/using-gba-backup-tool-r55/",children:"Rudolph's GBA Backup Tool"}),"导出存档。"]}),`
+`,e.jsxs(s.li,{children:["使用 ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," 或 ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," 加载存档，即可查看 SID。"]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你可以将宝可梦传输到已破解的 DSi、3DS 或 Switch"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"将宝可梦传输到你的破解主机。"}),`
+`,e.jsxs(s.li,{children:["导出存档：",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/DS-Homebrew/GodMode9i/releases/latest",children:"DSi 使用 GodMode9"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/BernardoGiordano/Checkpoint/releases/latest",children:"3DS 使用 Checkpoint"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://github.com/J-D-K/JKSV/releases/latest",children:"Switch 使用 JKSV"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(s.li,{children:["使用 ",e.jsx(s.a,{href:"https://github.com/kwsch/PKHeX/releases/latest",children:"PKHex"})," 或 ",e.jsx(s.a,{href:"https://pkmds.app/",children:"pkmds.app"})," 加载存档，即可查看 SID。"]}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你有一台连接 WiFi 的 NDS 和第四世代游戏"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["加入 ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"}),"。"]}),`
+`,e.jsxs(s.li,{children:["请求其他人使用 ",e.jsx(s.a,{href:"https://pkmnclassic.net/gts/",children:"PokeClassic Network GTS"}),"检查你的宝可梦 SID。"]}),`
+`,e.jsx(s.li,{children:"将宝可梦传输到第四世代游戏。"}),`
+`,e.jsxs(s.li,{children:["在 ",e.jsx(s.a,{href:"https://pkmnclassic.net/gts/",children:"PokeClassic Network GTS"}),"上上传宝可梦。"]}),`
+`,e.jsx(s.li,{children:"告诉对方你的宝可梦信息，让他们帮你查询。"}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你可以将宝可梦传输到 Pokemon Home"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"将宝可梦传输到 Pokemon Home。"}),`
+`,e.jsxs(s.li,{children:["加入 ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"}),"."]}),`
+`,e.jsx(s.li,{children:"请求其他人帮忙检查你的 SID。"}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你愿意使用 ACE（代码执行漏洞）并游玩《绿宝石》"})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["按照 ",e.jsx(s.a,{href:"https://e-sh4rk.github.io/ACE3/emerald/getting-started/introduction/",children:"本指南"}),"设置 ACE。"]}),`
+`,e.jsxs(s.li,{children:["在 ",e.jsx(s.a,{href:"https://pastebin.com/kYfBzVE3",children:"此 pastebin"}),'使用 "读取 Box 9 Slot 27 的 SID" 代码，它会将你的 TID 设为 SID。']}),`
+`,e.jsx(s.li,{children:"打开训练家卡片，此时显示的 TID 就是你的 SID。"}),`
+`,e.jsx(s.li,{children:"软重置游戏，避免保存变更。"}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果你愿意重新开始一个新的存档（适用于《绿宝石》、《火红》或《叶绿》）"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"开始一个新的存档。"}),`
+`,e.jsx(s.li,{children:'在 "YOUR NAME?" 界面按下 A 确认名字的同时，启动秒表计时。'}),`
+`,e.jsx(s.li,{children:"当角色缩小并变白（即即将进入卡车的瞬间），停止秒表计时。"}),`
+`,e.jsxs(s.li,{children:["打开 ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder/releases",children:"PokeFinder"}),'，在 "第三世代" 选项卡中点击 "IDs" 按钮。']}),`
+`,e.jsx(s.li,{children:'在新窗口中选择 "FRLGE" 选项卡。'}),`
+`,e.jsx(s.li,{children:"在 TID 字段输入你的训练师 ID。"}),`
+`,e.jsx(s.li,{children:"用秒表记录的秒数乘以 59.7275，并向下取整，即为可能的起始帧数。"}),`
+`,e.jsx(s.li,{children:"计算 (可能的起始帧数 - 50)，并输入到 Initial Advances 字段。"}),`
+`,e.jsx(s.li,{children:"计算 (可能的起始帧数 + 50)，并输入到 Max Advances 字段。"}),`
+`,e.jsx(s.li,{children:"点击生成可能的 SID 列表。"}),`
+`,e.jsx(s.li,{children:"如果有多个可能的 SID，请在选择初始宝可梦前存档。"}),`
+`,e.jsx(s.li,{children:"依次尝试用这些 SID 进行乱数刷闪，如果成功获得异色宝可梦，你就找到了正确的 SID。"}),`
+`]})]}),e.jsxs("details",{children:[e.jsx("summary",{children:e.jsx("b",{children:"如果以上方法都不适用（最后的选择）"})}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"试着随机遇到一只异色宝可梦。"}),`
+`,e.jsx(s.li,{children:'然后参考上方的 "如果你已经拥有异色宝可梦" 方法来查找 SID。'}),`
+`]})]}),e.jsxs(s.p,{children:["如果你发现有其他方法未被列出，请加入 ",e.jsx(s.a,{href:"https://www.discord.gg/d8JuAvg",children:"Pokemon RNG Discord"})," 并告诉我们！"]}),e.jsx(s.h2,{children:"特别鸣谢"}),e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})]})}function a(n={}){const{wrapper:s}={...l(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(r,{...n})}):r(n)}function h(n,s){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{a as default,t as frontmatter};
---
+++ assets/Wondercard-Dtxfv1ma.js
@@ -0,0 +1,6 @@
+import{u as o,j as e}from"./index-UwOjJzzj.js";const i={title:"HeartGold and SoulSilver Wondercard RNG",navDrawerTitle:"Wondercard RNG",description:"How to RNG Wondercards",slug:"emulator-hgss-wondercard",category:"HeartGold and SoulSilver",isRoughDraft:!0,tag:"emu"};function r(n){const t={li:"li",p:"p",ul:"ul",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.p,{children:"For the future:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Explain how to import WC with PokeFinder to have EXACT values and avoid any mistake"}),`
+`,e.jsx(t.li,{children:"Explain the right center to use"}),`
+`,e.jsx(t.li,{children:"Explain that PID IS NOT DOABLE because of system tics."}),`
+`]})]})}function s(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{s as default,i as frontmatter};

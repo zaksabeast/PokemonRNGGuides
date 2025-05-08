922d574eeac5d8d7aa87349f7256adfec52a22aa
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-08 15:36:48 -0500

    Reorganize nav drawer code

--- src/components/navDrawer.tsx
+++ src/components/navDrawer.tsx
@@ -1,40 +1,24 @@
 import React from "react";
 import { Link } from "~/routes";
-import { Menu, Drawer } from "antd";
+import { Menu, MenuProps, Drawer } from "antd";
 import { Flex } from "./flex";
 import { Divider } from "./divider";
 import { Typography } from "./typography";
 import { useMobileNavDrawerOpen } from "~/state/navDrawer";
 import { useActiveRoute } from "~/hooks/useActiveRoute";
 import { settings } from "~/settings";
-import {
-  getGuide,
-  guides,
-  categories,
-  Category,
-  GuideMeta,
-  GuideSlug,
-} from "~/guides";
-import { difference, upperFirst, sortBy, partition, groupBy } from "lodash-es";
+import { getGuide, guides, categories, Category, GuideMeta } from "~/guides";
+import { difference, upperFirst, sortBy, groupBy } from "lodash-es";
 import styled from "@emotion/styled";
 import { track } from "~/analytics";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { Tag } from "./tag";
 import { match } from "ts-pattern";
 import { Color } from "@emotion/react";
+import * as tst from "ts-toolbelt";
 
-const StyledMenu = styled(Menu)({
-  "&&&.ant-menu-root": {
-    border: 0,
-  },
-});
-
-type GuideTag = "new" | GuideMeta["tag"];
-
-type GuideConfig = {
-  color: Color;
-  backgroundColor: Color;
-};
+dayjs.extend(utc);
 
 type MenuItemTagProps = {
   tag: GuideTag;
@@ -70,61 +54,186 @@ const MenuItemTag = ({ tag }: MenuItemTagProps) => {
   );
 };
 
-const [roughDraftGuides, finalizedGuides] = partition(
-  guides,
-  (guide) => guide.meta.isRoughDraft,
-);
-
-type isNew<T> = { isNew: boolean; item: T };
-
 type MenuItem = {
+  type: "menuItem";
   key: string;
-  title: string;
-  tag: GuideTag;
   label: React.ReactNode;
   icon: React.ReactNode;
+  tag: GuideTag;
 };
 
 type MenuCategory = {
+  type: "menuCategory";
   key: string;
-  children: (MenuItem | MenuCategory)[];
   label: React.ReactNode;
   icon: React.ReactNode;
+  children: (MenuItem | MenuCategory)[];
 };
 
-const getMenuItemFromGuide = ({
-  slug,
-  showTag = true,
+type RootCategory = {
+  label: string;
+  categories: Readonly<Category[]>;
+  isRoughDraft?: true;
+};
+
+const rootCategories: RootCategory[] = [
+  { label: "GB", categories: ["Gold, Silver, Crystal"] },
+  {
+    label: "GBA",
+    categories: [
+      "GBA Tools",
+      "Ruby and Sapphire",
+      "FireRed and LeafGreen",
+      "Emerald",
+      "GBA Technical Documentation",
+    ],
+  },
+  {
+    label: "NDS",
+    categories: [
+      "NDS Tools",
+      "Diamond, Pearl, and Platinum",
+      "HeartGold and SoulSilver",
+      "Black and White",
+      "Black 2 and White 2",
+    ],
+  },
+  {
+    label: "3DS",
+    categories: [
+      "3DS Tools",
+      "X and Y",
+      "Omega Ruby and Alpha Sapphire",
+      "Sun and Moon",
+      "Ultra Sun and Ultra Moon",
+      "Transporter",
+    ],
+  },
+  { label: "Gamecube", categories: [] },
+  {
+    label: "Switch",
+    categories: [
+      "Switch Tools",
+      "Sword and Shield",
+      "Legends Arceus",
+      "Brilliant Diamond and Shining Pearl",
+    ],
+  },
+  { label: "Rough Drafts", categories, isRoughDraft: true },
+];
+
+const tagOrder: GuideMeta["tag"][] = [
+  "info",
+  "challenge",
+  "any",
+  "cfw",
+  "emu",
+  "retail",
+];
+
+const guideByCategory = groupBy(guides, (guide) => guide.meta.category);
+
+const roughDraftPrefix = "roughDraft-";
+
+const addPrefix = ({
+  key,
+  isRoughDraft,
 }: {
-  slug: GuideSlug;
-  showTag?: boolean;
-}): isNew<MenuItem> => {
-  const meta = getGuide(slug).meta;
-  const isNew = dayjs(meta.addedOn).isAfter(dayjs().subtract(7, "days"));
-  const tag = isNew ? "new" : meta.tag;
-  const title = meta.navDrawerTitle;
+  key: string;
+  isRoughDraft: boolean;
+}) => (isRoughDraft ? `${roughDraftPrefix}${key}` : key);
+
+const getMenuItem = (guide: GuideMeta): isNew<MenuItem> => {
+  const isNew = dayjs
+    .utc(guide.addedOn)
+    .isAfter(dayjs.utc().subtract(7, "days"));
+  const tag = isNew ? "new" : guide.tag;
   return {
     isNew,
     item: {
-      key: meta.slug,
-      title,
-      tag: meta.tag,
-      label: <Link href={meta.slug}>{title}</Link>,
-      icon: isNew || showTag ? <MenuItemTag tag={tag} /> : null,
+      type: "menuItem",
+      key: addPrefix({
+        key: guide.slug,
+        isRoughDraft: guide.isRoughDraft,
+      }),
+      label: <Link href={guide.slug}>{guide.navDrawerTitle}</Link>,
+      tag: guide.tag,
+      icon: <MenuItemTag tag={tag} />,
     },
   };
 };
 
-const getMenuCategory = (
-  category: Category,
-  keyPrefix: string,
-  guideItems: isNew<MenuItem>[],
-): isNew<MenuCategory> => {
-  const sortedGuideItems = sortBy(guideItems, ({ item }) => [
-    item.tag,
-    item.title,
-  ]).map(({ item }) => item);
-  const isNew = guideItems.some((item) => item.isNew);
+const shouldShowGuide = ({
+  guide,
+  showRoughDrafts,
+}: {
+  guide: GuideMeta;
+  showRoughDrafts: boolean;
+}) => {
+  if (guide.translation != null) {
+    return null;
+  }
+
+  if (guide.hideFromNavDrawer) {
+    return null;
+  }
+
+  return showRoughDrafts ? guide.isRoughDraft : !guide.isRoughDraft;
+};
+
+const getMenuItems = ({
+  guides,
+  isRoughDraft,
+}: {
+  guides: GuideMeta[];
+  isRoughDraft: boolean;
+}): isNew<MenuItem[]> | null => {
+  const filteredGuides = guides.filter((guide) =>
+    shouldShowGuide({ guide, showRoughDrafts: isRoughDraft }),
+  );
+
+  if (filteredGuides.length === 0) {
+    return null;
+  }
+
+  const sortedGuides = sortBy(filteredGuides, (guide) => guide.navDrawerTitle);
+  const menuItems = sortedGuides.map(getMenuItem);
+  return {
+    isNew: menuItems.some(({ isNew }) => isNew),
+    item: menuItems.map(({ item }) => item),
+  };
+};
+
+const getMiddleCategory = ({
+  category,
+  isRoughDraft,
+}: {
+  category: Category;
+  isRoughDraft: boolean;
+}): isNew<MenuItem | MenuCategory> | null => {
+  const guides = category == null ? null : guideByCategory[category];
+
+  if (guides == null) {
+    return null;
+  }
+
+  const guidesByTag = groupBy(guides, (guide) => guide.meta.tag);
+
+  const navItems = tagOrder
+    .filter((tag) => guidesByTag[tag] != null)
+    .flatMap((guide) => {
+      return getMenuItems({
+        isRoughDraft,
+        guides: guidesByTag[guide].map((guide) => guide.meta),
+      });
+    })
+    .filter((item) => item != null);
+
+  if (navItems.length === 0) {
+    return null;
+  }
+
+  const isNew = navItems.some(({ isNew }) => isNew);
 
   const label = match(category)
     .with("GBA Tools", () => "Tools")
@@ -138,143 +247,70 @@ const getMenuCategory = (
   return {
     isNew,
     item: {
-      key: `${keyPrefix}${category}`,
       label,
-      children: sortedGuideItems,
+      type: "menuCategory",
+      key: addPrefix({
+        key: category,
+        isRoughDraft,
+      }),
+      children: navItems.flatMap(({ item }) => item),
       icon: isNew ? <MenuItemTag tag="new" /> : null,
     },
   };
 };
 
-const getGuideMenu = (
-  guideList: (typeof guides)[keyof typeof guides][],
-  keyPrefix: string = "",
-) => {
-  const guidesByCategory = groupBy(guideList, (guide) => guide.meta.category);
-  return categories
-    .filter((category) => guidesByCategory[category] != null)
+const getRootCategory = (rootCategory: RootCategory): MenuCategory => {
+  const isRoughDraft = !!rootCategory.isRoughDraft;
+  const middleCategories = rootCategory.categories
     .map((category) => {
-      const guideItems = guidesByCategory[category]
-        .filter((guide) => !guide.meta.hideFromNavDrawer)
-        .map((guide) => getMenuItemFromGuide(guide.meta));
-      return getMenuCategory(category, keyPrefix, guideItems);
-    });
-};
+      return getMiddleCategory({
+        category,
+        isRoughDraft,
+      });
+    })
+    .filter((category) => category != null);
 
-const finalizedGuideMenu = getGuideMenu(finalizedGuides);
-
-const getMenuForCategory = (categories: Category[]) => {
-  return categories.flatMap((category) =>
-    finalizedGuideMenu.filter(({ item }) => item.key === category),
-  );
-};
-
-type CategoryConfig =
-  | {
-      key: string;
-      label: string;
-      pages?: isNew<MenuItem>[];
-      categories: Category[];
-      children?: never;
-    }
-  | {
-      key: string;
-      label: string;
-      pages?: never;
-      categories?: never;
-      children: ReturnType<typeof getGuideMenu>;
-    };
-
-const getCategory = ({
-  key,
-  label,
-  pages = [],
-  categories,
-  children: _children,
-}: CategoryConfig): isNew<MenuCategory> => {
-  const subCategories = _children ?? getMenuForCategory(categories);
-  const children = [...pages, ...subCategories];
-  const isNew = children.some((item) => item.isNew);
   return {
-    isNew,
-    item: {
-      key,
-      label,
-      children: children.map(({ item }) => item),
-      icon: isNew ? <MenuItemTag tag="new" /> : null,
-    },
+    type: "menuCategory",
+    key: addPrefix({
+      key: rootCategory.label,
+      isRoughDraft,
+    }),
+    label: rootCategory.label,
+    children: middleCategories.map(({ item }) => item),
+    icon: middleCategories.some((item) => item.isNew) ? (
+      <MenuItemTag tag="new" />
+    ) : null,
   };
 };
 
-const roughDraftPrefix = "roughDraft-";
+const guideMenu = rootCategories.map(getRootCategory);
 
-const challengesMenu = [
-  getCategory({
-    key: "Challenges",
+// Stricter item types
+type StyledMenuProps = tst.O.Overwrite<MenuProps, { items: MenuCategory[] }>;
+const StyledMenu = styled(Menu as unknown as React.FC<StyledMenuProps>)({
+  "&&&.ant-menu-root": {
+    border: 0,
+  },
+});
+
+type GuideTag = "new" | GuideMeta["tag"];
+
+type GuideConfig = {
+  color: Color;
+  backgroundColor: Color;
+};
+
+type isNew<T> = { isNew: boolean; item: T };
+
+const challengeCategories: RootCategory[] = [
+  {
     label: "Challenges",
     categories: ["USUM Challenges"],
-  }).item,
+  },
 ];
 
-const guideMenu = [
-  getMenuItemFromGuide({ slug: "/mystic-timer", showTag: false }).item,
-  getCategory({
-    key: "GB",
-    label: "GB",
-    categories: ["Gold, Silver, Crystal"],
-  }).item,
-  getCategory({
-    key: "GBA",
-    label: "GBA",
-    categories: [
-      "GBA Tools",
-      "Ruby and Sapphire",
-      "FireRed and LeafGreen",
-      "Emerald",
-      "GBA Technical Documentation",
-    ],
-    pages: [getMenuItemFromGuide({ slug: "/gba-overview", showTag: false })],
-  }).item,
-  getCategory({
-    key: "NDS",
-    label: "NDS",
-    categories: [
-      "NDS Tools",
-      "Diamond, Pearl, and Platinum",
-      "HeartGold and SoulSilver",
-      "Black and White",
-      "Black 2 and White 2",
-    ],
-  }).item,
-  getCategory({
-    key: "3DS",
-    label: "3DS",
-    categories: [
-      "3DS Tools",
-      "X and Y",
-      "Omega Ruby and Alpha Sapphire",
-      "Sun and Moon",
-      "Ultra Sun and Ultra Moon",
-      "Transporter",
-    ],
-  }).item,
-  getMenuForCategory(["Gamecube"])[0].item,
-  getCategory({
-    key: "Switch",
-    label: "Switch",
-    categories: [
-      "Switch Tools",
-      "Sword and Shield",
-      "Legends Arceus",
-      "Brilliant Diamond and Shining Pearl",
-    ],
-  }).item,
-  getCategory({
-    key: "Rough Drafts",
-    label: "Rough Drafts",
-    children: getGuideMenu(roughDraftGuides, roughDraftPrefix),
-  }).item,
-] satisfies (MenuItem | MenuCategory)[];
+const challengesMenu: MenuCategory[] = challengeCategories.map(getRootCategory);
 
 const NavDrawerContent = () => {
   const [route] = useActiveRoute();
@@ -288,7 +324,7 @@ const NavDrawerContent = () => {
       ? `${roughDraftPrefix}${guideMeta.category}`
       : guideMeta.category;
     const openTopLevelItem = [...challengesMenu, ...guideMenu].find((item) =>
-      "children" in item
+      item.type === "menuCategory"
         ? item.children.some((child) =>
             [openCategory, guideMeta.slug].includes(child.key),
           )

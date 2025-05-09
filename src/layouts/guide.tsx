860c63ee5afb33ae70c39482c5a26388bb9896c9
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-08 20:40:18 -0500

    Set heart icon as winner

--- src/layouts/guide.tsx
+++ src/layouts/guide.tsx
@@ -1,4 +1,3 @@
-import { Skeleton } from "antd";
 import { MainLayout } from "~/layouts/main";
 import { GuideMeta } from "~/guides";
 import {
@@ -10,35 +9,6 @@ import {
   LanguageButton,
 } from "~/components";
 import { settings } from "~/settings";
-import { useAbCohort } from "~/hooks/useAbTest";
-import { match } from "ts-pattern";
-
-const SupportButton = () => {
-  const { hydrated, cohort } = useAbCohort("supportUsIcon");
-
-  if (!hydrated) {
-    return <Skeleton.Button block />;
-  }
-
-  const iconName = match(cohort)
-    .with("heart", () => "Heart" as const)
-    .with("coffee", () => "Coffee" as const)
-    .exhaustive();
-
-  return (
-    <Button
-      trackerId="support_us_on_discord"
-      icon={<Icon name={iconName} />}
-      type="primary"
-      backgroundColor="BrandSecondary"
-      backgroundHoverColor="BrandSecondaryHover"
-      size="middle"
-      href={settings.supportUsUrl}
-    >
-      Keep Pokémon RNG Free & Growing
-    </Button>
-  );
-};
 
 type Props = {
   guideMeta: GuideMeta;
@@ -63,7 +33,17 @@ export const GuideLayout = ({ guideMeta, children }: Props) => {
       </Flex>
 
       <Flex>
-        <SupportButton />
+        <Button
+          trackerId="support_us_on_discord"
+          icon={<Icon name="Heart" />}
+          type="primary"
+          backgroundColor="BrandSecondary"
+          backgroundHoverColor="BrandSecondaryHover"
+          size="middle"
+          href={settings.supportUsUrl}
+        >
+          Keep Pokémon RNG Free & Growing
+        </Button>
       </Flex>
 
       {guideMeta.isRoughDraft && (

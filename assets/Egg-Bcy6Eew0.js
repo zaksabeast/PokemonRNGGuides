e2f8b7d77dad3569532b394e8287ed14fa2de438
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-10 01:03:32 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@0cafe66d4471363f434d4878cbe8cdba2be64730 🚀

---
+++ assets/Egg-Bcy6Eew0.js
@@ -0,0 +1,41 @@
+import{u as t,j as e}from"./index-kx7X6zXu.js";const h=[{title:"HeartGold and Soulsilver Egg RNG",navDrawerTitle:"Egg RNG",description:"Learn how to RNG eggs from the Daycare in HeartGold and SoulSilver for shiny, high-IV Pokémon.",slug:"emulator-hgss-egg",category:"HeartGold and SoulSilver",tag:"emu"},{title:"孵化乱数",description:"如何在培育屋进行孵化乱数",slug:"zh-emulator-hgss-egg",category:"HeartGold and SoulSilver",tag:"emu",translation:{enSlug:"emulator-hgss-egg",language:"zh"}}];function r(s){const n={a:"a",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...t(),...s.components},{ShowIf:i}=n;return i||o("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsxs(i,{slug:"/emulator-hgss-egg",children:[e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"PokeFinder"}),`
+`,e.jsxs(n.li,{children:["Tools needed for RNG abuse with ",e.jsx(n.a,{href:"/desmume-setup",children:"Desmume"})]}),`
+`,e.jsx(n.li,{children:"Access to the Daycare and info about your parents"}),`
+`]}),e.jsx(n.h2,{children:"Intro"}),e.jsx(n.p,{children:"This guide will show you how to advance the RNG to get the Pokémon you want. It assumes you know some basics about Gen 4 RNG, like starting and advancing the RNG. The Masuda Method is introduced in this generation, but it's not essential to use."}),e.jsx(n.h2,{children:"Setup"}),e.jsx(n.p,{children:"Getting ready is easy:"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open PokeFinder and go to Gen 4 => Eggs => Searcher."}),`
+`,e.jsx(n.li,{children:"Choose your profile and enter your parents' info. The Pokémon that went into the daycare first is Parent A, and the other is Parent B."}),`
+`,e.jsx(n.li,{children:"Drop off your parents at the daycare and save your game."}),`
+`]}),e.jsx(n.h2,{children:"Hitting the PID using Egg Advances"}),e.jsx(n.p,{children:"Aim for an egg that doesn't need to advance the RNG, as this is the easiest method:"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"In PokeFinder, set your Advance Range to 0-0."}),`
+`,e.jsx(n.li,{children:"Choose the traits you want for your Pokémon, like Shiny, Nature, Gender, Ability, etc."}),`
+`,e.jsx(n.li,{children:"Generate results based on your criteria."}),`
+`,e.jsx(n.li,{children:"Select your target RNG advance."}),`
+`,e.jsx(n.li,{children:"RNG your Initial Seed."}),`
+`]}),e.jsx(n.p,{children:"If you need to advance the RNG beyond 0, increase the egg advance before making your egg. Here are some ways to do it:"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Use the Magikarp Coin to advance the RNG by 1."}),`
+`,e.jsx(n.li,{children:"Open the Happiness App (loaded or used) – it advances the RNG 2 times the number of Pokémon on your team."}),`
+`,e.jsx(n.li,{children:"Calling Youngster Joey (available on Route 30) advances the RNG by 2."}),`
+`]}),e.jsx(n.p,{children:"Remember to save your game in front of the old man."}),e.jsx(n.h2,{children:"Hitting the IVs"}),e.jsx(n.p,{children:"The next part of the RNG is standard. Switch to the 'IVs' Method on PokeFinder and set your IV filters. Consider the IVs from your parents, but getting some 31 IVs is usually easy."}),e.jsx(n.p,{children:"Get a result, hit your target advance, and advance the RNG as usual."}),e.jsx(n.p,{children:"Once you’ve done everything correctly, grab your egg, and you’ll have your desired Pokémon."}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(i,{slug:"/zh-emulator-hgss-egg",children:[e.jsx(n.h2,{children:"所需工具"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"PokeFinder"}),`
+`,e.jsxs(n.li,{children:["适用于 ",e.jsx(n.a,{href:"/desmume-setup",children:"Desmume"})," 的乱数工具"]}),`
+`,e.jsx(n.li,{children:"能够进入培育屋，以及亲代信息"}),`
+`]}),e.jsx(n.h2,{children:"介绍"}),e.jsx(n.p,{children:"本指南将教你如何推进乱数以获取理想的宝可梦。假设你已经了解四代乱数的基础知识，如初始种子设定和帧数推进。Masuda 法（不同语言亲代孵蛋提高闪率）首次在本世代引入，但并非必需。"}),e.jsx(n.h2,{children:"准备工作"}),e.jsx(n.p,{children:"准备工作很简单："}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"打开 PokeFinder，进入“第四世代 => 孵化乱数 => 检索器”。"}),`
+`,e.jsx(n.li,{children:"选择你的个人资料，输入亲代信息。第一个放入培育屋的宝可梦为亲代 A，后放入的为亲代 B。"}),`
+`,e.jsx(n.li,{children:"将亲代放入培育屋后，存档。"}),`
+`]}),e.jsx(n.h2,{children:"通过蛋帧数推进命中 PID"}),e.jsx(n.p,{children:"最简单的方法是选择不需要额外推进的目标蛋："}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"在 PokeFinder 中，将 Advance 范围设为 0-0。"}),`
+`,e.jsx(n.li,{children:"选择目标属性，如异色、性格、性别、特性等。"}),`
+`,e.jsx(n.li,{children:"生成符合条件的结果。"}),`
+`,e.jsx(n.li,{children:"选择你的目标帧。"}),`
+`,e.jsx(n.li,{children:"进行初始种子乱数。"}),`
+`]}),e.jsx(n.p,{children:"如果目标蛋的帧数大于 0，需要在生成蛋前手动推进帧数，可使用以下方法："}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"拒绝一个蛋推进1帧。"}),`
+`,e.jsx(n.li,{children:"拨打短裤小子五郎/ゴロウ/Gorō（位于30号道路）的电话推进 2 帧。"}),`
+`]}),e.jsx(n.p,{children:"生成蛋后不需要领取，存档于培育屋老爷爷面前。"}),e.jsx(n.h2,{children:"命中个体"}),e.jsx(n.p,{children:"个体乱数的操作方式较为标准。在 PokeFinder 中仅设置你的个体筛选条件。考虑亲代个体继承情况，但获取高个体通常较容易。"}),e.jsx(n.p,{children:"找到目标后，命中你的目标推进值，并按照常规方法（鹦鹉等）推进帧数。"}),e.jsx(n.p,{children:"一切完成后，领取蛋，即可获得你的目标宝可梦。"}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})]})}function d(s={}){const{wrapper:n}={...t(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}function o(s,n){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{d as default,h as frontmatter};

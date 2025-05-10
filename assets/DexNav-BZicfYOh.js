a035c0280bca555df73ed274b4bfd08658e125a0
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-10 01:09:07 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@552469e8e5b3e487c168f0afc985c32fe66e38b8 🚀

---
+++ assets/DexNav-BZicfYOh.js
@@ -0,0 +1,26 @@
+import{u as r,j as e}from"./index-xQ0rCyMi.js";const i={title:"Omega Ruby and Alpha Sapphire DexNav RNG",navDrawerTitle:"DexNav RNG",description:"Learn how to RNG Pokémon using the DexNav feature in Omega Ruby and Alpha Sapphire.",slug:"emulator-oras-dexnav",category:"Omega Ruby and Alpha Sapphire",isRoughDraft:!0,tag:"emu"};function n(a){const t={code:"code",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...r(),...a.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Tools"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Citra (save-state builds) w/ CitraRNG"}),`
+`,e.jsx(t.li,{children:"3DSRNGTool (Real96's fork for TimeFinder)"}),`
+`,e.jsx(t.li,{children:"Optional: TimeFinder for a specific target"}),`
+`]}),`
+`,e.jsx(t.h2,{children:"Step 1: Setup"}),`
+`,e.jsx(t.p,{children:"Go to the location where you want to do the RNG with your target Pokémon, and use a repel. This helps increase total encounters and gives you better chances for Egg Moves and IVs."}),`
+`,e.jsxs(t.p,{children:['Next, find a target frame. Use TimeFinder (link will be provided later), or use the old method of SRing for an initial seed. 3DSRNGTool doesn’t have a DexNav RNG method, so go to the "Normal Wild" section and set the delay to ',e.jsx(t.code,{children:"217"}),". This is the longer default delay for DexNav RNG, helping you avoid skipping your frame."]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:"It's important to set a high minimum frame for the frame range. Set a DexNav encounter before, and this takes time. I recommend setting the minimum frame to at least `5000`. If the area is small or difficult for DexNav, consider a higher minimum frame.\n"})}),`
+`,e.jsx(t.h2,{children:"Step 2: Process"}),`
+`,e.jsx(t.p,{children:"Once you have your target frame, make sure to set it as your Target Frame, as it’s important to track since the delay is random."}),`
+`,e.jsx(t.p,{children:'Enter the game, and quickly access the bag. Create a save state; this will be used for the first part of the RNG, which involves triggering an encounter. After saving, close the bag and click on "Search." It’s likely you won’t find an encounter; that’s normal. Move around, open the bag again, create another save state (or overwrite the previous one, but don’t skip your Target Frame), close the bag, and search. Repeat this until you get an encounter.'}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:`If you want a specific egg move or criteria, check if the triggered Pokémon meets your needs, since these are independent of the main RNG factors. You can keep creating save states until you have the Pokémon you want.
+`})}),`
+`,e.jsx(t.p,{children:"After triggering an encounter, reload the save state in the bag. With this setup at the start of a chain, no TinyMT is necessary—just stay in the bag and advance until you’re near your target frame. To avoid failure, create as many save states as needed."}),`
+`,e.jsxs(t.p,{children:["The next step is to reach your target frame with the ",e.jsx(t.code,{children:"217"})," delay. Close your bag a few hundred frames before your target frame—",e.jsx(t.code,{children:"150"}),' frames is recommended. Going too far can mess up your chances to trigger an encounter, leading to a failed RNG. Once back at the main screen, click on "Search," hold it, and pause the game.']}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:`This process can be tedious if you're not careful, so consider slowing down the game or advancing frame by frame.
+`})}),`
+`,e.jsx(t.p,{children:"While holding the Search button, advance frame by frame until reaching your target."}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{children:`Your mouse must keep clicking on the Search button while you advance frames. You can stop clicking if the game is paused and you’re not advancing. It may be confusing at first, but you'll master it with practice.
+`})}),`
+`,e.jsx(t.p,{children:"When you reach your target frame, unpause the game and stop clicking. This will trigger the encounter at the exact frame. Since you know you triggered an encounter, go to the Pokémon and start the battle."}),`
+`,e.jsx(t.p,{children:"If the Pokémon matches, congratulations—you did your DexNav RNG!"}),`
+`,e.jsxs(t.p,{children:["However, often you may not get what you want. DexNav RNG has varying delays—though they are stable, they relate to the length of the search animation. Common delays are ",e.jsx(t.code,{children:"137"}),", ",e.jsx(t.code,{children:"177"}),", and ",e.jsx(t.code,{children:"217"}),", but other values will typically fall within the ",e.jsx(t.code,{children:"130-250"})," frame range."]}),`
+`,e.jsx(t.p,{children:"If this happens, write down the IVs and compare the delay with the one you’ve got. Adjust the delay, reload the save state, and repeat. You should eventually get your encounter. If not, keep adjusting."})]})}function s(a={}){const{wrapper:t}={...r(),...a.components};return t?e.jsx(t,{...a,children:e.jsx(n,{...a})}):n(a)}export{s as default,i as frontmatter};
---
+++ assets/White Forest RNG (Emu)-DN2EotEN.js
@@ -0,0 +1,32 @@
+import{u as i,j as e}from"./index-xQ0rCyMi.js";const l=[{title:"White Forest RNG",navDrawerTitle:"White Forest RNG",description:"Learn how to RNG Pokémon found exclusively in White Forest in Pokémon White for desired IVs, nature, and shininess.",slug:"emulator-bw-white-forest",category:"Black and White",tag:"emu"},{title:"白森林乱数",description:"白森林乱数",slug:"zh-emulator-bw-white-forest",category:"Black and White",tag:"emu",translation:{enSlug:"emulator-bw-white-forest",language:"zh"}}];function o(t){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",strong:"strong",ul:"ul",...i(),...t.components},{ShowIf:r}=n;return r||s("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsxs(r,{slug:"/emulator-bw-white-forest",children:[e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: This guide assumes you have basic knowledge. You should be able to set up a profile, do a Wild RNG, and understand noise advancing the Seeding.
+`})}),e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/desmume-setup",children:"Desmume"})}),`
+`,e.jsx(n.li,{children:"Pokémon White"}),`
+`,e.jsx(n.li,{children:"RNG Reporter"}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: You can edit Trainers/Pokémon available in the White Forest using the PokeCGear Tool (http://projectpokemon.org/forums/attachment.php?attachmentid=10632&d=1362758021), but this won't be covered in this guide.
+`})}),e.jsx(n.h2,{children:"Setup"}),e.jsxs(n.p,{children:["The White Forest has a large pool of Pokémon, making it interesting for RNG. Although the game doesn't allow you to use Sweet Scent, it has a helpful feature: ",e.jsx(n.strong,{children:"The first step in grass or water upon entering the White Forest will lead to an encounter 100% of the time."})]}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open Desmume and load the Lua scripts. Find a Target Spread on PokeFinder, preferably around 150-200 PID Frames."}),`
+`,e.jsx(n.li,{children:"Go outside the White Forest. It's best to save at Route 14, near the gates. The grass patch seems closer there, but you can also do this from Route 15."}),`
+`,e.jsx(n.li,{children:"Walk until your IV Frame Count increases. This means when you reset for RNG, you can reach the grass patch without worrying about random IV Frame advances."}),`
+`,e.jsx(n.li,{children:"Save and perform the Initial Seed RNG. Do not load the C-Gear."}),`
+`,e.jsx(n.li,{children:"Once in-game, enter quickly and go next to the patch that will trigger an encounter. Open the menu."}),`
+`,e.jsx(n.li,{children:"Create a save state here. Let the game run briefly and find a long advance (a moment when the PID Frame doesn't advance at all) to do a step. Once you find one, reload the state, advance to it again, and go into the party as soon as you hit it. Use Chatots to reach your target frame. Close all menus and take your step."}),`
+`]}),e.jsx(n.p,{children:"Congrats! You've set up your RNG in White Forest!"}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(r,{slug:"/zh-emulator-bw-white-forest",children:[e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意：本指南假设你已具备基础知识。你应当能够建立配置文件、进行野生乱数，并理解种子生成过程中的噪音推进。
+`})}),e.jsx(n.h2,{children:"所需工具"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/desmume-setup",children:"Desmume"})}),`
+`,e.jsx(n.li,{children:"Pokémon White"}),`
+`,e.jsx(n.li,{children:"RNG Reporter"}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意：你可以使用 PokeCGear 工具（http://projectpokemon.org/forums/attachment.php?attachmentid=10632&d=1362758021）编辑白森林中的训练家/可遇宝可梦，但本指南不会涉及此内容。
+`})}),e.jsx(n.h2,{children:"设置"}),e.jsx(n.p,{children:"白森林拥有丰富的宝可梦种类，因此乱数操作非常有趣。虽然无法使用甜甜香，但有一个非常有利的机制：每次进入白森林后，在草地或水面上迈出的第一步将 100% 遇敌。"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"打开 Desmume 并加载 Lua 脚本。在 PokeFinder 中选择目标个体（建议 PID 帧数为 150～200 左右）。"}),`
+`,e.jsx(n.li,{children:"将位置移动至白森林门外，建议在 14 号道路附近的门口保存。那里离草地更近，也可以从 15 号道路进入。"}),`
+`,e.jsx(n.li,{children:"行走一段距离，直到 IV 帧数开始计数。这表示重置后你可以安全走到草地而不会触发随机 IV 帧推进。"}),`
+`,e.jsx(n.li,{children:"保存并执行初始种子乱数。不要开启 C-Gear。"}),`
+`,e.jsx(n.li,{children:"进入游戏后，迅速进入白森林并走到能够触发遇敌的草丛旁。打开菜单。"}),`
+`,e.jsx(n.li,{children:"在此处创建即时存档。让游戏运行片刻，寻找一个“长推进”时机（即 PID 帧长时间不变的时候），以便在该帧推进一步。找到后，重新读取即时存档，再次推进至该帧，并立即进入宝可梦菜单。使用聒噪鸟提升到目标帧，关闭所有菜单并迈出那一步。"}),`
+`]}),e.jsx(n.p,{children:"恭喜！你已成功完成白森林的乱数设置！"}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})]})}function h(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}function s(t,n){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,l as frontmatter};

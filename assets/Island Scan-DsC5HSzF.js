3fbd25700667904c611cc024ba265d6d45c0c8bb
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-17 17:13:30 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@d8b4c82a0b85375b9ea25daba9b2843c4ebb67ea 🚀

---
+++ assets/Island Scan-DsC5HSzF.js
@@ -0,0 +1,60 @@
+import{u as o,j as e}from"./index-NOJ0DI6T.js";const a=[{title:"Ultra Sun and Ultra Moon Island Scan RNG",navDrawerTitle:"Island Scan RNG",description:"Learn how to RNG Island Scan Pokémon in Ultra Sun and Ultra Moon — great for getting shinies in Apricorn Balls.",slug:"retail-usum-island-scan",category:"Ultra Sun and Ultra Moon",tag:"cfw"},{title:"Sun and Moon Island Scan RNG",navDrawerTitle:"Island Scan RNG",description:"Learn how to RNG Island Scan Pokémon in Sun and Moon — great for getting shinies in Apricorn Balls.",slug:"retail-sm-island-scan",category:"Sun and Moon",tag:"cfw",canonical:"retail-usum-island-scan"}];function i(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...o(),...s.components},{ShowIf:r}=n;return r||l("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsx(r,{slug:"/retail-usum-island-scan",children:e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"A 3DS with PokeReader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`,e.jsxs(n.li,{children:["The ",e.jsx(n.code,{children:"honey"})," item"]}),`
+`,e.jsx(n.li,{children:"Have started an Island Scan and know the Pokémon you've scanned for"}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/misc-3ds-island-scan-usum",children:"List of Island Scan islands and days for USUM"})}),`
+`]})}),`
+`,e.jsx(r,{slug:"/retail-sm-island-scan",children:e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"A 3DS with PokeReader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`,e.jsxs(n.li,{children:["The ",e.jsx(n.code,{children:"honey"})," item"]}),`
+`,e.jsx(n.li,{children:"Have started an Island Scan and know the Pokémon you've scanned for"}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/misc-3ds-island-scan-sm",children:"List of Island Scan islands and days for SM"})}),`
+`]})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: You can get the "honey" item in any store after clearing three trials.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 1: Setup 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Type the ",e.jsx(n.code,{children:"Init Seed"})," from PokeReader into the ",e.jsx(n.code,{children:"Seed"})," box."]}),`
+`,e.jsx(n.li,{children:"Enter your game version and TSV."}),`
+`,e.jsxs(n.li,{children:["If you have the shiny charm, check the ",e.jsx(n.code,{children:"Shiny Charm"})," box."]}),`
+`,e.jsxs(n.li,{children:["Click the ",e.jsx(n.code,{children:"Wild RNG"})," tab and change the ",e.jsx(n.code,{children:"Category"})," dropdown to ",e.jsx(n.code,{children:"Island Scan"}),"."]}),`
+`,e.jsxs(n.li,{children:["Change the ",e.jsx(n.code,{children:"Pokemon"})," dropdown to your target Pokémon."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2: Create a timeline"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Follow the ",e.jsx(n.a,{href:"/retail-usum-timeline",children:"timeline guide"})," using the ",e.jsx(n.code,{children:"NPC count"})," from PokeReader."]}),`
+`,e.jsx(n.li,{children:"You should have a target frame to hit."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: The RNG"}),`
+`,e.jsx(n.p,{children:"If you've followed the guide, you should now:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Have made a timeline."}),`
+`,e.jsx(n.li,{children:"Have a target frame."}),`
+`,e.jsx(n.li,{children:"Be standing in the area to catch your Island Scan Pokémon."}),`
+`,e.jsxs(n.li,{children:["Have the ",e.jsx(n.code,{children:"honey"})," item."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If any of these are not true, start the guide over. Moving your character will mess up the RNG.
+`})}),`
+`,e.jsx(n.h3,{children:"Step 4: Hitting your target frame"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"X"})," to open the in-game menu."]}),`
+`,e.jsxs(n.li,{children:["Hold the cursor on the ",e.jsx(n.code,{children:"bag"})," option."]}),`
+`,e.jsxs(n.li,{children:["Watch the ",e.jsx(n.code,{children:"Advances"})," counter on PokeReader.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["When the ",e.jsx(n.code,{children:"Advances"})," counter is close to your target frame, press ",e.jsx(n.code,{children:"Start + Select"})," to pause the game."]}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"Select"})," multiple times to slowly advance frames to your target frame."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Once at your target frame, press and hold ",e.jsx(n.code,{children:"A"})," to unpause the game while entering the bag."]}),`
+`,e.jsxs(n.li,{children:["Find and use the ",e.jsx(n.code,{children:"honey"})," item to start a wild encounter."]}),`
+`,e.jsxs(n.li,{children:["If the Pokémon you encountered was not the Island Scan Pokémon, see how many frames off you were and adjust.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Search all frames for the IVs of the Pokémon you got. Check the frame range instead of the timeline and search using the IVs."}),`
+`,e.jsx(n.li,{children:"After finding your actual frame, adjust as necessary from the delay and restart from the beginning."}),`
+`,e.jsx(n.li,{children:"This should not happen frequently."}),`
+`]}),`
+`]}),`
+`]})]})}function d(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}function l(s,n){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{d as default,a as frontmatter};

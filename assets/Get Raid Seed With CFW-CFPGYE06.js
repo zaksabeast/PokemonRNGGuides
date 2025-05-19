1385290aaf0fe8e6fcea19f8e53ae0150c7acfec
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-19 18:37:31 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@54753b761697711fc91e28446d91d6e5ec00b7c8 🚀

---
+++ assets/Get Raid Seed With CFW-CFPGYE06.js
@@ -0,0 +1,25 @@
+import{u as s,j as e}from"./index-D45r6rW5.js";const r={title:"Sword and Shield find raid seed with custom firmware",navDrawerTitle:"Find Raid Seed",description:"Learn how to find raid seeds in Sword and Shield using a Switch with custom firmware.",slug:"retail-swsh-get-seed-with-cfw",category:"Sword and Shield",tag:"cfw"};function t(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",ul:"ul",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/zaksabeast/CaptureSight/releases",children:"CaptureSight"})," or ",e.jsx(n.a,{href:"https://github.com/Leanny/PKHeX_Raid_Plugin",children:"PKHeX Raid Plugin"})]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Method 1: Use CaptureSight"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Start your game."}),`
+`,e.jsxs(n.li,{children:["Press the ",e.jsx(n.code,{children:"Home"})," button and open CaptureSight."]}),`
+`,e.jsx(n.li,{children:"Look at the Active Den List and note the currently active Den Ids."}),`
+`,e.jsx(n.li,{children:"Go back to the game and save in front of your desired den."}),`
+`,e.jsx(n.li,{children:"Use your wishing piece on the den."}),`
+`,e.jsxs(n.li,{children:["Press the ",e.jsx(n.code,{children:"Home"})," button as fast as possible, before the game finishes saving."]}),`
+`,e.jsx(n.li,{children:"Load CaptureSight and go to the Active Den List."}),`
+`,e.jsx(n.li,{children:"Find the new Den Id in the list."}),`
+`,e.jsx(n.li,{children:"Close and reopen the game so it doesn't think the wishing piece was used."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Method 2: Use the PKHeX Raid Plugin"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Use CFW or RCM to get your save file."}),`
+`,e.jsx(n.li,{children:'Open the "main" file with PKHeX.'}),`
+`,e.jsx(n.li,{children:'Go to "Tools" => "Display Raids" to see all your raids.'}),`
+`,e.jsx(n.li,{children:"Search for a den you're interested in and get the den seed."}),`
+`,e.jsx(n.li,{children:"Use RaidFinder with the den seed to find Pokémon you can RNG."}),`
+`,e.jsx(n.li,{children:"If you don't find anything interesting, just re-use a wishing piece or check for natural dens."}),`
+`]})]})}function o(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{o as default,r as frontmatter};

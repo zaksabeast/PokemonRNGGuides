48efeb8927a4d95d80a33a533a0142aeb7e55d0c
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-20 22:13:43 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@7efdf3906e7d5da805d2e013831b69bdf963e873 🚀

---
+++ assets/BW2 Egg-CQ5zsD0A.js
@@ -0,0 +1,64 @@
+import{u as t,j as e}from"./index-B-vLSDpV.js";const d=[{title:"Black 2 and White 2 Egg RNG",navDrawerTitle:"Egg RNG",description:"Learn how to RNG eggs from the Daycare in Black 2 and White 2 for shiny, high-IV Pokémon.",slug:"bw2-egg",category:"Black 2 and White 2",tag:"any"},{title:"黑白2孵蛋乱数",description:"学习如何在黑白2的培育屋中进行孵蛋乱数，获取异色高个体宝可梦",slug:"zh-bw2-egg",category:"Black 2 and White 2",tag:"emu",translation:{enSlug:"bw2-egg",language:"zh"}}];function l(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",strong:"strong",ul:"ul",...t(),...i.components},{ShowIf:r}=n;return r||s("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsxs(r,{slug:"/bw2-egg",children:[e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`,e.jsxs(n.li,{children:["All the tools needed for your method (",e.jsx(n.a,{href:"/mystic-timer",children:"Mystic Timer"})," or ",e.jsx(n.a,{href:"/desmume-setup",children:"Luas"}),")"]}),`
+`,e.jsx(n.li,{children:"Access to the Daycare"}),`
+`,e.jsx(n.li,{children:"IVs of your parent Pokémon"}),`
+`]}),e.jsx(n.h2,{children:"Part 1: Setup"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open PokeFinder."}),`
+`,e.jsx(n.li,{children:"Go to Gen 5 Tab -> Egg."}),`
+`,e.jsx(n.li,{children:"Input the parent Pokémon details."}),`
+`,e.jsx(n.li,{children:"Deposit your parent Pokémon in the daycare."}),`
+`,e.jsx(n.li,{children:"Walk until you have an egg ready."}),`
+`,e.jsx(n.li,{children:"Save the game, preferably in the daycare to avoid advancing the RNG."}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Egg RNG in BW2 is different than in BW1. IVs are generated first, then the PID. If you're interested in only one part, you can skip to the relevant section.
+`})}),e.jsx(n.h2,{children:"Part 2: IV + Nature RNG"}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Important: If you are going for Nidoran or Volbeat/Illumise, this step will determine which species you get. Enter the correct egg species, and you'll know the gender of the egg (male for Nidoran M/Volbeat and female for Nidoran F/Illumise).
+`})}),e.jsxs(n.p,{children:["In contrast to Gen 6 Egg RNG, you'll RNG a seed and IVs for the next egg without needing a specific advance. ",e.jsx(n.strong,{children:"Only hitting the initial seed matters"}),"."]}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Input the IVs, nature, and ability you want in PokeFinder (don't add other filters yet)."}),`
+`,e.jsx(n.li,{children:"Search for an initial seed."}),`
+`,e.jsx(n.li,{children:"Start the game and hit the initial seed."}),`
+`,e.jsxs(n.li,{children:["Talk to the old man and ",e.jsx(n.strong,{children:"reject the egg"}),"."]}),`
+`,e.jsx(n.li,{children:"Walk around until you have another egg ready."}),`
+`,e.jsx(n.li,{children:"Save next to the old man."}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If you only want IVs and not a PID, you can grab the egg. The RNG is done.
+`})}),e.jsx(n.h2,{children:"Part 3: PID RNG"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Remove all the IV filters and input the PID filters (gender, shininess)."}),`
+`,e.jsx(n.li,{children:"Search for an initial seed that gives your PID."}),`
+`,e.jsx(n.li,{children:"Start the game and hit the initial seed."}),`
+`,e.jsx(n.li,{children:"Advance the RNG until you reach your PID spread."}),`
+`,e.jsx(n.li,{children:"Talk to the old man and accept the egg."}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Sometimes with PokeFinder, you may need to talk to the man one advance earlier than your target. If your target is at 100 advances, talk to him at 99.
+`})}),e.jsx(n.p,{children:"You should now have the egg with everything you wanted!"}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(r,{slug:"/zh-bw2-egg",children:[e.jsx(n.h2,{children:"所需工具"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`,e.jsxs(n.li,{children:["根据你的方法所需的所有工具（",e.jsx(n.a,{href:"/mystic-timer",children:"神秘计时器"})," 或 ",e.jsx(n.a,{href:"/desmume-setup",children:"Lua脚本"}),"）"]}),`
+`,e.jsx(n.li,{children:"可进入培育屋"}),`
+`,e.jsx(n.li,{children:"双亲宝可梦的个体值"}),`
+`]}),e.jsx(n.h2,{children:"第一部分：准备工作"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"打开 PokeFinder。"}),`
+`,e.jsx(n.li,{children:"前往 Gen 5 标签页 -> Egg。"}),`
+`,e.jsx(n.li,{children:"输入双亲宝可梦的相关信息。"}),`
+`,e.jsx(n.li,{children:"将父母放入培育屋。"}),`
+`,e.jsx(n.li,{children:"行走直到老爷爷准备好一个蛋。"}),`
+`,e.jsx(n.li,{children:"存档，建议在培育屋内存档，以避免推进帧数。"}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意：黑白2的蛋乱数与黑白不同。IV 会先生成，之后才生成 PID。如果你只关心其中一部分，可以跳到相应部分。
+`})}),e.jsx(n.h2,{children:"第二部分：个体值 + 性格乱数"}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`重要提示：如果你要孵化尼多兰或晃晃斑/粉蝶虫，这一步将决定蛋的种类。请确保在 PokeFinder 中选择正确的蛋种类，这样就能知道性别（尼多朗为雄性、尼多兰为雌性，晃晃斑为雄性、粉蝶虫为雌性）。
+`})}),e.jsxs(n.p,{children:["与第六世代蛋乱数不同，你只需命中初始seed并生成对应的个体值，不需要命中特定帧数。",e.jsx(n.strong,{children:"只需命中初始种子即可"}),"。"]}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"在 PokeFinder 中输入你想要的个体、性格与特性（此时不要添加其他筛选条件）。"}),`
+`,e.jsx(n.li,{children:"搜索符合条件的初始种子。"}),`
+`,e.jsx(n.li,{children:"启动游戏并命中该初始种子。"}),`
+`,e.jsx(n.li,{children:"与老爷爷对话并拒绝接收蛋。"}),`
+`,e.jsx(n.li,{children:"再次行走直到下一个蛋准备好。"}),`
+`,e.jsx(n.li,{children:"在老爷爷旁边存档。"}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意：如果你只在意个体而不在意 PID，可以直接拿蛋，乱数操作已完成。
+`})}),e.jsx(n.h2,{children:"第三部分：PID 乱数"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"移除所有 IV 条件，仅输入与 PID 相关的过滤条件（如性别、异色等）。"}),`
+`,e.jsx(n.li,{children:"搜索生成目标 PID 的初始种子。"}),`
+`,e.jsx(n.li,{children:"启动游戏并命中该初始种子。"}),`
+`,e.jsx(n.li,{children:"推进乱帧数直到目标 PID 帧数。"}),`
+`,e.jsx(n.li,{children:"与老爷爷对话并接受蛋。"}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意：使用 PokeFinder 时，有时需在目标前一帧与老爷爷对话。例如目标在第 100 帧时，请在第 99 帧交谈。
+`})}),e.jsx(n.p,{children:"现在，你应该就获得了满足所有条件的蛋啦！"}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})]})}function o(i={}){const{wrapper:n}={...t(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(l,{...i})}):l(i)}function s(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{o as default,d as frontmatter};

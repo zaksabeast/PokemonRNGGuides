65671508f3475d05028caac49cba704cabcdf9c2
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-21 03:19:52 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@d2352bf1c2dfaf549c2a5093f4939ed9affd3033 🚀

---
+++ assets/3DS PokeReader-BK0gNv8L.js
@@ -0,0 +1,42 @@
+import{u as i,j as e}from"./index-DtYX_4r6.js";const o=[{title:"PokeReader 3DS",description:"Learn how to install PokeReader on your 3DS to assist with RNG and perfect Pokémon hunting.",slug:"install-pokereader",category:"3DS Tools",tag:"cfw"},{title:"3DS安装PokeReader",description:"在3DS上安装工具来帮助乱数宝可梦",slug:"zh-install-pokereader",category:"3DS Tools",tag:"cfw",translation:{enSlug:"install-pokereader",language:"zh"}}];function r(s){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...i(),...s.components},{ShowIf:l}=n;return l||d("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsxs(l,{slug:"/install-pokereader",children:[e.jsx(n.p,{children:"PokeReader is a plugin to help RNG by bringing emulator-like functionality to physical consoles."}),e.jsxs(n.p,{children:["For how to install PokeReader on a 3DS emulator, ",e.jsx(n.a,{href:"/install-pokereader-emu",children:"go here"}),"."]}),e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"A 3DS with CFW (Custom Firmware)"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/LumaTeam/Luma3DS/releases",children:"Luma version 13.0.0 or higher"})}),`
+`]}),e.jsx(n.h2,{children:"Install PokeReader on 3DS"}),e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Download the ",e.jsx(n.a,{href:"https://github.com/zaksabeast/PokeReader/releases/latest",children:"latest release of PokeReader"}),"."]}),`
+`,e.jsxs(n.li,{children:["Create the ",e.jsx(n.code,{children:"/luma/plugins"})," folder if it does not exist."]}),`
+`,e.jsxs(n.li,{children:["Copy ",e.jsx(n.code,{children:"default.3gx"})," to the ",e.jsx(n.code,{children:"/luma/plugins/"})," folder."]}),`
+`,e.jsxs(n.li,{children:["Open Rosalina (",e.jsx(n.code,{children:"L"})," + ",e.jsx(n.code,{children:"Down"})," + ",e.jsx(n.code,{children:"Select"}),") and enable the ",e.jsx(n.code,{children:"Plugin Loader"}),"."]}),`
+`]}),e.jsx(n.h2,{children:"PokeReader Controls"}),e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Start"})," + ",e.jsx(n.code,{children:"Up"}),": Opens and closes the overlay"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Start"})," + ",e.jsx(n.code,{children:"Select"}),": Pauses the game"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Start"})," while paused: Advances the game by 1 frame"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Select"})," while paused: Unpauses the game"]}),`
+`,e.jsxs(n.li,{children:["Crystal VC only",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"L"})," + ",e.jsx(n.code,{children:"R"}),": Pauses the game"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"L"})," while paused: Advances the game by 1 frame"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"R"})," while paused: Unpauses the game"]}),`
+`]}),`
+`]}),`
+`]}),e.jsx(n.h2,{children:"Troubleshooting"}),e.jsx(n.p,{children:e.jsx(n.strong,{children:"PokeReader does not load and my game crashes!"})}),e.jsx(n.p,{children:"Make sure you have updated the game to the latest version. PokeReader will not work on older game versions."}),e.jsx(n.p,{children:"Game updates can be downloaded from the Nintendo eShop."}),e.jsx(n.h2,{children:"What next?"}),e.jsx(n.p,{children:"Try some of these RNGs!"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/gen2-celebi",children:"Shiny Celebi in Crystal"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng-offline",children:"Easy Transporter RNG for perfect Pokemon"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/retail-usum-wormhole",children:"Shiny Wormhole Legends in USUM"})}),`
+`]}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: Hakuhiro."}),`
+`]})]}),`
+`,e.jsxs(l,{slug:"/zh-install-pokereader",children:[e.jsx(n.p,{children:"PokeReader是一款插件，它通过为实机提供类似模拟器脚本的功能显示内部参数来辅助乱数操作。"}),e.jsxs(n.p,{children:["如需了解如何在3DS模拟器上安装PokeReader，",e.jsx(n.a,{href:"/install-pokereader-emu",children:"请参阅此处"}),"。"]}),e.jsx(n.h2,{children:"所需工具"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"3DS破解机及自定义固件"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/LumaTeam/Luma3DS/releases",children:"Luma版本为13.0.0或更高版本"})}),`
+`]}),e.jsx(n.h2,{children:"在3DS上安装PokeReader"}),e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["下载 ",e.jsx(n.a,{href:"https://github.com/zaksabeast/PokeReader/releases/latest",children:"PokeReader的最新版本"}),"。"]}),`
+`,e.jsxs(n.li,{children:["如果 ",e.jsx(n.code,{children:"/luma/plugins"})," 文件夹不存在则手动创建它。"]}),`
+`,e.jsxs(n.li,{children:["将 ",e.jsx(n.code,{children:"default.3gx"})," 复制到 ",e.jsx(n.code,{children:"/luma/plugins/"})," 文件夹下。"]}),`
+`,e.jsxs(n.li,{children:["打开 Rosalina 菜单（",e.jsx(n.code,{children:"L"})," + ",e.jsx(n.code,{children:"Down"})," + ",e.jsx(n.code,{children:"Select"}),"）将 ",e.jsx(n.code,{children:"Plugin Loader"})," 设置为enable。"]}),`
+`]}),e.jsx(n.h2,{children:"疑难解答"}),e.jsx(n.p,{children:e.jsx(n.strong,{children:"PokeReader无法加载且游戏崩溃！"})}),e.jsx(n.p,{children:"请先确保您的游戏已更新到最新版本！PokeReader仅兼容最新版本的游戏，旧版本将无法正常运行插件。"}),e.jsx(n.p,{children:"可以在Nintendo eShop下载游戏更新补丁。"}),e.jsx(n.h2,{children:"安装完之后呢？"}),e.jsx(n.p,{children:"试试这些乱数操作！"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/gen2-celebi",children:"水晶的异色时拉比"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng-offline",children:"宝可梦虚拟传送获得完美宝可梦"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/retail-usum-wormhole",children:"究极日月的异色时空洞传说宝可梦"})}),`
+`]}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"中文翻译：Hakuhiro/白希洛。"}),`
+`]})]})]})}function c(s={}){const{wrapper:n}={...i(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}function d(s,n){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{c as default,o as frontmatter};
---
+++ assets/Stationary RNG-DMonNj8g.js
@@ -0,0 +1,60 @@
+import{u as i,j as e}from"./index-DtYX_4r6.js";const a=[{title:"Ultra Sun and Ultra Moon Static RNG",navDrawerTitle:"Static RNG",description:"Learn how to RNG static Pokémon in Ultra Sun and Ultra Moon for perfect IVs, natures, and shinies.",slug:"retail-usum-stationary",category:"Ultra Sun and Ultra Moon",tag:"cfw"},{title:"Sun and Moon Static RNG",navDrawerTitle:"Static RNG",description:"Learn how to RNG static Pokémon in Sun and Moon for perfect IVs, natures, and shinies.",slug:"retail-sm-stationary",category:"Sun and Moon",tag:"cfw",canonical:"retail-usum-stationary"}];function r(t){const n={a:"a",code:"code",em:"em",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"A 3DS with PokeReader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Recommended Reading"}),`
+`,e.jsxs(n.p,{children:["If you want to RNG a Mystery Gift (or Event) Pokémon, follow the ",e.jsx(n.a,{href:"/retail-usum-mystery-gift",children:"Gen 7 Mystery Gift RNG guide"}),"."]}),`
+`,e.jsxs(n.p,{children:["If you want to RNG a Pokémon in a Wormhole, follow the ",e.jsx(n.a,{href:"/retail-usum-wormhole",children:"USUM Wormhole RNG guide"}),"."]}),`
+`,e.jsx(n.h2,{children:"Final Screens"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Tapus: ",e.jsx(n.em,{children:"Tapu ko-ko-ko-kooo!!!"})," / ",e.jsx(n.em,{children:"Ta-pu-leeeh!"})," / ",e.jsx(n.em,{children:"Ta-pu-looooo"})," / ",e.jsx(n.em,{children:"Ta-pu-fiiieee!"}),"."]}),`
+`,e.jsx(n.li,{children:'In-game gifts/Fossils/Cosmog: "You received xxx!".'}),`
+`,e.jsxs(n.li,{children:["Necrozma: ",e.jsx(n.em,{children:"Linooo!"}),"."]}),`
+`,e.jsxs(n.li,{children:["Starters:",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:`SM - "Having accepted one another, you'll surely be friends for life."`}),`
+`,e.jsx(n.li,{children:'USUM - "You chose xxx!".'}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Set Up 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"In the upper right, input your game version and your TSV."}),`
+`,e.jsx(n.li,{children:"Also in the upper right, input the initial seed."}),`
+`,e.jsx(n.li,{children:'If you have the Shiny Charm, check the "Shiny Charm" box.'}),`
+`,e.jsx(n.li,{children:`Ensure you're on the "Stationary RNG" tab in 3DSRNGTool. Choose the "Category" and then the Pokémon you are RNGing for.`}),`
+`,e.jsx(n.li,{children:'If you have a Pokémon with Synchronize in the first slot of your party, select its nature from the dropdown for "Synch Nature". Failure to do this if the lead Pokémon has Synchronize will result in incorrect frames.'}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: For gift Pokémon, Synchronize guarantees the Pokémon will have the same nature as the lead Pokémon. If aiming for a specific nature, having Synchronize in the first slot will significantly improve your chances of finding target frames, as every frame will have the desired nature.
+The gift Eevee egg and the move Pikachu gift are exceptions. Synchronize has no effect on their natures.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2 (with NPCs): Create a Timeline"}),`
+`,e.jsxs(n.p,{children:["Follow the ",e.jsx(n.a,{href:"/retail-usum-timeline",children:"timeline guide"})," to create a timeline and find a target frame."]}),`
+`,e.jsx(n.h2,{children:"Step 3: Obtaining the Wanted Pokémon"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Once you have a correct timeline and a target frame, the next step is to RNG the Pokémon."}),`
+`,e.jsxs(n.li,{children:["Advance to your target frame. When you land on it, press ",e.jsx(n.code,{children:"A"})," to obtain the Pokémon or start a battle with it."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Congrats! You should now have the Pokémon you wanted. If not, reset the game and try again."}),`
+`,e.jsx(n.h2,{children:"Step 2 (with 0 NPCs):"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Do not check the "Blink F Only" box.'}),`
+`,e.jsx(n.li,{children:"Advance to the final screen for the Pokémon you want to RNG. Input the current frame in the frame range."}),`
+`,e.jsx(n.li,{children:'Adjust filters as needed, then click "Calculate" and choose any result as your target frame. If there are no results, increase the frame range or restart the game to get a new initial seed. (Restart from the beginning of the guide if you restart the game.)'}),`
+`,e.jsxs(n.li,{children:["Advance frames by pressing ",e.jsx(n.code,{children:"Start"})," to unpause the game. Pause again near your target frame with ",e.jsx(n.code,{children:"Start + Select"}),", then slowly advance by pressing ",e.jsx(n.code,{children:"Select"})," while paused."]}),`
+`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"You can use Festival Plaza to advance frames quicker."}),`
+`,e.jsx(n.li,{children:"Exit Festival Plaza approximately a thousand frames before your target frame to avoid missing it."}),`
+`]}),`
+`,e.jsxs(n.ol,{start:"5",children:[`
+`,e.jsxs(n.li,{children:["Advance to your target frame. When you land on it, press ",e.jsx(n.code,{children:"A"})," to obtain the Pokémon or start a battle with it."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Congrats! You should now have the Pokémon you wanted. If not, reset the game and try again."}),`
+`,e.jsx(n.h2,{children:"If You Did Not Obtain the Pokémon You Wanted"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Double-check that all info has been input correctly, especially the initial seed."}),`
+`,e.jsx(n.li,{children:"Restart the guide from the beginning and ensure to follow all instructions. Not getting the correct Pokémon is usually due to user error."}),`
+`,e.jsx(n.li,{children:"Make sure you are using the correct method based on whether there are NPCs or not. Failing to create a timeline when NPCs are over 0 will skip target frames."}),`
+`]})]})}function s(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{s as default,a as frontmatter};

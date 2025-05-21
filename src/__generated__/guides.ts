4faf1209949f3fe6d1ada83869626568ba180d44
Alana Herbin-Debrincat <aherbin@student.42berlin.de>
2025-05-21 17:22:12 +0200

    capturesight guide update & french transl - pre proofread

--- src/__generated__/guides.ts
+++ src/__generated__/guides.ts
@@ -1784,7 +1784,7 @@ export const guides = {
         "Faire avancer la RNG sur pokemon Diamant Etincelant et Perle Scintillante",
       navDrawerTitle: "Faire avancer la rng",
       description:
-        "Comment les avances de RNG fonctionnent dans pokemon Diamand Etincelant et Perle Scintillante. Apprennez comment controler ces avances pour rencontrer des pokemon parfaits!",
+        "Comment les avances de RNG fonctionnent dans pokemon Diamand Etincelant et Perle Scintillante. Apprenez comment controler ces avances pour rencontrer des pokemon parfaits!",
       slug: "/fr-bdsp-advance-rng/",
       isRoughDraft: false,
       tag: "any",
@@ -1801,6 +1801,31 @@ export const guides = {
         import(
           "~/../guides/Brilliant Diamond and Shining Pearl/Advancing the RNG.mdx"
         ),
+    ),
+  },
+  "/fr-install-capturesight/": {
+    meta: {
+      categories: ["Switch Tools"],
+      title: "Installer CaptureSight",
+      navDrawerTitle: "Installer Capture Sight",
+      description:
+        "Apprenez a installer CaptureSight sur votre Switch pour faciliter la manipulation de RNG et attraper des Pokemon parfaits !",
+      slug: "/fr-install-capturesight/",
+      isRoughDraft: false,
+      tag: "cfw",
+      hideFromNavDrawer: true,
+      addedOn: "2025-05-21",
+      translation: { enSlug: "/install-capturesight/", language: "fr" },
+      layout: "guide",
+      canonical: null,
+      file: "guides/Tools and Emulators/CaptureSight Install.mdx",
+      translations: {
+        fr: "/fr-install-capturesight/",
+        en: "/install-capturesight/",
+      },
+    },
+    Guide: React.lazy(
+      () => import("~/../guides/Tools and Emulators/CaptureSight Install.mdx"),
     ),
   },
   "/frlg-gen3-sid/": {
@@ -2231,7 +2256,10 @@ export const guides = {
       layout: "guide",
       canonical: null,
       file: "guides/Tools and Emulators/CaptureSight Install.mdx",
-      translations: null,
+      translations: {
+        fr: "/fr-install-capturesight/",
+        en: "/install-capturesight/",
+      },
     },
     Guide: React.lazy(
       () => import("~/../guides/Tools and Emulators/CaptureSight Install.mdx"),
@@ -5046,6 +5074,7 @@ export const guideSlugs = [
   "/experiments/",
   "/fire-red-and-leaf-green/",
   "/fr-bdsp-advance-rng/",
+  "/fr-install-capturesight/",
   "/frlg-gen3-sid/",
   "/frlg-seeding-bot/",
   "/frlg-static/",

0615fe0bb3ecc4eb6dab3a7b5f3582c934489de9
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-22 14:55:06 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@f5660fabddf9abe15bbf80173143049a32f48a7d 🚀

---
+++ assets/Celebi-teYUhc_P.js
@@ -0,0 +1,84 @@
+import{u as t,j as e}from"./index-DLd5x-ME.js";const h=[{title:"Celebi in Crystal",navDrawerTitle:"Celebi RNG",description:"Learn how to obtain a shiny Celebi in Pokémon Crystal using RNG manipulation.",slug:"gen2-celebi",category:"Gold, Silver, Crystal",tag:"cfw",addedOn:"2025-03-02"},{title:"时拉比",description:"如何获得异色时拉比",slug:"zh-gen2-celebi",category:"Gold, Silver, Crystal",tag:"cfw",addedOn:"2025-03-27",translation:{enSlug:"gen2-celebi",language:"zh"}},{title:"Celebi",description:"How to get a shiny Fairy",slug:"es-gen2-celebi",category:"Gold, Silver, Crystal",tag:"cfw",addedOn:"2025-04-23",translation:{enSlug:"gen2-celebi",language:"es"}}];function d(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",strong:"strong",ul:"ul",...t(),...s.components},{Alert:i,Gen2PokemonRng:r,ShowIf:l,YouTubeVideo:a}=n;return i||o("Alert"),r||o("Gen2PokemonRng"),l||o("ShowIf"),a||o("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsxs(l,{slug:"/gen2-celebi",children:[e.jsx(i,{showIcon:!0,type:"warning",message:"Highly experimental!",description:"Gen 2 RNG is still in its early days. Expect to need multiple attempts!"}),e.jsx(n.h2,{children:"Tools"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"Video guide"}),e.jsx(a,{src:"https://www.youtube.com/embed/wTpI7MV_f4U?si=0qF9bFb0HrDyv2zw"}),e.jsx(n.h2,{children:"Written guide"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Save in front of the Celebi shrine. This lets you reset if needed."}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"A"})," to interact with the shrine and stop when you see ",e.jsx(n.code,{children:"Player put in the GS Ball."})]}),`
+`,e.jsxs(n.li,{children:["Open PokeReader to the RNG screen and wait until ",e.jsx(n.code,{children:"Finding ADIV Index"})," and ",e.jsx(n.code,{children:"Finding SDIV Index"})," are gone."]}),`
+`,e.jsxs(n.li,{children:["Pause PokeReader with ",e.jsx(n.code,{children:"L + R"}),' then enter the info from PokeReader into the RNG tool below. Press "Generate".']}),`
+`,e.jsxs(n.li,{children:["Unpause the game with ",e.jsx(n.code,{children:"R"})," until near the target advance."]}),`
+`,e.jsxs(n.li,{children:["Pause then advance with ",e.jsx(n.code,{children:"L"})," one at a time."]}),`
+`,e.jsxs(n.li,{children:['Stop when the "Advance" number in PokeReader matches one shown in the tool.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"(Optional) Make a save state if you're on an emulator."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"A"})," to accept the Pokemon. ",e.jsx(n.strong,{children:"Do not press anything else until Celebi appears!"})]}),`
+`,e.jsx(n.li,{children:"If you got your Pokemon, congrats! If not, keep trying!"}),`
+`]}),e.jsx(n.h3,{children:"Helpful tips"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"This is unstable! Expect it to take many tries!"}),`
+`,e.jsxs(n.li,{children:["Only use ",e.jsx(n.code,{children:"L + R"})," to pause and advance in Gen 2, not ",e.jsx(n.code,{children:"Start + Select"}),". ",e.jsx(n.code,{children:"Start + Select"})," will mess up the RNG!"]}),`
+`,e.jsxs(n.li,{children:["Do not press any buttons in between pressing ",e.jsx(n.code,{children:"A"})," and seeing Celebi.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Switching screens or pressing buttons during this time will mess up the RNG!"}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["The 3DS registers button presses instantly, but the Game Boy takes a few frames.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Pressing ",e.jsx(n.code,{children:"A"})," too quickly might unpause the game without starting the encounter."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Don't hold ",e.jsx(n.code,{children:"A"})," too long. Press it slightly longer than one frame."]}),`
+`]}),e.jsx(n.h2,{children:"RNG Tool"}),e.jsx(r,{type:"celebi",language:"en"}),e.jsx(n.h2,{children:"What next?"}),e.jsx(n.p,{children:"Now you can use Transporter to RNG your Celebi to have perfect IVs!"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng",children:"Transporter RNG Guide"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng-offline",children:"Easier Transporter RNG method using an offline patch"})}),`
+`]}),e.jsx(n.h2,{children:"Credits"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"Hakuhiro for the Chinese translation."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`]})]}),`
+`,e.jsxs(l,{slug:"/zh-gen2-celebi",children:[e.jsx(i,{showIcon:!0,type:"warning",message:"高度实验性！",description:"二代乱数还未完善。预计需要多次尝试！"}),e.jsx(n.h2,{children:"所需工具"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"视频教程"}),e.jsx(a,{src:"https://www.youtube.com/embed/wTpI7MV_f4U?si=0qF9bFb0HrDyv2zw"}),e.jsx(n.h2,{children:"文字教程"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"在栎树林祠堂前存档，以防需要复位游戏。"}),`
+`,e.jsxs(n.li,{children:["按 ",e.jsx(n.code,{children:"A"})," 键与祠堂交互，并在 ",e.jsx(n.code,{children:"Player put in the GS Ball."})," 这段文本下等待。"]}),`
+`,e.jsxs(n.li,{children:["进入PokeReader的RNG界面，等待直到停止显示 ",e.jsx(n.code,{children:"Finding ADIV Index"})," 和 ",e.jsx(n.code,{children:"Finding SDIV Index"})," 。"]}),`
+`,e.jsxs(n.li,{children:["按住 ",e.jsx(n.code,{children:"L + R"})," 键暂停PokeReader。"]}),`
+`,e.jsxs(n.li,{children:["将PokeReader上的信息输入下方的乱数工具中，然后点击 ",e.jsx(n.code,{children:"计算"}),"。"]}),`
+`,e.jsxs(n.li,{children:["持续按 ",e.jsx(n.code,{children:"L"})," 键推进帧数，直到PokeReader显示的",e.jsx(n.code,{children:"帧数"}),"与下方计算的某一列",e.jsx(n.code,{children:"帧数"}),"匹配。"]}),`
+`,e.jsx(n.li,{children:"（可选）如果你使用的是模拟器，可以使用即时存档以防失败复位。"}),`
+`,e.jsxs(n.li,{children:["当推进到目标帧数时，按 ",e.jsx(n.code,{children:"A"})," 键触发战斗。 ",e.jsx(n.strong,{children:"请勿在进入战斗之前进行任何操作！"})]}),`
+`,e.jsx(n.li,{children:"如果遇到目标宝可梦，恭喜你成功了！如果没有，继续多次尝试！很快就能成功。"}),`
+`]}),e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意: 相比于初始宝可梦时拉比更不稳定，要更多次地尝试。
+`})}),e.jsx(n.h2,{children:"乱数工具"}),e.jsx(r,{type:"celebi",language:"zh"}),e.jsx(n.h2,{children:"特别鸣谢"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"中文翻译：Hakuhiro/白希洛。"}),`
+`]})]}),`
+`,e.jsxs(l,{slug:"/es-gen2-celebi",children:[e.jsx(i,{showIcon:!0,type:"warning",message:"¡Altamente experimental!",description:"RNG de Gen 2 todavía está en sus primeros días. ¡Es normal que necesite múltiples intentos!"}),e.jsx(n.h2,{children:"Herramientas"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),e.jsx(n.h2,{children:"Video guía"}),e.jsx(a,{src:"https://www.youtube.com/embed/wTpI7MV_f4U?si=0qF9bFb0HrDyv2zw"}),e.jsx(n.h2,{children:"Guía escrita"}),e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Guarda frente a Celebi en su santuario, en caso de que necesites resetear el juego."}),`
+`,e.jsxs(n.li,{children:['Presiona "A" para interactuar con el santuario y detente cuando veas ',e.jsx(n.code,{children:"Jugador coloca la GS Ball"}),"."]}),`
+`,e.jsxs(n.li,{children:["Visualiza la ventana del RNG de Pokereader y espera hasta que no diga mas ",e.jsx(n.code,{children:"Finding ADIV Index"})," y ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pausa el Pokereader con ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsxs(n.li,{children:["Escribe la información del Pokereader en la herramienta RNG aquí abajo y presiona ",e.jsx(n.code,{children:"Generar"}),"."]}),`
+`,e.jsxs(n.li,{children:["Avanza el juego con ",e.jsx(n.code,{children:"L"})," hasta que el número de ",e.jsx(n.code,{children:"Advance"})," en el Pokereader coincida con algún ",e.jsx(n.code,{children:"Avance"})," generado en las columnas de abajo."]}),`
+`,e.jsx(n.li,{children:"(Opcional) Crea un estado de guardado si estas en emulador, en caso de que necesites resetear."}),`
+`,e.jsxs(n.li,{children:["Cuando caes en un avance deseado, presiona ",e.jsx(n.code,{children:"A"})," para aceptar el Pokémon. ¡No presiones nada mas hasta que veas a Celebi!"]}),`
+`,e.jsx(n.li,{children:"Si conseguiste a tu Pokémon, ¡felicidades!, si no, ¡sigue intentando!"}),`
+`]}),e.jsx(n.h3,{children:"Consejos útiles"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"¡Esto es inestable! ¡Espera que tome varios intentos!"}),`
+`,e.jsxs(n.li,{children:["Solo usa ",e.jsx(n.code,{children:"L + R"})," para pausar y avanzar en Gen 2, no ",e.jsx(n.code,{children:"Start + Select"}),". ¡",e.jsx(n.code,{children:"Start + Select"})," estropeara el RNG!"]}),`
+`,e.jsxs(n.li,{children:["No presiones ningún botón en el lapso en que presiones ",e.jsx(n.code,{children:"A"})," y veas a Celebi (ejemplo, no cambies la ventana de RNG a la de Pokémon en el Pokereader). ¡Presionar botones estropeara el RNG!"]}),`
+`,e.jsx(n.li,{children:"El gameboy necesita más de un frame de video para registrar cuando presionas un botón, pero la 3ds lo nota inmediatamente."}),`
+`,e.jsxs(n.li,{children:["Presionar ",e.jsx(n.code,{children:"A"})," demasiado rápido reanudara el juego sin iniciar el encuentro."]}),`
+`,e.jsxs(n.li,{children:["¡Presionar ",e.jsx(n.code,{children:"A"})," por demasiado tiempo podría estropear el RNG! Presiónalo por más de un frame, pero no más."]}),`
+`]}),e.jsx(n.h2,{children:"RNG Tool"}),e.jsx(r,{type:"celebi",language:"es"}),e.jsx(n.h2,{children:"Créditos"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Todas las personas quienes han investigado Gen 2 a través de los años: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast por elaborar el método actual de rng y construir la herramienta."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, y Lord Timothy en Discord por ayudar realizando pruebas."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano por ayudar con la traducción al español."}),`
+`]})]})]})}function x(s={}){const{wrapper:n}={...t(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(d,{...s})}):d(s)}function o(s,n){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{x as default,h as frontmatter};
---
+++ assets/Timeline Guide-B1q4iGfC.js
@@ -0,0 +1,37 @@
+import{u as r,j as e}from"./index-DLd5x-ME.js";const l=[{title:"Ultra Sun and Ultra Moon Timeline RNG",navDrawerTitle:"Timeline RNG",description:"Learn how to create a timeline in Ultra Sun and Ultra Moon.",slug:"retail-usum-timeline",category:"Ultra Sun and Ultra Moon",tag:"cfw"},{title:"Sun and Moon Timeline RNG",navDrawerTitle:"Timeline RNG",description:"Learn how to create a timeline in Sun and Moon.",slug:"retail-sm-timeline",category:"Sun and Moon",tag:"cfw",canonical:"retail-usum-timeline"}];function t(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"A 3DS with PokeReader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Setting Up 3DSRNGTool"}),`
+`,e.jsx(n.p,{children:"Only create a timeline if there are one or more NPCs in your area. If you have 0 NPCs, you don't need a timeline."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Enter your game version and TSV."}),`
+`,e.jsxs(n.li,{children:["Input the initial seed, found in the main RNG view under ",e.jsx(n.code,{children:"Init Seed:"}),"."]}),`
+`,e.jsx(n.li,{children:"If you have the Shiny Charm, check the Shiny Charm box."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: In Gen 7 games, NPCs affect the RNG frames. Usually each NPC advances the frame by one. Therefore, an area with four NPCs typically progresses five frames each time. Things like Rotom or character blinks also have an effect. Accurate predictions can be achieved if the timeline is correctly created, ensuring you don't miss your target frame due to NPCs.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Timeline Creation"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Enable “Safe F Only” in 3DSRNGTool."}),`
+`,e.jsx(n.li,{children:"Enter the count of NPCs for the area. Confirm this count with PokeReader."}),`
+`,e.jsx(n.li,{children:"In the game, advance to the Pokémon's final screen you are RNGing for. Input the current frame in the frame range."}),`
+`,e.jsx(n.li,{children:'Click "Calculate", then advance to any listed frames with a "-" in the Mark column. These are "Safe Frames", useful for accurate frame landing predictions.'}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"Start"})," to advance frames by unpausing the game. You can then pause again with ",e.jsx(n.code,{children:"Start + Select"})," when close to your frame and slowly advance with ",e.jsx(n.code,{children:"Select"})," button while paused."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: Timeline and NPC Number Confirmation"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'After reaching any safe frame, input your current frame into 3DSRNGTool, then enable “Create Timeline” and click "Calculate".'}),`
+`,e.jsxs(n.li,{children:["Advance several times by pressing ",e.jsx(n.code,{children:"Select"})," and compare with the subsequent frames in 3DSRNGTool.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If they match, the NPC count is correct."}),`
+`,e.jsx(n.li,{children:"If they do not match, recreate your timeline."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Errors in making the timeline are typically discovered at this point. Verify the initial seed and ensure PokeReader's NPC counter hasn't changed during timeline creation."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Do not adjust filters during timeline creation. Only "Safe F Only" box should have been activated by this point. If any other filter has been adjusted, start again from the beginning.
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: Check if your desired frame is still within the timeline by redoing the timeline with your current frame, then searching for your target. If it doesn't appear in the results, your timeline has shifted.
+`})})]})}function o(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{o as default,l as frontmatter};

70a6e2acc90d19d80b205e07d81380f15320910a
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-23 14:52:16 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@1698810bcd7889190dd9acbfac2726e40a87ef4d 🚀

---
+++ assets/Retail Egg-Mbxb3LPk.js
@@ -0,0 +1,73 @@
+import{u,j as e}from"./index-BAc4Igsm.js";const p={title:"Retail Emerald Egg RNG",navDrawerTitle:"Egg RNG",description:"Learn how to Retail RNG eggs in Pokémon Emerald using the Daycare. Get perfect IVs, natures, and shinies.",slug:"retail-emerald-egg",category:"Emerald",tag:"retail",addedOn:"2025-05-18"};function j(i){const t={a:"a",code:"code",h2:"h2",h3:"h3",hr:"hr",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...u(),...i.components},{CalibrateHeldEgg:r,CalibrateHeldEggTimer:l,CalibratePickupEgg:o,CalibratePickupEggTimer:a,PokeNavInput:h,RetailEmeraldHeldEgg:d,RetailEmeraldPickupEgg:c,Step:s,Stepper:g,YouTubeVideo:x}=t;return r||n("CalibrateHeldEgg"),l||n("CalibrateHeldEggTimer"),o||n("CalibratePickupEgg"),a||n("CalibratePickupEggTimer"),h||n("PokeNavInput"),d||n("RetailEmeraldHeldEgg"),c||n("RetailEmeraldPickupEgg"),s||n("Step"),g||n("Stepper"),x||n("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Video Guide"}),`
+`,e.jsx(x,{src:"https://www.youtube.com/embed/yNWmpMTBOlc?si=rMUHb89lp8lkWCQD"}),`
+`,e.jsx(t.h2,{children:"Written Guide"}),`
+`,e.jsxs(g,{titles:["PokeNav","Setup","Pick Target","Get PID","Pick IVs","Get Stats"],children:[e.jsxs(s,{step:0,children:[e.jsx(t.h3,{children:"PokeNav"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Register as many trainers to the PokeNav as possible."}),`
+`,e.jsx(t.li,{children:"Open the PokeNav Match Call feature and select all the trainers you registered in the tool below."}),`
+`,e.jsx(t.li,{children:"Proceed to the next step."}),`
+`]}),e.jsx(h,{})]}),e.jsxs(s,{step:1,children:[e.jsx(t.h3,{children:"Step 1: Prepare"}),e.jsxs(t.ol,{children:[`
+`,e.jsxs(t.li,{children:["Optional, but highly recommended: Put a Pokemon with Lightning Rod and Flame Body in your party.",`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Electrike with Lightning Rod is on Route 110, just South of Mauville."}),`
+`,e.jsx(t.li,{children:"Slugma with Flame Body is in Fiery Path, just North of Mauville."}),`
+`]}),`
+`]}),`
+`,e.jsx(t.li,{children:"Buy at least 3 normal repels from Verdanturf."}),`
+`,e.jsx(t.li,{children:"Go to the Daycare and withdraw any Pokemon currently there."}),`
+`,e.jsx(t.li,{children:"Stand in front of the top right edge of the white path on Route 118."}),`
+`,e.jsx(t.li,{children:"Use the first repel."}),`
+`]}),e.jsx(t.p,{children:e.jsx(t.img,{src:"/images/Emerald/Retail-Egg/start-point.webp",alt:"White Path"})}),e.jsx(t.h3,{children:"Step 2: Deposit Pokemon"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Walk left until you're in front of the Daycare, then walk up until you're in front of the Daycare lady."}),`
+`,e.jsx(t.li,{children:"Deposit your parent Pokemon into the Daycare."}),`
+`,e.jsx(t.li,{children:"Exit the Daycare."}),`
+`,e.jsx(t.li,{children:"Talk to the Daycare Man to get the egg compatibility and write it down for later."}),`
+`]}),e.jsx(t.h3,{children:"Step 3: Get into position"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Walk to Verdanturf and run around."}),`
+`,e.jsx(t.li,{children:"Use the second repel."}),`
+`,e.jsx(t.li,{children:"Run around Verdanturf and use the third repel."}),`
+`,e.jsx(t.li,{children:"Stand next to the tree closest to the PokeCenter (screenshot below)."}),`
+`,e.jsx(t.li,{children:"Walk back and forth on the two tiles in front of the PokeCenter until the repel wears off."}),`
+`,e.jsx(t.li,{children:"Save the game and proceed to the next step."}),`
+`]}),e.jsx(t.p,{children:e.jsx(t.img,{src:"/images/Emerald/Retail-Egg/end-point.webp",alt:"PokeCenter"})})]}),e.jsxs(s,{step:2,children:[e.jsx(t.h3,{children:"Pick a Target"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:'Enter your egg details in the tool below and press "Generate".'}),`
+`,e.jsx(t.li,{children:'Press "Select" on the target Pokemon you want to hatch.'}),`
+`]}),e.jsx(d,{})]}),e.jsxs(s,{step:3,children:[e.jsx(t.h3,{children:"Step 1: RNGing the PID"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Make sure the Lightning Rod Pokemon is in the front of your party and save."}),`
+`,e.jsx(t.li,{children:"Start the timer below."}),`
+`,e.jsx(t.li,{children:"When first timer goes off, soft reset your game and load your save as fast as possible."}),`
+`,e.jsx(t.li,{children:"Walk back and forth on the two tiles in front of the PokeCenter for 9 steps."}),`
+`,e.jsx(t.li,{children:"Open the PokeDex however many times the tool below says."}),`
+`,e.jsx(t.li,{children:"Take 1 step on the same two tiles when the timer goes off."}),`
+`,e.jsx(t.li,{children:'If you received a PokeNav call that matches your expected call and the Daycare man has an egg, save the game. If not, scroll down to the "Calibrate" section.'}),`
+`]}),e.jsx(l,{}),e.jsx(t.h3,{children:"Step 2: Calibrate"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Put the Flame Body Pokemon in the front of your party."}),`
+`,e.jsx(t.li,{children:"Pick up your egg from the daycare man and hatch it. If it's shiny, proceed to the next step."}),`
+`,e.jsx(t.li,{children:"If the egg wasn't shiny, fill in the tool below with the match call and egg nature."}),`
+`,e.jsx(t.li,{children:'Press "Find advances matching eggs" to view a list of possible eggs.'}),`
+`,e.jsx(t.li,{children:'Press "Calibrate" on the egg with the offset closest to 0 to adjust the timer.'}),`
+`,e.jsx(t.li,{children:"Restart the PID RNG process."}),`
+`]}),e.jsx(r,{})]}),e.jsxs(s,{step:4,children:[e.jsx(t.h3,{children:"Picking IVs"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:'Fill in the tool below and press "Generate" to get a list of possible IVs.'}),`
+`,e.jsx(t.li,{children:'Press "Select" on the target Pokemon you want to hatch.'}),`
+`]}),e.jsx(c,{})]}),e.jsxs(s,{step:5,children:[e.jsx(t.h3,{children:"Step 1: RNGing the IVs"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Stand next to the Daycare man and save."}),`
+`,e.jsx(t.li,{children:"Start the timer below."}),`
+`,e.jsx(t.li,{children:"When the first timer goes off, soft reset your game, load your save, and talk to the Daycare man as fast as possible."}),`
+`,e.jsx(t.li,{children:'Continue the dialogue until you see the text "Take good care of it".'}),`
+`,e.jsxs(t.li,{children:["When the second timer goes off, press ",e.jsx(t.code,{children:"A"})," to receive the egg."]}),`
+`,e.jsx(t.li,{children:"Hatch the egg."}),`
+`]}),e.jsx(a,{}),e.jsx(t.h3,{children:"Step 2: Calibrate"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:'Enter the stats in the tool below and press "Find advances matching hatched egg".'}),`
+`,e.jsx(t.li,{children:"If the result with offset 0 matches your target stats and method, congratulations! You got your egg!"}),`
+`,e.jsx(t.li,{children:"If the result with offset 0 doesn't match your target method, try again without calibrating."}),`
+`,e.jsx(t.li,{children:'If there is not a result with offset 0, click "Calibrate" on the result with the offset closest to 0 to adjust the timer.'}),`
+`,e.jsx(t.li,{children:"Restart the IV RNG process."}),`
+`]}),e.jsx(o,{})]})]}),`
+`,e.jsx(t.hr,{}),`
+`,e.jsx(t.h2,{children:"Credits"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Zaksabesat for writing this web tool."}),`
+`,e.jsxs(t.li,{children:["Lincoln for the ",e.jsx(t.a,{href:"https://github.com/Lincoln-LM/PokeNav-Egg-RNG-Tool",children:"PokeNav tool"}),", which this tool is based on."]}),`
+`,e.jsxs(t.li,{children:["Thanks to all ",e.jsx(t.a,{href:"https://github.com/Admiral-Fish/PokeFinder",children:"PokeFinder"})," contributors, which this tool is based on."]}),`
+`,e.jsxs(t.li,{children:["El Terapagos Mexicano, jafet0022, and everyone who contributed to ",e.jsx(t.a,{href:"https://discord.com/channels/285269328469950464/1257368429107875992",children:"this Discord thread"}),", which is filled with lots of retail Emerald egg research and info."]}),`
+`]})]})}function m(i={}){const{wrapper:t}={...u(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(j,{...i})}):j(i)}function n(i,t){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{m as default,p as frontmatter};
---
+++ assets/Using IPS Patches with Luma and Citra-COHL6VqO.js
@@ -0,0 +1,37 @@
+import{u as l,j as e}from"./index-BAc4Igsm.js";const c={title:"How to Use IPS Patches on a 3DS or Emulator (Luma3DS, Azahar, Citra)",navDrawerTitle:"IPS Patches",description:"Learn how to apply IPS patches like instant text and no outlines using Luma3DS, Azahar, or Citra. Includes 3DS setup, emulator mods, and a merge tool.",slug:"misc-3ds-ips-luma-citra",category:"3DS Tools",tag:"cfw"};function i(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",ul:"ul",...l(),...s.components},{IpsMerger:t,YouTubeVideo:r}=n;return t||o("IpsMerger"),r||o("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"What are IPS Patches?"}),`
+`,e.jsx(n.p,{children:"IPS patches are small files that change how a game runs without modifying the full ROM. Common patches include instant text, no outlines, or minor fixes. You can use them on real 3DS hardware or in emulators."}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"A 3DS with CFW"})," OR ",e.jsx(n.a,{href:"https://github.com/azahar-emu/azahar/releases/latest",children:"Azahar"})]}),`
+`,e.jsxs(n.li,{children:["The ",e.jsx(n.code,{children:"code.ips"})," file that you want to use"]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"3DS Instructions"}),`
+`,e.jsx(n.h3,{children:"Update Luma Settings"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Boot the console while holding ",e.jsx(n.code,{children:"Select"}),"."]}),`
+`,e.jsx(n.li,{children:'Select "Enable game patches".'}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Install the Patch"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Find your game's title ID ",e.jsx(n.a,{href:"http://www.3dsdb.com/",children:"using 3dsdb"}),"."]}),`
+`,e.jsxs(n.li,{children:["Place your ",e.jsx(n.code,{children:"code.ips"})," patch in ",e.jsx(n.code,{children:"/luma/titles/_title ID_/code.ips"}),"."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["For example, Ultra Moon would be at ",e.jsx(n.code,{children:"/luma/title/00040000001B5100/code.ips"}),"."]}),`
+`,e.jsx(n.h2,{children:"Azahar Instructions"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Right click your game in Azahar."}),`
+`,e.jsx(n.li,{children:'Select "Open Mods Location" to open the mods folder.'}),`
+`,e.jsxs(n.li,{children:["Copy your ",e.jsx(n.code,{children:"code.ips"})," into the mods folder."]}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Azahar patch video guide"}),`
+`,e.jsx(r,{src:"https://www.youtube.com/embed/klBkGQDaQR8?si=CbGCOKODlGu27Du4&start=241"}),`
+`,e.jsx(n.h2,{children:"Merging patches"}),`
+`,e.jsx(t,{}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Click the ",e.jsx(n.code,{children:"Upload"})," button above."]}),`
+`,e.jsx(n.li,{children:"Select your IPS patches."}),`
+`,e.jsxs(n.li,{children:["Click ",e.jsx(n.code,{children:"Download"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Suggested Patches"}),`
+`,e.jsxs(n.p,{children:["Use Dream Radar without needing a cartridge! ",e.jsx(n.a,{href:"/dream-radar-patches",children:"Dream Radar No-Cart Patch"})]}),`
+`,e.jsxs(n.p,{children:["Use Transporter Offline! ",e.jsx(n.a,{href:"/transporter-patches",children:"Offline Transporter Patch"})]})]})}function h(s={}){const{wrapper:n}={...l(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}function o(s,n){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,c as frontmatter};

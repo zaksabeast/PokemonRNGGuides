cd68ad4b98a80d7e98f68a67bf438a9e57fa158c
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-05-27 17:17:31 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@75d78aacf9b563fc9be1e8b1e54b64a8cec6fb8d 🚀

---
+++ assets/No Dolphin Patch-G6MnLQFe.js
@@ -0,0 +1,38 @@
+import{u as r,j as t}from"./index-DU0402vx.js";const c={title:"No Dolphin Patch",description:"Use this patch to RNG the Wishmaker Jirachi without needing the Dolphin emulator.",slug:"no-dolphin-patch",category:"GBA Tools",tag:"emu",addedOn:"2025-04-03"};function h(i){const e={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...r(),...i.components},{YouTubeVideo:n}=e;return n||s("YouTubeVideo"),t.jsxs(t.Fragment,{children:[t.jsx(e.h2,{children:"Tools"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsxs(e.li,{children:["A GBA emulator, such as ",t.jsx(e.a,{href:"https://mgba.io/downloads.html#desktop-os-1",children:"mGBA"})]}),`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"https://github.com/zaksabeast/Multiboot-Jirachi-Patches/releases/latest",children:"No Dolphin Patch"})}),`
+`,t.jsxs(e.li,{children:["A Jirachi multiboot ROM (e.g. the Wishmaker ",t.jsx(e.code,{children:"client.bin"}),")"]}),`
+`]}),`
+`,t.jsx(e.h2,{children:"Intro"}),`
+`,t.jsx(e.p,{children:"This patch lets you obtain a multibooted Wishmaker Jirachi without using Dolphin. It bypasses the GameCube connection requirement, allowing you to use only a GBA emulator."}),`
+`,t.jsx(e.h2,{children:"Video Guide"}),`
+`,t.jsx(n,{src:"https://www.youtube.com/embed/X_XV3EgzSIc?si=-K4fZ4m0Nl-LIRV6"}),`
+`,t.jsx(e.h2,{children:"Step 1: Download the Patch"}),`
+`,t.jsxs(e.ol,{children:[`
+`,t.jsxs(e.li,{children:["Download ",t.jsx(e.code,{children:"patches.zip"})," from the ",t.jsx(e.a,{href:"https://github.com/zaksabeast/Multiboot-Jirachi-Patches/releases/latest",children:"No Dolphin Patch releases page"}),"."]}),`
+`,t.jsxs(e.li,{children:["Unzip the zip file to get a ",t.jsx(e.code,{children:"patches"})," folder."]}),`
+`,t.jsx(e.li,{children:"Choose the appropriate patch for your emulator using the table below."}),`
+`]}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Emulator"}),t.jsx(e.th,{children:"Multiboot ROM"}),t.jsx(e.th,{children:"Patch File"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"mGBA"}),t.jsx(e.td,{children:"Wishmaker Jirachi"}),t.jsx(e.td,{children:t.jsx(e.code,{children:"mgba/client.ips"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"mGBA"}),t.jsx(e.td,{children:"Meteor Jirachi"}),t.jsx(e.td,{children:t.jsx(e.code,{children:"mgba/client.2003_1112.ips"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"mGBA"}),t.jsx(e.td,{children:"Wishing Star Jirachi"}),t.jsx(e.td,{children:t.jsx(e.code,{children:"mgba/sample0519.ips"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Other"}),t.jsx(e.td,{children:"Wishmaker Jirachi"}),t.jsx(e.td,{children:t.jsx(e.code,{children:"other/client.ips"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Other"}),t.jsx(e.td,{children:"Meteor Jirachi"}),t.jsx(e.td,{children:t.jsx(e.code,{children:"other/client.2003_1112.ips"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Other"}),t.jsx(e.td,{children:"Wishing Star Jirachi"}),t.jsx(e.td,{children:t.jsx(e.code,{children:"other/sample0519.ips"})})]})]})]}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{children:`Note: If an 'other' patch doesn't work, try the 'mGBA' patch, even for non-mGBA emulators.
+`})}),`
+`,t.jsx(e.h2,{children:"Step 2: Apply the Patch"}),`
+`,t.jsxs(e.ol,{children:[`
+`,t.jsxs(e.li,{children:["Go to ",t.jsx(e.a,{href:"https://www.marcrobledo.com/RomPatcher.js/",children:"this IPS patching website"}),"."]}),`
+`,t.jsxs(e.li,{children:["Upload your multiboot rom as the ROM file (e.g. ",t.jsx(e.code,{children:"client.bin"}),") and patch file."]}),`
+`,t.jsxs(e.li,{children:["Click ",t.jsx(e.code,{children:"Apply Patch"})," and download the patched ROM (e.g. ",t.jsx(e.code,{children:"client.patched.bin"}),")."]}),`
+`]}),`
+`,t.jsx(e.h2,{children:"Step 3: Load the Patched ROM"}),`
+`,t.jsxs(e.ol,{children:[`
+`,t.jsxs(e.li,{children:["Rename your Ruby or Sapphire save to match the patched ROM name (e.g. ",t.jsx(e.code,{children:"client.patched.sav"}),")."]}),`
+`,t.jsx(e.li,{children:"Load the patched ROM in your emulator."}),`
+`,t.jsx(e.li,{children:"Confirm the distribution succeeds."}),`
+`]}),`
+`,t.jsx(e.h2,{children:"Step 4: Verify Jirachi in Your Save"}),`
+`,t.jsxs(e.ol,{children:[`
+`,t.jsx(e.li,{children:"Once the transfer completes, rename the save file back to its original name."}),`
+`,t.jsx(e.li,{children:"Load your Ruby or Sapphire ROM in your emulator."}),`
+`,t.jsx(e.li,{children:"Check your party to make sure the Jirachi is there."}),`
+`]}),`
+`,t.jsx(e.p,{children:"Congrats! You've now got your Jirachi without needing Dolphin!"})]})}function l(i={}){const{wrapper:e}={...r(),...i.components};return e?t.jsx(e,{...i,children:t.jsx(h,{...i})}):h(i)}function s(i,e){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{l as default,c as frontmatter};

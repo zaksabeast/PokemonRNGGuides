d77e889b304f12301b9f025a7bdb0e6cd31149e0
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-05-27 17:35:18 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@e85b531ece8e5af8896df42d8a8fc3949e023a81 🚀

---
+++ assets/Egg RNG With Masuda Method or Shiny Charm-DQt85Qik.js
@@ -0,0 +1,66 @@
+import{u as i,j as e}from"./index-BqEsMc3A.js";const a=[{title:"Omega Ruby and Alpha Sapphire Egg RNG with Masuda Method or Shiny Charm",navDrawerTitle:"MM/SC Egg RNG",description:"Learn how to RNG eggs from the Daycare in Omega Ruby and Alpha Sapphire for shiny, high-IV Pokémon.",slug:"retail-oras-egg-mmsc",category:"Omega Ruby and Alpha Sapphire",tag:"cfw"},{title:"X and Y Egg RNG with Masuda Method or Shiny Charm",navDrawerTitle:"MM/SC Egg RNG",description:"Learn how to RNG eggs from the Daycare in X and Y for shiny, high-IV Pokémon.",slug:"retail-xy-egg-mmsc",category:"X and Y",tag:"cfw",canonical:"retail-oras-egg-mmsc"}];function t(r){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.pre,{children:e.jsx(n.code,{children:`This method differs from using without the Masuda Method or Shiny Charm. Here, you RNG for the egg once but waiting times for a target frame can be longer.
+`})}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"A 3DS with PokeReader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Enter RNG info"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Enter your game version and TSV."}),`
+`,e.jsx(n.li,{children:"Enter the initial seed."}),`
+`,e.jsx(n.li,{children:"Tick the Shiny Charm box if you have it."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`To RNG the egg with a specific ESV that's not yours, click on Edit TSV List and add TSV(s). Then check the Other TSVs Shiny box.
+
+Input YOUR TSV in the upper right so the RNG works correctly.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Input parent information"}),`
+`,e.jsx(n.p,{children:"Enter details according to the parents you are using. Tick the Masuda Method box if parents are of different languages."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`The Pokemon's region doesn't affect anything. It's the language that matters for the Masuda Method.
+`})}),`
+`,e.jsx(n.p,{children:"For a Ditto and genderless Pokémon pairing, Ditto will be the female."}),`
+`,e.jsx(n.h2,{children:"Step 3: Egg Seeds, Filters, and Target Frame"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Input the current egg seeds of your game."}),`
+`,e.jsx(n.li,{children:"Provide info for the egg you want in Filters."}),`
+`,e.jsx(n.li,{children:"Tick the Shiny Only box for a shiny egg."}),`
+`,e.jsxs(n.li,{children:["Pause the game with ",e.jsx(n.code,{children:"Start + Select"}),"."]}),`
+`,e.jsx(n.li,{children:"Enter your current frame and increase the frame range for more frames to search."}),`
+`,e.jsx(n.li,{children:`Select "To Accept" for the first egg or "To Reject" if not. We recommend accepting the first egg to verify frame -1. The Consider Delay counter will reset to 0 if you're rejecting the egg.`}),`
+`,e.jsx(n.li,{children:"Finally, click on Calculate for target frames. Increase frame range and recalculate if there are no results."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 4: RNGing the Egg Traits"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Choose your target frame."}),`
+`,e.jsxs(n.li,{children:["If you need an odd frame from an even one, or vice versa, save the game once after unpausing with ",e.jsx(n.code,{children:"Start"}),"."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Remember:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Frames advance by 2 outside of battles."}),`
+`,e.jsx(n.li,{children:"Frames are either always even or always odd."}),`
+`,e.jsx(n.li,{children:"Save once to switch from even to odd frames and vice versa."}),`
+`,e.jsx(n.li,{children:"The first egg collected/rejected is NOT the egg you RNG’d for."}),`
+`,e.jsx(n.li,{children:"Eggs are generated when the previous egg was collected/rejected and won't change."}),`
+`,e.jsx(n.li,{children:"Upon collecting/rejecting an egg, the NEXT egg will have the traits you RNG’d for."}),`
+`,e.jsx(n.li,{children:"Frame -1 signifies the current egg’s traits."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 4: RNGing the Egg Traits (Exluding PID/ESV)"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["In the game, talk to the daycare man until a specific dialogue appears.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Accepting the egg: “You’ll be wanting it, won’t you?” (ORAS) or “You do want it, don’t you?” (XY). Wait on Yes."}),`
+`,e.jsx(n.li,{children:"Rejecting the first egg: “Well then, I'll hang on to it. Thank you!” (XY/ORAS) after selecting No from previous dialogue."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"Start + Select"})," to pause the game near your target frame, then press ",e.jsx(n.code,{children:"Select"})," to advance frame by frame."]}),`
+`,e.jsxs(n.li,{children:["At your target frame, hold ",e.jsx(n.code,{children:"A"})," to both unpause the game and collect/reject the egg."]}),`
+`,e.jsx(n.li,{children:"The egg collected now will match the frame -1 from the 3DSRNGTool."}),`
+`]}),`
+`,e.jsx(n.p,{children:"Collect the NEXT egg, check or hatch it to confirm your Pokémon. Congratulations!"}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Verify the TSV in the upper right of 3DSRNGTool matches the TSV shown in the main RNG view."}),`
+`,e.jsx(n.li,{children:"Ensure you're using the correct daycare view for ORAS if the first egg collected doesn't match frame -1."}),`
+`,e.jsx(n.li,{children:"Egg seeds should change after each egg collected/rejected. If not, ensure you have the latest PokeReader and your game is updated to the latest version."}),`
+`]})]})}function s(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{s as default,a as frontmatter};

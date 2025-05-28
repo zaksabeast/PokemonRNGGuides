b7f40bad4c4500282b5c67b19a0531e9ea3eb28e
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-28 12:58:49 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@a199964dabd3e34329940914c245b72df71beadc 🚀

---
+++ assets/Find SID with Feebas-BhdT5m8W.js
@@ -0,0 +1,30 @@
+import{u as c,j as e}from"./index-DwuEbFCg.js";const a=[{title:"使用丑丑鱼查找 SID",description:"如何在《绿宝石》中利用丑丑鱼查找你的里ID (SID)。",slug:"zh-emerald-sid-feebas",translation:{enSlug:"emerald-sid-feebas",language:"zh"}},{title:"使用丑丑鱼查找 SID",description:"如何在《红宝石 / 蓝宝石》中利用丑丑鱼查找你的里ID (SID)。",slug:"zh-rs-sid-feebas",translation:{enSlug:"rs-sid-feebas",language:"zh"}}];function d(s){const n={a:"a",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...c(),...s.components},{Alert:t,Gen3Sid:r,ShowIf:i,YouTubeVideo:h}=n;return t||l("Alert"),r||l("Gen3Sid"),i||l("ShowIf"),h||l("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsxs(i,{slug:"/zh-rs-sid-feebas",children:[e.jsx(t,{showIcon:!0,type:"warning",message:"红宝石和蓝宝石有一个更简单的方法！",description:e.jsxs(e.Fragment,{children:["请参考",e.jsx(n.a,{href:"/gen3-sid",children:"本指南"}),`的"If you're playing Ruby or Sapphire"部分进行操作！`]})}),e.jsx(n.p,{children:"此方法仅适用于："}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"电池已耗尽。"}),`
+`,e.jsx(n.li,{children:"你没有更改武斗镇的流行话题。"}),`
+`]})]}),`
+`,e.jsxs(i,{slug:"/zh-emerald-sid-feebas",children:[e.jsx(n.p,{children:"此方法仅适用于："}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"电池已耗尽或游戏时间少于一天。"}),`
+`,e.jsx(n.li,{children:"你没有更改武斗镇的流行话题。"}),`
+`]})]}),`
+`,e.jsx(n.h2,{children:"视频指南"}),`
+`,e.jsx(h,{src:"https://www.youtube.com/embed/nOCbSmMRXLA"}),`
+`,e.jsx(n.h2,{children:"查找丑丑鱼钓点"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"前往武斗镇。"}),`
+`,e.jsx(n.li,{children:"与宝可梦中心正上方的 NPC 交谈，获取当前的流行话题。"}),`
+`,e.jsxs(n.li,{children:["使用 ",e.jsx(n.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator/",children:"mucksw 的丑丑鱼钓点计算器"})," 获取可能的丑丑鱼种子列表。"]}),`
+`,e.jsx(n.li,{children:"在计算器提供的钓点逐一垂钓，测试每个种子。"}),`
+`]}),`
+`,e.jsx(n.h2,{children:"查找你的 SID"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"在下方工具中输入你的丑丑鱼种子，计算可能的 SID。"}),`
+`,e.jsx(n.li,{children:"通过乱数异色宝可梦来确认你的真实 SID。"}),`
+`]}),`
+`,e.jsx(i,{slug:"/zh-rs-sid-feebas",children:e.jsx(r,{game:"rs"})}),`
+`,e.jsx(i,{slug:"/zh-emerald-sid-feebas",children:e.jsx(r,{game:"emerald"})}),`
+`,e.jsx(n.h2,{children:"特别鸣谢"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["感谢 Lincoln、HappyLappy 和 Shao 提供的",e.jsx(n.a,{href:"https://github.com/HappyLappy1/Lappy-Python-Scripts/tree/main/RSE_Trendy_Saying_2_SID",children:"Python SID 查找工具"})," 。"]}),`
+`,e.jsxs(n.li,{children:["感谢 mucksw 制作的",e.jsx(n.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator",children:" 丑丑鱼钓点计算器"}),"。"]}),`
+`,e.jsx(n.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})}function x(s={}){const{wrapper:n}={...c(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(d,{...s})}):d(s)}function l(s,n){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{x as default,a as frontmatter};

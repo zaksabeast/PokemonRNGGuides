d901dd52f6c37e454b7dba0fa021d26eb994b3ee
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-29 23:25:01 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@6c5c1f4d5d2d861035be45ccab4b45ca0bc3d8e8 🚀

---
+++ assets/Basic Rules of RNG-CVxYhlC-.js
@@ -0,0 +1,18 @@
+import{u as a,j as e}from"./index-n5_mBNut.js";const o={title:"Emerald RNG Info",navDrawerTitle:"RNG Info",description:"Learn how to advance the RNG and improve stability in Pokemon Emerald for consistent results.",slug:"e-tips-rng",category:"Emerald",tag:"any"};function i(t){const n={a:"a",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...a(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"RNG Methods"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"For stationary Pokemon: Method 1."}),`
+`,e.jsx(n.li,{children:"For wild encounters: Method H-2."}),`
+`]}),`
+`,e.jsxs(n.p,{children:["The different methods for wild encounters are caused by ",e.jsx(n.a,{href:"/gba-vblank",children:"vblank"}),", though H-2 is the most commonly seen."]}),`
+`,e.jsx(n.h3,{children:"Painting RNG"}),`
+`,e.jsxs(n.p,{children:["Painting RNG is a great method to obtain any desired initial seed. It's a more advanced technique and is covered in ",e.jsx(n.a,{href:"/emerald-painting-rng",children:"this guide"}),"."]}),`
+`,e.jsx(n.h2,{children:"Speeding Up RNG Advances"}),`
+`,e.jsx(n.h3,{children:"Battle Video"}),`
+`,e.jsx(n.p,{children:"This method helps with seeding by preserving the RNG's state as an initial seed. Engage in a battle at the Battle Frontier and save it as a video. The video retains the seed from the battle, allowing you to set it as the initial seed when loading. For example, if your target is 1 million advances away, go up to 950k, save a battle video, and upon loading, you'll start at 950k advances."}),`
+`,e.jsx(n.p,{children:"While this method is helpful, it's a bit outdated compared to Painting RNG. However, it's useful when you need to hit a specific advance multiple times."}),`
+`,e.jsx(n.h3,{children:"In-Battle"}),`
+`,e.jsx(n.p,{children:"Being in battle advances RNG twice as fast. This is particularly helpful for wild Pokemon RNG."}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]})}function l(t={}){const{wrapper:n}={...a(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{l as default,o as frontmatter};

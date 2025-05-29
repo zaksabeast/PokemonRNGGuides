b549650e3ceb5742e3aedaacf69d5e9d3c2741f6
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-29 15:06:07 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@cc7c69cbda7c53c6c90f5b693c44d615e0020b30 🚀

---
+++ assets/Egg-FVXAfViV.js
@@ -0,0 +1,38 @@
+import{u as s,j as n}from"./index-B0iah-4Q.js";const h=[{title:"孵化乱数",description:"如何在培育屋进行孵化乱数",slug:"zh-emulator-hgss-egg",translation:{enSlug:"emulator-hgss-egg",language:"zh"}}];function i(l){const e={a:"a",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...s(),...l.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h2,{children:"所需工具"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`,n.jsxs(e.li,{children:["适用于 ",n.jsx(e.a,{href:"/desmume-setup",children:"Desmume"})," 的乱数工具"]}),`
+`,n.jsx(e.li,{children:"能够进入培育屋，以及亲代信息"}),`
+`]}),`
+`,n.jsx(e.h2,{children:"介绍"}),`
+`,n.jsx(e.p,{children:"本指南将教你如何推进乱数以获取理想的宝可梦。假设你已经了解四代乱数的基础知识，如初始种子设定和帧数推进。Masuda 法（不同语言亲代孵蛋提高闪率）首次在本世代引入，但并非必需。"}),`
+`,n.jsx(e.h2,{children:"准备工作"}),`
+`,n.jsx(e.p,{children:"准备工作很简单："}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"打开 PokeFinder，进入“第四世代 => 孵化乱数 => 检索器”。"}),`
+`,n.jsx(e.li,{children:"选择你的个人资料，输入亲代信息。第一个放入培育屋的宝可梦为亲代 A，后放入的为亲代 B。"}),`
+`,n.jsx(e.li,{children:"将亲代放入培育屋后，存档。"}),`
+`]}),`
+`,n.jsx(e.h2,{children:"通过蛋帧数推进命中 PID"}),`
+`,n.jsx(e.p,{children:"最简单的方法是选择不需要额外推进的目标蛋："}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"在 PokeFinder 中，将 Advance 范围设为 0-0。"}),`
+`,n.jsx(e.li,{children:"选择目标属性，如异色、性格、性别、特性等。"}),`
+`,n.jsx(e.li,{children:"生成符合条件的结果。"}),`
+`,n.jsx(e.li,{children:"选择你的目标帧。"}),`
+`,n.jsx(e.li,{children:"进行初始种子乱数。"}),`
+`]}),`
+`,n.jsx(e.p,{children:"如果目标蛋的帧数大于 0，需要在生成蛋前手动推进帧数，可使用以下方法："}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"拒绝一个蛋推进1帧。"}),`
+`,n.jsx(e.li,{children:"拨打短裤小子五郎/ゴロウ/Gorō（位于30号道路）的电话推进 2 帧。"}),`
+`]}),`
+`,n.jsx(e.p,{children:"生成蛋后不需要领取，存档于培育屋老爷爷面前。"}),`
+`,n.jsx(e.h2,{children:"命中个体"}),`
+`,n.jsx(e.p,{children:"个体乱数的操作方式较为标准。在 PokeFinder 中仅设置你的个体筛选条件。考虑亲代个体继承情况，但获取高个体通常较容易。"}),`
+`,n.jsx(e.p,{children:"找到目标后，命中你的目标推进值，并按照常规方法（鹦鹉等）推进帧数。"}),`
+`,n.jsx(e.p,{children:"一切完成后，领取蛋，即可获得你的目标宝可梦。"}),`
+`,n.jsx(e.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"中文翻译：炫夜鳞、Hakuhiro/白希洛。"}),`
+`]})]})}function c(l={}){const{wrapper:e}={...s(),...l.components};return e?n.jsx(e,{...l,children:n.jsx(i,{...l})}):i(l)}export{c as default,h as frontmatter};

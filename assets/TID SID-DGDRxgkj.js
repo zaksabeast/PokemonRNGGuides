ff5c9a209a59a488df21218d94905891f0002249
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-29 23:52:06 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@43145b102d15a70d042d88e4415cb6c53af34f07 🚀

---
+++ assets/TID SID-DGDRxgkj.js
@@ -0,0 +1,14 @@
+import{u as r,j as e}from"./index-DBDaE7ej.js";const s={title:"HeartGold and SoulSilver TID/SID RNG",navDrawerTitle:"TID/SID RNG",description:"Learn how to obtain your desired Trainer ID (TID) and Secret ID (SID) combo in HeartGold and SoulSilver.",slug:"emulator-hgss-tid-sid",category:"HeartGold and SoulSilver",isRoughDraft:!0,tag:"emu"};function o(t){const n={code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Step 1: Understanding TID and SID"}),`
+`,e.jsx(n.p,{children:"Here's the basics about getting your desired PID."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Know that uppercase and lowercase can affect delay issues."}),`
+`,e.jsx(n.li,{children:"Consider special cases that may not be covered in this guide."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`If you need to find pairs faster, look for a special tool that can help.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Using Special Tools"}),`
+`,e.jsx(n.p,{children:"If you’re having trouble finding pairs with PokeFinder, here’s what to do."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Find and use a specific tool to help locate TID/SID pairs."}),`
+`,e.jsx(n.li,{children:"Follow the instructions from that tool."}),`
+`]})]})}function a(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{a as default,s as frontmatter};

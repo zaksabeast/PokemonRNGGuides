368d0a37232292fb5cab990039729b316cb58109
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-05-29 23:22:09 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@07ddb28931b30ed9ab3b56b661d70ed7acb03bad 🚀

---
+++ assets/Mirage Island-BNhcIHXh.js
@@ -0,0 +1,24 @@
+import{u as l,j as e}from"./index-CZpHfpHD.js";const c=[{title:"Mirage Island in Emerald",navDrawerTitle:"Mirage Island",description:"Learn how to access Mirage Island in Pokémon Emerald by catching a Pokémon with the correct PID using RNG manipulation.",slug:"emerald-mirage-island",category:"Emerald",tag:"any",addedOn:"2025-04-11"},{title:"Mirage Island in Ruby and Sapphire",navDrawerTitle:"Mirage Island",description:"Learn how to access Mirage Island in Pokémon Emerald by catching a Pokémon with the correct PID using RNG manipulation.",slug:"rs-mirage-island",category:"Ruby and Sapphire",tag:"any",addedOn:"2025-04-11"}];function o(i){const n={h2:"h2",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...l(),...i.components},{Gen3MirageIsland:a,Gist:r,ShowIf:s}=n;return a||t("Gen3MirageIsland"),r||t("Gist"),s||t("ShowIf"),e.jsxs(e.Fragment,{children:[e.jsx(r,{children:e.jsx(n.p,{children:"Gist: Access Mirage Island by catching a Pokémon with the right PID."})}),`
+`,e.jsx(n.h2,{children:"Mirage Island"}),`
+`,e.jsx(n.p,{children:"Accessing Mirage Island is the only way to obtain Liechi Berry. The island is only accessible if the player team has a Pokémon with PID matching a specific pattern. That pattern changes every day, and depends on the number of days since the game savefile was created."}),`
+`,e.jsx(n.h2,{children:"Step 1: Find the number of days since the game savefile was created"}),`
+`,e.jsx(n.p,{children:"If you have a dead battery, this step is not needed."}),`
+`,e.jsx(n.p,{children:"Talk to the scientist in the Mossdeep Space Center to know the number of rockets launched. This indicates the number of weeks since your savefile was created."}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Emerald/mirageIslandRocketLaunched.png",alt:"Scientist in Mossdeep Space Center"})}),`
+`,e.jsx(n.h2,{children:"Step #2: Catch a Pokémon matching the PID pattern"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Fill the form below to view results."}),`
+`,e.jsx(n.li,{children:"For live battery, choose any result 6 or more days in the future."}),`
+`,e.jsx(n.li,{children:"Catch a Pokémon with Method-1 PID generation on your chosen result's advance. Kecleon is recommended, but any Method-1 Pokémon will do."}),`
+`]}),`
+`,e.jsx(s,{slug:"/emerald-mirage-island",children:e.jsx(a,{game:"emerald"})}),`
+`,e.jsx(s,{slug:"/rs-mirage-island",children:e.jsx(a,{game:"rs"})}),`
+`,e.jsx(n.h2,{children:"Step #3: Visit Mirage Island"}),`
+`,e.jsx(n.p,{children:"Attempt to visit Mirage Island on Route 130 for the next 6 days. One of the days should have Mirage Island accessible."}),`
+`,e.jsx(n.p,{children:"Enjoy your Liechi Berry!"}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Emerald/mirageIslandLiechiBerry.png",alt:"Scientist in Mossdeep Space Center"})}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Guide and tools: RainingChain."}),`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]})}function h(i={}){const{wrapper:n}={...l(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(o,{...i})}):o(i)}function t(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,c as frontmatter};

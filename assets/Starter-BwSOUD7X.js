78db7e95f2f36d821255e622ed6c32c5b3ff413a
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-06-01 13:18:11 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@95fbf0a930fc969759105062207efd45f6c09a59 🚀

---
+++ assets/Starter-BwSOUD7X.js
@@ -0,0 +1,34 @@
+import{u as l,j as e}from"./index-DqbXmijk.js";const c={title:"RNG de Iniciales en Cristal",description:"Learn how to RNG shiny starters in Pokémon Crystal.",slug:"es-gen2-starters",translation:{enSlug:"gen2-starters",language:"es"}};function r(a){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",ul:"ul",...l(),...a.components},{Gen2PokemonRng:o,YouTubeVideo:i}=n;return o||s("Gen2PokemonRng"),i||s("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Herramientas"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Videoguía"}),`
+`,e.jsx(i,{src:"https://www.youtube.com/embed/1PFQv-S8a48?si=7N6RoITaJg0lyrPR"}),`
+`,e.jsx(n.h2,{children:"Guía escrita"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Guarda frente al inicial que quieras, en el caso en que necesites reiniciar el juego."}),`
+`,e.jsxs(n.li,{children:["Elige tu inicial y detente cuando veas el cuadro de texto ",e.jsx(n.code,{children:"Jugador recibió a Pokémon"})," con la flecha parpadeando para continuar."]}),`
+`,e.jsxs(n.li,{children:["Ve a la pantalla de RNG del Pokereader y espera hasta que deje de mostrar ",e.jsx(n.code,{children:"Finding ADIV Index"})," y ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pausa el Pokereader con ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsx(n.li,{children:'Escribe la información del Pokereader dentro de la herramienta de RNG aquí abajo y cuando termines, presiona "Generar".'}),`
+`,e.jsxs(n.li,{children:["Avanza el juego con ",e.jsx(n.code,{children:"L"})," hasta que los avances desplegados del Pokereader coincidan con lo generado en la columna ",e.jsx(n.code,{children:"Avance"})," aquí abajo."]}),`
+`,e.jsx(n.li,{children:"(Opcional) Crea un estado de guardado si es que estas en emulador, en el caso que necesites reiniciar el juego."}),`
+`,e.jsxs(n.li,{children:["Cuando alcances tu avance deseado, presiona ",e.jsx(n.code,{children:"A"})," para aceptar el Pokémon."]}),`
+`,e.jsx(n.li,{children:"Si conseguiste a tu Pokémon, ¡Felicidades! si no, ¡Sigue intentando! no tardará mucho."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"RNG Tool"}),`
+`,e.jsx(o,{type:"starter",language:"es"}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Creditos"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Todas las personas que han investigado a lo largo de los años la Gen 2: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast por elaborar el método actual de rng y construir la herramienta."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, y Lord Timothy en Discord por ayudar realizando pruebas."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano por ayudar con la traducción al español."}),`
+`]})]})}function t(a={}){const{wrapper:n}={...l(),...a.components};return n?e.jsx(n,{...a,children:e.jsx(r,{...a})}):r(a)}function s(a,n){throw new Error("Expected component `"+a+"` to be defined: you likely forgot to import, pass, or provide it.")}export{t as default,c as frontmatter};

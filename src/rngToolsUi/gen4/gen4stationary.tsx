69872cea1f901e6b70a5146f4aaabd406bb34e15
sorendog <shineingstar21@hotmail.com>
2025-06-04 22:04:59 -0500

    yay more changes

--- src/rngToolsUi/gen4/gen4stationary.tsx
+++ src/rngToolsUi/gen4/gen4stationary.tsx
@@ -4,9 +4,11 @@ import {
   FormikNumberInput,
   FormikSelect,
   ResultColumn,
+  RngToolForm,
   RngToolSubmit,
 } from "~/components";
 import { getPkmFilterFields, pkmFilterSchema } from "~/components/pkmFilter";
+import { toOptions } from "~/utils/options";
 import React from "react";
 import { maxIvs, minIvs } from "~/types/ivs";
 import {
@@ -16,76 +18,87 @@ import {
 } from "~/rngToolsUi/shared/ivColumns";
 import { z } from "zod";
 import { HexSchema } from "~/utils/number";
+import { startCase } from "lodash-es";
 
 type Result = FlattenIvs<Gen4SPokemon>;
 
-export enum GameVersion {
-   Diamond = 0,
-   Pearl,
-   Platinum,
-   HeartGold,
-   SoulSilver}
+ const LeadAbilities = [
+    "CutecharmF",
+    "CutecharmM",
+    "Synchronize",
+    "None"
+] as const;
+const LeadAbilitiesOpts = toOptions(LeadAbilities, startCase);
 
-export enum StaticEncounterId {
-    Turtwig = 0,
-    Chimchar,
-    Piplup,
-    Cyndaquil,
-    Chikorita,
-    Totodile,
-    Charmander,
-    Squirtle,
-    Bulbasaur,
-    Treecko,
-    Mudkip,
-    Torchic,
-    Omanyte,
-    Kabuto,
-    Aerodactyl,
-    Lileep,
-    Anorith,
-    Cranidos,
-    Shieldon,
-    Eevee,
-    Porygon,
-    Togepi,
-    Riolu,
-    Drifloon,
-    Spiritomb,
-    Rotom,
-    Lugia,
-    HoOh,
-    Dialga,
-    Palkia,
-    Giratina,
-    Regice,
-    Regirock,
-    Registeel,
-    Uxie,
-    Azelf,
-    Heatran,
-    Regigigas,
-    Mesprit,
-    Cresselia,
-    Zapdos,
-    Articuno,
-    Moltres,
-    Tentacool,
-    Dratini,
-    Tyrogue,
-    Mareep,
-    Wooper,
-    Slugma,
-    MrMime,
-    Abra,
-    Ekans,
-    Raikou,
-    Entei,
-    Suicune,
-    Voltorb,
-    Snorlax,
-}
+ const GameVersion = [
+  "Diamond",
+  "Pearl",
+  "Platinum",
+  "HeartGold",
+  "SoulSilver",
+ ] as const;
+ const GameVersionOpts = toOptions(GameVersion, startCase);
 
+ const  StaticEncounterId = [
+  "Turtwig",
+  "Chimchar",
+  "Piplup",
+  "Cyndaquil",
+  "Chikorita",
+  "Totodile",
+  "Charmander",
+  "Squirtle",
+  "Bulbasaur",
+  "Treecko",
+  "Mudkip",
+  "Torchic",
+  "Omanyte",
+  "Kabuto",
+  "Aerodactyl",
+  "Lileep",
+  "Anorith",
+  "Cranidos",
+  "Shieldon",
+  "Eevee",
+  "Porygon",
+  "Togepi",
+  "Riolu",
+  "Drifloon",
+  "Spiritomb",
+  "Rotom",
+  "Lugia",
+  "HoOh",
+  "Dialga",
+  "Palkia",
+  "Giratina",
+  "Regice",
+  "Regirock",
+  "Registeel",
+  "Uxie",
+  "Azelf",
+  "Heatran",
+  "Regigigas",
+  "Mesprit",
+  "Cresselia",
+  "Zapdos",
+  "Articuno",
+  "Moltres",
+  "Tentacool",
+  "Dratini",
+  "Tyrogue",
+  "Mareep",
+  "Wooper",
+  "Slugma",
+  "MrMime",
+  "Abra",
+  "Ekans",
+  "Raikou",
+  "Entei",
+  "Suicune",
+  "Voltorb",
+  "Snorlax",
+] as const;
+ const StaticEncounterIdOpts = toOptions(StaticEncounterId, startCase);
 
 const columns: ResultColumn<Result>[] = [
   {
@@ -117,8 +130,9 @@ const Validator = z
     sid: z.number().int().min(0).max(65535),
     initial_advances: z.number(),
     max_advances: z.number(),
-    game: z.nativeEnum(GameVersion),
-    encounter: z.nativeEnum(StaticEncounterId),
+    game: z.enum(GameVersion).nullable(),
+    encounter: z.enum(StaticEncounterId).nullable(),
+    lead: z.enum(LeadAbilities).nullable(),
   })
   .merge(pkmFilterSchema);
 
@@ -128,8 +142,9 @@ const initialValues: FormState = {
   seed: 0,
   tid: 0,
   sid: 0,
-  game: 0,
-  encounter: 0,
+  game: "Diamond",
+  encounter: "Turtwig",
+  lead: "None",
   initial_advances: 0,
   max_advances: 100,
   filter_shiny: false,
@@ -169,23 +184,37 @@ const fields: Field[] = [
     input: (
       <FormikSelect<FormState, "encounter">
         name="encounter"
-        options={toOptions(StaticEncounterId)}
+        options={StaticEncounterIdOpts}
       />
     ),
   },
   {
     label: "Game",
-    input: <FormikSelect<FormState, ""> name="game" options={GameVersion} />,
+    input: (
+      <FormikSelect<FormState, "game"> name="game" options={GameVersionOpts} />
+    ),
+  },
+  {
+    label: "Lead Ability",
+    input: (
+      <FormikSelect<FormState, "lead"> name="lead" options={LeadAbilitiesOpts} />
+    ),
   },
   ...getPkmFilterFields(),
 ];
 
-export const filter_4static = () => {
+export const Filter_4static = () => {
   const [results, setResults] = React.useState<Result[]>([]);
 
   const onSubmit = React.useCallback<RngToolSubmit<FormState>>(async (opts) => {
     const results = await rngTools.filter_4static({
-      ...opts,
+      tid: opts.tid,
+      sid: opts.sid,
+      game:opts.game,
+      encounter:opts.encounter,
+      lead:opts.lead,
+      initial_advances:opts.initial_advances,
+      max_advances:opts.max_advances,
       filter: {
         shiny: opts.filter_shiny,
         nature: opts.filter_nature,
@@ -194,8 +223,8 @@ export const filter_4static = () => {
         min_ivs: opts.filter_min_ivs,
         max_ivs: opts.filter_max_ivs,
         stats: null,
-      },
-    });
+      }
+    }, opts.seed,);
 
     setResults(results.map(flattenIvs));
   });

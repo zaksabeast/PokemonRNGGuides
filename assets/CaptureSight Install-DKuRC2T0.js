e528ab9143b840c66e93db3d801fd8457caaceea
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-06-08 18:40:27 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@5d8f775fdecd721a04ff26b39c8186f83b0f56c0 🚀

---
+++ assets/CaptureSight Install-DKuRC2T0.js
@@ -0,0 +1,50 @@
+import{q as l,j as e}from"./index-DDrxH26t.js";const t={title:"Installer CaptureSight",navDrawerTitle:"Installer Capture Sight",description:"Apprenez a installer CaptureSight sur votre Switch pour faciliter la manipulation de RNG et attraper des Pokemon parfaits !",slug:"fr-install-capturesight",translation:{enSlug:"install-capturesight",language:"fr"}};function r(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",ul:"ul",...l(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Prérequis"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Une Switch tournant sous Atmosphere (",e.jsx(n.a,{href:"https://switch.hacks.guide/",children:"Guide d'installation du CFW, en anglais"}),")"]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Étape 1 : Installer Tesla Overlay"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/WerWolv/nx-ovlloader/releases",children:"nx-ovlloader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/WerWolv/Tesla-Menu/releases",children:"Tesla Menu"})}),`
+`]}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Télécharger la dernière version de Tesla Menu et nx-ovlloader."}),`
+`,e.jsx(n.li,{children:"Décompresser les archives et fusionner les dossiers à la racine de la carte SD."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Étape 2 : Installer CaptureSight"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/zaksabeast/CaptureSight/releases",children:"CaptureSight"})}),`
+`]}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Télécharger ",e.jsx(n.code,{children:"capturesight.ovl"})," et le placer dans le dossier ",e.jsx(n.code,{children:"/switch/.overlays"})," sur la carte SD de la Switch."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Utiliser CaptureSight"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Démarrer un jeu Pokémon compatible. (Diamant Étincelant/Perle Scintillante, Épée/Bouclier ou Légendes Arceus)"}),`
+`,e.jsxs(n.li,{children:["Appuyer sur ",e.jsx(n.code,{children:"L + croix 'bas' + clic joystick droit"})," pour faire apparaître le menu de l'overlay Tesla."]}),`
+`,e.jsx(n.li,{children:"Ouvrir CaptureSight."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Résolution de problèmes"}),`
+`,e.jsx(n.h3,{children:"dmnt:cht is not running"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Si vous voyez cette erreur, c'est qu'il y a un problème avec Atmosphere. Suivez ",e.jsx(n.a,{href:"https://switch.hacks.guide/extras/updating.html",children:"ce guide"})," pour mettre à jour le CFW."]}),`
+`]}),`
+`,e.jsx(n.h3,{children:"No cheat process! Is a game running? Is something else using dmnt:cht?"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Soyez certain d'ouvrir le jeu AVANT d'afficher CaptureSight. Assurez-vous de ne pas utiliser sysbot-base en même temps, il y a conflit."}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Unsupported game update!"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Le jeu n'est pas à jour. Cela peut aussi arriver si CaptureSight n'est pas à la dernière version."}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Unsupported game! Title Id: <title_id>"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"CaptureSight n'est pas compatible avec ce jeu. Les seuls jeux supportés sont :"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Diamant Étincelant & Perle Scintillante."}),`
+`,e.jsx(n.li,{children:"Épée & Bouclier."}),`
+`,e.jsx(n.li,{children:"Légendes : Arceus."}),`
+`]}),`
+`]}),`
+`]})]})}function a(s={}){const{wrapper:n}={...l(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(r,{...s})}):r(s)}export{a as default,t as frontmatter};

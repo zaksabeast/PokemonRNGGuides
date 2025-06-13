1925798c6869a0b4d436dcbfbebfd3076f0f2ea8
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-06-13 15:07:06 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@a7bde34792f89532e5d211ede371a5da46a2f6b6 🚀

---
+++ assets/Starters-BIsVKuXj.js
@@ -0,0 +1,28 @@
+import{q as x,j as e}from"./index-Dd5lOLc6.js";const m=[{title:"HeartGold and Soulsilver Starter RNG",navDrawerTitle:"Starter RNG",description:"Learn how to RNG starters in HeartGold and SoulSilver for shiny, high-IV Pokémon.",slug:"retail-hgss-starter",category:"HeartGold and SoulSilver",tag:"retail",isRoughDraft:!0,hideFromNavDrawer:!0},{title:"Diamond, Pearl, and Platinum Starter RNG",navDrawerTitle:"Starter RNG",description:"Learn how to RNG starters in Diamond, Pearl, and Platinum for shiny, high-IV Pokémon.",slug:"retail-dppt-starter",category:"Diamond, Pearl, and Platinum",tag:"retail",isRoughDraft:!0,hideFromNavDrawer:!0}];function u(r){const t={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...x(),...r.components},{Alert:s,CalibrateStarter4:a,GetStarter4:l,PickStarter4:h,Starter4ConsoleSetDateString:o,Starter4Setup:d,Step:i,Stepper:c}=t;return s||n("Alert"),a||n("CalibrateStarter4"),l||n("GetStarter4"),h||n("PickStarter4"),o||n("Starter4ConsoleSetDateString"),d||n("Starter4Setup"),i||n("Step"),c||n("Stepper"),e.jsxs(e.Fragment,{children:[e.jsx(s,{showIcon:!0,type:"info",message:"This guide is interactive and updates instructions based on your settings!"}),`
+`,e.jsxs(c,{titles:["Set up","Pick a starter","Get the starter","Calibrate"],children:[e.jsxs(i,{step:0,children:[e.jsx(t.h2,{children:"Set up"}),e.jsx(t.p,{children:"Choose the game you're playing below then continue to the next step."}),e.jsx(s,{showIcon:!0,type:"info",message:"Platinum support is coming soon."}),e.jsx(d,{})]}),e.jsxs(i,{step:1,children:[e.jsx(t.h2,{children:"Pick a starter"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Fill out the tool below to find a starter that suits you."}),`
+`,e.jsx(t.li,{children:"If you can't find a starter, increase the min and max delays."}),`
+`]}),e.jsx(t.p,{children:"Shiny high IV starters are highly unlikely. Try for a max of 3 perfect IVs instead."}),e.jsx(h,{})]}),e.jsxs(i,{step:2,children:[e.jsx(t.h2,{children:"Prepare"}),e.jsxs(t.ol,{children:[`
+`,e.jsxs(t.li,{children:["Set your console date to ",e.jsx(t.strong,{children:e.jsx(o,{format:"date"})}),"."]}),`
+`,e.jsxs(t.li,{children:["Start setting your console time to ",e.jsx(t.strong,{children:e.jsx(o,{format:"time"})}),", but don't confirm it just yet."]}),`
+`]}),e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsx(t.tr,{children:e.jsx(t.th,{children:"Confirm time screen"})})}),e.jsx(t.tbody,{children:e.jsx(t.tr,{children:e.jsx(t.td,{children:e.jsx(t.img,{src:"/images/HeartGold-SoulSilver/SetTimeScreen.jpg",alt:"Confirm time screen"})})})})]}),e.jsx(t.h2,{children:"Hit your target"}),e.jsxs(t.ol,{children:[`
+`,e.jsxs(t.li,{children:["Simultaneously press ",e.jsx(t.code,{children:"A"})," to set the console time and start the timer below."]}),`
+`,e.jsx(t.li,{children:"Exit the settings on your console and restart your console."}),`
+`,e.jsxs(t.li,{children:["When the first timer ends, press ",e.jsx(t.code,{children:"A"})," to start the game."]}),`
+`,e.jsx(t.li,{children:"Get to the screen where you load your save."}),`
+`,e.jsx(t.li,{children:"If you've calibrated at least once and need to flip your delay, go back to the title screen one extra time before loading your save."}),`
+`,e.jsxs(t.li,{children:["Immediately press ",e.jsx(t.code,{children:"A"})," when the second timer to goes off."]}),`
+`,e.jsx(t.li,{children:"Button mash to get your starter as fast as possible."}),`
+`]}),e.jsx(l,{})]}),e.jsxs(i,{step:3,children:[e.jsx(t.h2,{children:"Calibrate"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Check your Pokemon stats."}),`
+`,e.jsx(t.li,{children:"If it matches your target, congratulations! You RNG'd your starter!"}),`
+`,e.jsx(t.li,{children:'If not, type your Pokemon info into the form below and click "Generate".'}),`
+`,e.jsx(t.li,{children:'Click "Calibrate" on the starter with the smallest delay offset (should be the first result).'}),`
+`,e.jsx(t.li,{children:`If the "Flip Delay" column is checked, you'll need to view the title screen one extra time before loading your save.`}),`
+`,e.jsx(t.li,{children:"Try to hit your target again."}),`
+`]}),e.jsx(t.p,{children:"In Diamond and Pearl it is possible for the delay to randomly switch from even to odd, or vice versa. When this happens, you'll need to keep trying until you get the target."}),e.jsx(a,{})]})]}),`
+`,e.jsx(t.h2,{children:"Credits"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Zaksabeast, Sorendog, EzPz, and RainingChain for various contributions to this tool"}),`
+`,e.jsxs(t.li,{children:["Thanks to all ",e.jsx(t.a,{href:"https://github.com/Admiral-Fish/PokeFinder",children:"PokeFinder"})," contributors, whose work this tool is built upon."]}),`
+`]})]})}function p(r={}){const{wrapper:t}={...x(),...r.components};return t?e.jsx(t,{...r,children:e.jsx(u,{...r})}):u(r)}function n(r,t){throw new Error("Expected component `"+r+"` to be defined: you likely forgot to import, pass, or provide it.")}export{p as default,m as frontmatter};

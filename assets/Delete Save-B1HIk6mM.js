9995a545b12c53edafcb73547e071bffa9059fcd
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-06-18 19:39:25 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@026a2c8a44e7f00eafa3f6fce21a0efdadfb170c 🚀

---
+++ assets/Delete Save-B1HIk6mM.js
@@ -0,0 +1,31 @@
+import{q as i,j as e}from"./index-DcEfuLT1.js";const o={title:"How to delete Pokemon save files",navDrawerTitle:"Delete Save Files",description:"How to delete a Pokemon save file to start a new game.",slug:"delete-pokemon-save",category:["GBA Tools","NDS Tools","3DS Tools","Switch Tools"],tag:"any"};function s(t){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"This page has directions for how to delete Pokemon save files for every cartridge/console to start a new save, including how to redeem multiple Manaphy Eggs from Pokemon Ranger."}),`
+`,e.jsx(n.h2,{children:"Delete a save file from a GB/GBC cartridge"}),`
+`,e.jsx(n.p,{children:"Start a new game and save. This will overwrite the older save file."}),`
+`,e.jsx(n.h2,{children:"Delete a save file from a GBA cartridge"}),`
+`,e.jsx(n.p,{children:"Start a new game and save. This will overwrite the older save file."}),`
+`,e.jsxs(n.p,{children:["You can also hold ",e.jsx(n.code,{children:"Up + Select + B"})," on the title screen."]}),`
+`,e.jsx(n.p,{children:"Alternatively, you can use homebrew on a Custom Firmware Wii to erase the save file."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://wii.hacks.guide/",children:"Guide for how to install Custom Firmware on a Wii"}),"."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/FIX94/gba-link-cable-dumper",children:"GBA Link Cable Dumper"}),"."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Delete a save file from a DS cartridge"}),`
+`,e.jsxs(n.p,{children:["Hold ",e.jsx(n.code,{children:"Up + Select + B"})," on the title screen."]}),`
+`,e.jsx(n.h3,{children:"Pokemon Ranger"}),`
+`,e.jsx(n.p,{children:"For Pokemon Ranger, deleting the save file will not make it possible to claim another Manaphy Egg."}),`
+`,e.jsxs(n.p,{children:["However, with a ",e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"Custom Firmware 3DS"})," it is possible to reset the cartridge entirely."]}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["With Pokemon Ranger inserted, go to ",e.jsx(n.a,{href:"https://github.com/Steveice10/FBI/releases",children:"FBI"})," -> titles -> game."]}),`
+`,e.jsxs(n.li,{children:["Go to erase save data and press ",e.jsx(n.code,{children:"A"}),"."]}),`
+`,e.jsxs(n.li,{children:["If you use ",e.jsx(n.a,{href:"https://github.com/BernardoGiordano/Checkpoint/releases",children:"Checkpoint"})," to back up the save right before transferring the Manaphy Egg you can easily get the Manaphy Egg repeatedly."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Delete a save file from a 3DS cartridge"}),`
+`,e.jsxs(n.p,{children:["Hold ",e.jsx(n.code,{children:"Up + B + X"})," on the title screen."]}),`
+`,e.jsx(n.h2,{children:"Delete a save file from an Nintendo Switch"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Go to the HOME Menu and select "System Settings".'}),`
+`,e.jsx(n.li,{children:'Scroll down and select "Data Management".'}),`
+`,e.jsx(n.li,{children:'Choose "Delete Save Data".'}),`
+`,e.jsx(n.li,{children:"Select the game title you want to delete the save data for."}),`
+`,e.jsx(n.li,{children:"Confirm the deletion when prompted."}),`
+`]})]})}function r(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{r as default,o as frontmatter};

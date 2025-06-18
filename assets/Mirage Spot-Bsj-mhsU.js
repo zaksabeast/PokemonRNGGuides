c3fca8f44c6a4cda0df379ec2ff43f38df85cf3d
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-06-18 16:16:11 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@ed58407c0e51eecdb8658f1cb54c16b46ef6dbee 🚀

---
+++ assets/Mirage Spot-Bsj-mhsU.js
@@ -0,0 +1,43 @@
+import{q as a,j as e}from"./index-Tdog0yQT.js";const d={title:"Omega Ruby and Alpha Sapphire Mirage Spot RNG",navDrawerTitle:"Mirage Spot RNG",description:"Learn how to RNG access to any Mirage Spots in Omega Ruby and Alpha Sapphire for rare encounters and shiny Pokémon.",slug:"oras-mirage-spots",category:"Omega Ruby and Alpha Sapphire",tag:"cfw",addedOn:"2025-03-14"};function i(o){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",strong:"strong",ul:"ul",...a(),...o.components},{OrAsMirageSpot:t,YouTubeVideo:r}=n;return t||s("OrAsMirageSpot"),r||s("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Video Guide"}),`
+`,e.jsx(r,{src:"https://www.youtube.com/embed/fifrq7Vjoe0"}),`
+`,e.jsx(n.h2,{children:"Advancing the Console Date"}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"3DS emulator (Azahar/Lime3DS/Citra):"})}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Open the "System" tab in the emulator settings.'}),`
+`,e.jsx(n.li,{children:'Change "Clock" to "Fixed Time".'}),`
+`,e.jsx(n.li,{children:'Change "Startup time" to your desired date.'}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"Physical console with time penalities removed:"})}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Follow our ",e.jsx(n.a,{href:"/oras-remove-time-penalty",children:"guide to remove time penalties"}),"."]}),`
+`,e.jsx(n.li,{children:"Set your console date like normal."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If you have problems with the above methods, start your game, save, then try again.
+`})}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"Physical console without time penalities removed (untested):"})}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Set your console date to be the day before your target date."}),`
+`,e.jsx(n.li,{children:"Start the game and save."}),`
+`,e.jsx(n.li,{children:"Wait 24 hours, then play the game again."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"The RNG"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:`Start the game and view PokeReader's "Mirage Spot" screen.`}),`
+`,e.jsx(n.li,{children:"Enter the Mirage Spot seed, your TID, and console's save date into the RNG Tool below."}),`
+`,e.jsx(n.li,{children:`Press "Generate" to get the Mirage Spot's details, and choose a Mirage Spot you want to visit.`}),`
+`,e.jsx(n.li,{children:"Turn off your game and set your console's date to the Mirage Spot's date."}),`
+`,e.jsx(n.li,{children:"Start the game and go to the Mirage Spot you chose. The Mirage Spot will update when you start to fly."}),`
+`]}),`
+`,e.jsx(t,{}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Shiny_Sylveon for helping find information about Mirage Spots."}),`
+`,e.jsxs(n.li,{children:["theSlayer and suloku for associating Mirage Spot Ids with names and Pokemon in ",e.jsx(n.a,{href:"https://projectpokemon.org/home/forums/topic/37218-mirage-islands-in-oras/page/4/",children:"this ProjectPokemon thread"}),"."]}),`
+`,e.jsxs(n.li,{children:["Falo for reverse engineering how the Mirage Spot Id is generated ",e.jsx(n.a,{href:"https://projectpokemon.org/home/forums/topic/37218-mirage-islands-in-oras/page/4/",children:"in the same ProjectPokemon thread"}),"."]}),`
+`,e.jsx(n.li,{children:"Zaksabeast for reverse engineering how Mirage Spot RNG is seeded and reseeded on save load, as well as building the tool in this guide."}),`
+`,e.jsx(n.li,{children:"Rebel on Discord for helping test."}),`
+`]})]})}function h(o={}){const{wrapper:n}={...a(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(i,{...o})}):i(o)}function s(o,n){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,d as frontmatter};
---
+++ assets/Stationary v2 Emu-CekBro3E.js
@@ -0,0 +1,25 @@
+import{q as a,j as e}from"./index-Tdog0yQT.js";const s={title:"FireRed and LeafGreen Static RNG",navDrawerTitle:"Static RNG",description:"Static v2 RNG",slug:"emulator-frlg-stationary",category:"FireRed and LeafGreen",isRoughDraft:!0,tag:"emu"};function t(r){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...a(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/mgba-setup",children:"mGBA with lua scripts"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`,e.jsxs(n.li,{children:["Vlad's doc for delays: ",e.jsx(n.a,{href:"https://docs.google.com/document/d/11jxX7bTSGf2vzkdHr-lEpz2bLRI_u7zRbXaOtbasTp0",children:"https://docs.google.com/document/d/11jxX7bTSGf2vzkdHr-lEpz2bLRI_u7zRbXaOtbasTp0"})]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Prepare for RNG"}),`
+`,e.jsx(n.p,{children:"These scripts improve frame counting and reduce mistakes. With some technique, you can hit any frame without calibration. You need to know your Pokémon's basic delay to do this. I've provided as much data as possible. If something is missing, feel free to contact me."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open your emulator and save in front of the target."}),`
+`,e.jsx(n.li,{children:'Open PokeFinder, select "Gen 3" => "Stationary" (Method 1). Select your profile.'}),`
+`,e.jsx(n.li,{children:"In the delay box, check it and add the delay for your target."}),`
+`,e.jsx(n.li,{children:"Search for a target."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2: Advance Frames"}),`
+`,e.jsx(n.p,{children:"The further you go in frames, the higher the wrong delay may get. To avoid this, do a Trainer Card Flip near your target frame to refresh and get the basic delay."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open the menu and go to your Trainer Card."}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"A"})," to flip the card."]}),`
+`,e.jsx(n.li,{children:"Do this around 2-3k frames before your target frame."}),`
+`]}),`
+`,e.jsx(n.p,{children:"You can advance many frames (about 1 million every 10 seconds with the Teachy TV)."}),`
+`,e.jsx(n.h2,{children:"Step 3: Target the Frame"}),`
+`,e.jsxs(n.p,{children:["After the card flip, go to the last input. When close to the target frame, pause the emulator (",e.jsx(n.code,{children:"ctrl + p"}),"), and advance frame by frame (",e.jsx(n.code,{children:"ctrl + n"}),") to the frame shown by PokeFinder. Press ",e.jsx(n.code,{children:"A"})," and unpause at the same time."]}),`
+`,e.jsx(n.p,{children:"Congrats! You're done!"})]})}function i(r={}){const{wrapper:n}={...a(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{i as default,s as frontmatter};

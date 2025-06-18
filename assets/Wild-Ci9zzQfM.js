71d28619d0f9b42f84889a4059524834722b3f9b
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-06-18 01:25:18 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@271c8c000c749e56ccb3325824c7a00acb1df663 🚀

---
+++ assets/Wild-Ci9zzQfM.js
@@ -0,0 +1,22 @@
+import{q as o,j as e}from"./index-CHQRQ4fA.js";const r={title:"Brilliant Diamond and Shining Pearl Wild RNG",navDrawerTitle:"Wild RNG",description:"RNG wild Pokémon in Brilliant Diamond and Shining Pearl using CFW.",slug:"cfw-bdsp-wild",category:"Brilliant Diamond and Shining Pearl",isRoughDraft:!0,tag:"cfw"};function i(s){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",ul:"ul",...o(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"For this guide, we'll assume you know how to use CaptureSight and the Chatot site for setup. You'll also need to understand how to advance your states and manage noise using the Wild tab of the Chatot site."}),`
+`,e.jsx(n.p,{children:"This guide focuses on basic information about delays and specific setups. Other details are covered in the basic knowledge guides."}),`
+`,e.jsx(n.p,{children:"It's important to set up a gender rate if you're not hunting for a genderless Pokémon. The opposite applies if you are hunting for a genderless Pokémon. The generation checks for this, so skipping it may lead to different Nature results for Pokémon."}),`
+`,e.jsx(n.h3,{children:"Pure Wild (Honey)"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Delay: 1 (2 for Unown)."}),`
+`,e.jsxs(n.li,{children:["Notes: Use Honey. Sweet Scent may lead to instability. Press ",e.jsx(n.code,{children:"A"}),' on the "Use" message at the right frame.']}),`
+`]}),`
+`,e.jsxs(n.p,{children:["For encounter slots, visit: ",e.jsx(n.a,{href:"https://www.pokemon-element-sh.fr/encounters-slots",children:"https://www.pokemon-element-sh.fr/encounters-slots"}),". The site is in French but is reliable. If it doesn't work, check: ",e.jsx(n.a,{href:"https://www.pokebip.com/page/jeuxvideo/dossier_shasse/encounter_slot",children:"https://www.pokebip.com/page/jeuxvideo/dossier_shasse/encounter_slot"}),"."]}),`
+`,e.jsxs(n.p,{children:["If you're a ",e.jsx(n.a,{href:"/pokefinder",children:"PokeFinder"})," user (Gen 8 Tab => Wild), the Encounter Tables are available. Fill in necessary information (like the route) to see available Pokémon. Remember to edit your profile for swarms or similar events."]}),`
+`,e.jsx(n.h3,{children:"Fishing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Delay: 1 (double-check recommended)."}),`
+`,e.jsxs(n.li,{children:["Notes: Press ",e.jsx(n.code,{children:"A"}),' on the "Landed a Pokémon!" message.']}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: The whole generation is being worked on. Some factors generate when using the rod, while PID and others generate once hooked. This will help with hunting rare Encounter Slots.
+`})}),`
+`,e.jsx(n.h2,{children:"Surfing"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Delay: 2."}),`
+`,e.jsx(n.li,{children:"Notes: Same as basic wild; use Honey instead of Sweet Scent."}),`
+`]})]})}function a(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}export{a as default,r as frontmatter};

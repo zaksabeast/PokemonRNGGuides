743a1eeeacbdeef4b87b30138bd7f86f8360d694
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-06-21 19:00:10 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@70fba81cfdb7e5ff0c22808bd7c2a8cf5cd2baa2 🚀

---
+++ assets/How to Install PCalc-KFl0SyWn.js
@@ -0,0 +1,84 @@
+import{q as r,j as e}from"./index-Bl9_Ks_5.js";const a={title:"How to Install PCalc",description:"Learn how to install PCalc on your 3DS to assist with RNG in Pokémon games like X/Y, ORAS, Sun/Moon, and USUM.",slug:"misc-3ds-installing-pcalc",category:"3DS Tools",tag:"cfw",hideFromNavDrawer:!0};function t(o){const n={a:"a",br:"br",code:"code",em:"em",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",strong:"strong",ul:"ul",...r(),...o.components},{Alert:s}=n;return s||l("Alert"),e.jsxs(e.Fragment,{children:[e.jsx(s,{showIcon:!0,type:"warning",message:"It's strongly advised to use PokeReader instead!",description:e.jsxs(e.Fragment,{children:["PokeReader is newer, easier to set up, and actively maintained!"," ",e.jsx(n.a,{href:"/install-pokereader",children:"Get it here!"})]})}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["A 3DS with CFW (Custom Firmware). Check out ",e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"https://3ds.hacks.guide/"})," for instructions on installing CFW."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Update Luma"}),`
+`,e.jsx(n.p,{children:"Ensure your installed version of Luma3ds is 13.0 or later. Older versions aren't compatible with NTR."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/LumaTeam/Luma3DS/releases",children:"Download Luma3ds"}),".",e.jsx(n.br,{}),`
+`,"Download the ",e.jsx(n.code,{children:"boot.firm"})," and overwrite the current ",e.jsx(n.code,{children:"boot.firm"})," on the root of the SD card."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Installing NTR"}),`
+`,e.jsxs(n.p,{children:["First, install NTR by getting ",e.jsx(n.a,{href:"https://github.com/Nanquitas/BootNTR/releases",children:"BootNTR Selector"}),"."]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"N3DS and N2DSXL:"})," Download and install BootNTR Selector.",e.jsx(n.br,{}),`
+`,"Download either of the CIAs that are ",e.jsx(n.em,{children:"not"})," Mode 3 and copy it to the SD card, then install using FBI."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"O3DS and O2DS:"})," Download and install ",e.jsx(n.em,{children:"BOTH"})," BootNTR Selector and BootNTR Selector Mode 3.",e.jsx(n.br,{}),`
+`,"Mode 3 is for Ultra Sun/Ultra Moon and Sun/Moon ONLY. Download one Mode 3 CIA and one regular CIA. Copy both CIAs to the SD card and install using FBI."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"To install a CIA using FBI, launch FBI on the console, then navigate to the CIA file on the SD card and press `A` on it to install.\n"})}),`
+`,e.jsx(n.p,{children:"Launch BootNTR Selector and choose default for settings."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Version 3.6 (replacing 3.4 and 3.5) is the only version that works for plugins, so choose it every time you boot NTR."}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"For O3DS and O2DS only:"})," Then launch BootNTR Selector Mode 3 and repeat the steps above."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2: Installing PCalc"}),`
+`,e.jsx(n.p,{children:"After installing NTR, the next step is to install the PCalc plugin."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/downloads/pcalc/pcalc-usum.zip",children:"Ultra Sun/Ultra Moon"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/downloads/pcalc/pcalc-sm.zip",children:"Sun/Moon"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/downloads/pcalc/pcalc-oras.zip",children:"OR/AS"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/downloads/pcalc/pcalc-xy.zip",children:"X/Y"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/downloads/pcalc/pcalc-tport.zip",children:"Transporter"})}),`
+`]}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Download the .zip for the version you want and unzip its contents."}),`
+`,e.jsx(n.li,{children:"Move the plugin folder from the zip to the root of the SD card. Merge and overwrite contents if prompted."}),`
+`,e.jsxs(n.li,{children:["You should now have a ",e.jsx(n.code,{children:"Plugin"})," folder on the root of the SD card and two/four/six/eight folders inside it (depends on which PCalcs you downloaded) with a ",e.jsx(n.code,{children:"cheat.plg"})," in each."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: Launching PCalc"}),`
+`,e.jsx(n.p,{children:"Launch BootNTR Selector and then the application of your choice."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"O3DS and O2DS Only:"})," Launch BootNTR Selector Mode 3 for Ultra Sun/Ultra Moon and Sun/Moon ONLY.",e.jsx(n.br,{}),`
+`,"Press the home button to return to the home screen, then launch Ultra Sun/Ultra Moon or Sun/Moon. Mode 3 should close ONLY when prompted to launch the game."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"If the screen flashes green, congrats, you did it!"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"The PCalc Menu can be opened by pressing `X` + `Up` on the D-pad.\n\nThe NTR Menu can be opened by pressing `X` + `Y`. This is useful for taking screenshots; all screenshots are saved to the root of the SD card as .bmp files.\n"})}),`
+`,e.jsx(n.h2,{children:"Troubleshooting"}),`
+`,e.jsx(n.p,{children:"If you updated to 3DS version 11.15 and NTR isn’t working, make sure you have the latest BootNTR Selector and/or BootNTR Selector Mode 3, and a compatible Luma version for NTR."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Hold ",e.jsx(n.code,{children:"X"})," while launching to update easily."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"O3DS and O2DS:"})," Update BOTH versions of BootNTR Selector."]}),`
+`,e.jsx(n.li,{children:"Check the section at the beginning of this guide for updating Luma for NTR."}),`
+`]}),`
+`,e.jsxs(n.p,{children:["If using Ultra Sun/Ultra Moon or Sun/Moon and PCalc displays ",e.jsx(n.code,{children:"Init Seed"})," as 0:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["You are missing the v1.2 update for Sun and Moon or the v1.2 update for Ultra Sun and Ultra Moon.",e.jsx(n.br,{}),`
+`,"This can be downloaded from the Eshop or extracted from another console using CFW."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"If using XY/ORAS and PCalc isn't working:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Ensure you have the latest update for the game.",e.jsx(n.br,{}),`
+`,"XY is 1.5, ORAS is 1.4. The game version can be found at the continue screen.",e.jsx(n.br,{}),`
+`,"The update can be downloaded from the Eshop or extracted from another console using CFW."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"If the game is updated to the latest version and PCalc and/or NTR isn't loading:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Delete all NTR related files on the SD card.",e.jsx(n.br,{}),`
+`,"Delete any NTR.bin files on the root of the SD card.",e.jsx(n.br,{}),`
+`,"Delete these folders and their contents:",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"SD:/Nintendo 3DS/EBNTR/"}),`
+`,e.jsx(n.li,{children:"SD:/3ds/ntr/"}),`
+`,e.jsx(n.li,{children:"SD:/3ds/BootNTRSelector/"}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"Redownload the necessary NTR files by launching BootNTR Selector and BootNTRSelector Mode 3."}),`
+`]}),`
+`,e.jsx(n.p,{children:"About Pokemon Gen 6/7 Game Updates:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"If you have a firmware region changed console or emunand, this can be obtained from other sources, such as dumping your update from another console, emunand, or sysnand."}),`
+`,e.jsx(n.li,{children:"Pokemon game updates are not region locked."}),`
+`,e.jsx(n.li,{children:"Cartridge and digital updates are stored on the SD card and are console/emunand specific, so they must be installed on the console/emunand you are using the game with."}),`
+`]})]})}function d(o={}){const{wrapper:n}={...r(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}function l(o,n){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{d as default,a as frontmatter};

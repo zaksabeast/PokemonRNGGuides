4e2548c43abb5623b07ba5641a2522f3b0dd0552
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-06-26 11:28:21 -0500

    Update 3ds helper instructions

--- src/rngToolsUi/gen4/shared/metronomeNotice.tsx
+++ src/rngToolsUi/gen4/shared/metronomeNotice.tsx
@@ -5,20 +5,20 @@ export const Metronome3dsNotice = () => {
     <Alert
       showIcon
       type="info"
-      message="3DS users"
+      message="Using the 3DS Helper"
       description={
         <Flex vertical gap={8}>
           <Typography.Text>
-            Start the metronome before starting the timer. This will help you
-            hit your target more accurately.
+            1. Click "Start 3ds Helper" before starting the timer. This will
+            limit when you can start the timer.
           </Typography.Text>
           <Typography.Text>
-            If the timer doesn't start, you missed the metronome - try again.
+            2. Try to start the timer. If it doesn't start, you missed the safe
+            time range and need to try again.
           </Typography.Text>
           <Typography.Text>
-            If your calibration results in the next step consistently have
-            advance and delay offsets of 0, but second offset of 1, toggle the
-            "Metronome Offset" below.
+            3. If your calibration results on the next screen have second offset
+            of +1, update the "3ds Offset" to the next option and try again.
           </Typography.Text>
         </Flex>
       }

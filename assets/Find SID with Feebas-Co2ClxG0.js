1eae9643dc8553776261ac057174db4876220858
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-06-26 17:52:02 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@e6dbdc8835acd56de7f13fbef514b610802e1a63 🚀

---
+++ assets/Find SID with Feebas-Co2ClxG0.js
@@ -0,0 +1,30 @@
+import{v as d,j as e}from"./index-CY1FzXMB.js";const c=[{title:"Find SID with Feebas in Emerald",navDrawerTitle:"Find SID with Feebas",description:"How to find your Secret ID (SID) in Emerald using Feebas.",slug:"emerald-sid-feebas",category:"Emerald",tag:"retail",addedOn:"2025-03-03"},{title:"Find SID with Feebas in Ruby and Sapphire",navDrawerTitle:"Find SID with Feebas",description:"How to find your Secret ID (SID) in Ruby or Sapphire using Feebas.",slug:"rs-sid-feebas",category:"Ruby and Sapphire",tag:"retail"}];function a(i){const n={a:"a",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...d(),...i.components},{Alert:o,Gen3Sid:t,ShowIf:s,YouTubeVideo:l}=n;return o||r("Alert"),t||r("Gen3Sid"),s||r("ShowIf"),l||r("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsxs(s,{slug:"/rs-sid-feebas",children:[e.jsx(o,{showIcon:!0,type:"warning",message:"There's an easier method for Ruby and Sapphire!",description:e.jsxs(e.Fragment,{children:[`Follow the "If you're playing Ruby or Sapphire" section of`," ",e.jsx(n.a,{href:"/gen3-sid",children:"this guide"})," instead!"]})}),e.jsx(n.p,{children:"This method only works if:"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"You have a dead battery."}),`
+`,e.jsx(n.li,{children:"You haven't changed the Dewford Trend."}),`
+`]})]}),`
+`,e.jsxs(s,{slug:"/emerald-sid-feebas",children:[e.jsx(n.p,{children:"This method only works if:"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"You have a dead battery or have played for less than 1 day."}),`
+`,e.jsx(n.li,{children:"You haven't changed the Dewford Trend."}),`
+`]})]}),`
+`,e.jsx(n.h2,{children:"Video Guide"}),`
+`,e.jsx(l,{src:"https://www.youtube.com/embed/nOCbSmMRXLA"}),`
+`,e.jsx(n.h2,{children:"Find your Feebas Tiles"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Travel to Dewford."}),`
+`,e.jsx(n.li,{children:"Talk to the NPC directly above the Pokemon Center to get the Dewford Trend."}),`
+`,e.jsxs(n.li,{children:["Use ",e.jsx(n.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator/",children:"mucksw's Feebas Tile Calculator"})," to get a list of possible Feebas seeds."]}),`
+`,e.jsx(n.li,{children:"Test each seed by fishing in the tiles provided by the calculator."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Find your SID"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Use the Feebas seed with the tool below to find your possible SIDs."}),`
+`,e.jsx(n.li,{children:"Try to RNG a shiny Pokemon to confirm the correct SID."}),`
+`]}),`
+`,e.jsx(s,{slug:"/rs-sid-feebas",children:e.jsx(t,{game:"rs"})}),`
+`,e.jsx(s,{slug:"/emerald-sid-feebas",children:e.jsx(t,{game:"emerald"})}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Lincoln, HappyLappy, and Shao for ",e.jsx(n.a,{href:"https://github.com/HappyLappy1/Lappy-Python-Scripts/tree/main/RSE_Trendy_Saying_2_SID",children:"the Python SID Finder"})," this is based on."]}),`
+`,e.jsxs(n.li,{children:["mucksw for the ",e.jsx(n.a,{href:"https://mucksw.github.io/Feebas-Tile-Calculator",children:"Feebas Tile Calculator"}),"."]}),`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]})}function u(i={}){const{wrapper:n}={...d(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(a,{...i})}):a(i)}function r(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{u as default,c as frontmatter};

764c1f542fa1a3495de691c2ce216415274d32bd
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-06-27 23:07:01 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@33c76b2b9687152dc8bc4c04a66c7f201cf39ba3 🚀

---
+++ assets/RSE Wild RNG - Dry Battery Emulator-n9H2W_qP.js
@@ -0,0 +1,41 @@
+import{v as t,j as e}from"./index-Qwe0mBDb.js";const l={title:"Ruby and Sapphire Wild RNG",navDrawerTitle:"Wild RNG",description:"Learn how to RNG wild Pokémon in Ruby and Sapphire for shiny and high-IV results.",slug:"emulator-rs-wild",category:"Ruby and Sapphire",isRoughDraft:!0,tag:"emu"};function i(r){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...t(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"This guide covers Fixed Initial Seed in Gen 3, specifically for RS dry and Emerald."}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/mgba-setup",children:"mGBA with lua scripts"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Set Up PokeFinder"}),`
+`,e.jsx(n.p,{children:"Open PokeFinder with the correct profile and input the initial seed:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"0"})," for Emerald."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"5A0"})," for Ruby/Sapphire dry."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: To achieve dry Ruby/Sapphire, uncheck the real-time clock in VBA options.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Find Your Target"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Find a target and go to the location."}),`
+`,e.jsx(n.li,{children:"Save your game."}),`
+`,e.jsx(n.li,{children:"In PokeFinder (Gen 3 => Wild), select the route and hit the criteria you want. For Ruby/Sapphire, use Method H-1; for Emerald, use Method H-2 (you can hit alternate spreads as well) once you've selected your target."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 3: Calibrate"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Launch VBA and create regular save states."}),`
+`,e.jsxs(n.li,{children:["When close to your frame, do a calibration:",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Save state."}),`
+`,e.jsx(n.li,{children:"Note the frame you're on."}),`
+`,e.jsx(n.li,{children:"Encounter a Pokémon using Sweet Scent."}),`
+`,e.jsx(n.li,{children:"Search that spread with PokeFinder."}),`
+`,e.jsx(n.li,{children:'Calculate the delay and enter it into the "delay box."'}),`
+`,e.jsx(n.li,{children:"Regenerate your target to get your new frame."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 4: Capture Your Pokémon"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Go to your target frame to catch what you want."}),`
+`,e.jsx(n.li,{children:"If it doesn’t work, redo the calibration."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`You can copy from my FRLG guide for 90% of this information. This guide is before the update with Real's scripts.
+`})})]})}function s(r={}){const{wrapper:n}={...t(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{s as default,l as frontmatter};
---
+++ assets/Retail TID RNG-DC45NOUf.js
@@ -0,0 +1,23 @@
+import{v as d,j as e}from"./index-Qwe0mBDb.js";const u={title:"Ruby and Sapphire Retail TID RNG",navDrawerTitle:"TID RNG",description:"Learn how to obtain your desired Trainer ID (TID) and Secret ID (SID) combo in Ruby and Sapphire.",slug:"retail-rubysapphire-tid",category:"Ruby and Sapphire",tag:"retail"};function h(n){const t={code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...d(),...n.components},{RsTidSearcher:o,RsTidSidGenerator:s,RsTidTimer:a,Step:r,Stepper:l}=t;return o||i("RsTidSearcher"),s||i("RsTidSidGenerator"),a||i("RsTidTimer"),r||i("Step"),l||i("Stepper"),e.jsxs(e.Fragment,{children:[e.jsx(t.p,{children:"RNG manipulation in Gen 3 lets you get the exact Trainer ID (TID) and Secret ID (SID) you want. This is useful for things like knowing your SID to RNG for shiny Pokémon. This guide covers how to do it on a physical console for Ruby and Sapphire."}),`
+`,e.jsx(t.p,{children:`This guide assumes you have a dead battery in your Ruby
+& Sapphire cartridge. If you have a working battery, this method will not work.`}),`
+`,e.jsxs(l,{titles:["Find Target TID","RNG TID"],children:[e.jsxs(r,{step:0,children:[e.jsx(t.h2,{children:"Step 1: Find a Target TID"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Search for a desired TID using the tool below."}),`
+`,e.jsx(t.li,{children:`If you are not finding a result, you can increase the max advances to search
+through, though keep in mind it will take longer for each attempt.`}),`
+`,e.jsx(t.li,{children:"Select your target TID and continue to Step 2."}),`
+`]}),e.jsx(s,{})]}),e.jsxs(r,{step:1,children:[e.jsx(t.h2,{children:"Step 2: RNG your TID"}),e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Start the game."}),`
+`,e.jsxs(t.li,{children:["Start the timer below. When the first timer ends, reset the game using ",e.jsx(t.code,{children:"Start + Select + A + B"}),"."]}),`
+`,e.jsx(t.li,{children:"Play until the final screen (screenshot below)."}),`
+`,e.jsxs(t.li,{children:["Wait until the second timer ends, then press ",e.jsx(t.code,{children:"A"})," to generate your TID."]}),`
+`,e.jsx(t.li,{children:'Input the TID you got into the "Hit TID" of the tool below and click "Generate".'}),`
+`,e.jsx(t.li,{children:"Select the TID in the results to update the timer. In the case of multiple results, choose the one closest to your target."}),`
+`,e.jsx(t.li,{children:"Repeat the above steps to attempt to hit your target TID. If you're off by 1-2 advances, you don't need to calibrate. You can try again with the same timer settings."}),`
+`]}),e.jsx(t.p,{children:e.jsx(t.img,{src:"/images/Ruby-Sapphire/TID/RubyTIDFinalScreen.png",alt:"Final Screen"})}),e.jsx(a,{}),e.jsx(o,{})]})]}),`
+`,e.jsx(t.h2,{children:"Credits"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"EzPz for the tool logic and building the UI"}),`
+`,e.jsx(t.li,{children:"Shiny_Sylveon for writing the guide"}),`
+`,e.jsx(t.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]})}function p(n={}){const{wrapper:t}={...d(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(h,{...n})}):h(n)}function i(n,t){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{p as default,u as frontmatter};

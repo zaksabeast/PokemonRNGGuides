30443561c74528782744e0dd5d5c18eb3b9e2d12
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-03 14:53:35 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@dd0ea5572f14bd7fe062b14a925f3ffb37589242 🚀

---
+++ assets/Consistent Platinum RNG-BX8A5wRP.js
@@ -0,0 +1,10 @@
+import{v as o,j as e}from"./index-C_ocThL9.js";const r={title:"More consistent Platinum RNG",navDrawerTitle:"Consistent Platinum RNG",description:"New research for more consistent Platinum RNG—get shiny, high-IV Pokémon more easily.",slug:"consistent-platinum-rng",category:"Diamond, Pearl, and Platinum",tag:"info",addedOn:"2025-07-03"};function s(n){const t={h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.p,{children:"Platinum RNG is notoriously inconsistent because the game seems to advance the RNG randomly."}),`
+`,e.jsx(t.p,{children:"New research has found a way to make it much more consistent!"}),`
+`,e.jsx(t.h2,{children:"More consistent advances"}),`
+`,e.jsx(t.p,{children:"When the save loads, the game does a lot of extra RNG advances when the current time is after the last save date."}),`
+`,e.jsxs(t.p,{children:["To prevent these extra advances, set your time to the max console date (set your console date to ",e.jsx(t.strong,{children:"12/31/2099"})," on NDS or ",e.jsx(t.strong,{children:"12/31/2050"})," on 3DS) before starting your RNG."]}),`
+`,e.jsx(t.p,{children:"After that, you'll find that the game advances the RNG much more consistently, making it easier to hit your target."}),`
+`,e.jsx(t.h2,{children:"Credits"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"Zaksabeast for researching this"}),`
+`]})]})}function i(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(s,{...n})}):s(n)}export{i as default,r as frontmatter};

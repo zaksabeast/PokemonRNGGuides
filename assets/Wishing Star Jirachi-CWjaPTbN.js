66729643ca7950a7b2cd633ea3378eef5801c239
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-06 21:19:28 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@92db4ab1de2d4334b8276591e739b747e38266cc 🚀

---
+++ assets/Wishing Star Jirachi-CWjaPTbN.js
@@ -0,0 +1,44 @@
+import{x as s,j as d}from"./index-DX4HlVow.js";const l={title:"Ruby and Sapphire Wishing Star Jirachi RNG",navDrawerTitle:"Wishing Star Jirachi RNG",description:"Learn how to RNG the Wishing Star Jirachi from the Colosseum Bonus Disc in Ruby and Sapphire.",slug:"wishing-star-jirachi",category:"Ruby and Sapphire",addedOn:"2025-04-23",tag:"emu"};function t(n){const e={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...s(),...n.components},{YouTubeVideo:i}=e;return i||r("YouTubeVideo"),d.jsxs(d.Fragment,{children:[d.jsx(e.h2,{children:"Tools"}),`
+`,d.jsxs(e.ul,{children:[`
+`,d.jsx(e.li,{children:d.jsx(e.a,{href:"https://github.com/visualboyadvance-m/visualboyadvance-m/releases/latest",children:"VBA-M"})}),`
+`,d.jsx(e.li,{children:d.jsx(e.a,{href:"https://www.nirsoft.net/utils/run_as_date.html",children:"RunAsDate"})}),`
+`,d.jsxs(e.li,{children:["Wishing Star Multiboot Rom with the ",d.jsx(e.a,{href:"/no-dolphin-patch",children:"No Dolphin patch"})]}),`
+`]}),`
+`,d.jsx(e.h2,{children:"Video Guide"}),`
+`,d.jsx(i,{src:"https://www.youtube.com/embed/bw2XLZzsfdU?si=LUD28rVsZBDUzn0m"}),`
+`,d.jsx(e.h2,{children:"Intro"}),`
+`,d.jsx(e.p,{children:"Wishing Star was an event distributed in 2003 whose distribution rom can be found on and extracted from the Colosseum bonus disc, making it an extra special type of RNG."}),`
+`,d.jsx(e.p,{children:"It's seeded with the current date and time and generated immediately, making it one of the easiest RNGs you will ever do."}),`
+`,d.jsx(e.p,{children:"Wishing Star has better spreads than Wishmaker and Meteor, but it cannot be shiny."}),`
+`,d.jsx(e.h2,{children:"The RNG"}),`
+`,d.jsxs(e.ol,{children:[`
+`,d.jsx(e.li,{children:"Find a target spread in the list below."}),`
+`,d.jsx(e.li,{children:"Configure RunAsDate to match the time and date of your target spread."}),`
+`,d.jsx(e.li,{children:"Launch VBA with RunAsDate, and load the multiboot rom."}),`
+`,d.jsx(e.li,{children:'Load your Ruby or Sapphire save in the emulator with "File" > "Import" > "Battery File".'}),`
+`,d.jsx(e.li,{children:"Reset the game, and wait for the Jirachi to be distributed."}),`
+`,d.jsx(e.li,{children:"If Jirachi does not appear, retry the import and reset process."}),`
+`,d.jsxs(e.li,{children:["VBA will create a new save file matching the ROM name.",`
+`,d.jsxs(e.ul,{children:[`
+`,d.jsxs(e.li,{children:["Example: If the ROM is ",d.jsx(e.code,{children:"sample0519.gba"}),", a new file ",d.jsx(e.code,{children:"sample0519.sav"})," will be created."]}),`
+`]}),`
+`]}),`
+`,d.jsx(e.li,{children:"Load your Ruby or Sapphire game and import the new battery file."}),`
+`,d.jsx(e.li,{children:"Reset the emulator and load your save to confirm Jirachi is in your party."}),`
+`,d.jsx(e.li,{children:"Save your game so the Jirachi is saved to your Ruby or Sapphire save."}),`
+`]}),`
+`,d.jsx(e.p,{children:"Congrats! You have a Jirachi with the target spread."}),`
+`,d.jsx(e.h2,{children:"Troubleshooting"}),`
+`,d.jsxs(e.p,{children:["If your Jirachi has a PID of ",d.jsx(e.code,{children:"15b6163b"}),", the RTC was not set."]}),`
+`,d.jsx(e.p,{children:"Ensure you configured RunAsDate correctly."}),`
+`,d.jsx(e.h2,{children:"Target Spreads"}),`
+`,d.jsx(e.h3,{children:"Wishing Star spreads with the highest 31 IV count"}),`
+`,d.jsxs(e.table,{children:[d.jsx(e.thead,{children:d.jsxs(e.tr,{children:[d.jsx(e.th,{children:"Seed"}),d.jsx(e.th,{children:"Date Time"}),d.jsx(e.th,{children:"PID"}),d.jsx(e.th,{children:"IVs"})]})}),d.jsxs(e.tbody,{children:[d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"08c4"}),d.jsx(e.td,{children:"02/15/2000 19:29"}),d.jsx(e.td,{children:"a09ba2a2"}),d.jsx(e.td,{children:"31/31/31/17/30/31"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"5e33"}),d.jsx(e.td,{children:"01/16/2000 11:37"}),d.jsx(e.td,{children:"16c8ffa4"}),d.jsx(e.td,{children:"10/31/13/31/31/31"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"bf76"}),d.jsx(e.td,{children:"02/02/2000 18:36"}),d.jsx(e.td,{children:"5c31c17d"}),d.jsx(e.td,{children:"31/27/31/31/27/31"})]})]})]}),`
+`,d.jsx(e.h3,{children:"Wishing Star spreads with all IVs 25+"}),`
+`,d.jsxs(e.table,{children:[d.jsx(e.thead,{children:d.jsxs(e.tr,{children:[d.jsx(e.th,{children:"Seed"}),d.jsx(e.th,{children:"Date Time"}),d.jsx(e.th,{children:"PID"}),d.jsx(e.th,{children:"IVs"})]})}),d.jsxs(e.tbody,{children:[d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"1d0b"}),d.jsx(e.td,{children:"01/05/2000 03:37"}),d.jsx(e.td,{children:"1638285d"}),d.jsx(e.td,{children:"31/26/28/25/28/27"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"37f2"}),d.jsx(e.td,{children:"04/09/2000 16:48"}),d.jsx(e.td,{children:"ccd89921"}),d.jsx(e.td,{children:"29/25/28/31/31/30"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"5a11"}),d.jsx(e.td,{children:"01/15/2000 18:11"}),d.jsx(e.td,{children:"0f2248cb"}),d.jsx(e.td,{children:"29/26/25/26/26/28"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"7671"}),d.jsx(e.td,{children:"01/20/2000 18:51"}),d.jsx(e.td,{children:"feec4120"}),d.jsx(e.td,{children:"31/29/25/28/29/28"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"bf76"}),d.jsx(e.td,{children:"02/02/2000 18:36"}),d.jsx(e.td,{children:"5c31c17d"}),d.jsx(e.td,{children:"31/27/31/31/27/31"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"c9e5"}),d.jsx(e.td,{children:"02/04/2000 14:55"}),d.jsx(e.td,{children:"fc153744"}),d.jsx(e.td,{children:"31/25/29/31/25/25"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"cfa3"}),d.jsx(e.td,{children:"02/05/2000 15:37"}),d.jsx(e.td,{children:"ce160092"}),d.jsx(e.td,{children:"28/26/31/25/28/27"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"daf4"}),d.jsx(e.td,{children:"02/07/2000 15:48"}),d.jsx(e.td,{children:"dcfb9816"}),d.jsx(e.td,{children:"31/27/30/29/28/26"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"ea8a"}),d.jsx(e.td,{children:"05/11/2000 10:48"}),d.jsx(e.td,{children:"84b67156"}),d.jsx(e.td,{children:"27/25/31/31/31/29"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"ec03"}),d.jsx(e.td,{children:"05/11/2000 16:59"}),d.jsx(e.td,{children:"bde1f8e8"}),d.jsx(e.td,{children:"30/29/31/27/31/27"})]})]})]}),`
+`,d.jsx(e.h3,{children:"Wishing Star spreads with the highest 0 IV count"}),`
+`,d.jsxs(e.table,{children:[d.jsx(e.thead,{children:d.jsxs(e.tr,{children:[d.jsx(e.th,{children:"Seed"}),d.jsx(e.th,{children:"Date Time"}),d.jsx(e.th,{children:"PID"}),d.jsx(e.th,{children:"IVs"})]})}),d.jsxs(e.tbody,{children:[d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"137e"}),d.jsx(e.td,{children:"01/02/2000 22:46"}),d.jsx(e.td,{children:"e555d451"}),d.jsx(e.td,{children:"00/00/00/30/00/07"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"4fd7"}),d.jsx(e.td,{children:"01/14/2000 04:27"}),d.jsx(e.td,{children:"09461a5b"}),d.jsx(e.td,{children:"00/00/14/00/20/00"})]}),d.jsxs(e.tr,{children:[d.jsx(e.td,{children:"ddc0"}),d.jsx(e.td,{children:"02/07/2000 21:44"}),d.jsx(e.td,{children:"3ca75241"}),d.jsx(e.td,{children:"21/20/00/00/00/00"})]})]})]}),`
+`,d.jsx(e.h2,{children:"Credits"}),`
+`,d.jsxs(e.ul,{children:[`
+`,d.jsx(e.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]})}function c(n={}){const{wrapper:e}={...s(),...n.components};return e?d.jsx(e,{...n,children:d.jsx(t,{...n})}):t(n)}function r(n,e){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{c as default,l as frontmatter};

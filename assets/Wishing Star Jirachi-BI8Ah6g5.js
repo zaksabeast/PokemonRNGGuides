a2fcc45acd5f007e1d984151333a275acc9c5538
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-09 00:00:18 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@6b5408e9b1bdc074a5118431ca51f3634af4d8b0 🚀

---
+++ assets/Wishing Star Jirachi-BI8Ah6g5.js
@@ -0,0 +1,44 @@
+import{x as i,j as n}from"./index-Cv3Gfjnx.js";const c={title:"红蓝宝石 许愿星基拉祈乱数",description:"学习如何在红宝石和蓝宝石中，通过圆形竞技场发放的许愿星基拉祈进行乱数。",slug:"zh-wishing-star-jirachi",translation:{enSlug:"wishing-star-jirachi",language:"zh"}};function r(e){const d={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...e.components},{YouTubeVideo:s}=d;return s||h("YouTubeVideo"),n.jsxs(n.Fragment,{children:[n.jsx(d.h2,{children:"所需工具"}),`
+`,n.jsxs(d.ul,{children:[`
+`,n.jsx(d.li,{children:n.jsx(d.a,{href:"https://github.com/visualboyadvance-m/visualboyadvance-m/releases/latest",children:"VBA-M"})}),`
+`,n.jsx(d.li,{children:n.jsx(d.a,{href:"https://www.nirsoft.net/utils/run_as_date.html",children:"RunAsDate"})}),`
+`,n.jsxs(d.li,{children:["带有许愿星多启动ROM的 ",n.jsx(d.a,{href:"/no-dolphin-patch",children:"无补丁海豚模拟器"})]}),`
+`]}),`
+`,n.jsx(d.h2,{children:"视频教程"}),`
+`,n.jsx(s,{src:"https://www.youtube.com/embed/bw2XLZzsfdU?si=LUD28rVsZBDUzn0m"}),`
+`,n.jsx(d.h2,{children:"简介"}),`
+`,n.jsx(d.p,{children:"许愿星基拉祈是 2003 年的活动赠送，可以从圆形竞技场提取其分发 ROM，使其成为一种特别的乱数方式。"}),`
+`,n.jsx(d.p,{children:"它使用当前的日期与时间生成种子，并立即生成基拉祈，因此是整个系列中最简单的乱数之一。"}),`
+`,n.jsx(d.p,{children:"许愿星的个体值组合通常比 Wishmaker 和 Meteor 更优秀，但它无法成为异色。"}),`
+`,n.jsx(d.h2,{children:"RNG 流程"}),`
+`,n.jsxs(d.ol,{children:[`
+`,n.jsx(d.li,{children:"在下方的列表中找到一个目标个体组。"}),`
+`,n.jsx(d.li,{children:"使用 RunAsDate 设置与目标个体 对应的日期与时间。"}),`
+`,n.jsx(d.li,{children:"用 RunAsDate 启动 VBA 模拟器，并载入多启动 ROM。"}),`
+`,n.jsx(d.li,{children:"通过“文件” > “导入” > “电池存档”加载你的红宝石或蓝宝石的存档。"}),`
+`,n.jsx(d.li,{children:"重启游戏，等待基拉祈的生成。"}),`
+`,n.jsx(d.li,{children:"如果没有生成基拉祈，请重新导入电池存档并重启。"}),`
+`,n.jsxs(d.li,{children:["VBA 会根据 ROM 的名称创建一个新的存档文件。",`
+`,n.jsxs(d.ul,{children:[`
+`,n.jsxs(d.li,{children:["例如：ROM 文件名为 ",n.jsx(d.code,{children:"sample0519.gba"}),"，则会生成 ",n.jsx(d.code,{children:"sample0519.sav"}),"。"]}),`
+`]}),`
+`]}),`
+`,n.jsx(d.li,{children:"打开红宝石或蓝宝石 ROM，并导入新生成的电池文件。"}),`
+`,n.jsx(d.li,{children:"重启模拟器，进入游戏确认你的队伍中已收到基拉祈。"}),`
+`,n.jsx(d.li,{children:"保存游戏，将基拉祈正式写入你的红宝石或蓝宝石存档中。"}),`
+`]}),`
+`,n.jsx(d.p,{children:"恭喜！你已获得一个拥有目标个体值组合的基拉祈。"}),`
+`,n.jsx(d.h2,{children:"问题排查"}),`
+`,n.jsxs(d.p,{children:["如果你得到的基拉祈 PID 是 ",n.jsx(d.code,{children:"15b6163b"}),"，说明 RTC（实时时钟）未正确设置。"]}),`
+`,n.jsx(d.p,{children:"请检查 RunAsDate 的配置是否准确。"}),`
+`,n.jsx(d.h2,{children:"目标个体值列表"}),`
+`,n.jsx(d.h3,{children:"个体值为 31 数量最多的许愿星基拉祈"}),`
+`,n.jsxs(d.table,{children:[n.jsx(d.thead,{children:n.jsxs(d.tr,{children:[n.jsx(d.th,{children:"种子"}),n.jsx(d.th,{children:"日期时间"}),n.jsx(d.th,{children:"PID"}),n.jsx(d.th,{children:"个体值"})]})}),n.jsxs(d.tbody,{children:[n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"08c4"}),n.jsx(d.td,{children:"2000/02/15 19:29"}),n.jsx(d.td,{children:"a09ba2a2"}),n.jsx(d.td,{children:"31/31/31/17/30/31"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"5e33"}),n.jsx(d.td,{children:"2000/01/16 11:37"}),n.jsx(d.td,{children:"16c8ffa4"}),n.jsx(d.td,{children:"10/31/13/31/31/31"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"bf76"}),n.jsx(d.td,{children:"2000/02/02 18:36"}),n.jsx(d.td,{children:"5c31c17d"}),n.jsx(d.td,{children:"31/27/31/31/27/31"})]})]})]}),`
+`,n.jsx(d.h3,{children:"所有个体值均为 25 以上的许愿星基拉祈"}),`
+`,n.jsxs(d.table,{children:[n.jsx(d.thead,{children:n.jsxs(d.tr,{children:[n.jsx(d.th,{children:"种子"}),n.jsx(d.th,{children:"日期时间"}),n.jsx(d.th,{children:"PID"}),n.jsx(d.th,{children:"个体值"})]})}),n.jsxs(d.tbody,{children:[n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"1d0b"}),n.jsx(d.td,{children:"2000/01/05 03:37"}),n.jsx(d.td,{children:"1638285d"}),n.jsx(d.td,{children:"31/26/28/25/28/27"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"37f2"}),n.jsx(d.td,{children:"2000/04/09 16:48"}),n.jsx(d.td,{children:"ccd89921"}),n.jsx(d.td,{children:"29/25/28/31/31/30"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"5a11"}),n.jsx(d.td,{children:"2000/01/15 18:11"}),n.jsx(d.td,{children:"0f2248cb"}),n.jsx(d.td,{children:"29/26/25/26/26/28"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"7671"}),n.jsx(d.td,{children:"2000/01/20 18:51"}),n.jsx(d.td,{children:"feec4120"}),n.jsx(d.td,{children:"31/29/25/28/29/28"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"bf76"}),n.jsx(d.td,{children:"2000/02/02 18:36"}),n.jsx(d.td,{children:"5c31c17d"}),n.jsx(d.td,{children:"31/27/31/31/27/31"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"c9e5"}),n.jsx(d.td,{children:"2000/02/04 14:55"}),n.jsx(d.td,{children:"fc153744"}),n.jsx(d.td,{children:"31/25/29/31/25/25"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"cfa3"}),n.jsx(d.td,{children:"2000/02/05 15:37"}),n.jsx(d.td,{children:"ce160092"}),n.jsx(d.td,{children:"28/26/31/25/28/27"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"daf4"}),n.jsx(d.td,{children:"2000/02/07 15:48"}),n.jsx(d.td,{children:"dcfb9816"}),n.jsx(d.td,{children:"31/27/30/29/28/26"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"ea8a"}),n.jsx(d.td,{children:"2000/05/11 10:48"}),n.jsx(d.td,{children:"84b67156"}),n.jsx(d.td,{children:"27/25/31/31/31/29"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"ec03"}),n.jsx(d.td,{children:"2000/05/11 16:59"}),n.jsx(d.td,{children:"bde1f8e8"}),n.jsx(d.td,{children:"30/29/31/27/31/27"})]})]})]}),`
+`,n.jsx(d.h3,{children:"个体值为 0 数量最多的许愿星基拉祈"}),`
+`,n.jsxs(d.table,{children:[n.jsx(d.thead,{children:n.jsxs(d.tr,{children:[n.jsx(d.th,{children:"种子"}),n.jsx(d.th,{children:"日期时间"}),n.jsx(d.th,{children:"PID"}),n.jsx(d.th,{children:"个体值"})]})}),n.jsxs(d.tbody,{children:[n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"137e"}),n.jsx(d.td,{children:"2000/01/02 22:46"}),n.jsx(d.td,{children:"e555d451"}),n.jsx(d.td,{children:"00/00/00/30/00/07"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"4fd7"}),n.jsx(d.td,{children:"2000/01/14 04:27"}),n.jsx(d.td,{children:"09461a5b"}),n.jsx(d.td,{children:"00/00/14/00/20/00"})]}),n.jsxs(d.tr,{children:[n.jsx(d.td,{children:"ddc0"}),n.jsx(d.td,{children:"2000/02/07 21:44"}),n.jsx(d.td,{children:"3ca75241"}),n.jsx(d.td,{children:"21/20/00/00/00/00"})]})]})]}),`
+`,n.jsx(d.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(d.ul,{children:[`
+`,n.jsx(d.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro。"}),`
+`]})]})}function t(e={}){const{wrapper:d}={...i(),...e.components};return d?n.jsx(d,{...e,children:n.jsx(r,{...e})}):r(e)}function h(e,d){throw new Error("Expected component `"+e+"` to be defined: you likely forgot to import, pass, or provide it.")}export{t as default,c as frontmatter};

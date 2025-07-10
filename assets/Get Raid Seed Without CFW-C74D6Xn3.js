cd8a84630480d15f2ac9f0f376a91e652db31870
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-10 16:28:42 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@585590111deee5eff017fd2b915e1a976335bad1 🚀

---
+++ assets/Get Raid Seed Without CFW-C74D6Xn3.js
@@ -0,0 +1,36 @@
+import{y as s,j as e}from"./index-CdmiPL9X.js";const o={title:"Sword and Shield - Find Raid Seed Without CFW",navDrawerTitle:"Find Raid Seed",description:"Learn how to find raid seeds in Sword and Shield without custom firmware.",slug:"retail-swsh-get-seed-without-cfw",category:"Sword and Shield",tag:"retail"};function i(n){const t={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Method 1: Have a friend use CaptureSight"}),`
+`,e.jsx(t.p,{children:e.jsx(t.strong,{children:"Use this method if you have Nintendo online and know someone with a custom firmware Switch."})}),`
+`,e.jsx(t.p,{children:"There are two ways to use CaptureSight: by hosting a raid, or by trading a caught Pokemon."}),`
+`,e.jsx(t.p,{children:"For this method do not save after starting the raid, or entering the trade."}),`
+`,e.jsx(t.h3,{children:"Method 1A: Host the raid"}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Save the CFWless switch in front of the den and host the raid."}),`
+`,e.jsx(t.li,{children:"Have the CFW switch join the raid."}),`
+`,e.jsxs(t.li,{children:["On the CFW switch, press the ",e.jsx(t.code,{children:"Home"})," button and load CaptureSight."]}),`
+`,e.jsx(t.li,{children:'View the raid Pokémon in the "Wild Pokémon" menu and click the "Find Seed" button for the raid Pokémon.'}),`
+`,e.jsx(t.li,{children:"Have the CFWless switch restart the game without saving."}),`
+`,e.jsx(t.li,{children:"If it's not a good seed, either beat the raid or use a new wishing piece. Then try again."}),`
+`]}),`
+`,e.jsx(t.h3,{children:"Method 1B: Start a trade"}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Have the CFWless switch catch a raid Pokémon."}),`
+`,e.jsx(t.li,{children:"Have the CFWless switch join a trade with the CFW switch."}),`
+`,e.jsxs(t.li,{children:["On the CFW switch, press the ",e.jsx(t.code,{children:"Home"})," button and load CaptureSight."]}),`
+`,e.jsx(t.li,{children:'View the raid Pokémon in the "Wild Pokémon" menu and click the "Find Seed" button for the trade Pokémon.'}),`
+`,e.jsx(t.li,{children:"Do not trade the Pokémon and exit the trade so the CFWless switch doesn't save."}),`
+`,e.jsx(t.li,{children:"Restart the game on the CFWless switch so the den's seed is reset and can be RNG'd."}),`
+`]}),`
+`,e.jsx(t.h2,{children:"Method 2: Find a Discord server with a Sysbot"}),`
+`,e.jsx(t.p,{children:"Various Discord servers have Sysbots that can automatically trade and dump a Pokemon's info to find raid seeds that way. You can search online for Discord servers running Sword & Shield Sysbots. currently running Sysbots for Sword & Shield raid seeds."}),`
+`,e.jsx(t.p,{children:"You'll need to catch the raid Pokémon and trade it to the bot. In most cases, the bot will reply with the Pokemon's info and den seed."}),`
+`,e.jsx(t.h2,{children:"Method 3: Use SeedSearcher"}),`
+`,e.jsx(t.p,{children:"If the other options are not available, then your only option is to try this method to find a raid seed yourself. This takes time and may need multiple tries, so it's recommended to try the other options first."}),`
+`,e.jsx(t.p,{children:"Keep in mind, the program is very CPU/GPU intensive and can cause your computer to run very hot."}),`
+`,e.jsx(t.h3,{children:"Tools"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Leanny's ",e.jsx(t.a,{href:"https://github.com/Leanny/SeedSearcher/releases",children:"SeedSearcher"})]}),`
+`]}),`
+`,e.jsx(t.h3,{children:"Instructions"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["The ",e.jsx(t.a,{href:"https://github.com/Leanny/SeedSearcher/blob/master/README.md",children:"SeedSearcher ReadMe"})," has instructions for how to use it."]}),`
+`]})]})}function h(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{h as default,o as frontmatter};
---
+++ assets/Initial Seed RNG-CwQG2awR.js
@@ -0,0 +1,18 @@
+import{y as s,j as e}from"./index-CdmiPL9X.js";const r={title:"Ruby and Sapphire Initial Seed RNG",navDrawerTitle:"Initial Seed RNG",description:"Learn how to RNG your initial seed in Ruby and Sapphire.",slug:"rs-initial-seed",category:"Ruby and Sapphire",isRoughDraft:!0,tag:"emu"};function i(t){const n={code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsx(n.h2,{children:"Step 1: Runasdate Setup"}),`
+`,e.jsx(n.p,{children:"You need to use the 32-bit version of Runasdate to get your desired initial seed."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Download and install the 32-bit version of Runasdate."}),`
+`,e.jsx(n.li,{children:"Open Runasdate."}),`
+`,e.jsx(n.li,{children:"Set the date and time to match your desired initial seed."}),`
+`,e.jsx(n.li,{children:"Use Runasdate to launch your game."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Make sure to check the game version compatibility.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 2: Generating the Seed"}),`
+`,e.jsx(n.p,{children:"To generate the seed, follow these steps."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Start your game."}),`
+`,e.jsx(n.li,{children:"Check the initial seed based on the date and time you set."}),`
+`]}),`
+`,e.jsx(n.p,{children:"Congrats! You've now set up your initial seed for RNG."})]})}function a(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{a as default,r as frontmatter};

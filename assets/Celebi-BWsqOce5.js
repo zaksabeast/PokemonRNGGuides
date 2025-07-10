e77d4413d90775ee4bea35cbc67f3f019b22ac11
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-10 12:43:55 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@9906ae3fab1fefda75a84631e13e88e9c942ad79 🚀

---
+++ assets/Celebi-BWsqOce5.js
@@ -0,0 +1,37 @@
+import{y as d,j as e}from"./index-DkvAmXX8.js";const t={title:"Celebi",description:"How to get a shiny Fairy",slug:"es-gen2-celebi",translation:{enSlug:"gen2-celebi",language:"es"}};function l(a){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",ul:"ul",...d(),...a.components},{Alert:o,Gen2PokemonRng:r,YouTubeVideo:i}=n;return o||s("Alert"),r||s("Gen2PokemonRng"),i||s("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(o,{showIcon:!0,type:"warning",message:"¡Altamente experimental!",description:"RNG de Gen 2 todavía está en sus primeros días. ¡Es normal que necesite múltiples intentos!"}),`
+`,e.jsx(n.h2,{children:"Herramientas"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Video guía"}),`
+`,e.jsx(i,{src:"https://www.youtube.com/embed/wTpI7MV_f4U?si=0qF9bFb0HrDyv2zw"}),`
+`,e.jsx(n.h2,{children:"Guía escrita"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Guarda frente a Celebi en su santuario, en caso de que necesites resetear el juego."}),`
+`,e.jsxs(n.li,{children:['Presiona "A" para interactuar con el santuario y detente cuando veas ',e.jsx(n.code,{children:"Jugador coloca la GS Ball"}),"."]}),`
+`,e.jsxs(n.li,{children:["Visualiza la ventana del RNG de Pokereader y espera hasta que no diga mas ",e.jsx(n.code,{children:"Finding ADIV Index"})," y ",e.jsx(n.code,{children:"Finding SDIV Index"}),"."]}),`
+`,e.jsxs(n.li,{children:["Pausa el Pokereader con ",e.jsx(n.code,{children:"L + R"}),"."]}),`
+`,e.jsxs(n.li,{children:["Escribe la información del Pokereader en la herramienta RNG aquí abajo y presiona ",e.jsx(n.code,{children:"Generar"}),"."]}),`
+`,e.jsxs(n.li,{children:["Avanza el juego con ",e.jsx(n.code,{children:"L"})," hasta que el número de ",e.jsx(n.code,{children:"Advance"})," en el Pokereader coincida con algún ",e.jsx(n.code,{children:"Avance"})," generado en las columnas de abajo."]}),`
+`,e.jsx(n.li,{children:"(Opcional) Crea un estado de guardado si estas en emulador, en caso de que necesites resetear."}),`
+`,e.jsxs(n.li,{children:["Cuando caes en un avance deseado, presiona ",e.jsx(n.code,{children:"A"})," para aceptar el Pokémon. ¡No presiones nada mas hasta que veas a Celebi!"]}),`
+`,e.jsx(n.li,{children:"Si conseguiste a tu Pokémon, ¡felicidades!, si no, ¡sigue intentando!"}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Consejos útiles"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"¡Esto es inestable! ¡Espera que tome varios intentos!"}),`
+`,e.jsxs(n.li,{children:["Solo usa ",e.jsx(n.code,{children:"L + R"})," para pausar y avanzar en Gen 2, no ",e.jsx(n.code,{children:"Start + Select"}),". ¡",e.jsx(n.code,{children:"Start + Select"})," estropeara el RNG!"]}),`
+`,e.jsxs(n.li,{children:["No presiones ningún botón en el lapso en que presiones ",e.jsx(n.code,{children:"A"})," y veas a Celebi (ejemplo, no cambies la ventana de RNG a la de Pokémon en el Pokereader). ¡Presionar botones estropeara el RNG!"]}),`
+`,e.jsx(n.li,{children:"El gameboy necesita más de un frame de video para registrar cuando presionas un botón, pero la 3ds lo nota inmediatamente."}),`
+`,e.jsxs(n.li,{children:["Presionar ",e.jsx(n.code,{children:"A"})," demasiado rápido reanudara el juego sin iniciar el encuentro."]}),`
+`,e.jsxs(n.li,{children:["¡Presionar ",e.jsx(n.code,{children:"A"})," por demasiado tiempo podría estropear el RNG! Presiónalo por más de un frame, pero no más."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"RNG Tool"}),`
+`,e.jsx(r,{type:"celebi",language:"es"}),`
+`,e.jsx(n.h2,{children:"Créditos"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Todas las personas quienes han investigado Gen 2 a través de los años: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast por elaborar el método actual de rng y construir la herramienta."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, y Lord Timothy en Discord por ayudar realizando pruebas."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano por ayudar con la traducción al español."}),`
+`]})]})}function u(a={}){const{wrapper:n}={...d(),...a.components};return n?e.jsx(n,{...a,children:e.jsx(l,{...a})}):l(a)}function s(a,n){throw new Error("Expected component `"+a+"` to be defined: you likely forgot to import, pass, or provide it.")}export{u as default,t as frontmatter};
---
+++ assets/Gen 3 Wild-B02MrI3j.js
@@ -0,0 +1,17 @@
+import{y as o,j as n}from"./index-DkvAmXX8.js";const a=[{title:"Emerald Wild Searcher",description:"Emerald Wild Searcher",slug:"emerald-wild",category:"Emerald",tag:"retail",isRoughDraft:!0}];function i(r){const e={h2:"h2",li:"li",ul:"ul",...o(),...r.components},{Wild3SearcherFindTarget:t}=e;return t||l("Wild3SearcherFindTarget"),n.jsxs(n.Fragment,{children:[n.jsx(t,{game:"emerald"}),`
+`,n.jsx(e.h2,{children:"Coming soon"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Textual guide for the Emerald Wild Searcher."}),`
+`,n.jsx(e.li,{children:"Calibration tool."}),`
+`,n.jsx(e.li,{children:"Support for Sweet Scent on water."}),`
+`,n.jsx(e.li,{children:"Tool to calculate the cycle speed of a lead from its PID."}),`
+`,n.jsx(e.li,{children:"Add a filter for the lead cycle speed to help obtain a lead with the desired cycle speed."}),`
+`,n.jsx(e.li,{children:"Add a filter for Hidden Power."}),`
+`,n.jsx(e.li,{children:"Performance improvements."}),`
+`]}),`
+`,n.jsx(e.h2,{children:"Credits"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"RainingChain: Research about cycle range to estimate method likelihood. Creation of the multi-lead searcher, and the web UI tool."}),`
+`,n.jsx(e.li,{children:"Sorendog: Original Wild generation tool."}),`
+`,n.jsx(e.li,{children:"Shao: Function to calculate cycles taken by modulo operations."}),`
+`]})]})}function c(r={}){const{wrapper:e}={...o(),...r.components};return e?n.jsx(e,{...r,children:n.jsx(i,{...r})}):i(r)}function l(r,e){throw new Error("Expected component `"+r+"` to be defined: you likely forgot to import, pass, or provide it.")}export{c as default,a as frontmatter};

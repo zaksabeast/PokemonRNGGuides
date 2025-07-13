43a4f08d7bb61b65954a962000ea25d60283d536
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-13 13:28:07 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@a1a7c2b898499f7a4440ffa1be226d60cf955cdb 🚀

---
+++ assets/Celebi-DfTbWuFW.js
@@ -0,0 +1,30 @@
+import{A as c,j as e}from"./index-Qc7SyZpJ.js";const h={title:"时拉比",description:"如何获得异色时拉比",slug:"zh-gen2-celebi",translation:{enSlug:"gen2-celebi",language:"zh"}};function d(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",pre:"pre",strong:"strong",ul:"ul",...c(),...i.components},{Alert:r,Gen2PokemonRng:l,YouTubeVideo:s}=n;return r||o("Alert"),l||o("Gen2PokemonRng"),s||o("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(r,{showIcon:!0,type:"warning",message:"高度实验性！",description:"二代乱数还未完善。预计需要多次尝试！"}),`
+`,e.jsx(n.h2,{children:"所需工具"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"视频教程"}),`
+`,e.jsx(s,{src:"https://www.youtube.com/embed/wTpI7MV_f4U?si=0qF9bFb0HrDyv2zw"}),`
+`,e.jsx(n.h2,{children:"文字教程"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"在栎树林祠堂前存档，以防需要复位游戏。"}),`
+`,e.jsxs(n.li,{children:["按 ",e.jsx(n.code,{children:"A"})," 键与祠堂交互，并在 ",e.jsx(n.code,{children:"Player put in the GS Ball."})," 这段文本下等待。"]}),`
+`,e.jsxs(n.li,{children:["进入PokeReader的RNG界面，等待直到停止显示 ",e.jsx(n.code,{children:"Finding ADIV Index"})," 和 ",e.jsx(n.code,{children:"Finding SDIV Index"})," 。"]}),`
+`,e.jsxs(n.li,{children:["按住 ",e.jsx(n.code,{children:"L + R"})," 键暂停PokeReader。"]}),`
+`,e.jsxs(n.li,{children:["将PokeReader上的信息输入下方的乱数工具中，然后点击 ",e.jsx(n.code,{children:"计算"}),"。"]}),`
+`,e.jsxs(n.li,{children:["持续按 ",e.jsx(n.code,{children:"L"})," 键推进帧数，直到PokeReader显示的",e.jsx(n.code,{children:"帧数"}),"与下方计算的某一列",e.jsx(n.code,{children:"帧数"}),"匹配。"]}),`
+`,e.jsx(n.li,{children:"（可选）如果你使用的是模拟器，可以使用即时存档以防失败复位。"}),`
+`,e.jsxs(n.li,{children:["当推进到目标帧数时，按 ",e.jsx(n.code,{children:"A"})," 键触发战斗。 ",e.jsx(n.strong,{children:"请勿在进入战斗之前进行任何操作！"})]}),`
+`,e.jsx(n.li,{children:"如果遇到目标宝可梦，恭喜你成功了！如果没有，继续多次尝试！很快就能成功。"}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`注意: 相比于初始宝可梦时拉比更不稳定，要更多次地尝试。
+`})}),`
+`,e.jsx(n.h2,{children:"乱数工具"}),`
+`,e.jsx(l,{type:"celebi"}),`
+`,e.jsx(n.h2,{children:"特别鸣谢"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"中文翻译：白希洛/Hakuhiro。"}),`
+`]})]})}function x(i={}){const{wrapper:n}={...c(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(d,{...i})}):d(i)}function o(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{x as default,h as frontmatter};

afc964febaf7727309215d8edd77183aeb23eb4c
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-13 23:58:41 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@b07bfc9e8981c4b1649d849683c9d818085d0756 🚀

---
+++ assets/Advancing the RNG-Bn2_9y9l.js
@@ -0,0 +1,13 @@
+import{E as t,j as e}from"./index-DTuxGDdZ.js";const i={title:"Faire avancer la RNG sur pokemon Diamant Etincelant et Perle Scintillante",navDrawerTitle:"Faire avancer la rng",description:"Comment les avances de RNG fonctionnent dans pokemon Diamand Etincelant et Perle Scintillante. Apprenez comment controler ces avances pour rencontrer des pokemon parfaits!",slug:"fr-bdsp-advance-rng",translation:{enSlug:"bdsp-advance-rng",language:"fr"}};function s(r){const n={h2:"h2",p:"p",...t(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"Apprenez à avancer et manipuler la RNG de Pokémon Diamant Étincelant et Perle Scintillante. La méthode la plus efficace consiste en l'usage du Pokédex, mais il en existe d'autres."}),`
+`,e.jsx(n.h2,{children:"Mouvements du Joueur"}),`
+`,e.jsx(n.p,{children:"Lorsque le joueur reste statique, le personnage tend à cligner des yeux ou agiter les doigts, avec pour effet de faire avancer la RNG. Cette méthode est lente, mais il reste possible d'atteindre une avance ciblée dans une zone calme. Pour les œufs et les cadeaux-mystères, il est préférable de rester dans la chambre du joueur, afin que seuls ces mouvements ne puissent influencer la RNG."}),`
+`,e.jsx(n.p,{children:'Ouvrir le menu, ou avoir une boîte de dialogue ouverte (en utilisant un objet interdit, comme le vélo en intérieur), interrompt ces mouvements, permettant de limiter le bruit parasite dans les zones dites "bruyantes".'}),`
+`,e.jsx(n.h2,{children:"PNJs"}),`
+`,e.jsx(n.p,{children:"Les PNJs font progresser la RNG y compris lorsqu'ils ne sont pas visibles. Les zones avec plus de PNJs verront la RNG avancer plus rapidement. Mieux vaut s'isoler dans une zone calme afin d'éviter de rater sa cible."}),`
+`,e.jsx(n.p,{children:`Si le Pokédex n'est pas disponible, il est possible d'utiliser ce "bruit" en se plaçant dans une zone riche en PNJs pour faire progresser la RNG plus rapidement.
+Par exemple, la route 201 ou le village de Bonaugure peuvent être utiles pour les starters.`}),`
+`,e.jsx(n.h2,{children:"Carte de Dresseur"}),`
+`,e.jsx(n.p,{children:"Afficher la Carte de Dresseur fait progresser la RNG rapidement."}),`
+`,e.jsx(n.h2,{children:"Pokédex"}),`
+`,e.jsx(n.p,{children:"Le défilement du Pokédex est la méthode la plus efficace pour faire progresser la RNG."}),`
+`,e.jsx(n.p,{children:"Faire défiler les Pokémon ayant leur entrée dans le Pokédex fait progresser la RNG rapidement. Certaines entrées, comme celle de Goinfrex, Capumain ou Créfadet, tendent à faire avancer plus vite encore que d'autres."})]})}function l(r={}){const{wrapper:n}={...t(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(s,{...r})}):s(r)}export{l as default,i as frontmatter};
---
+++ assets/Initial Seed RNG-D7ME5asb.js
@@ -0,0 +1,46 @@
+import{E as s,j as e}from"./index-DTuxGDdZ.js";const o={title:"Initial Seed RNG",description:"How to use Dolphin to set up Initial Seed RNG for all GameCube games.",slug:"gc-initial",category:"Gamecube",tag:"emu"};function t(i){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Dolphin with Lua support (only available on Discord)"}),`
+`,e.jsx(n.li,{children:"Lua scripts for GameCube (only available on Discord)"}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`,e.jsx(n.li,{children:"RunAsDate (x64)"}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Intro"}),`
+`,e.jsx(n.p,{children:"This guide helps you perform Initial Seed RNG using Dolphin and Lua scripts for all GameCube games, including Pokémon Channel. Understanding this process is key for all RNG methods in other guides in this section."}),`
+`,e.jsx(n.h2,{children:"How to Obtain Your Origin Seed"}),`
+`,e.jsx(n.p,{children:"The Origin Seed is the Initial Seed for a specific date and time, used to calibrate results when searching for an Initial Seed. This is a quick process:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Open RunAsDate and set the date and time to '2000-01-01 00:00.'"}),`
+`,e.jsx(n.li,{children:"Run Dolphin with this config, load the game and Lua script."}),`
+`,e.jsx(n.li,{children:"Write down the Initial Seed displayed; this is your Origin Seed."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: The Origin Seed will differ for each game. The Origin Seed for Colosseum will be different from XD, so keep these values recorded to avoid repeating this step.
+`})}),`
+`,e.jsx(n.h2,{children:"Method 1: Performing an Initial Seed RNG"}),`
+`,e.jsx(n.p,{children:"Now that you have your Origin Seed, you're ready to conduct an Initial Seed RNG. This method helps you find the right Initial Seed to achieve your desired Target Seed within an Advances Range."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Open PokeFinder and navigate to 'Gen 3 Tool' > 'GameCube' > 'GameCube RTC.' Fill in the following:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Origin Seed: This is your Origin Seed."}),`
+`,e.jsx(n.li,{children:"Target Seed: Enter the Seed of the spread you want to RNG, obtained from PokeFinder or another tool."}),`
+`,e.jsx(n.li,{children:"End Date: Set a date a few years in the future to maximize results."}),`
+`,e.jsx(n.li,{children:"Min Advance/Max Advance: Specify the Advances range according to your RNG needs."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Input your Origin Seed, Target Seed, Advances Range, and set an appropriate End Date. Start the search for results."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Once you find an Initial Seed that fits your needs, note the 'Time' result and input it into RunAsDate."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Relaunch Dolphin using RunAsDate, load the game and the Lua script. The Initial Seed will match the one displayed in the GameCube RTC results. You've successfully completed your Initial Seed RNG!"}),`
+`]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If you're new to GameCube RNG, you can use a random Target Seed to learn how the process works.
+`})}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]})}function a(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}export{a as default,o as frontmatter};

58da12dc7db75dfcd8fa4253614bf95a8a9bb5ea
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-14 23:54:05 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@a279ac421d2de967899cec2081fdbd94c1bd6609 🚀

---
+++ assets/3DS PokeReader-CLDv1zG6.js
@@ -0,0 +1,29 @@
+import{E as s,j as e}from"./index-hdUAuoS3.js";const d=[{title:"3DS安装PokeReader",description:"在3DS上安装工具来帮助乱数宝可梦",slug:"zh-install-pokereader",translation:{enSlug:"install-pokereader",language:"zh"}}];function r(l){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...s(),...l.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"PokeReader是一款插件，它通过为实机提供类似模拟器脚本的功能显示内部参数来辅助乱数操作。"}),`
+`,e.jsxs(n.p,{children:["如需了解如何在3DS模拟器上安装PokeReader，",e.jsx(n.a,{href:"/install-pokereader-emu",children:"请参阅此处"}),"。"]}),`
+`,e.jsx(n.h2,{children:"所需工具"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://3ds.hacks.guide/",children:"3DS破解机及自定义固件"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/LumaTeam/Luma3DS/releases",children:"Luma版本为13.0.0或更高版本"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"在3DS上安装PokeReader"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["下载 ",e.jsx(n.a,{href:"https://github.com/zaksabeast/PokeReader/releases/latest",children:"PokeReader的最新版本"}),"。"]}),`
+`,e.jsxs(n.li,{children:["如果 ",e.jsx(n.code,{children:"/luma/plugins"})," 文件夹不存在则手动创建它。"]}),`
+`,e.jsxs(n.li,{children:["将 ",e.jsx(n.code,{children:"default.3gx"})," 复制到 ",e.jsx(n.code,{children:"/luma/plugins/"})," 文件夹下。"]}),`
+`,e.jsxs(n.li,{children:["打开 Rosalina 菜单（",e.jsx(n.code,{children:"L"})," + ",e.jsx(n.code,{children:"Down"})," + ",e.jsx(n.code,{children:"Select"}),"）将 ",e.jsx(n.code,{children:"Plugin Loader"})," 设置为enable。"]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"疑难解答"}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:"PokeReader无法加载且游戏崩溃！"})}),`
+`,e.jsx(n.p,{children:"请先确保您的游戏已更新到最新版本！PokeReader仅兼容最新版本的游戏，旧版本将无法正常运行插件。"}),`
+`,e.jsx(n.p,{children:"可以在Nintendo eShop下载游戏更新补丁。"}),`
+`,e.jsx(n.h2,{children:"安装完之后呢？"}),`
+`,e.jsx(n.p,{children:"试试这些乱数操作！"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/gen2-celebi",children:"水晶的异色时拉比"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng-offline",children:"宝可梦虚拟传送获得完美宝可梦"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/retail-usum-wormhole",children:"究极日月的异色时空洞传说宝可梦"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"特别鸣谢"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"中文翻译：白希洛/Hakuhiro。"}),`
+`]})]})}function h(l={}){const{wrapper:n}={...s(),...l.components};return n?e.jsx(n,{...l,children:e.jsx(r,{...l})}):r(l)}export{h as default,d as frontmatter};

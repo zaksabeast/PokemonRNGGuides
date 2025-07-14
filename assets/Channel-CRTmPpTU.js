87a180df9f9a79708ff39eeb65f2f68f90aafcdb
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-14 21:37:34 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@4abf3778811c929695ebbcc79237929396c1f068 🚀

---
+++ assets/Channel-CRTmPpTU.js
@@ -0,0 +1,35 @@
+import{E as r,j as n}from"./index-D5dZrs21.js";const h={title:"(PAL) Channel 乱数",description:"一步步教你乱数出 Channel 基拉祈",slug:"zh-channel-jirachi",translation:{enSlug:"channel-jirachi",language:"zh"}};function i(l){const e={code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...r(),...l.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h2,{children:"所需工具"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"一份已完成流程、可领取基拉祈的 Channel 存档"}),`
+`,n.jsx(e.li,{children:"一份已通关的欧美版红宝石 / 蓝宝石存档"}),`
+`,n.jsx(e.li,{children:"Toolbox 工具（由 Lincoln 开发）"}),`
+`,n.jsx(e.li,{children:"支持 Lua 的 Dolphin 模拟器 + Real96 提供的 Channel 专用 Lua 脚本"}),`
+`]}),`
+`,n.jsx(e.h2,{children:"简介"}),`
+`,n.jsx(e.p,{children:"在第三世代中，有两种方式能获取基拉祈：Wishmaker（美版 Bonus Disc）和 Channel（欧版 Bonus Disc）。Wishmaker 的乱数较简单，但能获取的个体值组合较少。本指南聚焦于通过 PAL 欧版 Channel 获取基拉祈，使用 Dolphin 模拟器配合更新后的工具简化流程。本指南默认你已掌握 GameCube 初始种子乱数的基础知识，特别是如何命中初始种子。"}),`
+`,n.jsx(e.h2,{children:"事前设置"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"打开 Toolbox，主界面选择「GameCube」。"}),`
+`,n.jsx(e.li,{children:"点击「Searcher」标签页，在未加载 Profile 的情况下设定筛选条件（性格、异色、个体值等）。"}),`
+`,n.jsx(e.li,{children:"找到目标个体后，记下其种子（Seed）。"}),`
+`,n.jsx(e.li,{children:"转到 Gen 3 Tools => GameCube => GameCube RTC。输入你的初始种子（Origin Seed）与目标种子（Target Seed），设定查找范围为 10 到 100 之间。"}),`
+`,n.jsx(e.li,{children:"得到结果后，将对应的日期时间设定给 RunAsDate 并启动 Dolphin。"}),`
+`]}),`
+`,n.jsx(e.h2,{children:"开始乱数与推进"}),`
+`,n.jsx(e.p,{children:"现在，准备领取基拉祈。当你加载了 Dolphin 和 Lua 脚本时，主菜单暂停游戏，检查当前种子是否已与目标匹配。"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"在 Toolbox 中，导航至 Gen 3 Tools => GameCube => Jirachi Advancer。"}),`
+`,n.jsx(e.li,{children:"在 Starting Seed 输入当前种子。"}),`
+`,n.jsx(e.li,{children:"在 Target Seed 中输入目标基拉祈的种子。"}),`
+`,n.jsx(e.li,{children:"将 Max Advances 设为 100 左右，和你之前 RTC 工具中设定的推进范围一致。"}),`
+`,n.jsx(e.li,{children:"不要填写 Bruteforce Range，不要勾选 “Min Actions” 选项，然后点击Generate。"}),`
+`]}),`
+`,n.jsx(e.p,{children:"如果一切设置无误，工具会生成一系列操作步骤，用来推进至目标种子。你也可以更新 Origin Seed 来重新计算操作步骤。"}),`
+`,n.jsx(e.p,{children:"完成所有操作后，领取基拉祈即可完成最终推进，成功获取目标个体。"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`注意: 如果没有搜索到结果，这种情况很少见，但确实可能发生。可以尝试换一个初始种子或再次搜索，通常都能解决。
+`})}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:"注意: 如果 Lua 脚本界面提示 “Reload Menu”，请按 Options 然后按下 `B` 键返回。\n"})}),`
+`,n.jsx(e.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro。"}),`
+`]})]})}function c(l={}){const{wrapper:e}={...r(),...l.components};return e?n.jsx(e,{...l,children:n.jsx(i,{...l})}):i(l)}export{c as default,h as frontmatter};
---
+++ assets/Shiny Starter-DV5-zd4l.js
@@ -0,0 +1,42 @@
+import{E as h,j as n}from"./index-D5dZrs21.js";const t=[{title:"异色御三家",description:"通过捕捉异色御三家确定你的 SID",slug:"zh-emerald-shiny-starter",translation:{enSlug:"emerald-shiny-starter",language:"zh"}}];function d(i){const e={code:"code",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...h(),...i.components},{GenerateHoennTidSid:s,Gist:r,ShinyHoennStarter:c}=e;return s||l("GenerateHoennTidSid"),r||l("Gist"),c||l("ShinyHoennStarter"),n.jsxs(n.Fragment,{children:[n.jsx(r,{children:"要点：通过捕捉异色御三家确定你的 SID"}),`
+`,n.jsx(e.h2,{children:"策略概览"}),`
+`,n.jsxs(e.p,{children:["在创建新存档时，你的 SID 取决于随机生成的 TID 以及在特定文字处按下 ",n.jsx(e.code,{children:"A"}),` 的时间。
+基于这两个变量，将生成一组可能的 SID。
+为了确定哪一个是正确的 SID，你需要在该 SID 下能生成异色的推进帧上获得御三家宝可梦。如果获得的是异色宝可梦，那么该 SID 就是正确的。`]}),`
+`,n.jsx(e.h2,{children:"第一步：生成 TID 和可能的 SID 列表"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsxs(e.li,{children:["在标题画面进入“选项”，将文字速度设为 ",n.jsx(e.code,{children:"快"}),"。"]}),`
+`,n.jsxs(e.li,{children:["选择",n.jsx(e.code,{children:"新游戏"}),"，输入你的名字后将光标移动到",n.jsx(e.code,{children:"确定"}),"按钮上。"]}),`
+`,n.jsx(e.li,{children:"在下方工具中启动 TID/SID 定时器。"}),`
+`,n.jsxs(e.li,{children:["当第一个计时器归零时，按下 ",n.jsx(e.code,{children:"A"}),"确认名字。"]}),`
+`,n.jsxs(e.li,{children:["继续对话，直到出现 ",n.jsx(e.code,{children:"Well, I'll be expecting you later. Come see me in my POKEMON LAB."})]}),`
+`,n.jsxs(e.li,{children:["当第二个计时器归零时按下 ",n.jsx(e.code,{children:"A"}),"。"]}),`
+`,n.jsx(e.li,{children:"查看训练师卡上的 TID。"}),`
+`,n.jsx(e.li,{children:"将这个 TID 填入下方工具的“获得的 TID”栏中，点击“生成可能的 SID”。"}),`
+`,n.jsx(e.li,{children:"根据下方的建议，判断是否应重新进行第一步获取更理想的 TID，或继续进行第二步。"}),`
+`]}),`
+`,n.jsx(s,{game:"emerald"}),`
+`,n.jsx(e.h2,{children:"第二步：确定正确的 SID"}),`
+`,n.jsx(e.p,{children:"简述：对第一步中每一个可能的 SID，尝试在其对应的最早 Method-1 异色推进帧获得御三家。如果获得的是异色宝可梦，则该 SID 为你的存档所使用的 SID。"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"在御三家的包前面存档。"}),`
+`,n.jsx(e.li,{children:"在下方工具中输入你的 TID 和你尚未测试过的第一个可能的 SID。"}),`
+`,n.jsx(e.li,{children:"点击“生成”，然后开始计时器。"}),`
+`,n.jsxs(e.li,{children:["当第一个计时器归零时，同时按下 ",n.jsx(e.code,{children:"Start + Select + A + B"})," 以重置游戏。"]}),`
+`,n.jsx(e.li,{children:"快速打开包以避免活动NPC 带来的帧数推进。"}),`
+`,n.jsxs(e.li,{children:["选择你要的御三家，等到画面出现 ",n.jsx(e.code,{children:"你要选择这只宝可梦吗？"}),"。"]}),`
+`,n.jsxs(e.li,{children:["当第二个计时器归零时，按下 ",n.jsx(e.code,{children:"A"})," 确认选择。"]}),`
+`,n.jsx(e.li,{children:"完成战斗后查看你的御三家。"}),`
+`,n.jsx(e.li,{children:"如果是异色，恭喜你！你输入的 SID 就是你存档的真实 SID。"}),`
+`,n.jsx(e.li,{children:"如果不是异色，在下方表单中填写种类、性别、性格、能力值，然后点击“查找与捕获的御三家匹配的帧数”。"}),`
+`,n.jsx(e.li,{children:"如果没有结果显示，表示你输入的信息有误。"}),`
+`,n.jsx(e.li,{children:"如果第一行显示“若 SID 正确则为异色”，但你获得的宝可梦不是异色，那么该 SID 是错误的。请使用下一个可能的 SID 重新执行第二步。"}),`
+`,n.jsx(e.li,{children:"否则，点击“更新校准”按钮，并使用相同的 SID 重新执行第二步。"}),`
+`]}),`
+`,n.jsx(c,{game:"emerald"}),`
+`,n.jsx(e.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"指南与交互工具：RainingChain"}),`
+`,n.jsx(e.li,{children:"第三世代固定遇敌生成器工具：EzPz"}),`
+`,n.jsx(e.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro。"}),`
+`]})]})}function x(i={}){const{wrapper:e}={...h(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(d,{...i})}):d(i)}function l(i,e){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{x as default,t as frontmatter};

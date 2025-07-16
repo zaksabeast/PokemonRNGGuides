a45d080ab51ce9b722c36e2afff71074648025c0
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-16 22:00:05 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@7e4fcc10e95bf6a0c92e7ab703117d87b57446d4 🚀

---
+++ assets/Initial Seed Botting-BRsy0lLm.js
@@ -0,0 +1,32 @@
+import{E as r,j as n}from"./index-DaCaf2w_.js";const d={title:"初始种子自动刷取",description:"使用初始种子自动脚本，实现更高自由度的乱数控制",slug:"zh-frlg-seeding-bot",translation:{enSlug:"frlg-seeding-bot",language:"zh"}};function l(i){const e={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...r(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h3,{children:"所需工具："}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"/mgba-setup",children:"带有 Lua 脚本的 mGBA 模拟器"})}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`,n.jsxs(e.li,{children:["由 Real96 开发的 ",n.jsx(e.a,{href:"https://github.com/Real96/FRLGRSEInitialSeedsFinder",children:"FRLGRSEInitialSeedsFinder"})]}),`
+`]}),`
+`,n.jsx(e.h2,{children:"简介"}),`
+`,n.jsx(e.p,{children:"本指南介绍一种适用于《火红》《叶绿》的替代乱数方式，借助 Lua 自动脚本实现对初始种子的更高控制。"}),`
+`,n.jsx(e.h3,{children:"设置步骤："}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"首先确定你的目标。在 PokeFinder 中进入 第三世代 => 定点乱数 或 野生乱数（两者操作方式相似）。"}),`
+`,n.jsx(e.li,{children:'在 "检索器" 标签页中设置所需的筛选条件。'}),`
+`,n.jsx(e.li,{children:"生成结果后，记录你选中的种子。"}),`
+`,n.jsx(e.li,{children:"运行 FRLGRSEInitialSeedsFinder 工具。它会要求输入一个种子，此时输入刚刚记录的那个。"}),`
+`,n.jsx(e.li,{children:"当工具提示“请输入想生成的结果数量”时，建议输入一个较大的数字，例如 100。"}),`
+`,n.jsx(e.li,{children:"等待工具生成结果，它将输出一个文本文件，列出所有可能的初始种子。打开该文件并复制全部内容。"}),`
+`,n.jsxs(e.li,{children:["打开 FRLG 的 Lua 脚本，找到如下行： 'local botTargetInitSeeds ='。将你复制的种子内容以 "," 括起来并填入该行。"]}),`
+`,n.jsx(e.li,{children:"保存脚本。"}),`
+`]}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`对于刚开始游戏、尚未获得教学电视的初始 ID 乱数，建议只考虑较低帧范围内的结果。
+`})}),`
+`,n.jsx(e.h3,{children:"自动执行流程："}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"编辑 Lua 脚本的第一行。"}),`
+`,n.jsx(e.li,{children:"进入游戏的继续画面（读取存档的界面）。"}),`
+`,n.jsx(e.li,{children:"同时按下电脑的 Shift 键和 GBA 的 SELECT 键启动自动脚本。"}),`
+`]}),`
+`,n.jsx(e.p,{children:"一旦脚本完成命中，游戏将自动暂停。此时记录下你命中的初始种子，在 PokeFinder 中输入该种子后续进行乱数操作。你现在可以确定当前帧可用于继续乱数流程。"}),`
+`,n.jsx(e.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro。"}),`
+`]})]})}function h(i={}){const{wrapper:e}={...r(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(l,{...i})}):l(i)}export{h as default,d as frontmatter};
---
+++ assets/Roamers-D2wP6i-E.js
@@ -0,0 +1,70 @@
+import{E as t,j as e}from"./index-DaCaf2w_.js";const i=[{title:"Black and White Roamer RNG",navDrawerTitle:"Roamer RNG",description:"Learn how to RNG Tornadus and Thundurus in Black and White for shiny and high-IV results.",slug:"emulator-bw-roamers",category:"Black and White",tag:"emu"}];function s(r){const n={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...t(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/desmume-setup",children:"Desmume"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Admiral-Fish/RNGReporter/releases",children:"RNG Reporter"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 1: Finding a spread"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"In RNG Reporter, set the Month to 2 and 11."}),`
+`,e.jsx(n.li,{children:"Set the Encounter Type to Roaming Pokémon and Method as IVs (Standard Seed)."}),`
+`,e.jsx(n.li,{children:"Choose a nice IV Spread; do not worry about Shininess right now."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Black-and-White/Roamer/Setup.png",alt:"Setup"})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If you choose a different month, your Frames will be way more unstable/random and thus more luck-based.
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: This should take a while.
+`})}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Black-and-White/Roamer/Target.png",alt:"Target Seed"})}),`
+`,e.jsx(n.h2,{children:"Step 2: Getting the target PID and Nature"}),`
+`,e.jsx(n.p,{children:"Before we only searched for an IV spread because TID abuse is recommended for shiny roamers with good IVs."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`If you only want IVs or Shininess and not both, feel free to skip this and the next step.
+`})}),`
+`,e.jsx(n.p,{children:"Choose a Frame (recommended: at least 1500 Frames, perhaps 2000 if using a month other than 2 or 11). Note the PID and the Frame."}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Black-and-White/Roamer/Pandora.png",alt:"Pandora's Box"})}),`
+`,e.jsx(n.h2,{children:"Step 3: Save preparation"}),`
+`,e.jsx(n.p,{children:"Similar to Generation 3 or 4 RNG abuse, it is recommended that you TID/SID abuse for a good shiny. Skip this step if you only care about IVs or only care about Shininess."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Choose a TID/SID combo you like (or specify a TID/SID) and get your TID/SID."}),`
+`,e.jsx(n.li,{children:"Hit your Initial Seed and advance frames."}),`
+`,e.jsx(n.li,{children:"Note your TID/SID and get to the point you can release the roamer."}),`
+`,e.jsx(n.li,{children:"Then save inside."}),`
+`]}),`
+`,e.jsx(n.p,{children:"This is where you should save (the house on Route 7):"}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Black-and-White/Roamer/Save.png",alt:"Where to save"})}),`
+`,e.jsx(n.h2,{children:"Step 4: Calibration"}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Black-and-White/Roamer/Final-Screen.png",alt:"Final Screen"})}),`
+`,e.jsx(n.p,{children:"On the RNG Reporter screen, you got a Frame Number, most likely between 1-6."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Advance frames by walking around in the house."}),`
+`,e.jsx(n.li,{children:"Make a Save State and walk outside. Get to the screen pictured above."}),`
+`,e.jsx(n.li,{children:"Start advancing frames."}),`
+`,e.jsxs(n.li,{children:["Once you're at the target frame, press ",e.jsx(n.code,{children:"A"}),"."]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: It will most likely not have hit your Frame. You can check your Pokédex to see if you succeeded. (Assuming you are aiming for a shiny)
+`})}),`
+`,e.jsxs(n.ol,{start:"5",children:[`
+`,e.jsx(n.li,{children:"Run into the roamer and note the PID. You can use a Lua Script to see the PID or catch it and use PkHeX to view the stats."}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Black-and-White/Roamer/Calibration.png",alt:"Calibration"})}),`
+`,e.jsx(n.h2,{children:"Step 5: Getting the target"}),`
+`,e.jsx(n.p,{children:"At this point, you should have found the PID you hit."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Go back to RNG Reporter."}),`
+`,e.jsx(n.li,{children:"On the main screen, enter your Seed and ensure it searches Generation 5 PIDRNG abuse."}),`
+`,e.jsx(n.li,{children:"Hit Search and look for your PID."}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:"Note: Exporting the results to .txt makes this easier, so you can use `Ctrl` + `F` to find the PID.\n"})}),`
+`,e.jsxs(n.ol,{start:"4",children:[`
+`,e.jsxs(n.li,{children:["Find out how many frames advanced after hitting ",e.jsx(n.code,{children:"A"}),", and subtract that from your target. This is your new target frame."]}),`
+`,e.jsx(n.li,{children:"Try to hit this new target frame. If you do not hit your Pokémon,"}),`
+`,e.jsx(n.li,{children:"Repeat these steps once more."}),`
+`,e.jsx(n.li,{children:"You may need to repeat this multiple times, but you will succeed eventually."}),`
+`,e.jsxs(n.li,{children:["Keep track of what Frames you have hit and what Frame you pressed ",e.jsx(n.code,{children:"A"})," on before in Notepad or Google Docs."]}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/Black-and-White/Roamer/Success.png",alt:"Success"})}),`
+`,e.jsx(n.h2,{children:"Celebrate!"}),`
+`,e.jsx(n.p,{children:"Catch your roamer with a Master Ball or use a Mean Look/Arena Trap/Shadow Tag Trapper. Then share your success with your friends or the Pokemon RNG Discord's bragging channel."}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Chinese translation: xuanyelin, Hakuhiro."}),`
+`]})]})}function o(r={}){const{wrapper:n}={...t(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(s,{...r})}):s(r)}export{o as default,i as frontmatter};

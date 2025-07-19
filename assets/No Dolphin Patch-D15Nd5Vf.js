bc05ba0c682600bf501d32c0de5896cea0fefb19
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-19 17:18:34 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@e4dfdd700f293bd81e2756f69518a7f7cac33baf 🚀

---
+++ assets/No Dolphin Patch-D15Nd5Vf.js
@@ -0,0 +1,38 @@
+import{E as r,j as i}from"./index-eK5-Hse0.js";const c={title:"No Dolphin Patch",description:"Use this patch to RNG the Wishmaker Jirachi without needing the Dolphin emulator.",slug:"no-dolphin-patch",category:"GBA Tools",tag:"emu",addedOn:"2025-04-03"};function h(t){const e={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...r(),...t.components},{YouTubeVideo:n}=e;return n||s("YouTubeVideo"),i.jsxs(i.Fragment,{children:[i.jsx(e.h2,{children:"Tools"}),`
+`,i.jsxs(e.ul,{children:[`
+`,i.jsxs(e.li,{children:["A GBA emulator, such as ",i.jsx(e.a,{href:"https://mgba.io/downloads.html#desktop-os-1",children:"mGBA"})]}),`
+`,i.jsx(e.li,{children:i.jsx(e.a,{href:"https://github.com/zaksabeast/Multiboot-Jirachi-Patches/releases/latest",children:"No Dolphin Patch"})}),`
+`,i.jsxs(e.li,{children:["A Jirachi multiboot ROM (e.g. the Wishmaker ",i.jsx(e.code,{children:"client.bin"}),")"]}),`
+`]}),`
+`,i.jsx(e.h2,{children:"Intro"}),`
+`,i.jsx(e.p,{children:"This patch lets you obtain a multibooted Wishmaker Jirachi without using Dolphin. It bypasses the GameCube connection requirement, allowing you to use only a GBA emulator."}),`
+`,i.jsx(e.h2,{children:"Video Guide"}),`
+`,i.jsx(n,{id:"X_XV3EgzSIc"}),`
+`,i.jsx(e.h2,{children:"Step 1: Download the Patch"}),`
+`,i.jsxs(e.ol,{children:[`
+`,i.jsxs(e.li,{children:["Download ",i.jsx(e.code,{children:"patches.zip"})," from the ",i.jsx(e.a,{href:"https://github.com/zaksabeast/Multiboot-Jirachi-Patches/releases/latest",children:"No Dolphin Patch releases page"}),"."]}),`
+`,i.jsxs(e.li,{children:["Unzip the zip file to get a ",i.jsx(e.code,{children:"patches"})," folder."]}),`
+`,i.jsx(e.li,{children:"Choose the appropriate patch for your emulator using the table below."}),`
+`]}),`
+`,i.jsxs(e.table,{children:[i.jsx(e.thead,{children:i.jsxs(e.tr,{children:[i.jsx(e.th,{children:"Emulator"}),i.jsx(e.th,{children:"Multiboot ROM"}),i.jsx(e.th,{children:"Patch File"})]})}),i.jsxs(e.tbody,{children:[i.jsxs(e.tr,{children:[i.jsx(e.td,{children:"mGBA"}),i.jsx(e.td,{children:"Wishmaker Jirachi"}),i.jsx(e.td,{children:i.jsx(e.code,{children:"mgba/client.ips"})})]}),i.jsxs(e.tr,{children:[i.jsx(e.td,{children:"mGBA"}),i.jsx(e.td,{children:"Meteor Jirachi"}),i.jsx(e.td,{children:i.jsx(e.code,{children:"mgba/client.2003_1112.ips"})})]}),i.jsxs(e.tr,{children:[i.jsx(e.td,{children:"mGBA"}),i.jsx(e.td,{children:"Wishing Star Jirachi"}),i.jsx(e.td,{children:i.jsx(e.code,{children:"mgba/sample0519.ips"})})]}),i.jsxs(e.tr,{children:[i.jsx(e.td,{children:"Other"}),i.jsx(e.td,{children:"Wishmaker Jirachi"}),i.jsx(e.td,{children:i.jsx(e.code,{children:"other/client.ips"})})]}),i.jsxs(e.tr,{children:[i.jsx(e.td,{children:"Other"}),i.jsx(e.td,{children:"Meteor Jirachi"}),i.jsx(e.td,{children:i.jsx(e.code,{children:"other/client.2003_1112.ips"})})]}),i.jsxs(e.tr,{children:[i.jsx(e.td,{children:"Other"}),i.jsx(e.td,{children:"Wishing Star Jirachi"}),i.jsx(e.td,{children:i.jsx(e.code,{children:"other/sample0519.ips"})})]})]})]}),`
+`,i.jsx(e.pre,{children:i.jsx(e.code,{children:`Note: If an 'other' patch doesn't work, try the 'mGBA' patch, even for non-mGBA emulators.
+`})}),`
+`,i.jsx(e.h2,{children:"Step 2: Apply the Patch"}),`
+`,i.jsxs(e.ol,{children:[`
+`,i.jsxs(e.li,{children:["Go to ",i.jsx(e.a,{href:"https://www.marcrobledo.com/RomPatcher.js/",children:"this IPS patching website"}),"."]}),`
+`,i.jsxs(e.li,{children:["Upload your multiboot rom as the ROM file (e.g. ",i.jsx(e.code,{children:"client.bin"}),") and patch file."]}),`
+`,i.jsxs(e.li,{children:["Click ",i.jsx(e.code,{children:"Apply Patch"})," and download the patched ROM (e.g. ",i.jsx(e.code,{children:"client.patched.bin"}),")."]}),`
+`]}),`
+`,i.jsx(e.h2,{children:"Step 3: Load the Patched ROM"}),`
+`,i.jsxs(e.ol,{children:[`
+`,i.jsxs(e.li,{children:["Rename your Ruby or Sapphire save to match the patched ROM name (e.g. ",i.jsx(e.code,{children:"client.patched.sav"}),")."]}),`
+`,i.jsx(e.li,{children:"Load the patched ROM in your emulator."}),`
+`,i.jsx(e.li,{children:"Confirm the distribution succeeds."}),`
+`]}),`
+`,i.jsx(e.h2,{children:"Step 4: Verify Jirachi in Your Save"}),`
+`,i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"Once the transfer completes, rename the save file back to its original name."}),`
+`,i.jsx(e.li,{children:"Load your Ruby or Sapphire ROM in your emulator."}),`
+`,i.jsx(e.li,{children:"Check your party to make sure the Jirachi is there."}),`
+`]}),`
+`,i.jsx(e.p,{children:"Congrats! You've now got your Jirachi without needing Dolphin!"})]})}function l(t={}){const{wrapper:e}={...r(),...t.components};return e?i.jsx(e,{...t,children:i.jsx(h,{...t})}):h(t)}function s(t,e){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{l as default,c as frontmatter};
---
+++ assets/ORAS TID-CB8b6DG6.js
@@ -0,0 +1,38 @@
+import{E as a,j as e}from"./index-eK5-Hse0.js";const l={title:"Omega Ruby and Alpha Sapphire TID RNG",navDrawerTitle:"TID RNG",description:"Learn how to obtain your desired Trainer ID (TID) and Secret ID (SID) combo in Omega Ruby and Alpha Sapphire.",slug:"retail-oras-tid",category:"Omega Ruby and Alpha Sapphire",tag:"cfw",addedOn:"2025-04-13"};function o(i){const n={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...a(),...i.components},{OrasId:r,YouTubeVideo:t}=n;return r||s("OrasId"),t||s("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(n.pre,{children:e.jsx(n.code,{children:`If a save file is present, delete it by pressing B + X + Up on the d-pad at the title screen. This will remove the save file and start the game at the language select screen.
+`})}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Video Guide"}),`
+`,e.jsx(t,{id:"RuwwNFSqIIE"}),`
+`,e.jsx(n.h2,{children:"Step 1: Set up the tool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Input the TinyMT u32 seed from PokeReader into the tool at the bottom of the page."}),`
+`,e.jsx(n.li,{children:`If you're on a physical console, turn the "Only Current Seed" on. If you're on an emulator, you can leave it turned off.`}),`
+`,e.jsxs(n.li,{children:["Filter for your desired TID, SID, or TSV.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Finding a specific TID/SID/TSV combination is very rare. It is advised to RNG for only one at a time."}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:"If no results are found, increase the max advances."}),`
+`,e.jsx(n.li,{children:"If the target advance is too high or the TID/SID/TSV combination is not possible with your current seed, you can reset the game to start over with a different seed."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2: Advance the RNG"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Unpause the game and continue to name your character."}),`
+`,e.jsx(n.li,{children:"After confirming your name, you will be asked if the name is correct."}),`
+`,e.jsx(n.li,{children:'Selecting "No" and going back to the naming screen advances the RNG by one.'}),`
+`,e.jsx(n.li,{children:'Repeat choosing "No" until the target advance is reached.'}),`
+`,e.jsx(n.li,{children:'One you have reached the target advance choose "Yes" to generate the TID.'}),`
+`,e.jsx(n.li,{children:"When you can control your character, check if you got your TID/SID/TSV correct."}),`
+`]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.img,{src:"/images/OmegaRuby-AlphaSapphire/TID/Success.png",alt:"Success"}),`
+`,e.jsx(n.img,{src:"/images/OmegaRuby-AlphaSapphire/TID/Success-2.png",alt:"Success"})]}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["wwwwwwzx for reverse engineering this logic and ",e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool",children:"3DSRNGTool"}),"."]}),`
+`,e.jsxs(n.li,{children:["Bambo_Rambo for ",e.jsx(n.a,{href:"https://github.com/Bambo-Rambo/TinyFinder",children:"TinyFinder"}),", which this tool is based on."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"RNG Tool"}),`
+`,e.jsx(r,{})]})}function h(i={}){const{wrapper:n}={...a(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(o,{...i})}):o(i)}function s(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,l as frontmatter};

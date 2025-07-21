ccc8aaed9b4d72150dcb38ea0f97ae0214b07644
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-21 23:43:05 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@0e472ab7d924ae11fb22c4ee7d77cbba3fdd4fdf 🚀

---
+++ assets/Vblank-Cdv--NYb.js
@@ -0,0 +1,26 @@
+import{E as i,j as n}from"./index-BMwHshYJ.js";const c={title:"绿宝石中的垂直空白（VBlank）",description:"什么是 VBlank 以及它在宝可梦生成中的影响。",slug:"zh-gba-vblank",translation:{enSlug:"gba-vblank",language:"zh"}};function t(l){const e={a:"a",h2:"h2",li:"li",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...l.components},{Gist:s,Text:r}=e;return s||d("Gist"),r||d("Text"),n.jsxs(n.Fragment,{children:[n.jsx(s,{children:"要点：什么是 VBlank，以及它在宝可梦生成中的影响。"}),`
+`,n.jsx(e.h2,{children:"什么是 VBlank？"}),`
+`,n.jsx(e.p,{children:"VBlank 中断是 GBA 用于刷新画面的机制。它每 1/60 秒触发一次，独立于正常的游戏逻辑执行。"}),`
+`,n.jsx(e.p,{children:"当 VBlank 中断发生时，正常的游戏程序执行会被暂停，转而执行与 VBlank 相关的代码，完成后再恢复原本的游戏程序流程。"}),`
+`,n.jsx(e.p,{children:"在 GBA 的宝可梦游戏中，VBlank 相关的代码会将乱数值推进一次。也就是说，每 1/60 秒，乱数会推进 1 帧。"}),`
+`,n.jsx(e.h2,{children:"VBlank 的影响"}),`
+`,n.jsx(e.p,{children:"以下是宝可梦生成的简化流程："}),`
+`,n.jsxs(e.table,{children:[n.jsx(e.thead,{children:n.jsxs(e.tr,{children:[n.jsx(e.th,{children:"步骤"}),n.jsx(e.th,{children:"在该步骤开始时推进的帧数"})]})}),n.jsxs(e.tbody,{children:[n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"生成 PID"}),n.jsx(e.td,{children:"1"})]}),n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"生成个体值 IV"}),n.jsx(e.td,{children:"2"})]})]})]}),`
+`,n.jsx(e.p,{children:"在大多数情况下，流程正如所示，宝可梦的 PID 用的是第 1 帧的乱数值，IV 用的是第 2 帧的乱数值。"}),`
+`,n.jsx(e.p,{children:"但在少数情况下，会在过程中突发 VBlank，从而改变了生成结果："}),`
+`,n.jsxs(e.table,{children:[n.jsx(e.thead,{children:n.jsxs(e.tr,{children:[n.jsx(e.th,{children:"步骤"}),n.jsx(e.th,{children:"在该步骤开始时推进的帧数"})]})}),n.jsxs(e.tbody,{children:[n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"生成 PID"}),n.jsx(e.td,{children:"1"})]}),n.jsxs(e.tr,{children:[n.jsx(e.td,{children:n.jsx(r,{color:"Green",strong:!0,children:"VBLANK：乱数推进"})}),n.jsx(e.td,{children:n.jsx(r,{color:"Green",strong:!0,children:"2"})})]}),n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"生成个体值 IV"}),n.jsx(e.td,{children:n.jsx(r,{color:"Red",strong:!0,children:"3"})})]})]})]}),`
+`,n.jsx(e.p,{children:"尽管宝可梦的生成流程是在同一乱数帧开始的，但最终 IV 却不同，因为本应使用第 2 帧的乱数值，现在使用的是第 3 帧。"}),`
+`,n.jsx(e.h2,{children:"VBlank 发生频率"}),`
+`,n.jsx(e.p,{children:"如前所述，VBlank 每 1/60 秒发生一次，确切地说是每 280,896 条 CPU 指令周期。GBA 执行的每一项操作（例如加法、读取数据）都会占用一定的 CPU 指令周期。"}),`
+`,n.jsx(e.p,{children:"宝可梦生成中的某个步骤所花的周期越多，就越有可能在执行时碰到 VBlank。"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"例：一个步骤如果耗时 28,000 指令周期，约有 10% 的几率发生 VBlank。"}),`
+`,n.jsx(e.li,{children:"例：一个步骤耗时 300,000 指令周期，几乎必定会遇到 1 次甚至 2 次 VBlank。"}),`
+`]}),`
+`,n.jsx(e.p,{children:"定点宝可梦生成的指令很少，因此 VBlank 比较罕见。"}),`
+`,n.jsx(e.p,{children:"相反，野生宝可梦若遇到同步特性作为首发宝可梦，会触发更多的乱数判定，导致指令周期大幅上升，因此 VBlank 更为常见。"}),`
+`,n.jsxs(e.p,{children:["幸运的是，在大多数情况下，宝可梦生成中发生的 VBlank 并不会影响结果。但在某些时候，它确实会影响宝可梦的生成，造成我们所说的 ",n.jsx(e.a,{href:"/gba-methods",children:"方法 2-4"}),"。"]}),`
+`,n.jsx(e.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro。"}),`
+`]})]})}function x(l={}){const{wrapper:e}={...i(),...l.components};return e?n.jsx(e,{...l,children:n.jsx(t,{...l})}):t(l)}function d(l,e){throw new Error("Expected component `"+l+"` to be defined: you likely forgot to import, pass, or provide it.")}export{x as default,c as frontmatter};
---
+++ assets/Wild RNG-CStoI83a.js
@@ -0,0 +1,61 @@
+import{E as r,j as e}from"./index-BMwHshYJ.js";const a=[{title:"Ultra Sun and Ultra Moon Wild RNG",navDrawerTitle:"Wild RNG",description:"Learn how to RNG wild Pokémon in Ultra Sun and Ultra Moon for shiny and high-IV results.",slug:"retail-usum-wild",category:"Ultra Sun and Ultra Moon",tag:"cfw"},{title:"Sun and Moon Wild RNG",navDrawerTitle:"Wild RNG",description:"Learn how to RNG wild Pokémon in Sun and Moon for shiny and high-IV results.",slug:"retail-sm-wild",category:"Sun and Moon",tag:"cfw",canonical:"retail-usum-wild"}];function i(t){const n={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"A 3DS with PokeReader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/wwwwwwzx/3DSRNGTool/releases",children:"3DSRNGTool"})}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: In the game, you will have to use "honey" to initiate the wild encounter. You can get the "honey" item in any store after clearing three trials.
+`})}),`
+`,e.jsx(n.p,{children:"It's best to be in the area where you want to RNG."}),`
+`,e.jsxs(n.p,{children:["For Sun/Moon games, refer to this list of ",e.jsx(n.a,{href:"http://pokerng.forumcommunity.net/?t=59613020",children:"encounter slots with 0 NPC or the least number of NPC spots"}),"."]}),`
+`,e.jsx(n.h2,{children:"Step 1: Set Up 3DSRNGTool"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"In the upper right, input your game version and TSV."}),`
+`,e.jsxs(n.li,{children:["Input the initial seed in the upper right. You can find this in the main RNG view as ",e.jsx(n.code,{children:"Init Seed:"}),"."]}),`
+`,e.jsx(n.li,{children:'If you have the Shiny Charm, check the "Shiny Charm" box.'}),`
+`,e.jsx(n.li,{children:'Choose the "Wild" tab, then select "Normal Wilds" or "UBs" for Ultra Beasts. Select "Location" and choose the Pokémon you want from the "Slot" dropdown to automatically fill in its info.'}),`
+`,e.jsx(n.li,{children:'If using a Pokémon with Synchronize in the first slot, choose its nature from the "Synch Nature" dropdown. This is crucial for accurate RNG predictions.'}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: For wild Pokémon, Synchronize has a 50% chance of having the same nature as the lead Pokémon. Using Synchronize increases your chances of finding target frames.
+`})}),`
+`,e.jsxs(n.ol,{start:"6",children:[`
+`,e.jsx(n.li,{children:'Check "Day" or "Night" based on the game time. If it is raining in the game, also check the "Raining" box to ensure the RNG is accurate.'}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2 (with NPCs) Create a Timeline:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: If in an area with 0 NPCs, skip to the 0 NPC section. Do not check the "Blink F Only" box if there are NPCs.
+`})}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Follow the ",e.jsx(n.a,{href:"/retail-usum-timeline",children:"timeline guide"})," to create a timeline and find a target frame. Open the in-game menu with ",e.jsx(n.code,{children:"X"})," and hover over the bag before making the timeline."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Step 2 (with 0 NPCs):"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Do not check the "Blink F Only" box.'}),`
+`,e.jsxs(n.li,{children:["Open the in-game menu with ",e.jsx(n.code,{children:"X"})," and hover over the bag. Input your current frame in the frame range. You can pause the game by pressing ",e.jsx(n.code,{children:"Start + Select"}),"."]}),`
+`,e.jsxs(n.li,{children:['Adjust filters as needed, then click "Calculate" to select one of the results. If no results, increase the Frame Range or restart the game for a new initial seed.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Restart from the beginning of the guide if you restart the game."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Note: With 0 NPCs, use Festival Plaza to advance frames faster, but do this only with 0 NPCs. Exit Festival Plaza about a thousand frames before your target frame to avoid missing it.
+`})}),`
+`,e.jsx(n.h2,{children:"Step 3: Obtain the Wanted Pokémon"}),`
+`,e.jsx(n.p,{children:"Once you have a timeline (if there are NPCs) and target frame, proceed with the RNG."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Advance frames by pressing ",e.jsx(n.code,{children:"Start"})," to unpause. Pause again near your target frame with ",e.jsx(n.code,{children:"Start + Select"})," and slowly advance by pressing ",e.jsx(n.code,{children:"Select"})," while paused."]}),`
+`,e.jsxs(n.li,{children:["When on the target frame, press ",e.jsx(n.code,{children:"A"})," to open the bag. The delay from opening the bag is already accounted for in 3DSRNGTool, so no worries here."]}),`
+`,e.jsx(n.li,{children:'Use "Honey" and wait for the animation to finish to start the battle.'}),`
+`]}),`
+`,e.jsx(n.p,{children:"Congrats! You should now have the Pokémon you wanted. If not, you may need to adjust the correction value. See the end of this guide for more info."}),`
+`,e.jsx(n.h2,{children:"Adjusting Correction"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`Double-check that all info is correctly input, especially the initial seed. Use the correct method for the presence of NPCs; otherwise, target frames may be skipped.
+`})}),`
+`,e.jsx(n.p,{children:"If the Pokémon is not what you wanted after following the steps, adjust the correction value in 3DSRNGTool. The correction value determines the frames used to load the map, and it varies by where your character stands."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:'Set the "Target Frame" to your target frame.'}),`
+`,e.jsx(n.li,{children:'Reset filters by clicking the cog icon in "Filters."'}),`
+`,e.jsx(n.li,{children:'In the "Slots" box, change it to the Pokémon encountered in battle.'}),`
+`,e.jsx(n.li,{children:`Change the "Nature" to the Pokémon's nature.`}),`
+`,e.jsx(n.li,{children:'Click "Search" to find a frame matching the filters near the target frame. The IVs may not match initially due to an incorrect correction value.'}),`
+`,e.jsx(n.li,{children:'Adjust the correction value until the IVs match the encountered Pokémon. You can increase or decrease the value by one and click "Search" to generate new results.'}),`
+`]}),`
+`,e.jsx(n.p,{children:"Once you find the correct correction value, it remains stable for future RNG attempts, as long as you don't move your character. Moving may require further adjustments."})]})}function s(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{s as default,a as frontmatter};

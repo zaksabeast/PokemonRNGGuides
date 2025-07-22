cfee0504550f67d313a834e4bb909d31ac751203
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-07-22 13:27:08 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@32a61b6c642b26058206efe4ba54b13c4a68c433 🚀

---
+++ assets/Pokerus Retail-pwNz5YRq.js
@@ -0,0 +1,35 @@
+import{E as c,j as n}from"./index-266lr8R7.js";const d=[{title:"红宝石·蓝宝石中的宝可病毒",description:"如何感染宝可病毒",slug:"zh-rs-pokerus-retail",translation:{enSlug:"rs-pokerus-retail",language:"zh"}}];function l(i){const e={a:"a",code:"code",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...c(),...i.components},{Gen3Pokerus:s,Gist:r}=e;return s||t("Gen3Pokerus"),r||t("Gist"),n.jsxs(n.Fragment,{children:[n.jsx(r,{children:"要点：感染宝可病毒"}),`
+`,n.jsx(e.h2,{children:"什么是宝可病毒"}),`
+`,n.jsx(e.p,{children:"在每场野外战斗后，你的宝可梦有 1 / 21845 的几率感染宝可病毒。感染状态下，战斗获得的努力值将翻倍。若想获得如 5 级会龙之怒的最佳低等级宝可梦，感染宝可病毒是必要条件。"}),`
+`,n.jsx(e.h2,{children:"第一步：准备工作"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsxs(e.li,{children:["如果你已进入殿堂，并且还没有触发过",n.jsx(e.a,{href:"https://bulbapedia.bulbagarden.net/wiki/Mass_outbreak#Generation_III",children:"大量出现"}),"，建议在尝试乱数宝可病毒前先触发一次。或者，直接开一个新档。"]}),`
+`,n.jsx(e.li,{children:"将会甜甜香气（如溜溜糖球）设为队首，同时准备 5 只特性为捡拾的宝可梦（如土狼犬），且它们身上不携带道具。"}),`
+`,n.jsx(e.li,{children:"前往 103 号道路最西边的草丛区域（那里没有活动NPC）。"}),`
+`,n.jsx(e.li,{children:"存档。"}),`
+`,n.jsx(e.li,{children:"在下方工具中填写计时器上方的字段。保持捡拾宝可梦数量为 5。"}),`
+`]}),`
+`,n.jsx(e.h2,{children:"第二步：尝试感染宝可病毒"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"启动下方工具中的计时器。"}),`
+`,n.jsxs(e.li,{children:["当第一个计时器归零时，按下 ",n.jsx(e.code,{children:"Start + Select + A + B"})," 复位游戏。"]}),`
+`,n.jsx(e.li,{children:"打开宝可梦菜单，将游标停在“甜甜香气”选项上。"}),`
+`,n.jsxs(e.li,{children:["当第二个计时器归零时，按 ",n.jsx(e.code,{children:"A"})," 触发甜甜香气并引发野外战斗。"]}),`
+`,n.jsx(e.li,{children:"击败野外宝可梦后，将界面停留在“XXX 获得了 YY 点经验值。”这条信息上。"}),`
+`,n.jsxs(e.li,{children:["当第三个计时器归零时，按 ",n.jsx(e.code,{children:"A"})," 结束战斗。"]}),`
+`]}),`
+`,n.jsx(e.h2,{children:"第三步：校准"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsx(e.li,{children:"检查队伍中的所有宝可梦是否被感染了宝可病毒。如果有，恭喜你成功了！"}),`
+`,n.jsx(e.li,{children:"如果没有，但至少有一只捡拾宝可梦获得了道具，请在下方表格中填写获得的道具，并点击“生成”。"}),`
+`,n.jsx(e.li,{children:"在第一行中点击“更新校准”按钮。"}),`
+`,n.jsx(e.li,{children:"返回第二步重新尝试获取宝可病毒。"}),`
+`]}),`
+`,n.jsx(s,{}),`
+`,n.jsx(e.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"指南与工具：RainingChain"}),`
+`,n.jsxs(e.li,{children:["灵感来源：",n.jsx(e.a,{href:"https://www.reddit.com/r/pokemonrng/comments/xgx146/gen3_pokerus_rng_guide_for_retail/",children:"hatsunetsumikos 的 Reddit 帖子"})]}),`
+`,n.jsxs(e.li,{children:["解构工程支持：",n.jsx(e.a,{href:"https://github.com/pret",children:"pret 团队"})]}),`
+`,n.jsx(e.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro。"}),`
+`]})]})}function h(i={}){const{wrapper:e}={...c(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(l,{...i})}):l(i)}function t(i,e){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,d as frontmatter};
---
+++ assets/Retail Egg-h8vXJiOi.js
@@ -0,0 +1,74 @@
+import{E as p,j as i}from"./index-266lr8R7.js";const f={title:"Retail Emerald Egg RNG",description:"Learn how to Retail RNG eggs in Pokémon Emerald using the Daycare. Get perfect IVs, natures, and shinies.",slug:"zh-retail-emerald-egg",translation:{enSlug:"retail-emerald-egg",language:"zh"}};function g(l){const e={a:"a",code:"code",h2:"h2",h3:"h3",hr:"hr",img:"img",li:"li",ol:"ol",p:"p",ul:"ul",...p(),...l.components},{CalibrateHeldEgg:r,CalibrateHeldEggTimer:h,CalibratePickupEgg:d,CalibratePickupEggTimer:c,PokeNavInput:t,RetailEmeraldHeldEgg:x,RetailEmeraldPickupEgg:o,Step:s,Stepper:j,YouTubeTable:a}=e;return r||n("CalibrateHeldEgg"),h||n("CalibrateHeldEggTimer"),d||n("CalibratePickupEgg"),c||n("CalibratePickupEggTimer"),t||n("PokeNavInput"),x||n("RetailEmeraldHeldEgg"),o||n("RetailEmeraldPickupEgg"),s||n("Step"),j||n("Stepper"),a||n("YouTubeTable"),i.jsxs(i.Fragment,{children:[i.jsx(e.h2,{children:"视频教程"}),`
+`,i.jsx(a,{videos:[{title:"如何获得异色蛋",id:"yNWmpMTBOlc"},{title:"如何获得完美个体蛋",id:"Kdqbl08xmVQ"},{title:"El Terapagos Mexicano 制作的所有电话训练家位置",id:"oQl6Knrydnc"}]}),`
+`,i.jsx(e.h2,{children:"图文教程"}),`
+`,i.jsxs(j,{titles:["宝可梦导航器","准备工作","选择目标","获取 PID","选择个体","获取能力值"],children:[i.jsxs(s,{step:0,children:[i.jsx(e.h3,{children:"宝可梦导航器（PokeNav）"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"尽可能多地在宝可梦导航器中注册训练家。"}),`
+`,i.jsx(e.li,{children:"打开宝可梦导航器的“电话功能”，并在下方工具中选择你注册过的所有训练家。"}),`
+`,i.jsx(e.li,{children:"完成后进入下一步。"}),`
+`]}),i.jsx(t,{})]}),i.jsxs(s,{step:1,children:[i.jsx(e.h3,{children:"第一步：准备工作"}),i.jsxs(e.ol,{children:[`
+`,i.jsxs(e.li,{children:["可选但强烈推荐：在队伍中放一只带有避雷针和一只带有火焰之躯的宝可梦。",`
+`,i.jsxs(e.ul,{children:[`
+`,i.jsx(e.li,{children:"避雷针落雷兽在110号道路（紫堇市正下方）。"}),`
+`,i.jsx(e.li,{children:"火焰之躯的熔岩虫在火山小径（紫堇市正上方）。"}),`
+`]}),`
+`]}),`
+`,i.jsx(e.li,{children:"在绿荫镇商店购买至少 3 瓶普通的除虫喷雾。"}),`
+`,i.jsx(e.li,{children:"前往培育屋，并取出当前放置在里面的宝可梦。"}),`
+`,i.jsx(e.li,{children:"站在 118 号道路右上角的白色道路边缘。"}),`
+`,i.jsx(e.li,{children:"使用第一瓶除虫喷雾。"}),`
+`]}),i.jsx(e.p,{children:i.jsx(e.img,{src:"/images/Emerald/Retail-Egg/start-point.webp",alt:"白色道路起点"})}),i.jsx(e.h3,{children:"第二步：存入宝可梦"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"向左走到培育屋门口，然后向上走到培育屋老奶奶前。"}),`
+`,i.jsx(e.li,{children:"将亲代宝可梦存入培育屋。"}),`
+`,i.jsx(e.li,{children:"走出培育屋。"}),`
+`,i.jsx(e.li,{children:"与培育屋老爷爷对话，获取亲代的相性信息并记录备用。"}),`
+`]}),i.jsx(e.h3,{children:"第三步：站好位置"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"前往绿荫镇，四处奔跑。"}),`
+`,i.jsx(e.li,{children:"使用第二瓶除虫喷雾。"}),`
+`,i.jsx(e.li,{children:"继续在绿荫镇奔跑并使用第三瓶除虫喷雾。"}),`
+`,i.jsx(e.li,{children:"站在最靠近宝可梦中心的树旁（如下图所示）。"}),`
+`,i.jsx(e.li,{children:"在宝可梦中心门前的两格地上来回走，直到除虫喷雾效果结束。"}),`
+`,i.jsx(e.li,{children:"存档，并进入下一步。"}),`
+`]}),i.jsx(e.p,{children:i.jsx(e.img,{src:"/images/Emerald/Retail-Egg/end-point.webp",alt:"宝可梦中心前"})})]}),i.jsxs(s,{step:2,children:[i.jsx(e.h3,{children:"选择目标"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"在下方工具中输入蛋的信息并点击“生成”。"}),`
+`,i.jsx(e.li,{children:"点击你想孵化的目标宝可梦旁的“选择”。"}),`
+`]}),i.jsx(e.p,{children:"如果你使用了百变怪与一只无性别或雄性的亲代，请在“雌性”栏中填写非百变怪的那只宝可梦。"}),i.jsx(x,{})]}),i.jsxs(s,{step:3,children:[i.jsx(e.h3,{children:"第一步：乱数 PID"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"确保避雷针宝可梦处于队伍首位，并进行存档。"}),`
+`,i.jsx(e.li,{children:"启动下方计时器。"}),`
+`,i.jsx(e.li,{children:"当第一个计时器结束时，快速软重启游戏并读取存档。"}),`
+`,i.jsx(e.li,{children:"在宝可梦中心前的两格地上来回走 9 步。"}),`
+`,i.jsx(e.li,{children:"根据下方工具提示按照指定次数打开图鉴。"}),`
+`,i.jsx(e.li,{children:"当第二个计时器结束时，在这两格地上再走一步。"}),`
+`,i.jsx(e.li,{children:"如果你收到了与你预期匹配的电话，并且培育屋老爷爷已经准备好蛋了，就保存游戏。否则，请继续查看“校准”部分。"}),`
+`]}),i.jsx(h,{}),i.jsx(e.h3,{children:"第二步：校准"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"将火焰之躯宝可梦放到队伍首位。"}),`
+`,i.jsx(e.li,{children:"向培育屋老爷爷领取蛋并孵化。如果是异色，继续下一步。"}),`
+`,i.jsx(e.li,{children:"如果不是异色，在下方表单中填写电话内容和蛋的性格。"}),`
+`,i.jsx(e.li,{children:"点击“查找与蛋匹配的帧数”以获取可能结果。"}),`
+`,i.jsx(e.li,{children:"找到最接近 0 的 offset，并点击“校准”来调整计时器。"}),`
+`,i.jsx(e.li,{children:"重新开始 PID 的乱数步骤。"}),`
+`]}),i.jsx(r,{})]}),i.jsxs(s,{step:4,children:[i.jsx(e.h3,{children:"选择个体值"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"在下方工具中填写条件后点击“生成”来获取可选的个体组合。"}),`
+`,i.jsx(e.li,{children:"点击你想孵化的目标宝可梦旁的“选择”。"}),`
+`]}),i.jsx(o,{})]}),i.jsxs(s,{step:5,children:[i.jsx(e.h3,{children:"第一步：乱数个体值"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"站在培育屋老爷爷旁边并进行存档。"}),`
+`,i.jsx(e.li,{children:"启动下方计时器。"}),`
+`,i.jsx(e.li,{children:"当第一个计时器结束时，软重启游戏，读取存档后尽快与老爷爷对话。"}),`
+`,i.jsx(e.li,{children:"继续对话，直到出现“Take good care of it”。"}),`
+`,i.jsxs(e.li,{children:["当第二个计时器结束时按下 ",i.jsx(e.code,{children:"A"})," 领取蛋。"]}),`
+`,i.jsx(e.li,{children:"孵化这个蛋。"}),`
+`]}),i.jsx(c,{}),i.jsx(e.h3,{children:"第二步：校准"}),i.jsxs(e.ol,{children:[`
+`,i.jsx(e.li,{children:"在下方工具中填写孵化出的个体值并点击“查找匹配的帧数”。"}),`
+`,i.jsx(e.li,{children:"如果 offset 为 0 的结果与你目标一致，恭喜你，乱数成功！"}),`
+`,i.jsx(e.li,{children:"如果 offset 为 0 的结果方法不对，请重新尝试一次，无需校准。"}),`
+`,i.jsx(e.li,{children:"如果没有 offset 为 0 的结果，点击最接近 0 的那行的“校准”以调整计时器。"}),`
+`,i.jsx(e.li,{children:"重新执行个体值乱数步骤。"}),`
+`]}),i.jsx(d,{})]})]}),`
+`,i.jsx(e.hr,{}),`
+`,i.jsx(e.h2,{children:"特别鸣谢"}),`
+`,i.jsxs(e.ul,{children:[`
+`,i.jsx(e.li,{children:"Zak for writing this web tool."}),`
+`,i.jsxs(e.li,{children:["Lincoln for the ",i.jsx(e.a,{href:"https://github.com/Lincoln-LM/PokeNav-Egg-RNG-Tool",children:"PokeNav tool"}),", which this tool is based on."]}),`
+`,i.jsxs(e.li,{children:["Thanks to all ",i.jsx(e.a,{href:"https://github.com/Admiral-Fish/PokeFinder",children:"PokeFinder"})," contributors, which this tool is based on."]}),`
+`,i.jsxs(e.li,{children:["El Terapagos Mexicano, jafet0022, and everyone who contributed to ",i.jsx(e.a,{href:"https://discord.com/channels/285269328469950464/1257368429107875992",children:"this Discord thread"}),", which is filled with lots of retail Emerald egg research and info."]}),`
+`,i.jsx(e.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro。"}),`
+`]})]})}function u(l={}){const{wrapper:e}={...p(),...l.components};return e?i.jsx(e,{...l,children:i.jsx(g,{...l})}):g(l)}function n(l,e){throw new Error("Expected component `"+l+"` to be defined: you likely forgot to import, pass, or provide it.")}export{u as default,f as frontmatter};

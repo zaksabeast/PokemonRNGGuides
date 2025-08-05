938a5890b17f45da0430ed0d5fd6ebe92fddd2a0
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-08-05 22:01:34 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@bea3836bcded6226f30563d97a6ca94e8b51c59b 🚀

---
+++ assets/Celebi-D0nFVZ_x.js
@@ -0,0 +1,55 @@
+import{O as h,j as e}from"./index-DpO0waUy.js";const d={title:"Celebi in Crystal",navDrawerTitle:"Celebi RNG",description:"Learn how to obtain a shiny Celebi in Pokémon Crystal using RNG manipulation.",slug:"gen2-celebi",category:"Gold, Silver, Crystal",tag:"cfw",addedOn:"2025-03-02"};function l(i){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...h(),...i.components},{Alert:t,Gen2PokemonRng:r,YouTubeVideo:o}=n;return t||s("Alert"),r||s("Gen2PokemonRng"),o||s("YouTubeVideo"),e.jsxs(e.Fragment,{children:[e.jsx(t,{showIcon:!0,type:"warning",message:"Highly experimental!",description:"Gen 2 RNG is still in its early days. Expect to need multiple attempts!"}),`
+`,e.jsx(n.h2,{children:"Tools"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/install-pokereader",children:"PokeReader"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Video guide"}),`
+`,e.jsx(o,{id:"wTpI7MV_f4U"}),`
+`,e.jsx(n.h2,{children:"Written guide"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Save in front of the Celebi shrine. This lets you reset if needed."}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"A"})," to interact with the shrine and stop when you see ",e.jsx(n.code,{children:"Player put in the GS Ball."})]}),`
+`,e.jsxs(n.li,{children:["Open PokeReader to the RNG screen and wait until ",e.jsx(n.code,{children:"Finding ADIV Index"})," and ",e.jsx(n.code,{children:"Finding SDIV Index"})," are gone."]}),`
+`,e.jsxs(n.li,{children:["Pause PokeReader with ",e.jsx(n.code,{children:"L + R"}),' then enter the info from PokeReader into the RNG tool below. Press "Generate".']}),`
+`,e.jsxs(n.li,{children:["Unpause the game with ",e.jsx(n.code,{children:"R"})," until near the target advance."]}),`
+`,e.jsxs(n.li,{children:["Pause then advance with ",e.jsx(n.code,{children:"L"})," one at a time."]}),`
+`,e.jsxs(n.li,{children:['Stop when the "Advance" number in PokeReader matches one shown in the tool.',`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"(Optional) Make a save state if you're on an emulator."}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Press ",e.jsx(n.code,{children:"A"})," to accept the Pokemon. ",e.jsx(n.strong,{children:"Do not press anything else until Celebi appears!"})]}),`
+`,e.jsx(n.li,{children:"If you got your Pokemon, congrats! If not, keep trying!"}),`
+`]}),`
+`,e.jsx(n.h3,{children:"Helpful tips"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"This is unstable! Expect it to take many tries!"}),`
+`,e.jsxs(n.li,{children:["Only use ",e.jsx(n.code,{children:"L + R"})," to pause and advance in Gen 2, not ",e.jsx(n.code,{children:"Start + Select"}),". ",e.jsx(n.code,{children:"Start + Select"})," will mess up the RNG!"]}),`
+`,e.jsxs(n.li,{children:["Do not press any buttons in between pressing ",e.jsx(n.code,{children:"A"})," and seeing Celebi.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Switching screens or pressing buttons during this time will mess up the RNG!"}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["The 3DS registers button presses instantly, but the Game Boy takes a few frames.",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Pressing ",e.jsx(n.code,{children:"A"})," too quickly might unpause the game without starting the encounter."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Don't hold ",e.jsx(n.code,{children:"A"})," too long. Press it slightly longer than one frame."]}),`
+`]}),`
+`,e.jsx(n.h2,{children:"RNG Tool"}),`
+`,e.jsx(r,{type:"celebi"}),`
+`,e.jsx(n.h2,{children:"What next?"}),`
+`,e.jsx(n.p,{children:"Now you can use Transporter to RNG your Celebi to have perfect IVs!"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng",children:"Transporter RNG Guide"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/transporter-rng-offline",children:"Easier Transporter RNG method using an offline patch"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Credits"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"All the people who have looked into Gen 2 over the years: Admiral Fish, EzPzStreamz, Zep, Real, Amab, wwwwwwzx, Vlad, Bambo_Rambo, Lincoln, Shiny_Sylveon, and Zaksabeast."}),`
+`,e.jsx(n.li,{children:"Zaksabeast for coming up with the current RNG method and building the tool."}),`
+`,e.jsx(n.li,{children:"Shiny_Sylveon, El Terapagos Mexicano, and Lord Timothy on Discord for helping test."}),`
+`,e.jsx(n.li,{children:"Hakuhiro for the Chinese translation."}),`
+`,e.jsx(n.li,{children:"El Terapagos Mexicano for the Spanish translation."}),`
+`]})]})}function c(i={}){const{wrapper:n}={...h(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(l,{...i})}):l(i)}function s(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{c as default,d as frontmatter};

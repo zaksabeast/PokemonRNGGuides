32109c977ffa1a4382ea54dfedb2e0f5f26aaf31
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-08-08 15:42:18 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@3691b1fe4a253048f2dfcac2e9c80631741bee75 🚀

---
+++ assets/Starters-CedqxHX-.js
@@ -0,0 +1,45 @@
+import{O as p,j as e}from"./index-Mcf0XAZ1.js";const u=[{title:"心金魂银初始宝可梦乱数",description:"学习如何在心金与魂银中进行初始宝可梦的乱数，以获得异色、高个体值的宝可梦。",slug:"zh-retail-hgss-starter",translation:{enSlug:"retail-hgss-starter",language:"zh"}},{title:"钻石珍珠白金初始宝可梦乱数",description:"学习如何在钻石、珍珠和白金中进行初始宝可梦的乱数，以获得异色、高个体值的宝可梦。",slug:"zh-retail-dppt-starter",translation:{enSlug:"retail-dppt-starter",language:"zh"}}];function S(n){const s={a:"a",code:"code",h2:"h2",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...p(),...n.components},{Alert:l,CalibrateStarter4:h,GetStarter4:c,PickStarter4:x,Starter4ConsoleSetDateString:d,Starter4Setup:j,Starter4ShowIf:r,Step:i,Stepper:o,YouTubeTable:a}=s;return l||t("Alert"),h||t("CalibrateStarter4"),c||t("GetStarter4"),x||t("PickStarter4"),d||t("Starter4ConsoleSetDateString"),j||t("Starter4Setup"),r||t("Starter4ShowIf"),i||t("Step"),o||t("Stepper"),a||t("YouTubeTable"),e.jsxs(e.Fragment,{children:[e.jsx(s.h2,{children:"视频教程"}),`
+`,e.jsx(a,{videos:[{title:"Starter RNG",id:"PMEuRlT83Qs"},{title:"3DS RNG",id:"-ayvPKWNkNw"}]}),`
+`,e.jsx(s.h2,{children:"图文教程"}),`
+`,e.jsx(l,{showIcon:!0,type:"info",message:"本指南为交互式，会根据你设置的机种和游戏版本动态更新内容。"}),`
+`,e.jsxs(o,{titles:["环境准备","选择目标","执行乱数","校准偏差"],children:[e.jsxs(i,{step:0,children:[e.jsx(s.h2,{children:"环境准备"}),e.jsx(s.p,{children:"请先选择你使用的游戏版本和机型，然后继续下一步。"}),e.jsx(r,{game:"Platinum",children:e.jsx(l,{showIcon:!0,type:"info",message:"注意：白金版是最难进行乱数的一版，可能会出现不稳定情况。"})}),e.jsx(j,{}),e.jsx(r,{is3dsNormalSettings:!0,children:e.jsxs(s.p,{children:[e.jsx(s.a,{href:"/3ds-helper",children:"点击此处查看 3DS Helper 使用指南"}),"."]})}),e.jsx(r,{is3dsAltSettings:!0,children:e.jsxs(s.p,{children:[e.jsx(s.a,{href:"/3ds-alt-settings",children:"点击此处下载 3DS Alt Settings 应用"}),"."]})})]}),e.jsxs(i,{step:1,children:[e.jsx(s.h2,{children:"选择目标初始宝可梦"}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"使用下方工具设置筛选条件，寻找你想要的初始宝可梦。"}),`
+`,e.jsx(s.li,{children:"如果没有喜欢的目标，可以适当提高最小与最大延迟，来拓宽筛选范围。"}),`
+`]}),e.jsx(s.p,{children:"带有高个体值的异色宝可梦通常非常稀有。建议目标是最多 3 个完美个体。"}),e.jsx(x,{})]}),e.jsxs(i,{step:2,children:[e.jsx(s.h2,{children:"准备存档"}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["如果你使用的是白金版，请将主机日期设置为： ",e.jsx(s.strong,{children:"12/31/2099"})," on NDS or ",e.jsx(s.strong,{children:"12/31/2050"})," on 3DS."]}),`
+`,e.jsx(s.li,{children:"根据你游戏的版本，在下方图示位置存档："}),`
+`]}),e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"钻石/珍珠"}),e.jsx(s.th,{children:"白金"}),e.jsx(s.th,{children:"心金/魂银"})]})}),e.jsx(s.tbody,{children:e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.img,{src:"/images/Diamond-Pearl-Platinum/Starter/dpp-starter.webp",alt:"钻珍存档位置"})}),e.jsx(s.td,{children:e.jsx(s.img,{src:"/images/Diamond-Pearl-Platinum/Starter/plat-starter.webp",alt:"白金存档位置"})}),e.jsx(s.td,{children:e.jsx(s.img,{src:"/images/HeartGold-SoulSilver/Starter/hgss-starter.webp",alt:"心金魂银存档位置"})})]})})]}),e.jsx(s.h2,{children:"设置主机时间"}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["将主机日期设置为：",e.jsx(s.strong,{children:e.jsx(d,{format:"date"})})]}),`
+`,e.jsxs(s.li,{children:["开始设置时间为：",e.jsx(s.strong,{children:e.jsx(d,{format:"time"})}),"，但不要立刻确认。"]}),`
+`]}),e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsx(s.tr,{children:e.jsx(s.th,{children:"确认时间画面"})})}),e.jsx(s.tbody,{children:e.jsx(s.tr,{children:e.jsx(s.td,{children:e.jsx(s.img,{src:"/images/HeartGold-SoulSilver/SetTimeScreen.jpg",alt:"时间确认画面"})})})})]}),e.jsx(s.h2,{children:"命中目标帧"}),e.jsx(r,{is3dsNormalSettings:!0,children:e.jsx(s.p,{children:e.jsx(s.a,{href:"/3ds-helper",children:"点此学习如何使用 3DS Helper"})})}),e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:["同时按下 ",e.jsx(s.code,{children:"A"})," 键设定时间，并启动下方计时器。"]}),`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(r,{is3dsNormalSettings:!0,children:e.jsx(s.p,{children:"离开设定画面，重启主机并开启游戏。"})}),`
+`,e.jsx(r,{is3dsAltSettings:!0,children:e.jsxs(s.p,{children:["按下 ",e.jsx(s.code,{children:"Start"})," 退出 Alt Settings，然后启动游戏。"]})}),`
+`,e.jsx(r,{isNdsDsi:!0,children:"离开设定画面并重启主机。"}),`
+`]}),`
+`,e.jsxs(s.li,{children:["第一段计时结束时，",e.jsxs(r,{is3dsNormalSettings:!0,is3dsAltSettings:!0,children:["按 ",e.jsx(s.code,{children:"L + R + Start + Select"})," 软重启游戏。"]}),e.jsxs(r,{isNdsDsi:!0,children:["按 ",e.jsx(s.code,{children:"A"})," 启动游戏。"]})]}),`
+`,e.jsx(s.li,{children:"进入读取存档界面。"}),`
+`,e.jsxs(s.li,{children:["第二段计时结束时，立即按 ",e.jsx(s.code,{children:"A"})," 读取存档。"]}),`
+`,e.jsx(s.li,{children:"若是白金版，请快速猛按按键以尽快领取御三家。"}),`
+`]}),e.jsx(c,{})]}),e.jsxs(i,{step:3,children:[e.jsx(s.h2,{children:"校准偏差"}),e.jsxs(s.ol,{children:[`
+`,e.jsx(s.li,{children:"查看起始宝可梦的属性。"}),`
+`,e.jsx(s.li,{children:"如果与目标一致，恭喜你命中了目标帧！"}),`
+`,e.jsx(s.li,{children:"若不一致，请将宝可梦资料填入下方表单并点击“Generate”。"}),`
+`,e.jsx(s.li,{children:"选择延迟偏差最小的那一行，点击“Calibrate”进行校准。"}),`
+`,e.jsx(s.li,{children:"按照修正值重新进行一次乱数尝试。"}),`
+`,e.jsxs(s.li,{children:["如果命中的结果提示“Flip Delay”需勾选，请依照以下方法操作：",`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[e.jsx(s.strong,{children:"任意版本"}),"：插入一张 GBA 卡带。"]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.strong,{children:"心金魂银"}),"：在继续界面使用触控笔点击上下箭头。"]}),`
+`,e.jsxs(s.li,{children:[e.jsx(s.strong,{children:"钻石珍珠白金"}),"：点击“新游戏”，随后按 ",e.jsx(s.code,{children:"B"})," 返回继续界面。"]}),`
+`,e.jsxs(s.li,{children:["若你已进行 Flip Delay 但再次被提示翻转，则",e.jsx(s.strong,{children:"不要"}),"重复翻转操作。"]}),`
+`]}),`
+`]}),`
+`]}),e.jsx(s.p,{children:"注意：在钻石珍珠白金中，延迟有可能会随机从奇数变成偶数，反之亦然。遇到这种情况，只能不断尝试直至命中目标。"}),e.jsx(h,{})]})]}),`
+`,e.jsx(s.h2,{children:"特别鸣谢"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Zaksabeast, Sorendog, EzPz, and RainingChain for various contributions to this tool"}),`
+`,e.jsxs(s.li,{children:["Thanks to all ",e.jsx(s.a,{href:"https://github.com/Admiral-Fish/PokeFinder",children:"PokeFinder"})," contributors, whose work this tool is built upon."]}),`
+`,e.jsx(s.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro"}),`
+`]})]})}function m(n={}){const{wrapper:s}={...p(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(S,{...n})}):S(n)}function t(n,s){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{m as default,u as frontmatter};

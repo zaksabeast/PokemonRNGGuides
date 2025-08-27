eb25969a90be1f6deaa8865dbd0688ae15e5482e
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-08-27 21:19:04 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@8410e00c274dbce4a1c3c27d1dd701cca8888879 🚀

---
+++ assets/3DS Helper-BcIFVoXp.js
@@ -0,0 +1,25 @@
+import{g5 as h,j as e}from"./index-DVcJqcS4.js";const d={title:"3DS Timer Helper",description:"Easier 3DS RNG without homebrew using precise timer starts.",slug:"zh-3ds-helper",translation:{enSlug:"3ds-helper",language:"zh"}};function o(i){const n={a:"a",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...h(),...i.components},{Alert:s,Gist:t,YouTubeTable:l}=n;return s||r("Alert"),t||r("Gist"),l||r("YouTubeTable"),e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"使用 3DS 辅助工具，让你的 3DS 乱数操作更轻松。"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"无需破解 CFW"})," - 在原装主机上也可使用！"]}),`
+`,e.jsxs(n.p,{children:["本工具适用于使用 ",e.jsx(n.a,{href:"/mystic-timer",children:"Mystic Timer"})," 的 ",e.jsx(n.a,{href:"/pokefinder",children:"PokeFinder"}),"，也整合在我们的一些网页工具中，例如 ",e.jsx(n.a,{href:"/hgss-tid-sid",children:"迷人之躯乱数"})," 和 ",e.jsx(n.a,{href:"/retail-hgss-starter",children:"御三家乱数"}),"。"]}),`
+`,e.jsx(n.h2,{children:"视频教程"}),`
+`,e.jsx(l,{videos:[{title:"Using the 3DS Helper",id:"-ayvPKWNkNw"}]}),`
+`,e.jsx(n.h2,{children:"图文教程"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"在你所使用的工具（如 Mystic Timer、迷人之躯、御三家等）中，将主机类型设为“3DS”。"}),`
+`,e.jsx(n.li,{children:"设置你的 3DS 日期。"}),`
+`,e.jsxs(n.li,{children:["开始设置时间，但 ",e.jsx(n.strong,{children:"不要立刻确认"}),"。"]}),`
+`,e.jsx(n.li,{children:"在网页工具中点击“启动 3DS Helper”。"}),`
+`,e.jsx(n.li,{children:"当你确认 3DS 时间时，同时点击计时器的“Start”按钮。"}),`
+`,e.jsx(n.li,{children:"如果计时器没有启动，说明你点“确定”太早或太晚 - 请重试。"}),`
+`,e.jsx(n.li,{children:"如果计时器启动了，继续进行你的乱数流程。"}),`
+`,e.jsx(n.li,{children:"如果你经常偏差 1 秒，请将 3DS Helper 的偏移值增加 100ms。"}),`
+`,e.jsx(n.li,{children:"重复步骤 2–5，直到你在乱数操作中稳定地命中正确秒数为止。"}),`
+`]}),`
+`,e.jsx(n.p,{children:"恭喜！你已经可以使用 3DS Helper 工具来稳定控制秒数，进行精准的乱数操作了。"}),`
+`,e.jsx(s,{showIcon:!0,type:"info",message:"小技巧",description:"3DS 的系统时间是在你松开“确定”按钮后才会真正设定。为了更精准地操作，你可以先按住“确定”，在准备好的同时松手并按下计时器的“Start”。这样更容易把握时机。"}),`
+`,e.jsx(n.h2,{children:"特别鸣谢"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Zaksabeast for researching this and making the 3DS Helper tool"}),`
+`,e.jsx(n.li,{children:`Rainlan on Discord for noticing the 3DS time isn't set until you lift your finger from the "Ok" button, which makes timing much easier`}),`
+`,e.jsx(n.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro"}),`
+`]})]})}function a(i={}){const{wrapper:n}={...h(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(o,{...i})}):o(i)}function r(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{a as default,d as frontmatter};

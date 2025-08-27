1260b180c02e54d2e2d4607399629bca078bb432
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2025-08-27 21:31:57 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@ee7799cd4c5761355e13ec61bffdcf012cf75607 🚀

---
+++ assets/Friend Safari Patch-DHZtPrUl.js
@@ -0,0 +1,32 @@
+import{g5 as l,j as n}from"./index-CFYbhQRX.js";const r={title:"XY All Friend Safaris Patch",navDrawerTitle:"All Friend Safaris Patch",description:"How to use a game patch to unlock all Friend Safari Pokemon in Pokemon X and Y, including Ditto and Vivillon.",slug:"xy-friend-safari-patch",category:"X and Y",tag:"patch"};function t(i){const e={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...l(),...i.components},{YouTubeVideo:s}=e;return s||o("YouTubeVideo"),n.jsxs(n.Fragment,{children:[n.jsx(e.p,{children:"This patch allows Pokemon X and Y to access any Friend Safari Pokemon. It makes the game think the console has 71 friends that have every available Friend Safari with all three slots unlocked."}),`
+`,n.jsx(e.p,{children:"This does not modify your actual 3DS friends list, it just makes the game think fake friends are available."}),`
+`,n.jsx(e.h2,{children:"Tools"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.a,{href:"https://3ds.hacks.guide/",children:"A 3DS with CFW"})," OR ",n.jsx(e.a,{href:"https://github.com/azahar-emu/azahar/releases/latest",children:"Azahar"})]}),`
+`,n.jsx(e.li,{children:n.jsx(e.a,{href:"https://github.com/zaksabeast/XY-All-Friend-Safaris-Patch/releases",children:"All Friend Safaris Patch Download"})}),`
+`]}),`
+`,n.jsx(e.h2,{children:"Video Guide"}),`
+`,n.jsx(s,{id:"_pvOsA7Q0KY"}),`
+`,n.jsx(e.h2,{children:"3DS Instructions"}),`
+`,n.jsx(e.h3,{children:"Update Luma Settings"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsxs(e.li,{children:["Boot the console while holding ",n.jsx(e.code,{children:"Select"}),"."]}),`
+`,n.jsx(e.li,{children:'Select "Enable game patches".'}),`
+`]}),`
+`,n.jsx(e.h3,{children:"Install the Patch"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsxs(e.li,{children:["Download the ",n.jsx(e.code,{children:"code.ips"})," from the link above."]}),`
+`,n.jsxs(e.li,{children:["Copy the ",n.jsx(e.code,{children:"code.ips"})," to the SD card.",`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"X"}),": ",n.jsx(e.code,{children:"/luma/titles/0004000000055D00/code.ips"})]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Y"}),": ",n.jsx(e.code,{children:"/luma/titles/0004000000055E00/code.ips"})]}),`
+`]}),`
+`]}),`
+`]}),`
+`,n.jsx(e.h2,{children:"Azahar Instructions"}),`
+`,n.jsx(e.h3,{children:"Install the Patch"}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsxs(e.li,{children:["Download ",n.jsx(e.code,{children:"code.ips"})," from the link above."]}),`
+`,n.jsx(e.li,{children:'Right click on the game and select "Open Mods Location".'}),`
+`,n.jsxs(e.li,{children:["Copy the ",n.jsx(e.code,{children:"code.ips"})," into the directory that appears."]}),`
+`]})]})}function h(i={}){const{wrapper:e}={...l(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(t,{...i})}):t(i)}function o(i,e){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{h as default,r as frontmatter};

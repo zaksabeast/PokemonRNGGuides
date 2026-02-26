e4112c95274934d6b77a5165c1163524ee5a98ee
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-02-26 13:02:38 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@0e4ecece17b09f23ffa520d9d6e36cd569afa30d 🚀

---
+++ assets/Gen 3 TID SID Generator--6n4KxUk.js
@@ -0,0 +1,63 @@
+const n=`---
+- title: "RS TID and SID Generator"
+  description: "Generator for TID and SID in RS"
+  slug: "rs-tidsid-generator"
+  category: "Ruby and Sapphire"
+  subCategory: "Tools"
+  section: "guide"
+  variant:
+    - "retail"
+    - "cfw-emu"
+  guideKey: "tid-sid"
+  isRoughDraft: true
+- title: "FRLG TID and SID Generator"
+  description: "Generator for TID and SID in FRLG"
+  slug: "frlg-tidsid-generator"
+  category: "FireRed and LeafGreen"
+  subCategory: "Tools"
+  section: "guide"
+  variant:
+    - "retail"
+    - "cfw-emu"
+  guideKey: "tid-sid"
+  isRoughDraft: true
+- title: "Emerald TID and SID Generator"
+  description: "Generator for TID/SID in Emerald"
+  slug: "emerald-tidsid-generator"
+  category: "Emerald"
+  subCategory: "Tools"
+  section: "guide"
+  variant:
+    - "retail"
+    - "cfw-emu"
+  guideKey: "tid-sid"
+  isRoughDraft: true
+- title: "XD and Colo TID and SID Generator"
+  description: "Generator for TID and SID in XD and Colo"
+  slug: "xdcolo-tidsid-generator"
+  category: "Gamecube"
+  subCategory: "Tools"
+  section: "guide"
+  variant:
+    - "retail"
+    - "cfw-emu"
+  guideKey: "tid-sid"
+  isRoughDraft: true
+---
+
+<ShowIf slug="/rs-tidsid-generator">
+  <Gen3TidSidGenerator game="rs" />
+</ShowIf>
+
+<ShowIf slug="/frlg-tidsid-generator">
+  <Gen3TidSidGenerator game="frlge" />
+</ShowIf>
+
+<ShowIf slug="/emerald-tidsid-generator">
+  <Gen3TidSidGenerator game="frlge" />
+</ShowIf>
+
+<ShowIf slug="/xdcolo-tidsid-generator">
+  <Gen3TidSidGenerator game="xdcolo" />
+</ShowIf>
+`;export{n as default};

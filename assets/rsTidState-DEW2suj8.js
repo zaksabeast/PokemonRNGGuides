9acd9809c05b7464aabe0a8ee786bda58f038f74
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-03-03 02:59:41 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@751712a04775b4b14898c3b231cbc3f387b70a38 🚀

---
+++ assets/rsTidState-DEW2suj8.js
@@ -0,0 +1,1 @@
+import{l as t,m as s}from"./index-Do8L7Sg4.js";const a={targetAdvance:0,desiredIds:{tid:0,sid:0}},e=s(a),d=()=>t(e);export{d as u};
---
+++ assets/setup-B1oX3hwc.js
@@ -0,0 +1,1 @@
+import{j as e,g as s,hr as t}from"./index-Do8L7Sg4.js";import{G as o}from"./gen4types-ura_m44U.js";import{S as m,G as a}from"./consoleSelect-C85sRMFh.js";import"./startCase-CFc8EiHf.js";const r=[{label:"Game",input:e.jsx(m,{stateAtom:t,games:o})},{label:"Console",input:e.jsx(a,{stateAtom:t})}],G=()=>e.jsx(s,{fields:r});export{G as Gen4IdSetup};

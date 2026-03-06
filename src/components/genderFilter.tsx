93971e2224123b44dad88358f3180377c2ec2401
RainingChain <rainingchain@gmail.com>
2026-03-06 12:36:06 -0500

    Fix lint issues

--- src/components/genderFilter.tsx
+++ src/components/genderFilter.tsx
@@ -1,59 +1,6 @@
-import { GenderRatio } from "~/rngTools";
-import { gender, getPossibleGenders } from "~/types/gender";
-import { useFormContext } from "~/hooks/form";
-import { useEffect } from "react";
-import { useWatch } from "react-hook-form";
-import { type PkmFilterFields } from "./pkmFilter";
-import { FormikSelect } from "./select";
+import { gender } from "~/types/gender";
 
 export const genderOptions = ([null, ...gender] as const).map((gen) => ({
   label: gen ?? ("Any" as const),
   value: gen,
 }));
-
-const getGenderFilterOptions = (genderRatio?: GenderRatio) => {
-  if (genderRatio == null) {
-    return genderOptions;
-  }
-
-  const possibleGenders = getPossibleGenders(genderRatio);
-  const permitNull = possibleGenders.length > 1;
-  return genderOptions.filter((option) => {
-    if (option.value == null) {
-      return permitNull;
-    }
-    return possibleGenders.includes(option.value);
-  });
-};
-
-export const GenderFilter = ({
-  genderRatio,
-}: {
-  genderRatio?: GenderRatio;
-}) => {
-  const { setFieldValue } = useFormContext<PkmFilterFields>();
-  const filter_gender = useWatch<PkmFilterFields, "filter_gender">({
-    name: "filter_gender",
-  });
-
-  const options = getGenderFilterOptions(genderRatio);
-
-  useEffect(() => {
-    if (options.some((opt) => opt.value === filter_gender)) {
-      return;
-    }
-    if (options.length === 0) {
-      return;
-    }
-    // Currently selected value is invalid.
-    setFieldValue("filter_gender", options[0].value);
-  }, [filter_gender, options, setFieldValue]);
-
-  return (
-    <FormikSelect<PkmFilterFields, "filter_gender">
-      name="filter_gender"
-      options={options}
-      disabled={options.length <= 1}
-    />
-  );
-};

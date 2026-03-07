a01315d2d6d011eff0001851ed0f93de086cb7a3
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-03-07 01:21:02 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@f74e3383a04df4ebbed35930cf2c0b025e7fee5b 🚀

---
+++ assets/Stationary RNG Emu-CRw-1Txa.js
@@ -0,0 +1,52 @@
+import{ic as s,j as n}from"./index-B39TZJtA.js";const h={title:"定点乱数",description:"定点宝可梦的乱数",slug:"zh-emulator-hgss-stationary",translation:{enSlug:"emulator-hgss-stationary",language:"zh"}};function e(l){const i={a:"a",code:"code",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...s(),...l.components};return n.jsxs(n.Fragment,{children:[n.jsx(i.h2,{children:"所需工具"}),`
+`,n.jsxs(i.ul,{children:[`
+`,n.jsx(i.li,{children:n.jsx(i.a,{href:"/pokefinder",children:"PokeFinder"})}),`
+`,n.jsxs(i.li,{children:["适用于 ",n.jsx(i.a,{href:"/desmume-setup",children:"Desmume"})," 的乱数工具"]}),`
+`]}),`
+`,n.jsx(i.h2,{children:"介绍"}),`
+`,n.jsx(i.p,{children:"本指南列出了定点宝可梦，并标注了可能的特殊情况。乱数的基本流程是一致的，建议先熟悉初始种子指南和推进帧数指南，掌握基础操作后再进行定点乱数。"}),`
+`,n.jsx(i.p,{children:"请确保 PokeFinder 处于 第四世代 => 定点乱数 模式。"}),`
+`,n.jsx(i.h2,{children:"Method K 列表"}),`
+`,n.jsxs(i.ul,{children:[`
+`,n.jsx(i.li,{children:"拉普拉斯"}),`
+`,n.jsx(i.li,{children:"顽皮雷弹 (*3)."}),`
+`,n.jsx(i.li,{children:"卡比兽"}),`
+`,n.jsx(i.li,{children:"急冻鸟、闪电鸟、火焰鸟"}),`
+`,n.jsx(i.li,{children:"超梦"}),`
+`,n.jsx(i.li,{children:"胡说树"}),`
+`,n.jsx(i.li,{children:"水君"}),`
+`,n.jsx(i.li,{children:"凤王 / 洛奇亚"}),`
+`,n.jsx(i.li,{children:"无限事件中的拉帝欧斯 / 拉帝亚斯(非游走)."}),`
+`,n.jsx(i.li,{children:"固拉多、盖欧卡、烈空坐"}),`
+`]}),`
+`,n.jsx(i.h2,{children:"Method 1 列表"}),`
+`,n.jsxs(i.ul,{children:[`
+`,n.jsx(i.li,{children:"菊草叶 (初始帧 1)."}),`
+`,n.jsx(i.li,{children:"火球鼠 (初始帧 5)."}),`
+`,n.jsx(i.li,{children:"小锯鳄 (初始帧 9)."}),`
+`,n.jsx(i.li,{children:"关都御三家 (初始帧 5)."}),`
+`,n.jsx(i.li,{children:"丰缘御三家"}),`
+`,n.jsx(i.li,{children:"波克比蛋"}),`
+`,n.jsx(i.li,{children:"伊布（正辉赠送）"}),`
+`,n.jsx(i.li,{children:"游走宝可梦"}),`
+`,n.jsx(i.li,{children:"游戏厅奖品"}),`
+`,n.jsx(i.li,{children:"化石"}),`
+`,n.jsx(i.li,{children:"迷你龙（带神速/普通）"}),`
+`,n.jsx(i.li,{children:"阿尔宙斯事件赠送的帝牙卢卡、帕路奇亚、骑拉帝纳"}),`
+`,n.jsx(i.li,{children:"阿始赠送的蛋（桔梗市宝可梦中心）"}),`
+`,n.jsx(i.li,{children:"玛瑙水母（湛蓝市宝可梦中心）"}),`
+`,n.jsx(i.li,{children:"壶壶"}),`
+`,n.jsx(i.li,{children:"无畏小子"}),`
+`]}),`
+`,n.jsx(i.pre,{children:n.jsx(i.code,{children:`
+游走宝可梦推荐使用"四天王法"：先遭遇目标并击败，在挑战渡之前完成乱数。若不使用此法，雷公初始帧6，炎帝初始帧1.水都兄妹需在离开粉丝俱乐部大门后立即调整。
+
+`})}),`
+`,n.jsx(i.h2,{children:"连锁机制"}),`
+`,n.jsxs(i.ul,{children:[`
+`,n.jsx(i.li,{children:"红色暴鲤龙（愤怒湖定点）"}),`
+`]}),`
+`,n.jsx(i.h2,{children:"特别鸣谢"}),`
+`,n.jsxs(i.ul,{children:[`
+`,n.jsx(i.li,{children:"中文翻译：炫夜鳞、白希洛/Hakuhiro"}),`
+`]})]})}function c(l={}){const{wrapper:i}={...s(),...l.components};return i?n.jsx(i,{...l,children:n.jsx(e,{...l})}):e(l)}export{c as default,h as frontmatter};
---
+++ assets/Wondercard-ZWmN8_8o.js
@@ -0,0 +1,6 @@
+import{ic as o,j as e}from"./index-B39TZJtA.js";const i={title:"HeartGold and SoulSilver Wondercard RNG",navDrawerTitle:"Wondercard RNG",description:"How to RNG Wondercards",slug:"emulator-hgss-wondercard",category:"HeartGold and SoulSilver",isRoughDraft:!0,hideFromNavDrawer:!0,section:"pokemon_rng",variant:"cfw-emu"};function r(t){const n={li:"li",p:"p",ul:"ul",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"For the future:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Explain how to import WC with PokeFinder to have EXACT values and avoid any mistake"}),`
+`,e.jsx(n.li,{children:"Explain the right center to use"}),`
+`,e.jsx(n.li,{children:"Explain that PID IS NOT DOABLE because of system tics."}),`
+`]})]})}function s(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{s as default,i as frontmatter};

e8ca5b281d2bd37b0d45abc2004eb6c6ec00fe78
Samuel Magnan <rainingchain@gmail.com>
2026-03-06 20:23:09 -0500

    Split wild3FindTarget.tsx into multiple files (#396)

---
+++ src/rngToolsUi/gen3/wild/resultSetupInfos.tsx
@@ -0,0 +1,367 @@
+import { Wild3SearcherCycleData, Gen3Method } from "~/rngTools";
+import { ResultColumn, ResultTable, Icon, Link } from "~/components";
+import { formatLargeInteger } from "~/utils/formatLargeInteger";
+import { formatProbability } from "~/utils/formatProbability";
+import React from "react";
+import { match, P } from "ts-pattern";
+
+import { Tooltip } from "antd";
+
+import { formatLeadName, formatMassOutbreakStateName } from "./utils";
+import { formatDuration } from "~/utils/formatDuration";
+import { formatHex } from "~/utils/formatHex";
+import { PidPathResult, ResultSetupInfo } from "./wild3FindTarget";
+
+const getMethodLikelihoodColumValue = (
+  cycleData: Wild3SearcherCycleData,
+  method: Gen3Method,
+) => {
+  const probAsTxt = formatProbability(cycleData.method_probability);
+  const end =
+    cycleData.pre_sweet_scent_cycle_range.start +
+    cycleData.pre_sweet_scent_cycle_range.len;
+  const rangeAsTxt =
+    end === 0
+      ? `Method ${method} can't be triggered.`
+      : `Method ${method} is triggered if the cycle counter is between ${cycleData.pre_sweet_scent_cycle_range.start} and ${end}.`;
+  return (
+    <Tooltip title={rangeAsTxt}>
+      <div>{probAsTxt}</div>
+    </Tooltip>
+  );
+};
+
+const getResultSetupInfoColumns = ({
+  rngManipulatedLeadPid,
+  showMassOutbreak,
+  usesPainting,
+}: {
+  rngManipulatedLeadPid: boolean;
+  showMassOutbreak: boolean;
+  usesPainting: boolean;
+}): ResultColumn<ResultSetupInfo>[] => {
+  const columns: ResultColumn<ResultSetupInfo>[] = [];
+  if (!usesPainting) {
+    columns.push({
+      title: "Advances",
+      dataIndex: "advance",
+      monospace: true,
+      render: (adv, { seed }) => {
+        const painting_seed = formatHex(seed);
+        const durInMinutes = formatDuration(adv / 59.7275);
+        return (
+          <Tooltip
+            title={`Seed when performing the player action: ${painting_seed}. (~${durInMinutes} min)`}
+          >
+            {formatLargeInteger(adv)}
+          </Tooltip>
+        );
+      },
+    });
+  } else {
+    columns.push({
+      title: "Advances",
+      type: "group",
+      columns: [
+        {
+          title: "Before Reseed",
+          dataIndex: "painting_advs",
+          monospace: true,
+          render: (painting_advs) => {
+            if (painting_advs == null) {
+              return "";
+            }
+
+            const adv = painting_advs.adv_before_painting;
+            return (
+              <Tooltip title={`Painting Seed: ${formatHex(adv, 2)}`}>
+                {formatLargeInteger(adv)}
+              </Tooltip>
+            );
+          },
+        },
+        {
+          title: "After Reseed",
+          dataIndex: "painting_advs",
+          monospace: true,
+          render: (painting_advs, { seed, actionName }) => {
+            if (painting_advs == null) {
+              return "";
+            }
+
+            return (
+              <Tooltip
+                title={`Seed at start of ${actionName}: ${formatHex(seed)}`}
+              >
+                {formatLargeInteger(painting_advs.adv_after_painting)}
+              </Tooltip>
+            );
+          },
+        },
+      ],
+    });
+  }
+
+  columns.push(
+    { title: "Map", dataIndex: "mapName" },
+    { title: "Player action", dataIndex: "actionName" },
+    /*
+    TODO: Support roamer and fishing in Feebas tile to catch non-Feebas Pokémon. 
+    {
+      title: "Roamer",
+      dataIndex: "roamer_state",
+      render: (roamerState) => {
+        return match(roamerState)
+          .with("Inactive", () => "Inactive")
+          .with("ActiveNotInMap", () => "Roaming in another map")
+          .with("ActiveInMapLatios", () => "Latios in map")
+          .with("ActiveInMapLatias", () => "Latias in map")
+          .exhaustive();
+      },
+    },
+    {
+      title: "Feebas Tile?",
+      dataIndex: "feebas_state",
+      render: (feebasState) => {
+        return match(feebasState)
+          .with("NotInMap", () => "N/A")
+          .with("OnFeebasTile", () => "Yes")
+          .with("InMapButNotOnFeebasTile", () => "No")
+          .exhaustive();
+      },
+    },*/
+    ...(showMassOutbreak
+      ? ([
+          {
+            title: "Mass outbreak",
+            dataIndex: "mass_outbreak_state",
+            render: formatMassOutbreakStateName,
+          },
+        ] as const)
+      : []),
+  );
+
+  if (!rngManipulatedLeadPid) {
+    columns.push({
+      title: (
+        <>
+          Method <br />
+          Likelihood
+        </>
+      ),
+      key: "<>Method <br />Likelihood</>",
+      dataIndex: "cycle_data_by_lead",
+      render: (cycle_data_by_lead, values) => {
+        const text = (() => {
+          if (cycle_data_by_lead == undefined) {
+            return "";
+          }
+          const [least_likely, most_likely] =
+            cycle_data_by_lead.common_lower_lead.method_probability <
+            cycle_data_by_lead.common_upper_lead.method_probability
+              ? [
+                  cycle_data_by_lead.common_lower_lead,
+                  cycle_data_by_lead.common_upper_lead,
+                ]
+              : [
+                  cycle_data_by_lead.common_upper_lead,
+                  cycle_data_by_lead.common_lower_lead,
+                ];
+
+          const leastFormat = formatProbability(
+            least_likely.method_probability,
+          );
+          const mostFormat = formatProbability(most_likely.method_probability);
+
+          if (leastFormat === mostFormat) {
+            return leastFormat;
+          }
+
+          if (
+            least_likely.method_probability === 0 ||
+            most_likely.method_probability - least_likely.method_probability >
+              0.1
+          ) {
+            return `${leastFormat} - ${mostFormat}`;
+          }
+
+          return leastFormat;
+        })();
+
+        return (
+          <Tooltip title={`Method ${values.method}`}>
+            <div>{text}</div>
+          </Tooltip>
+        );
+      },
+    });
+  }
+
+  columns.push({
+    title: "Lead",
+    dataIndex: "lead",
+    render: formatLeadName,
+  });
+
+  if (rngManipulatedLeadPid) {
+    columns.push(
+      {
+        title: "Ideal Lead Speed",
+        dataIndex: "cycle_data_by_lead",
+        render: (cycle_data_by_lead, values) => {
+          if (cycle_data_by_lead == undefined) {
+            return "";
+          }
+          if (values.lead === "Egg") {
+            return "";
+          }
+          if (
+            cycle_data_by_lead.ideal_lead.method_probability ===
+              cycle_data_by_lead.slowest_lead.method_probability &&
+            cycle_data_by_lead.ideal_lead.method_probability ===
+              cycle_data_by_lead.fastest_lead.method_probability
+          ) {
+            return "Any";
+          }
+          const cycle = cycle_data_by_lead.ideal_lead.lead_pid_cycle_count;
+          const label = match(cycle)
+            .with(18, () => "Fastest")
+            .with(900, () => "Slowest")
+            .with(P.number, () => {
+              return cycle + " cycles";
+            })
+            .exhaustive();
+
+          return label;
+        },
+      },
+      {
+        title: (
+          <div>
+            {"Method Likelihood by "}
+            <Link href="/gba-methods-lead-impact/" newTab>
+              Lead Speed
+            </Link>
+          </div>
+        ),
+        key: "methodLikelihoodByLeadSpeed",
+        type: "group",
+        columns: [
+          {
+            title: "Ideal",
+            dataIndex: "cycle_data_by_lead",
+            render: (cycle_data_by_lead, values) => {
+              if (cycle_data_by_lead == undefined) {
+                return "";
+              }
+              return getMethodLikelihoodColumValue(
+                cycle_data_by_lead.ideal_lead,
+                values.method,
+              );
+            },
+          },
+          {
+            title: (
+              <Tooltip title="Lead PID speed equal to 18 cycles">
+                <div>
+                  Fastest <Icon name="InformationCircle" size={16} />
+                </div>
+              </Tooltip>
+            ),
+            key: "methodLikelihoodFastest",
+            dataIndex: "cycle_data_by_lead",
+            render: (cycle_data_by_lead, values) => {
+              if (cycle_data_by_lead == undefined) {
+                return "";
+              }
+              return getMethodLikelihoodColumValue(
+                cycle_data_by_lead.fastest_lead,
+                values.method,
+              );
+            },
+          },
+          {
+            title: (
+              <Tooltip title="Lead PID speed between 608 and 868 cycles (99.9% of all PIDs)">
+                <div>
+                  Common <Icon name="InformationCircle" size={16} />
+                </div>
+              </Tooltip>
+            ),
+            key: "methodLikelihoodCommon",
+            dataIndex: "cycle_data_by_lead",
+            render: (cycle_data_by_lead, values) => {
+              if (cycle_data_by_lead == undefined) {
+                return "";
+              }
+              const least_likely_common =
+                cycle_data_by_lead.common_lower_lead.method_probability <
+                cycle_data_by_lead.common_upper_lead.method_probability
+                  ? cycle_data_by_lead.common_lower_lead
+                  : cycle_data_by_lead.common_upper_lead;
+              return getMethodLikelihoodColumValue(
+                least_likely_common,
+                values.method,
+              );
+            },
+          },
+          {
+            title: (
+              <Tooltip title="Lead PID speed equal to 900 cycles">
+                <div>
+                  Slowest <Icon name="InformationCircle" size={16} />
+                </div>
+              </Tooltip>
+            ),
+            key: "methodLikelihoodSlowest",
+            dataIndex: "cycle_data_by_lead",
+            render: (cycle_data_by_lead, values) => {
+              if (cycle_data_by_lead == undefined) {
+                return "";
+              }
+              return getMethodLikelihoodColumValue(
+                cycle_data_by_lead.slowest_lead,
+                values.method,
+              );
+            },
+          },
+        ],
+      },
+    );
+  }
+  return columns;
+};
+
+export const Wild3ResultSetupInfos = ({
+  selectedPidPathResult,
+  rngManipulatedLeadPid,
+}: {
+  selectedPidPathResult: PidPathResult | null;
+  rngManipulatedLeadPid: boolean;
+}) => {
+  const resultSetupInfoColumns = React.useMemo(() => {
+    const showMassOutbreak =
+      selectedPidPathResult != null &&
+      selectedPidPathResult.resultSetupInfos.some(
+        (setup) => setup.mass_outbreak_state !== "Inactive",
+      );
+    const usesPainting =
+      selectedPidPathResult?.resultSetupInfos.some(
+        (res) => res.painting_advs != null,
+      ) ?? false;
+
+    return getResultSetupInfoColumns({
+      rngManipulatedLeadPid,
+      showMassOutbreak,
+      usesPainting,
+    });
+  }, [rngManipulatedLeadPid, selectedPidPathResult]);
+
+  return selectedPidPathResult != null ? (
+    <ResultTable<ResultSetupInfo>
+      columns={resultSetupInfoColumns}
+      rowKey="uid"
+      dataSource={selectedPidPathResult.resultSetupInfos}
+    />
+  ) : null;
+};
---
+++ src/rngToolsUi/gen3/wild/wild3TargetMon.component.tsx
@@ -0,0 +1,73 @@
+import { Species } from "~/rngTools";
+import {
+  Field,
+  FormikSelect,
+  FormFieldTable,
+  Flex,
+  Typography,
+} from "~/components";
+import { toOptions } from "~/utils/options";
+import { useFormContext } from "~/hooks/form";
+import { getPkmFilterFields } from "~/components/pkmFilter";
+import React from "react";
+import { genderRatioBySpecies, gen3SpeciesHasVariableSize } from "~/types";
+import { getGen3PkmFilterFields } from "~/components/gen3PkmFilter";
+
+import { getWild3EmeraldGameData } from "./data/wild3GameData";
+import { useWatch } from "react-hook-form";
+import { getPossibleValuesForSpecies } from "./wild3TargetMon";
+import { FormState } from "./wild3FindTarget";
+
+const emeraldWildGameData = getWild3EmeraldGameData();
+
+const getTargetMonFields = (species: Species): Field[] => {
+  const genderRatio = genderRatioBySpecies[species];
+
+  const targetMonFields: Field[] = [
+    {
+      label: "Species",
+      input: (
+        <FormikSelect<FormState, "species">
+          name="species"
+          options={toOptions(emeraldWildGameData.species)}
+        />
+      ),
+    },
+    ...getPkmFilterFields({ genderRatio }),
+    ...getGen3PkmFilterFields({
+      max_size: gen3SpeciesHasVariableSize(species),
+    }),
+  ];
+  return targetMonFields;
+};
+
+export const TargetMon = () => {
+  const { setFieldValue } = useFormContext<FormState>();
+  const species = useWatch<FormState, "species">({
+    name: "species",
+  });
+
+  const fields = React.useMemo((): Field[] => {
+    return getTargetMonFields(species);
+  }, [species]);
+
+  // when user changes species, select all the possible setup values
+  React.useEffect(() => {
+    const possVals = getPossibleValuesForSpecies(species);
+
+    setFieldValue("maps", possVals.maps);
+    setFieldValue("actions", possVals.actions);
+    setFieldValue("roamerStates", possVals.roamerStates);
+    setFieldValue("massOutbreakStates", possVals.massOutbreakStates);
+    setFieldValue("feebasStates", possVals.feebasStates);
+  }, [species, setFieldValue]);
+
+  return (
+    <Flex vertical gap={8}>
+      <Typography.Title level={5} p={0} m={0}>
+        Target Pokémon
+      </Typography.Title>
+      <FormFieldTable fields={fields} />
+    </Flex>
+  );
+};

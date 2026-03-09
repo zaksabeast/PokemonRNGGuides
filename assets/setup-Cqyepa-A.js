386afc15e40e9da6a0421ec065f5139e7dcc6644
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-03-09 01:11:31 +0000

    Deploying to gh-pages from @ zaksabeast/PokemonRNGGuides@3f2d7145dfe1146751d3b171fd0a0e28b1892d8c 🚀

---
+++ assets/setup-Cqyepa-A.js
@@ -0,0 +1,1 @@
+import{j as e,g as s,hq as t}from"./index-BF7GX39r.js";import{G as o}from"./gen4types-DTkW4KiR.js";import{S as m,G as a}from"./consoleSelect-DMvzq0eV.js";import"./startCase-C08GPDF9.js";const r=[{label:"Game",input:e.jsx(m,{stateAtom:t,games:o})},{label:"Console",input:e.jsx(a,{stateAtom:t})}],G=()=>e.jsx(s,{fields:r});export{G as Gen4IdSetup};
---
+++ assets/uniqueId-CpGd1v4l.js
@@ -0,0 +1,1 @@
+import{hJ as i}from"./index-BF7GX39r.js";var n=0;function u(r){var t=++n;return i(r)+t}export{u};

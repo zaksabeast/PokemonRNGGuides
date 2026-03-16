cb422ac9b6eaacbbd9464fc3c8e1398bda7aa061
RainingChain <rainingchain@gmail.com>
2026-03-15 21:08:21 -0400

    .

--- src/components/genderFilter/options.ts
+++ src/components/genderFilter/options.ts
@@ -23,21 +23,3 @@ export const genderOptions = ([null, ...gender] as const).map((gen) => ({
   label: gen ?? ("Any" as const),
   value: gen,
 }));
-
-export const getGenderFilterOptions = (
-  genderRatio?: GenderRatio,
-  permitAny = true,
-) => {
-  if (genderRatio == null) {
-    return genderOptions;
-  }
-
-  const possibleGenders = getPossibleGenders(genderRatio);
-  permitAny = permitAny && possibleGenders.length > 1;
-  return genderOptions.filter((option) => {
-    if (option.value == null) {
-      return permitAny;
-    }
-    return possibleGenders.includes(option.value);
-  });
-};

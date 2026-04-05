79e463dce19e184cbcac27b115b4e77d5cb30f0d
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-04-05 06:31:38 -0500

    Fix overly aggressive react 19 suspending

--- src/hooks/useHydrate.ts
+++ src/hooks/useHydrate.ts
@@ -1,6 +1,29 @@
 import React from "react";
 import { HydrationLock } from "~/utils/hydration";
 import * as tst from "ts-toolbelt";
+import { atom, useAtom, useAtomValue } from "jotai";
+
+const isHydratedAtom = atom(false);
+
+/**
+ * This hook should be used in the root of the app to update hydration status.
+ * It will trigger a re-render once the app is hydrated,
+ * allowing components to switch from pre-rendered content to client-side content.
+ */
+export const useUpdateHydration = () => {
+  const [, setHydrated] = useAtom(isHydratedAtom);
+
+  React.useEffect(() => {
+    setHydrated(true);
+  }, [setHydrated]);
+};
+
+/**
+ * Used anywhere in the app to check if the app is hydrated.
+ */
+export const useIsHydrated = () => {
+  return useAtomValue(isHydratedAtom);
+};
 
 type _HydrationUnlock<T> = T extends HydrationLock<infer U> ? U : T;
 
@@ -30,12 +53,13 @@ type HydratedResult<T> =
       client: HydrationUnlock<T>;
     };
 
+/**
+ * Takes a value locked for hydration,
+ * and returns the unlocked value once the app is hydrated,
+ * or null if not hydrated yet.
+ */
 export const useHydrate = <T>(value: LockedValue<T>): HydratedResult<T> => {
-  const [hydrated, setHydrated] = React.useState(false);
-
-  React.useEffect(() => {
-    setHydrated(true);
-  }, []);
+  const hydrated = useIsHydrated();
 
   return hydrated
     ? { hydrated, client: unlockHydration(value) }

31e36dfd8ae0e86903b4fbd7471733b5df9686f9
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-05-08 21:46:19 -0500

    Only run test on honey Pokemon

--- src/rngToolsUi/gen4/static/chatterFilter.tsx
+++ src/rngToolsUi/gen4/static/chatterFilter.tsx
@@ -1,12 +1,27 @@
 import { RelativeChatterFilterBase } from "../shared/relativeChatterFilter";
+import { ChatterFilterBase } from "../shared/chatterFilter";
 import { useStatic4State } from "./state";
 
-export const Static4ChatterFilter = () => {
+type Static4ChatterFilterProps = {
+  honey?: boolean;
+};
+
+export const Static4ChatterFilter = ({ honey }: Static4ChatterFilterProps) => {
   const [state] = useStatic4State();
   const target = state.target;
 
+  if (honey) {
+    return (
+      <RelativeChatterFilterBase
+        seed={target?.seed ?? null}
+        targetAdvance={target?.advance ?? null}
+        submitTrackerId="static4_chatter_filter"
+      />
+    );
+  }
+
   return (
-    <RelativeChatterFilterBase
+    <ChatterFilterBase
       seed={target?.seed ?? null}
       targetAdvance={target?.advance ?? null}
       submitTrackerId="static4_chatter_filter"

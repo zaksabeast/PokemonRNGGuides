2731c8dc7d8affa51a384a2225d218dff1030724
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-05-10 21:13:45 -0500

    Use primary color as link color

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -24,6 +24,7 @@ export const getTheme = ({
     algorithm,
     token: {
       colorPrimary: tempThemeColor ?? "#6200EE", // consider "#40a9ff",
+      colorLink: tempThemeColor ?? "#6200EE",
     },
   });
 

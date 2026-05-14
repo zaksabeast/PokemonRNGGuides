90f0ef212b30c71d6df02dca922d9254e4e255f4
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-05-14 06:59:42 -0500

    Fix map marker dark mode hover color

--- src/components/interactableMap/marker.tsx
+++ src/components/interactableMap/marker.tsx
@@ -8,6 +8,7 @@ export const MapMarker = styled(BaseButton)(({ theme }) => ({
     filter: `drop-shadow(0 1px 0 rgba(0,0,0,0.25))
     drop-shadow(0 3px 6px rgba(0,0,0,0.2))`,
     ":hover": {
+      backgroundColor: theme.token.colorWhite,
       filter: `drop-shadow(0 1px 0 rgba(0,0,0,0.18))
     drop-shadow(0 2px 4px rgba(0,0,0,0.22))
     drop-shadow(0 6px 10px rgba(0,0,0,0.18))`,

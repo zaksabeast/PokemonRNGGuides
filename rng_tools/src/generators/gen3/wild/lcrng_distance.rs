d9b554972f810e184320711ad094679d1cf199ca
RainingChain <rainingchain@gmail.com>
2026-05-26 21:33:46 -0400

    .

--- rng_tools/src/generators/gen3/wild/lcrng_distance.rs
+++ rng_tools/src/generators/gen3/wild/lcrng_distance.rs
@@ -18,37 +18,10 @@ const DISTANCE_LOOKUP: [[DistanceLookup; 0x100]; 4] = build_distance_lookup();
 
 #[wasm_bindgen]
 pub fn lcrng_distance(start: u32, end: u32) -> u32 {
-    if start == 0 {
-        lcrng_distance_from_zero(end)
-    } else {
-        lcrng_distance_from_non_zero(start, end)
-    }
+    lcrng_distance_from_zero(end).wrapping_sub(lcrng_distance_from_zero(start))
 }
 
-pub fn lcrng_distance_from_non_zero(mut start: u32, end: u32) -> u32 {
-    let mut count = 0_u32;
-    let mut p = 1_u32;
-    let mut i = 0_usize;
-
-    while i < 32 {
-        if start == end {
-            break;
-        }
-
-        if ((start ^ end) & p) != 0 {
-            let (mult, add) = bit_jump_params(i);
-            start = mult.wrapping_mul(start).wrapping_add(add);
-            count += p;
-        }
-
-        p <<= 1;
-        i += 1;
-    }
-
-    count
-}
-
-pub fn lcrng_distance_from_zero(mut state: u32) -> u32 {
+fn lcrng_distance_from_zero(mut state: u32) -> u32 {
     let mut distance = 0_u32;
     let mut byte = 0_usize;
 
@@ -136,67 +109,124 @@ const fn lcrng_distance_const(mut start: u32, end: u32) -> u32 {
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::{
-        hint::black_box,
-        time::{Duration, Instant},
-    };
+
+    pub fn lcrng_distance_common_impl(mut start: u32, end: u32) -> u32 {
+        const POKERNG_JUMP_TABLE: [(u32, u32); 32] = [
+            (0x41C64E6D, 0x6073),
+            (0xC2A29A69, 0xE97E7B6A),
+            (0xEE067F11, 0x31B0DDE4),
+            (0xCFDDDF21, 0x67DBB608),
+            (0x5F748241, 0xCBA72510),
+            (0x8B2E1481, 0x1D29AE20),
+            (0x76006901, 0xBA84EC40),
+            (0x1711D201, 0x79F01880),
+            (0xBE67A401, 0x8793100),
+            (0xDDDF4801, 0x6B566200),
+            (0x3FFE9001, 0x803CC400),
+            (0x90FD2001, 0xA6B98800),
+            (0x65FA4001, 0xE6731000),
+            (0xDBF48001, 0x30E62000),
+            (0xF7E90001, 0xF1CC4000),
+            (0xEFD20001, 0x23988000),
+            (0xDFA40001, 0x47310000),
+            (0xBF480001, 0x8E620000),
+            (0x7E900001, 0x1CC40000),
+            (0xFD200001, 0x39880000),
+            (0xFA400001, 0x73100000),
+            (0xF4800001, 0xE6200000),
+            (0xE9000001, 0xCC400000),
+            (0xD2000001, 0x98800000),
+            (0xA4000001, 0x31000000),
+            (0x48000001, 0x62000000),
+            (0x90000001, 0xC4000000),
+            (0x20000001, 0x88000000),
+            (0x40000001, 0x10000000),
+            (0x80000001, 0x20000000),
+            (0x1, 0x40000000),
+            (0x1, 0x80000000),
+        ];
+
+        let mut count = 0_u32;
+        let mut p = 1_u32;
+
+        for (mult, add) in POKERNG_JUMP_TABLE {
+            if start == end {
+                break;
+            }
+
+            if ((start ^ end) & p) != 0 {
+                start = mult.wrapping_mul(start).wrapping_add(add);
+                count += p;
+            }
+            p <<= 1
+        }
+
+        count
+    }
 
     #[test]
     fn test_lcrng_distance() {
         assert_eq!(lcrng_distance(0, 0xEA55834D), 475);
         assert_eq!(lcrng_distance(0, 0x7700E49D), 1259);
-    }
 
-    #[test]
-    fn test_lcrng_distance_from_zero() {
-        assert_eq!(lcrng_distance_from_zero(0), 0);
-        assert_eq!(lcrng_distance_from_zero(0xEA55834D), 475);
-        assert_eq!(lcrng_distance_from_zero(0x7700E49D), 1259);
+        assert_eq!(lcrng_distance(0xEA55834D, 0x7700E49D), 784);
         assert_eq!(
-            lcrng_distance_from_zero(0xDEADBEEF),
-            lcrng_distance(0, 0xDEADBEEF)
+            lcrng_distance(0x7700E49D, 0xEA55834D),
+            475_u32.wrapping_sub(1259)
         );
+        assert_eq!(lcrng_distance(0x12345678, 0x0B71C18B), 1);
+        assert_eq!(lcrng_distance(0x12345678, 0x5C704960), 1000);
     }
 
-    fn benchmark_lcrng_distance_from_zero() -> (Duration, u32) {
-        let mut checksum = 0_u32;
-        let start_time = Instant::now();
-
-        for i in 0..=u32::MAX {
-            checksum = checksum.wrapping_add(lcrng_distance_from_zero(black_box(i)));
-        }
-
-        (start_time.elapsed(), checksum)
-    }
-
-    fn benchmark_lcrng_distance_from_non_zero() -> (Duration, u32) {
-        let mut checksum = 0_u32;
-        let start_time = Instant::now();
-
-        for i in 0..=u32::MAX {
-            checksum = checksum.wrapping_add(lcrng_distance_from_non_zero(0, black_box(i)));
-        }
-
-        (start_time.elapsed(), checksum)
-    }
-
-    // cargo test --release benchmark_lcrng_distance_from_zero_vs_non_zero -- --ignored --nocapture
+    // cargo test --release benchmark_lcrng_distance_random_start_and_end -- --ignored --nocapture
     #[test]
     #[ignore]
-    fn benchmark_lcrng_distance_from_zero_vs_non_zero() {
-        let (from_zero_duration, from_zero_checksum) = benchmark_lcrng_distance_from_zero();
-        let (from_non_zero_duration, from_non_zero_checksum) =
-            benchmark_lcrng_distance_from_non_zero();
+    fn benchmark_lcrng_distance_random_start_and_end() {
+        use std::{hint::black_box, time::Instant};
 
-        assert_eq!(from_zero_checksum, from_non_zero_checksum);
+        const ITERATIONS: usize = 1_000_000_000;
+
+        fn next_random(state: &mut u32) -> u32 {
+            *state ^= *state << 13;
+            *state ^= *state >> 17;
+            *state ^= *state << 5;
+            *state
+        }
+
+        let mut random_state = 0x1234_5678_u32;
+        let mut common_checksum = 0_u32;
+        let start_time = Instant::now();
+
+        for _ in 0..ITERATIONS {
+            let start = next_random(&mut random_state);
+            let end = next_random(&mut random_state);
+            common_checksum = common_checksum
+                .wrapping_add(lcrng_distance_common_impl(black_box(start), black_box(end)));
+        }
 
         println!(
-            "lcrng_distance_from_zero: {:?} (checksum {})",
-            from_zero_duration, from_zero_checksum
+            "lcrng_distance_common_impl random start and end: {:?} (checksum {})",
+            start_time.elapsed(),
+            common_checksum
         );
+
+        random_state = 0x1234_5678;
+        let mut lookup_checksum = 0_u32;
+        let start_time = Instant::now();
+
+        for _ in 0..ITERATIONS {
+            let start = next_random(&mut random_state);
+            let end = next_random(&mut random_state);
+            lookup_checksum =
+                lookup_checksum.wrapping_add(lcrng_distance(black_box(start), black_box(end)));
+        }
+
         println!(
-            "lcrng_distance_from_non_zero: {:?} (checksum {})",
-            from_non_zero_duration, from_non_zero_checksum
+            "lcrng_distance random start and end: {:?} (checksum {})",
+            start_time.elapsed(),
+            lookup_checksum
         );
+
+        assert_eq!(lookup_checksum, common_checksum);
     }
 }
--- rng_tools/src/rng/lcrng.rs
+++ rng_tools/src/rng/lcrng.rs
@@ -355,6 +355,55 @@ mod test {
         assert_eq!(rng.rand::<u32>(), 0x52713895);
     }
 
+    fn pokerng_jump_common_impl(mut state: u32, mut advances: usize) -> u32 {
+        const POKERNG_JUMP_TABLE: [(u32, u32); 32] = [
+            (0x41C64E6D, 0x6073),
+            (0xC2A29A69, 0xE97E7B6A),
+            (0xEE067F11, 0x31B0DDE4),
+            (0xCFDDDF21, 0x67DBB608),
+            (0x5F748241, 0xCBA72510),
+            (0x8B2E1481, 0x1D29AE20),
+            (0x76006901, 0xBA84EC40),
+            (0x1711D201, 0x79F01880),
+            (0xBE67A401, 0x8793100),
+            (0xDDDF4801, 0x6B566200),
+            (0x3FFE9001, 0x803CC400),
+            (0x90FD2001, 0xA6B98800),
+            (0x65FA4001, 0xE6731000),
+            (0xDBF48001, 0x30E62000),
+            (0xF7E90001, 0xF1CC4000),
+            (0xEFD20001, 0x23988000),
+            (0xDFA40001, 0x47310000),
+            (0xBF480001, 0x8E620000),
+            (0x7E900001, 0x1CC40000),
+            (0xFD200001, 0x39880000),
+            (0xFA400001, 0x73100000),
+            (0xF4800001, 0xE6200000),
+            (0xE9000001, 0xCC400000),
+            (0xD2000001, 0x98800000),
+            (0xA4000001, 0x31000000),
+            (0x48000001, 0x62000000),
+            (0x90000001, 0xC4000000),
+            (0x20000001, 0x88000000),
+            (0x40000001, 0x10000000),
+            (0x80000001, 0x20000000),
+            (0x1, 0x40000000),
+            (0x1, 0x80000000),
+        ];
+
+        for (mult, add) in POKERNG_JUMP_TABLE {
+            if (advances & 1) != 0 {
+                state = state.wrapping_mul(mult).wrapping_add(add);
+            }
+            advances >>= 1;
+            if advances == 0 {
+                break;
+            }
+        }
+
+        state
+    }
+
     //1.56s
     #[test]
     #[ignore]
@@ -375,6 +424,8 @@ mod test {
                 Pokerng::with_jump(black_box(seed), black_box(advances as usize)).seed(),
             );
         }
+
+        // TODO:
 
         println!(
             "Pokerng::with_jump random seed and advances: {:?} (checksum {})",

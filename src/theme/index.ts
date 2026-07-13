deb4fbf71940490755911d738ec0a45afd75fbce
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2026-07-12 21:17:30 -0500

    Fix shadows

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -35,15 +35,15 @@ export const getTheme = ({
 
   const colorTextBase = "var(--ant-color-text-base)";
   const shadows = {
-    boxShadow: `0 6px 16px 0 rgb(from ${colorTextBase} r g b 0.08),
-  0 3px 6px -4px rgb(from ${colorTextBase} r g b 0.12),
-  0 9px 28px 8px rgb(from ${colorTextBase} r g b 0.05)`,
-    boxShadowSecondary: `0 6px 16px 0 rgb(from ${colorTextBase} r g b 0.08),
-  0 3px 6px -4px rgb(from ${colorTextBase} r g b 0.12),
-  0 9px 28px 8px rgb(from ${colorTextBase} r g b 0.05)`,
-    boxShadowTertiary: `0 1px 2px 0 rgb(from ${colorTextBase} r g b 0.03),
-      0 1px 6px -1px rgb(from ${colorTextBase} r g b 0.02),
-      0 2px 4px 0 rgb(from ${colorTextBase} r g b 0.02)`,
+    boxShadow: `0 6px 16px 0 rgb(from ${colorTextBase} r g b / 0.08),
+  0 3px 6px -4px rgb(from ${colorTextBase} r g b / 0.12),
+  0 9px 28px 8px rgb(from ${colorTextBase} r g b / 0.05)`,
+    boxShadowSecondary: `0 6px 16px 0 rgb(from ${colorTextBase} r g b / 0.08),
+  0 3px 6px -4px rgb(from ${colorTextBase} r g b / 0.12),
+  0 9px 28px 8px rgb(from ${colorTextBase} r g b / 0.05)`,
+    boxShadowTertiary: `0 1px 2px 0 rgb(from ${colorTextBase} r g b / 0.03),
+      0 1px 6px -1px rgb(from ${colorTextBase} r g b / 0.02),
+      0 2px 4px 0 rgb(from ${colorTextBase} r g b / 0.02)`,
   };
 
   return {
